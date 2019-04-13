97f20a19d3fc8f0eabebd59db5c21ffba71c0b51
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2019-04-14 03:03:59 +0800

    add

--- config/update_c.py
+++ config/update_c.py
@@ -38,7 +38,7 @@ from QUANTAXIS.QASU.main import (QA_SU_save_etf_day, QA_SU_save_etf_min,
 from QUANTTOOLS.QAStockETL import (QA_SU_save_report_calendar_his, QA_SU_save_stock_divyield_his, QA_SU_save_fianacialTTM_momgo,
                                    QA_SU_save_stock_divyield_day,QA_SU_save_report_calendar_day,
                                    QA_SU_save_stock_fianacial_momgo,QA_SU_save_stock_fianacial_momgo_his,
-                                   QA_SU_save_stock_financial_ths_day,QA_SU_save_stock_financial_ths_his)
+                                   QA_SU_save_stock_financial_sina_day,QA_SU_save_stock_financial_sina_his)
 from QUANTTOOLS.QAStockETL import (QA_etl_stock_list, QA_etl_stock_info,
                                    QA_etl_stock_xdxr, QA_etl_stock_day,
                                    QA_etl_stock_financial, QA_etl_stock_calendar,
@@ -49,7 +49,7 @@ QA_SU_save_stock_divyield_day()
 QA_SU_save_report_calendar_day()
 QA_SU_save_stock_info_tushare()
 QA_SU_save_financialfiles()
-QA_SU_save_stock_financial_ths_day()
+QA_SU_save_stock_financial_sina_day()
 
 print("write data into sqldatabase")
 
