dc562879d81dc6f9fbd0e31cf022e488dbc8fa13
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2019-04-14 02:24:20 +0800

    add

--- QUANTTOOLS/QAStockETL/QAUtil/QADate.py
+++ QUANTTOOLS/QAStockETL/QAUtil/QADate.py
@@ -9,3 +9,11 @@ def QA_util_add_days(dt, days):
     dt = datetime.datetime.strptime(
         dt, "%Y-%m-%d") + relativedelta(days=days)
     return(dt)
+
+def QA_util_add_years(dt, days):
+    """
+    #返回dt隔months个月后的日期，months相当于步长
+    """
+    dt = datetime.datetime.strptime(
+        dt, "%Y-%m-%d") + relativedelta(years=days)
+    return(dt)
