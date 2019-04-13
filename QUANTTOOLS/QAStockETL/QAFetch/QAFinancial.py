2029caa957e4eeada473cb04a031e2d9e8e67a15
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2019-04-14 02:34:58 +0800

    add

--- QUANTTOOLS/QAStockETL/QAFetch/QAFinancial.py
+++ QUANTTOOLS/QAStockETL/QAFetch/QAFinancial.py
@@ -3,9 +3,11 @@ from QUANTAXIS.QAUtil import QA_util_date_stamp
 
 def QA_fetch_get_stock_report_ths(code):
     data = get_stock_report_ths(code)
-    res = data.assign(report_date=data['report_date'].apply(lambda x: QA_util_date_stamp(str(x)[0:10])))
-    return(res)
+    data = data.assign(report_date=data['report_date'].apply(lambda x: QA_util_date_stamp(str(x)[0:10])))
+    return(data)
 
 def QA_fetch_get_stock_report_sina(code, report_year):
     data = get_stock_report_sina(code, report_year)
+    data = data.assign(report_date=data['report_date'].apply(lambda x: QA_util_date_stamp(str(x)[0:10])))
+    data = data.assign(crawl_date=data['crawl_date'].apply(lambda x: QA_util_date_stamp(str(x)[0:10])))
     return(data)
