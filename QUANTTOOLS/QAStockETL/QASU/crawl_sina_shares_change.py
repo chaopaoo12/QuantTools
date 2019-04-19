61267c37c6f792bca9635e95810162177bc3ff10
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2019-04-20 02:25:09 +0800

    add

--- QUANTTOOLS/QAStockETL/QASU/crawl_sina_shares_change.py
+++ QUANTTOOLS/QAStockETL/QASU/crawl_sina_shares_change.py
@@ -18,7 +18,7 @@ def QA_SU_save_stock_shares_day(client=DATABASE, ui_log = None, ui_progress = No
 
     code = list(QA_fetch_stock_list_adv()['code'])
     stock_shares = client.stock_shares
-    stock_shares.create_index([("code", pymongo.ASCENDING), ("begin_date", pymongo.ASCENDING)], unique=True)
+    stock_shares.create_index([("code", pymongo.ASCENDING), ("begin_date", pymongo.ASCENDING), ('total_shares', pymongo.DESCENDING)], unique=True)
     err = []
 
     def __saving_work(code, stock_shares):
@@ -60,7 +60,7 @@ def QA_SU_save_stock_shares_his(client=DATABASE, ui_log = None, ui_progress = No
     '''
     code = list(QA_fetch_stock_list_adv()['code'])
     stock_shares = client.stock_shares
-    stock_shares.create_index([("code", pymongo.ASCENDING), ("begin_date", pymongo.ASCENDING)], unique=False)
+    stock_shares.create_index([("code", pymongo.ASCENDING), ("begin_date", pymongo.ASCENDING), ('total_shares', pymongo.DESCENDING)], unique=True)
     err = []
 
     def __saving_work(code, stock_shares):
