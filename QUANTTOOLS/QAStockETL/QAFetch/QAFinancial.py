be3b5cbc54bde61fd10f5b739f1a0043b19d22ac
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2019-04-27 15:28:09 +0800

    add

--- QUANTTOOLS/QAStockETL/QAFetch/QAFinancial.py
+++ QUANTTOOLS/QAStockETL/QAFetch/QAFinancial.py
@@ -13,7 +13,7 @@ def QA_fetch_get_stock_report_sina(code, report_year):
     return(data)
 
 def QA_fetch_get_stock_report_wy(code):
-    data = get_stock_report_ths(code)
+    data = get_stock_report_wy(code)
     data = data.assign(report_date=data['report_date'].apply(lambda x: QA_util_date_stamp(str(x)[0:10])))
     data = data.assign(crawl_date=data['crawl_date'].apply(lambda x: QA_util_date_stamp(str(x)[0:10])))
     return(data)
