347ad8abed4d0ad11a464a8cefe589ec8f156553
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2019-05-05 00:38:56 +0800

    add

--- QUANTTOOLS/QAStockETL/QAFetch/QAAlpha.py
+++ QUANTTOOLS/QAStockETL/QAFetch/QAAlpha.py
@@ -1,10 +1,13 @@
 from QUANTTOOLS.QAStockETL.QAUtil import alpha
-from QUANTAXIS.QAUtil import QA_util_date_stamp
+from QUANTAXIS.QAUtil import QA_util_date_stamp,QA_util_if_trade
 
 def QA_fetch_get_stock_alpha(code, date):
-    data = alpha(code, date).reset_index()
-    names = list(data.columns)
-    names[0] = 'code'
-    data.columns = names
-    data = data.assign(date_stamp=data['date'].apply(lambda x: QA_util_date_stamp(str(x)[0:10])))
-    return(data)
+    if QA_util_if_trade(date) == True:
+        data = alpha(code, date).reset_index()
+        names = list(data.columns)
+        names[0] = 'code'
+        data.columns = names
+        data = data.assign(date_stamp=data['date'].apply(lambda x: QA_util_date_stamp(str(x)[0:10])))
+        return(data)
+    else:
+        print("Not a Trading Day")
