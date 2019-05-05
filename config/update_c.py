7619f52a513eea7e0f2cec326c2c710dc146d2bb
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2019-05-05 22:10:40 +0800

    add

--- config/update_c.py
+++ config/update_c.py
@@ -46,11 +46,12 @@ from QUANTTOOLS.QAStockETL import (QA_etl_stock_list, QA_etl_stock_info,
                                    QA_etl_stock_block, QA_etl_stock_divyield,
                                    QA_etl_process_financial_day,QA_util_process_quantdata)
 
-
-
-print("processing quant data in sqldatabase")
-QA_etl_process_financial_day('day')
+print("download index & etf data ")
+QA_SU_save_index_day('tdx')
+QA_SU_save_etf_day('tdx')
 print("done")
-print("write quant data into mongodb")
-QA_SU_save_stock_fianacial_momgo()
+print("download min data ")
+QA_SU_save_stock_min('tdx')
+QA_SU_save_index_min('tdx')
+QA_SU_save_etf_min('tdx')
 print("done")
--- config/update_x.py
+++ config/update_x.py
@@ -41,7 +41,10 @@ from QUANTAXIS.QASU.save_binance import (QA_SU_save_binance,
                                          QA_SU_save_binance_1min,
                                          QA_SU_save_binance_symbol)
 from QUANTTOOLS.QAStockETL import (QA_SU_save_report_calendar_his, QA_SU_save_stock_divyield_his, QA_SU_save_fianacialTTM_momgo,
-                                   QA_SU_save_stock_divyield_day,QA_SU_save_report_calendar_day, QA_SU_save_stock_fianacial_momgo_his)
+                                   QA_SU_save_stock_divyield_day,QA_SU_save_report_calendar_day,
+                                   QA_SU_save_stock_fianacial_momgo,QA_SU_save_stock_fianacial_momgo_his,
+                                   QA_SU_save_stock_financial_sina_day,QA_SU_save_stock_financial_sina_his,
+                                   QA_SU_save_stock_technical_index_day)
 from QUANTTOOLS.QAStockETL import (QA_etl_stock_list, QA_etl_stock_info,
                                    QA_etl_stock_xdxr, QA_etl_stock_day,
                                    QA_etl_stock_financial, QA_etl_stock_calendar,
@@ -52,8 +55,6 @@ from QUANTTOOLS.QAStockETL import (QA_etl_stock_list, QA_etl_stock_info,
 print("download day data ")
 QA_SU_save_stock_day('tdx')
 QA_SU_save_stock_xdxr('tdx')
-QA_SU_save_index_day('tdx')
-QA_SU_save_etf_day('tdx')
 QA_SU_save_stock_list('tdx')
 QA_SU_save_stock_block('tdx')
 QA_SU_save_stock_info('tdx')
@@ -68,8 +69,9 @@ QA_etl_stock_xdxr(type == "all")
 QA_etl_stock_day()
 QA_etl_stock_block()
 print("done")
-print("download min data ")
-QA_SU_save_stock_min('tdx')
-QA_SU_save_index_min('tdx')
-QA_SU_save_etf_min('tdx')
+print("processing quant data in sqldatabase")
+QA_etl_process_financial_day('day')
+print("done")
+print("write quant data into mongodb")
+QA_SU_save_stock_fianacial_momgo()
 print("done")
