cb7fe55c5487ac8d6331da58d701e0d274c23792
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2019-05-11 00:42:18 +0800

    add

--- QUANTTOOLS/QAStockETL/QASU/QAMySQL.py
+++ QUANTTOOLS/QAStockETL/QASU/QAMySQL.py
@@ -5,7 +5,7 @@ from QUANTAXIS.QAFetch.QAQuery_Advance import (QA_fetch_stock_list_adv, QA_fetch
 
 from QUANTTOOLS.QAStockETL.QAFetch import (QA_fetch_financial_report_adv,QA_fetch_stock_financial_calendar_adv,
                                            QA_fetch_stock_divyield_adv,QA_fetch_stock_shares_adv,
-                                           QA_fetch_financial_report_wy_adv)
+                                           QA_fetch_financial_report_wy_adv, QA_fetch_stock_alpha_adv, QA_fetch_stock_technical_index_adv)
 from QUANTAXIS.QAFetch.QAQuery import ( QA_fetch_stock_basic_info_tushare, QA_fetch_stock_xdxr)
 
 from QUANTTOOLS.QAStockETL.QAUtil import QA_util_sql_store_mysql
@@ -114,3 +114,27 @@ def QA_etl_stock_financial_wy(type = "crawl", start_date = str(datetime.date.tod
         else:
             data = data.reset_index(drop=True).fillna(0)
             QA_util_sql_store_mysql(data, "stock_financial_wy",if_exists='append')
+
+def QA_etl_stock_alpha_day(type = "day", mark_day = str(datetime.date.today())):
+    if type == "all":
+        data = QA_fetch_stock_alpha_adv(list(QA_fetch_stock_list_adv()['code'])).data.reset_index()
+        QA_util_sql_store_mysql(data, "stock_alpha",if_exists='replace')
+    elif type == "day":
+        data = QA_fetch_stock_alpha_adv(list(QA_fetch_stock_list_adv()['code']), mark_day)
+        if data is None:
+            print("We have no MARKET data for the day {}".format(str(datetime.date.today())))
+        else:
+            data = data.data.reset_index()
+            QA_util_sql_store_mysql(data, "stock_alpha",if_exists='append')
+
+def QA_etl_stock_technical_day(type = "day", mark_day = str(datetime.date.today())):
+    if type == "all":
+        data = QA_fetch_stock_technical_index_adv(list(QA_fetch_stock_list_adv()['code'])).data.reset_index()
+        QA_util_sql_store_mysql(data, "stock_technical",if_exists='replace')
+    elif type == "day":
+        data = QA_fetch_stock_technical_index_adv(list(QA_fetch_stock_list_adv()['code']), mark_day)
+        if data is None:
+            print("We have no MARKET data for the day {}".format(str(datetime.date.today())))
+        else:
+            data = data.data.reset_index()
+            QA_util_sql_store_mysql(data, "stock_technical",if_exists='append')
