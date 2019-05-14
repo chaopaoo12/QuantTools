c525cc345ea282b900bd034c82aa0a7c1728e871
zc132232 <zc132232@alibaba-inc.com>
2019-05-14 16:27:24 +0800

    Default Changelist

--- QUANTTOOLS/FactorTools/base_func.py
+++ QUANTTOOLS/FactorTools/base_func.py
@@ -8,7 +8,7 @@ import numpy as np
 def standardize_series(series): #原始值法
     std = series.std()
     mean = series.mean()
-    return (series-mean)/std
+    return(round((series-mean)/std * 100,4))
 
 def filter_extreme_3sigma(array,n=3): #3 sigma
     sigma = array.std()
@@ -81,8 +81,8 @@ def get_quant_data(start_date, end_date):
                                                                                                         'alpha_152', 'alpha_153', 'alpha_155', 'alpha_156', 'alpha_158', 'alpha_159', 'alpha_160', 'alpha_161', 'alpha_162',
                                                                                                         'alpha_163', 'alpha_164', 'alpha_167', 'alpha_168', 'alpha_169', 'alpha_170', 'alpha_171', 'alpha_172', 'alpha_173',
                                                                                                         'alpha_175', 'alpha_176', 'alpha_177', 'alpha_178', 'alpha_179', 'alpha_180', 'alpha_184', 'alpha_185', 'alpha_186',
-                                                                                                        'alpha_187', 'alpha_188', 'alpha_189', 'alpha_191']].groupby('code').apply(series_to_supervised,[30,10,7,5,3,1]).groupby('date').apply(get_trans)
-    technical = QA_fetch_stock_technical_index_adv(list(QA_fetch_stock_list_adv()['code']),start_date,end_date).data.groupby('code').apply(series_to_supervised,[10,7,5,4,3,2,1]).groupby('date').apply(get_trans)
+                                                                                                        'alpha_187', 'alpha_188', 'alpha_189', 'alpha_191']].groupby('date').apply(get_trans).groupby('code').apply(series_to_supervised,[30,10,7,5,3,1])
+    technical = QA_fetch_stock_technical_index_adv(list(QA_fetch_stock_list_adv()['code']),start_date,end_date).data.groupby('date').apply(get_trans).groupby('code').apply(series_to_supervised,[10,7,5,4,3,2,1])
     fianacial['TOTAL_MARKET']= fianacial['TOTAL_MARKET'].apply(lambda x:math.log(x))
     cols = [i for i in list(fianacial.columns) if i not in ['GROSSMARGIN', 'GROSSMARGIN_L2Y', 'GROSSMARGIN_L3Y', 'GROSSMARGIN_L4Y', 'GROSSMARGIN_LY',
                                                         'NETCASHOPERATINRATE', 'NETCASHOPERATINRATE_L2Y', 'NETCASHOPERATINRATE_L3Y', 'NETCASHOPERATINRATE_LY',
