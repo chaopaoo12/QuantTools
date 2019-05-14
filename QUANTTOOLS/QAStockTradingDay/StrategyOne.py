a4124bfbe3dd0724e10821bfd8b834fbac3a9853
zc132232 <zc132232@alibaba-inc.com>
2019-05-14 18:22:17 +0800

    Default Changelist

---
+++ QUANTTOOLS/QAStockTradingDay/StrategyOne.py
@@ -0,0 +1,20 @@
+
+class Stacking():
+
+    def __init__(self, X_train, X_test, Y_train, Y_test):
+        self.X_train = X_train
+        self.X_test = X_test
+        self.Y_train = Y_train
+        self.Y_test = Y_test
+
+    def prepare(self):
+        pass
+
+    def level_one(self):
+        pass
+
+    def level_two(self):
+        pass
+
+    def level_three(self):
+        pass
