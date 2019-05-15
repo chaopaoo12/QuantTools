c57e5ee7cbb54ab8e8aa80cd6e0b4b1a167eec76
zc132232 <zc132232@alibaba-inc.com>
2019-05-15 18:20:15 +0800

    Default Changelist

--- QUANTTOOLS/QAStockTradingDay/StrategyOne.py
+++ QUANTTOOLS/QAStockTradingDay/StrategyOne.py
@@ -7,10 +7,36 @@ from sklearn.ensemble import AdaBoostClassifier as ADA
 from sklearn.ensemble import BaggingClassifier
 from sklearn.manifold import t_sne
 from sklearn.cluster import KMeans
+from sklearn.linear_model import LogisticRegression
+
+from sklearn.model_selection import GridSearchCV
 from sklearn.externals import joblib
 from sklearn.model_selection import StratifiedKFold
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_auc_score
+
+
+#####prepare
+#kmeans
+#t-sne
+
+#####base model (20)
+#randomforest
+#knn(2,3,4,5,6,7,8) 10
+#GBDT
+#XGBOOST
+#ExtraTreesClassifier
+#AdaBoostClassifier
+#BaggingClassifier
+#neural_network
+
+#####lv2 model
+#AdaBoostClassifier
+#TF
+#XGBOOST
+
+#####submit model
+#LogisticRegression
 
 def RandomForest():
     pass
