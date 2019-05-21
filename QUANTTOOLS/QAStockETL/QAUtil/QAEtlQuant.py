5f572342d2af01dd43603ff1703fb6b44a3e3c16
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2019-05-21 21:45:44 +0800

    add

--- QUANTTOOLS/QAStockETL/QAUtil/QAEtlQuant.py
+++ QUANTTOOLS/QAStockETL/QAUtil/QAEtlQuant.py
@@ -495,7 +495,7 @@ def QA_util_etl_stock_quant(deal_date = None):
                 AVG_PRE_MARKET / TOTAL_MARKET - 1
              end) * 100,
              2) as avg_target
-  from (select * QUANT_ANALYSIS_DATA 
+  from (select * from QUANT_ANALYSIS_DATA 
  where order_date = to_date('{start_date}', 'yyyy-mm-dd'))A
  left join (select CODE,
                     COUNT(DISTINCT decode(blockname, '上证50', 1, 0)) as SZ50,
