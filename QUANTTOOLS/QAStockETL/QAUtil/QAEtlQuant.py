18f1acae9bb0f6f636978edc7ed785b4ddd83bd8
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2019-05-24 22:56:13 +0800

    add

--- QUANTTOOLS/QAStockETL/QAUtil/QAEtlQuant.py
+++ QUANTTOOLS/QAStockETL/QAUtil/QAEtlQuant.py
@@ -492,7 +492,7 @@ def QA_util_etl_stock_quant(deal_date = None):
                when TOTAL_MARKET = 0 then
                 0
                else
-                AVG_PRE_MARKET / TOTAL_MARKET - 1
+                AVG_PRE_MARKET / 100 / TOTAL_MARKET - 1
              end) * 100,
              2) as avg_target
   from (select * from QUANT_ANALYSIS_DATA 
