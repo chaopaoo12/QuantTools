bd5fb4c53169f1287b0d29759e40af12b036c7b7
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2019-05-25 15:03:45 +0800

    add

--- QUANTTOOLS/QAStockETL/QASU/QAMySQL.py
+++ QUANTTOOLS/QAStockETL/QASU/QAMySQL.py
@@ -42,14 +42,20 @@ def QA_etl_stock_xdxr(type = "day", mark_day = str(datetime.date.today())):
 
 def QA_etl_stock_day(type = "day", mark_day = str(datetime.date.today())):
     if type == "all":
-        data = QA_fetch_stock_day_adv(list(QA_fetch_stock_list_adv()['code'])).data.reset_index()
+        data = QA_fetch_stock_day_adv(list(QA_fetch_stock_list_adv()['code']))
+        res1 = data.to_qfq().data
+        res1.columns = [x + '_qfq' for x in res1.columns]
+        data = data.data.join(res1)
         QA_util_sql_store_mysql(data, "stock_market_day",if_exists='replace')
     elif type == "day":
         data = QA_fetch_stock_day_adv(list(QA_fetch_stock_list_adv()['code']), mark_day)
+        res1 = data.to_qfq().data
+        res1.columns = [x + '_qfq' for x in res1.columns]
+        data = data.data.join(res1)
         if data is None:
             print("We have no MARKET data for the day {}".format(mark_day))
         else:
-            data = data.data.reset_index()
+            data = data.data
             QA_util_sql_store_mysql(data, "stock_market_day",if_exists='append')
 
 def QA_etl_stock_financial(type = "crawl", start_date = str(datetime.date.today())):
