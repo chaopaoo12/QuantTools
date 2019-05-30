5b4a828272acc5b4d1aba817e79187a6fc050e7c
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2019-05-30 16:11:48 +0800

    add

--- QUANTTOOLS/QAStockTradingDay/StrategyOne.py
+++ QUANTTOOLS/QAStockTradingDay/StrategyOne.py
@@ -105,6 +105,17 @@ model.fit(X, y)
 
 #####submit model
 #LogisticRegression
+import time
+from functools import wraps
+def time_this_function(func):
+    #作为装饰器使用，返回函数执行需要花费的时间
+    @wraps(func)
+    def wrapper(*args,**kwargs):
+        start=time.time()
+        result=func(*args,**kwargs)
+        end=time.time()
+        print(func.__name__,end-start)
+        return result
 
 from sklearn.metrics import confusion_matrix
 def customedscore(preds, dtrain):
