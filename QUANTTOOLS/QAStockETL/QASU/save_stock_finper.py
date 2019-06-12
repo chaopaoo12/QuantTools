cb74cf4d347aabdfecfb4c0c14a2097c45a7606a
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2019-06-12 17:55:47 +0800

    add

--- QUANTTOOLS/QAStockETL/QASU/save_stock_finper.py
+++ QUANTTOOLS/QAStockETL/QASU/save_stock_finper.py
@@ -46,11 +46,11 @@ def QA_SU_save_stock_fianacial_percent(code, start_date=None,end_date=None,clien
         except Exception as error0:
             print(error0)
             err.append(str(code))
-
-    #strProgressToLog = 'DOWNLOAD PROGRESS {}'.format(str(float((code.index(code) +1) / len(code) * 100))[0:4] + '%', ui_log)
-    #intProgressToLog = int(float((code.index(code) +1) / len(code) * 100))
-    #QA_util_log_info(strProgressToLog, ui_log= ui_log, ui_progress= ui_progress, ui_progress_int_value= intProgressToLog)
-    __saving_work( code, start_date, end_date, stock_financial_percent)
+    for item in code:
+        strProgressToLog = 'DOWNLOAD PROGRESS {}'.format(str(float((code.index(item) +1) / len(code) * 100))[0:4] + '%', ui_log)
+        intProgressToLog = int(float((code.index(item) +1) / len(code) * 100))
+        QA_util_log_info(strProgressToLog, ui_log= ui_log, ui_progress= ui_progress, ui_progress_int_value= intProgressToLog)
+        __saving_work( code, start_date, end_date, stock_financial_percent)
 
     if len(err) < 1:
         QA_util_log_info('SUCCESS save stock_fianacial_percent ^_^',  ui_log)
