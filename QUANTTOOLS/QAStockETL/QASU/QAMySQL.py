d55ee8b5e5b16cf2a3ded584a250a62e2342915a
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2019-06-14 15:34:31 +0800

    add

--- QUANTTOOLS/QAStockETL/QASU/QAMySQL.py
+++ QUANTTOOLS/QAStockETL/QASU/QAMySQL.py
@@ -67,7 +67,7 @@ def pct(data):
     data['AVG60_C_MARKET']= data['AVG60_T_MARKET'].rolling(window=5).apply(rolling_ols,raw=True)
     return(data)
 
-def QA_ETL_stock_day(codes, start=None,end=None):
+def ETL_stock_day(codes, start=None,end=None):
     if start is None:
         data = QA_fetch_stock_day_adv(codes)
         res1 = data.to_qfq().data
@@ -113,10 +113,10 @@ def QA_etl_stock_xdxr(type = "day", mark_day = str(datetime.date.today())):
 def QA_etl_stock_day(type = "day", mark_day = str(datetime.date.today())):
     codes = list(QA_fetch_stock_list_adv()['code'])
     if type == "all":
-        data = QA_ETL_stock_day(codes).reset_index()
+        data = ETL_stock_day(codes).reset_index()
         QA_util_sql_store_mysql(data, "stock_market_day",if_exists='replace')
     elif type == "day":
-        data = QA_ETL_stock_day(codes, mark_day, mark_day).reset_index()
+        data = ETL_stock_day(codes, mark_day, mark_day).reset_index()
         if data is None:
             print("We have no MARKET data for the day {}".format(mark_day))
         else:
