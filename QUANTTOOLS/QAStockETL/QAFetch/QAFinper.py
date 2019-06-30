a9bbb84afecac84fbeec1eac7d666e1737d125b0
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2019-06-30 22:36:18 +0800

    add

--- QUANTTOOLS/QAStockETL/QAFetch/QAFinper.py
+++ QUANTTOOLS/QAStockETL/QAFetch/QAFinper.py
@@ -63,7 +63,7 @@ def perank(data):
 
 def QA_fetch_get_stock_financial_percent(code,start_date,end_date):
     start = QA_util_get_pre_trade_date(start_date,91)
-    fianacial = QA_fetch_stock_fianacial_adv(code,start,end_date).data[['PB', 'PE', 'PEG', 'PS','PB_RANK','PE_RANK']]
+    fianacial = QA_fetch_stock_fianacial_adv(code,start,end_date).data[['PB', 'PE', 'PEG', 'PS','PB_RANK','PE_RANK']].sort_index()
     try:
         fianacial = fianacial.groupby('code').apply(perank).loc[pd.Series(pd.date_range(start_date, end_date, freq='D')).apply(lambda x: str(x)[0:10])].reset_index()
         fianacial = fianacial[[x for x in list(fianacial.columns) if x not in ['PB', 'PE', 'PEG', 'PS','PB_RANK','PE_RANK']]]
