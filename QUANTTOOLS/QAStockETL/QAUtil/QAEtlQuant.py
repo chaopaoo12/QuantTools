1dd466733b85bd51a8e779db2daca682cf9abb25
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2019-07-02 19:27:15 +0800

    add

--- QUANTTOOLS/QAStockETL/QAUtil/QAEtlQuant.py
+++ QUANTTOOLS/QAStockETL/QAUtil/QAEtlQuant.py
@@ -370,7 +370,7 @@ def QA_util_etl_stock_quant(deal_date = None):
        avg90_c_market,
        pe_rank,
        pb_rank
-  from stock_analysis_data
+  from stock_analysis_data a
  where order_date = to_date('{start_date}', 'yyyy-mm-dd')
 '''
     if deal_date is None:
