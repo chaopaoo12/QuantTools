b4f909a3a8201a8ff4d4a6957641743bc89559b9
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2019-08-04 13:58:12 +0800

    add

--- QUANTTOOLS/QAStockETL/QAFetch/QAQuery.py
+++ QUANTTOOLS/QAStockETL/QAFetch/QAQuery.py
@@ -707,10 +707,10 @@ def QA_fetch_stock_quant_pre(code, start, end=None, format='pd'):
         print("QA Error QA_fetch_stock_quant_data format parameter %s is none of  \"P, p, pandas, pd , json, dict , n, N, numpy, list, l, L, !\" " % format)
         return None
 
-def QA_fetch_financial_code(N=10):
-    START_DATE = QA_util_get_pre_trade_date(QA_util_today_str(),N)
-    code = list(QA_fetch_stock_financial_calendar(QA.QA_fetch_stock_list_adv().code.tolist(),START_DATE)['code'])
-    market_day = pd.DataFrame(QA_fetch_stock_basic_info_tushare())[['code','timeToMarket']].set_index('code')
+def QA_fetch_financial_code(ndays=10):
+    START = str(QA_util_get_pre_trade_date(QA_util_today_str(),ndays))
+    code = list(QA_fetch_stock_financial_calendar(QA.QA_fetch_stock_list_adv().code.tolist(),start = START)['code'])
+    market_day = pd.DataFrame(QA_fetch_stock_basic_info_tushare())[['code','timeToMarket']]
     market_day['TM'] = market_day['timeToMarket'].apply(lambda x:str(QA_util_add_months(QA_util_date_int2str(int(x)),0) if x >0 else None)[0:10])
-    code = list(market_day[market_day['TM']>= START_DATE]['code']) + code
+    code = list(market_day[market_day['TM'] >= START]['code'].values) + code
     return(code)
