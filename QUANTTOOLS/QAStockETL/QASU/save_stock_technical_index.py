142a49488971dc9f0637c947c8fab0b36d3c779b
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2019-08-07 01:17:11 +0800

    add

--- QUANTTOOLS/QAStockETL/QASU/save_stock_technical_index.py
+++ QUANTTOOLS/QAStockETL/QASU/save_stock_technical_index.py
@@ -40,16 +40,7 @@ def QA_SU_save_stock_technical_index_day(START_DATE=None,END_DATE=None,client=DA
             print(error0)
             err.append(str(code))
 
-    for item in codes:
-
-        QA_util_log_info('The {} of Total {}'.format
-                         ((codes.index(item) +1), len(codes)))
-
-        strProgressToLog = 'DOWNLOAD PROGRESS {}'.format(str(float((codes.index(item) +1) / len(codes) * 100))[0:4] + '%', ui_log)
-        intProgressToLog = int(float((codes.index(item) +1) / len(codes) * 100))
-        QA_util_log_info(strProgressToLog, ui_log= ui_log, ui_progress= ui_progress, ui_progress_int_value= intProgressToLog)
-
-        __saving_work( item,START_DATE,END_DATE, stock_technical_index)
+    __saving_work( codes,START_DATE,END_DATE, stock_technical_index)
 
     if len(err) < 1:
         QA_util_log_info('SUCCESS save stock_technical_index ^_^',  ui_log)
@@ -145,16 +136,7 @@ def QA_SU_save_stock_technical_week_day(START_DATE=None,END_DATE=None,client=DAT
             print(error0)
             err.append(str(code))
 
-    for item in codes:
-
-        QA_util_log_info('The {} of Total {}'.format
-                         ((codes.index(item) +1), len(codes)))
-
-        strProgressToLog = 'DOWNLOAD PROGRESS {}'.format(str(float((codes.index(item) +1) / len(codes) * 100))[0:4] + '%', ui_log)
-        intProgressToLog = int(float((codes.index(item) +1) / len(codes) * 100))
-        QA_util_log_info(strProgressToLog, ui_log= ui_log, ui_progress= ui_progress, ui_progress_int_value= intProgressToLog)
-
-        __saving_work( item,START_DATE,END_DATE, stock_technical_week)
+    __saving_work( codes,START_DATE,END_DATE, stock_technical_week)
 
     if len(err) < 1:
         QA_util_log_info('SUCCESS save stock_technical_week ^_^',  ui_log)
@@ -250,16 +232,7 @@ def QA_SU_save_stock_technical_month_day(START_DATE=None,END_DATE=None,client=DA
             print(error0)
             err.append(str(code))
 
-    for item in codes:
-
-        QA_util_log_info('The {} of Total {}'.format
-                         ((codes.index(item) +1), len(codes)))
-
-        strProgressToLog = 'DOWNLOAD PROGRESS {}'.format(str(float((codes.index(item) +1) / len(codes) * 100))[0:4] + '%', ui_log)
-        intProgressToLog = int(float((codes.index(item) +1) / len(codes) * 100))
-        QA_util_log_info(strProgressToLog, ui_log= ui_log, ui_progress= ui_progress, ui_progress_int_value= intProgressToLog)
-
-        __saving_work( item,START_DATE,END_DATE, stock_technical_month)
+    __saving_work( codes,START_DATE,END_DATE, stock_technical_month)
 
     if len(err) < 1:
         QA_util_log_info('SUCCESS save stock_technical_month ^_^',  ui_log)
