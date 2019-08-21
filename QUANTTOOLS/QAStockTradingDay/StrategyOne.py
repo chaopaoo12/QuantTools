d03a92bc0a48be3e96b6ff9928934f4e89e20560
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2019-08-21 19:40:21 +0800

    add

--- QUANTTOOLS/QAStockTradingDay/StrategyOne.py
+++ QUANTTOOLS/QAStockTradingDay/StrategyOne.py
@@ -62,10 +62,11 @@ class model():
         self.info['dev_status']=dict()
 
     def get_data(self, start, end, type='crawl', block=True):
-        self.data = get_quant_data(start , end, type, block)
+        self.data = get_quant_data(start = start, end = end, type = type, block=block)
+        print(self.data.shape)
 
     def set_target(self, mark):
-        self.data['star'] = self.data['TARGET'].groupby('date').apply(lambda x: x.rank(ascending=False,pct=True)).apply(lambda x :1 if x >= mark else 0)
+        self.data['star'] = self.data['TARGET'].groupby('date').apply(lambda x :1 if x >= mark else 0)
         self.data.loc[self.data['PASS_MARK'] >= 9.95,'star'] = 0
         self.cols = [i for i in self.data.columns if i not in ['moon','star','mars','venus','sun','MARK','DAYSO','RNG_LO',
                                                                'LAG_TORO','OPEN_MARK','PASS_MARK','TARGET','TARGET3',
