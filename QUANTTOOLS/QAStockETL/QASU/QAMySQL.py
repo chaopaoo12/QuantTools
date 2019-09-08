72d0d9b5a725044456262f6a19dcd31ed8ac3dad
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2019-09-08 22:11:33 +0800

    add

--- QUANTTOOLS/QAStockETL/QASU/QAMySQL.py
+++ QUANTTOOLS/QAStockETL/QASU/QAMySQL.py
@@ -102,7 +102,7 @@ def ETL_stock_day(codes, start=None,end=None):
         res1.columns = [x + '_qfq' for x in res1.columns]
         data = data.data.join(res1).fillna(0).reset_index()
         res = data.groupby('code').apply(pct)
-        res = res.reset_index(level = 0,drop = True).reset_index().set_index(['date','code']).loc[pd.date_range(start, end, freq='D')]
+        res = res.reset_index(level = 0,drop = True).reset_index().set_index(['date','code']).loc[pd.date_range(start, end, freq='D').apply(lambda x: str(x)[0:10])]
     return(res)
 
 def QA_etl_stock_list():
