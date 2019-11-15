a0b1f61a3d61a7d7ea67f8b49ff54fe1adfc0449
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2019-11-15 23:51:05 +0800

    add

--- QUANTTOOLS/message_func/wechat.py
+++ QUANTTOOLS/message_func/wechat.py
@@ -8,12 +8,10 @@ from QUANTTOOLS.message_func.setting import QAPRO_ID
 def send_actionnotice(strategy_id,
                       account,
                       code,
-                      name,
-                      industry,
                       direction,
                       offset,
-                      price,
                       volume,
+                      price=None,
                       user = QAPRO_ID,
                       now = str(datetime.datetime.now)):
     requests.post("http://www.yutiansut.com/signal?user_id={}&template=xiadan_report&strategy_id={}&realaccount={}&code={}&order_direction={}&order_offset={}&price={}&volume={}&order_time={}".format(user = user, strategy_id = strategy_id, account = account, code = code, direction= direction, offset= offset, price = price, volume = volume, now =now))
