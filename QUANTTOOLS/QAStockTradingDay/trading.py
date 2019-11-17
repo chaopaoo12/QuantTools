ec33c11997bdb7c50cdc560ce5b3c61d89f57723
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2019-11-17 23:34:54 +0800

    add

--- QUANTTOOLS/QAStockTradingDay/trading.py
+++ QUANTTOOLS/QAStockTradingDay/trading.py
@@ -1,4 +1,4 @@
-
+import QUANTAXIS as QA
 from QUANTTOOLS.message_func.wechat import send_actionnotice
 from QUANTTOOLS.QAStockTradingDay.StrategyOne import model, load_model, model_predict, check_model
 from QUANTAXIS.QAFetch.QAQuery_Advance import QA_fetch_stock_day_adv
@@ -34,24 +34,19 @@ def trading(date,strategy_id='机器学习1号', account1='name:client-1', worki
 
     tar = model_predict(model_temp, str(date[0:7])+"-01",date,info_temp['cols'])
     res = pd.concat([tar[tar['RANK'] <= 5].loc[date][['Z_PROB','O_PROB','RANK']],
-                     QA_fetch_stock_day_adv(list(tar[tar['RANK'] <= 5].loc[date].index),date,date).data.loc[date].reset_index('date')['close'],
+                     QA.QAFetch.QA_fetch_get_stock_realtime('tdx',code=list(tar[tar['RANK'] <= 5].loc[date].index)).reset_index('datetime')[['ask1','ask_vol1','bid1','bid_vol1']],
                      QA_fetch_stock_fianacial_adv(list(tar[tar['RANK'] <= 5].loc[date].index),date,date).data.reset_index('date')[['NAME','INDUSTRY']]],
                     axis=1)
     #计算资金分配
     avg_account = sub_accounts['可用金额']/tar[tar['RANK'] <= 5].loc[date].shape[0]
     res = res.assign(tar=avg_account[0])
-    res['cnt'] = (res['tar']/res['close']/100).apply(lambda x:round(x,0)*100)
-    res['real'] = res['cnt'] * res['close']
-
-    ##近日盈利情况
-    table1 = tar[tar['RANK']<=5].groupby('date').mean()
+    res['cnt'] = (res['tar']/res['ask1']/100).apply(lambda x:round(x,0)*100)
+    res['real'] = res['cnt'] * res['ask1']
 
     ##当前持仓
     positions = client.get_positions(account1)['positions'][['证券代码','股票余额','可用余额','冻结数量','参考盈亏','盈亏比例(%)','当前持仓']]
 
     for i in list(positions['证券代码']) + list(res.index):
-        #print(res.loc[i]['cnt'])
-        #print(positions[positions['证券代码'] == i]['当前持仓'].get(0,default=0))
         mark = res.loc[i]['cnt'] - positions[positions['证券代码'] == i]['当前持仓'].get(0,default=0)
         if mark < -100:
             #卖出mark i
@@ -62,7 +57,7 @@ def trading(date,strategy_id='机器学习1号', account1='name:client-1', worki
                                                                                         target=res.loc[i]['cnt'],
                                                                                         tar=res.loc[i]['real']))
             try:
-                client.sell(account1, symbol=i, type='MARKET', priceType=4, amount=abs(mark))
+                client.sell(account1, symbol=i, type='MARKET', priceType=4, amount=abs(mark)/100)
                 send_actionnotice(strategy_id,
                                   account_info,
                                   '{code}({NAME},{INDUSTRY})'.format(code=i,NAME= res.loc[i]['NAME'], INDUSTRY=res.loc[i]['INDUSTRY']),
@@ -87,7 +82,7 @@ def trading(date,strategy_id='机器学习1号', account1='name:client-1', worki
                                                                                         target=res.loc[i]['cnt'],
                                                                                         tar=res.loc[i]['real']))
             try:
-                client.buy(account1, symbol=i, type='MARKET', priceType=4, amount=abs(mark))
+                client.buy(account1, symbol=i, type='MARKET', priceType=4, amount=abs(mark)/100)
                 send_actionnotice(strategy_id,
                                   account_info,
                                   '{code}({NAME},{INDUSTRY})'.format(code=i,NAME= res.loc[i]['NAME'], INDUSTRY=res.loc[i]['INDUSTRY']),
