25e3779b2f1b12cb1aba82e2df482f9a778575aa
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2019-11-18 14:50:16 +0800

    add

--- config/aotu_trading.py
+++ config/aotu_trading.py
@@ -1,4 +1,4 @@
 from QUANTTOOLS.QAStockTradingDay.daily_job import trading
-from  QUANTAXIS.QAUtil import QA_util_today_str
-mark_day = QA_util_today_str()
+from  QUANTAXIS.QAUtil import QA_util_today_str,QA_util_get_last_day
+mark_day = QA_util_get_last_day(QA_util_today_str())
 trading(mark_day)
