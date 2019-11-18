737c19e66c9e8c9bd0ac279060dc1f635e58a3ad
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2019-11-18 14:53:18 +0800

    add

--- QUANTTOOLS/QAStockTradingDay/trading.py
+++ QUANTTOOLS/QAStockTradingDay/trading.py
@@ -1,8 +1,6 @@
 import QUANTAXIS as QA
 from QUANTTOOLS.message_func.wechat import send_actionnotice
 from QUANTTOOLS.QAStockTradingDay.StrategyOne import model, load_model, model_predict, check_model
-from QUANTAXIS.QAFetch.QAQuery_Advance import QA_fetch_stock_day_adv
-from QUANTAXIS.QAFetch.QAQuery import QA_fetch_stock_basic_info_tushare
 from QUANTTOOLS.QAStockETL.QAFetch import QA_fetch_stock_fianacial_adv
 import pandas as pd
 import logging
@@ -16,7 +14,7 @@ delta3 = timedelta(days=7)
 delta4 = timedelta(days=8)
 
 
-def trading(date,strategy_id='机器学习1号', account1='name:client-1', working_dir=working_dir):
+def trading(date, strategy_id='机器学习1号', account1='name:client-1', working_dir=working_dir):
     try:
         logging.basicConfig(level=logging.DEBUG)
         client = strategyease_sdk.Client(host=yun_ip, port=yun_port, key=easytrade_password)
@@ -37,13 +35,13 @@ def trading(date,strategy_id='机器学习1号', account1='name:client-1', worki
                      QA.QAFetch.QA_fetch_get_stock_realtime('tdx',code=list(tar[tar['RANK'] <= 5].loc[date].index)).reset_index('datetime')[['ask1','ask_vol1','bid1','bid_vol1']],
                      QA_fetch_stock_fianacial_adv(list(tar[tar['RANK'] <= 5].loc[date].index),date,date).data.reset_index('date')[['NAME','INDUSTRY']]],
                     axis=1)
-    #计算资金分配
+    print('计算资金分配')
     avg_account = sub_accounts['可用金额']/tar[tar['RANK'] <= 5].loc[date].shape[0]
     res = res.assign(tar=avg_account[0])
     res['cnt'] = (res['tar']/res['ask1']/100).apply(lambda x:round(x,0)*100)
     res['real'] = res['cnt'] * res['ask1']
 
-    ##当前持仓
+    print('近日盈利情况')
     positions = client.get_positions(account1)['positions'][['证券代码','股票余额','可用余额','冻结数量','参考盈亏','盈亏比例(%)','当前持仓']]
 
     for i in list(positions['证券代码']) + list(res.index):
