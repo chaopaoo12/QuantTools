c70b6f2d50dd877fb046a3e62cce3d1e11b122fb
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2019-11-18 15:23:52 +0800

    add

--- QUANTTOOLS/QAStockTradingDay/daily_job.py
+++ QUANTTOOLS/QAStockTradingDay/daily_job.py
@@ -3,7 +3,7 @@ from QUANTTOOLS.QAStockTradingDay.train import train
 from QUANTTOOLS.QAStockTradingDay.running import predict
 from QUANTTOOLS.QAStockTradingDay.setting import working_dir, yun_ip, yun_port, easytrade_password
 from QUANTTOOLS.QAStockTradingDay.trading import trading
-import QUANTAXIS as QA
+from  QUANTAXIS.QAUtil import QA_util_today_str,QA_util_get_last_day
 
 def job111(date):
     if datetime.strptime(date, "%Y-%m-%d").weekday() == 5:
@@ -15,7 +15,4 @@ def job111(date):
 
 
 def trading(date):
-    if QA.QA_util_if_trade(date) == True:
-        trading(date)
-    else:
-        pass
+    trading(QA_util_get_last_day(date))
