f55addfccc43b379a0df35360b624b91ccdd8e66
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2019-11-28 23:33:59 +0800

    add

--- QUANTTOOLS/account_manage/trading_message.py
+++ QUANTTOOLS/account_manage/trading_message.py
@@ -1,7 +1,7 @@
 from QUANTTOOLS.message_func.wechat import send_actionnotice
 
 def send_trading_message(account1, strategy_id, account_info, code, NAME, INDUSTRY, mark, direction, type, priceType, client):
-    e = []
+    codes = []
     try:
         if direction == 'SELL':
             client.sell(account1, symbol=code, type=type, priceType=priceType, amount=abs(mark))
@@ -16,7 +16,7 @@ def send_trading_message(account1, strategy_id, account_info, code, NAME, INDUST
                           offset='OPEN',
                           volume=abs(mark)
                           )
-    except:
+    except Exception as e:
         send_actionnotice(strategy_id,
                           account_info,
                           '{code}({NAME},{INDUSTRY}) 交易失败'.format(code=code,NAME= NAME, INDUSTRY=INDUSTRY),
@@ -24,5 +24,6 @@ def send_trading_message(account1, strategy_id, account_info, code, NAME, INDUST
                           offset= 'OPEN',
                           volume=abs(mark)
                           )
-        e.append(code)
+        print(e)
+        codes.append(code)
     return(e)
