b0e4685e2a17aab557fe650dae782c7cdfa6f17e
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2019-11-28 23:08:44 +0800

    add

--- QUANTTOOLS/QAStockTradingDay/running.py
+++ QUANTTOOLS/QAStockTradingDay/running.py
@@ -12,10 +12,10 @@ from QUANTAXIS.QAUtil import (QA_util_log_info)
 from QUANTTOOLS.message_func.wechat import send_actionnotice
 
 
-def predict(date, strategy_id='机器学习1号', account1='name:client-1', working_dir=working_dir, ui_log = None):
+def predict(trading_date, strategy_id='机器学习1号', account1='name:client-1', working_dir=working_dir, ui_log = None):
     try:
         QA_util_log_info(
-            '##JOB01 Now Got Account Info ==== {}'.format(str(date)), ui_log)
+            '##JOB01 Now Got Account Info ==== {}'.format(str(trading_date)), ui_log)
         logging.basicConfig(level=logging.DEBUG)
         client = strategyease_sdk.Client(host=yun_ip, port=yun_port, key=easytrade_password)
         account1=account1
@@ -23,9 +23,9 @@ def predict(date, strategy_id='机器学习1号', account1='name:client-1', work
         print(account_info)
         sub_accounts = client.get_positions(account1)['sub_accounts']
     except:
-        send_email('错误报告', '云服务器错误,请检查', 'date')
+        send_email('错误报告', '云服务器错误,请检查', trading_date)
         send_actionnotice(strategy_id,
-                          '错误报告:{}'.format(date),
+                          '错误报告:{}'.format(trading_date),
                           '云服务器错误,请检查',
                           direction = 'HOLD',
                           offset='HOLD',
@@ -33,12 +33,12 @@ def predict(date, strategy_id='机器学习1号', account1='name:client-1', work
                           )
     try:
         QA_util_log_info(
-            '##JOB02 Now Load Model ==== {}'.format(str(date)), ui_log)
+            '##JOB02 Now Load Model ==== {}'.format(str(trading_date)), ui_log)
         model_temp,info_temp = load_model(working_dir = working_dir)
     except:
-        send_email('错误报告', '无法正确加载模型,请检查', 'date')
+        send_email('错误报告', '无法正确加载模型,请检查', trading_date)
         send_actionnotice(strategy_id,
-                          '错误报告:{}'.format(date),
+                          '错误报告:{}'.format(trading_date),
                           '无法正确加载模型,请检查',
                           direction = 'HOLD',
                           offset='HOLD',
@@ -46,34 +46,34 @@ def predict(date, strategy_id='机器学习1号', account1='name:client-1', work
                           )
 
     QA_util_log_info(
-        '##JOB03 Now Model Predict ==== {}'.format(str(date)), ui_log)
-    tar = model_predict(model_temp, str(date[0:7])+"-01",date,info_temp['cols'])
+        '##JOB03 Now Model Predict ==== {}'.format(str(trading_date)), ui_log)
+    tar = model_predict(model_temp, str(trading_date[0:7])+"-01",trading_date,info_temp['cols'])
 
     QA_util_log_info(
-        '##JOB03 Now Concat Result ==== {}'.format(str(date)), ui_log)
-    tar1 = tar[tar['RANK'] <= 5].loc[[date]]
+        '##JOB03 Now Concat Result ==== {}'.format(str(trading_date)), ui_log)
+    tar1 = tar[tar['RANK'] <= 5].loc[trading_date]
     tar2 = tar1[['Z_PROB','O_PROB','RANK']]
-    close = QA_fetch_stock_day_adv(list(tar1.index),date,date).data.loc[date].reset_index('date')['close']
-    info = QA_fetch_stock_fianacial_adv(list(tar1.index),date,date).data.reset_index('date')[['NAME','INDUSTRY']]
+    close = QA_fetch_stock_day_adv(list(tar1.index),trading_date,trading_date).data.loc[trading_date].reset_index('date')['close']
+    info = QA_fetch_stock_fianacial_adv(list(tar1.index),trading_date,trading_date).data.reset_index('date')[['NAME','INDUSTRY']]
     res = pd.concat([tar2,close,info],axis=1)
 
     QA_util_log_info(
-        '##JOB04 Now Funding Decision ==== {}'.format(str(date)), ui_log)
-    avg_account = sub_accounts['总 资 产']/tar[tar['RANK'] <= 5].loc[date].shape[0]
+        '##JOB04 Now Funding Decision ==== {}'.format(str(trading_date)), ui_log)
+    avg_account = sub_accounts['总 资 产']/tar1.shape[0]
     res = res.assign(tar=avg_account[0])
     res['cnt'] = (res['tar']/res['close']/100).apply(lambda x:round(x,0)*100)
     res['real'] = res['cnt'] * res['close']
 
     QA_util_log_info(
-        '##JOB05 Now Current Report ==== {}'.format(str(date)), ui_log)
+        '##JOB05 Now Current Report ==== {}'.format(str(trading_date)), ui_log)
     table1 = tar[tar['RANK']<=5].groupby('date').mean()
 
     QA_util_log_info(
-        '##JOB06 Now Current Holding ==== {}'.format(str(date)), ui_log)
+        '##JOB06 Now Current Holding ==== {}'.format(str(trading_date)), ui_log)
     positions = client.get_positions(account1)['positions'][['证券代码','证券名称','股票余额','可用余额','冻结数量','参考盈亏','盈亏比例(%)']]
 
     QA_util_log_info(
-        '##JOB07 Now Message Building ==== {}'.format(str(date)), ui_log)
+        '##JOB07 Now Message Building ==== {}'.format(str(trading_date)), ui_log)
     msg1 = '模型训练日期:{model_date}'.format(model_date=info_temp['date'])
     body1 = build_table(table1, '近段时间内模型盈利报告')
     body2 = build_table(res, '目标持仓')
@@ -81,7 +81,7 @@ def predict(date, strategy_id='机器学习1号', account1='name:client-1', work
 
     msg = build_email(build_head(),msg1,body1,body2,body3)
 
-    send_email('交易报告:'+ date, msg, 'date')
+    send_email('交易报告:'+ trading_date, msg, 'date')
 
 
 
