29545a0ce4cd65af9827d310c2940275d5c0b9f7
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2019-12-24 14:51:59 +0800

    add

--- QUANTTOOLS/QAStockTradingDay/trading.py
+++ QUANTTOOLS/QAStockTradingDay/trading.py
@@ -145,7 +145,7 @@ def trading(trading_date, strategy_id= '机器学习1号', account1= 'name:clien
                                                                                     target=cnt,
                                                                                     tar=tar))
         e = send_trading_message(account1, strategy_id, account_info, i, NAME, INDUSTRY, mark, direction = 'BUY', type='MARKET', priceType=4, client=client)
-        time.sleep(10)
+        time.sleep(15)
         #while len(e) > 0:
         #    e = send_trading_message(account1, strategy_id, account_info, i, NAME, INDUSTRY, mark, direction = 'BUY', type='MARKET', priceType=4, client=client)
     return(res)
