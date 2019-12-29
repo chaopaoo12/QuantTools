adf7ab67b993fa06b9b3e0bc1ee7d8c0df926437
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2019-12-29 12:17:55 +0800

    add

--- QUANTTOOLS/message_func/setting.py
+++ QUANTTOOLS/message_func/setting.py
@@ -1,6 +1,6 @@
 QAPRO_ID='oL-C4w1_ETCiWO42fo_kakd6Sq_Q'
 smtpserver = "smtp.qq.com"
-smtpport = 587
+smtpport = 465
 msg_from ='738105084@qq.com'                                 #发送方邮箱
 passwd ='opmrbzwsqtcgbdaa'                                   #填入发送方邮箱的授权码
 msg_to =['738105084@qq.com']                                 #收件人邮箱
