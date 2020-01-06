36fd54f10b00bf9824d55d82fbe674c7e9d01e43
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-01-06 13:29:24 +0800

    add

--- QUANTTOOLS/QAStockTradingDay/daily_job.py
+++ QUANTTOOLS/QAStockTradingDay/daily_job.py
@@ -5,6 +5,7 @@ from QUANTTOOLS.QAStockTradingDay.setting import working_dir, yun_ip, yun_port, 
 from  QUANTAXIS.QAUtil import QA_util_today_str,QA_util_get_last_day
 
 def job111(date):
+    print(datetime.strptime(date, "%Y-%m-%d").weekday())
     if datetime.strptime(date, "%Y-%m-%d").weekday() == 4:
         train(date, working_dir=working_dir)
     elif datetime.strptime(date, "%Y-%m-%d").weekday() <= 4:
