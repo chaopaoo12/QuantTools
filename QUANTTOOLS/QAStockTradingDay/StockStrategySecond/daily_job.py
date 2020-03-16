8174c20c31628b6b1f24543fd970f080049756c8
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-03-16 09:43:39 +0800

    ADD

--- QUANTTOOLS/QAStockTradingDay/StockStrategySecond/daily_job.py
+++ QUANTTOOLS/QAStockTradingDay/StockStrategySecond/daily_job.py
@@ -7,7 +7,7 @@ from QUANTAXIS.QAUtil.QADate_trade import QA_util_if_trade,QA_util_get_real_date
 def job111(trading_date):
     if QA_util_if_trade(trading_date):
         if datetime.strptime(trading_date, "%Y-%m-%d").weekday() == 4:
-            #train(trading_date, working_dir=working_dir)
+            train(trading_date, working_dir=working_dir)
             predict(trading_date)
         else:
             predict(trading_date)
