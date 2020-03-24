110105766327c6c0c056c2a669f6dbda0efd6a99
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-03-24 18:06:23 +0800

    ADD

--- QUANTTOOLS/FactorTools/base_tools.py
+++ QUANTTOOLS/FactorTools/base_tools.py
@@ -95,7 +95,7 @@ def combine_model(index_d, stock_d, safe_d, start, end):
     res = pd.DataFrame()
     for i in QA.QAUtil.QA_util_get_trade_range(start,end):
         try:
-            index_res = index_d[index_d['y_pred']==1][index_d['RANK']<=5].loc[i].sort_values(by='O_PROB', ascending=False)
+            index_res = index_d[index_d['y_pred']==1][index_d['RANK']<=5].loc[i].sort_values(by='O_PROB', ascending=False, sort = True)
         except:
             index_res = None
 
