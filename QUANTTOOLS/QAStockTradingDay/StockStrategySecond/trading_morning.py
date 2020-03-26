27c5cc67cefe5365e977325a73ffd2a1aca09593
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-03-26 08:38:39 +0800

    ADD

--- QUANTTOOLS/QAStockTradingDay/StockStrategySecond/trading_morning.py
+++ QUANTTOOLS/QAStockTradingDay/StockStrategySecond/trading_morning.py
@@ -69,7 +69,7 @@ def trading(trading_date, percent=percent, strategy_id= '机器学习1号', acco
 
     h1 = int(datetime.datetime.now().strftime("%H"))
     m1 = int(datetime.datetime.now().strftime("%M"))
-    while h1 == 9 and m1 <= 25 :
+    while h1 == 9 and m1 <= 29 :
         h1 = int(datetime.datetime.now().strftime("%H"))
         m1 = int(datetime.datetime.now().strftime("%M"))
         time.sleep(30)
