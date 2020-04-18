2f9605037a57365062a5b5ba9e66e9b61eaa9ab2
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-04-19 01:08:33 +0800

    ADD

--- QUANTTOOLS/QAStockTradingDay/StockStrategySecond/daily_job.py
+++ QUANTTOOLS/QAStockTradingDay/StockStrategySecond/daily_job.py
@@ -5,19 +5,14 @@ from QUANTTOOLS.QAStockTradingDay.StockStrategySecond.setting import working_dir
 from QUANTAXIS.QAUtil.QADate_trade import QA_util_if_trade,QA_util_get_real_date
 
 def job111(trading_date):
-    print(trading_date)
-    print(QA_util_if_trade(trading_date))
-    print(datetime.strptime(trading_date, "%Y-%m-%d").weekday())
     if QA_util_if_trade(trading_date):
-        print('a')
         if datetime.strptime(trading_date, "%Y-%m-%d").weekday() == 4:
+            pass
             train(trading_date, working_dir=working_dir)
             predict(trading_date)
         else:
             predict(trading_date)
     elif datetime.strptime(trading_date, "%Y-%m-%d").weekday() == 5:
-        print('b')
         pass
     else:
-        print('c')
         predict(QA_util_get_real_date(trading_date))
