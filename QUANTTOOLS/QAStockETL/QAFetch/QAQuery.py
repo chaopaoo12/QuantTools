7a880cfd20fd9b2463da8e78affc98f951d02692
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-05-02 15:54:39 +0800

    ADD

--- QUANTTOOLS/QAStockETL/QAFetch/QAQuery.py
+++ QUANTTOOLS/QAStockETL/QAFetch/QAQuery.py
@@ -858,6 +858,7 @@ def QA_fetch_index_target(codes, start_date, end_date):
         end_date = QA_util_get_real_date(end_date)
     else:
         pass
+    end = QA_util_get_next_datetime(end_date,5)
     rng1 = pd.Series(pd.date_range(start_date, end_date, freq='D')).apply(lambda x: str(x)[0:10])
     data = QA.QA_fetch_index_day_adv(codes,start_date,end).data.fillna(0).reset_index()
     res = data.groupby('code').apply(index_pct)[['date','code',
