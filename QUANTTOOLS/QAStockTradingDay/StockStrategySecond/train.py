db423f1dbcea306225b1e790dbeb5a3ec81ff1c7
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-05-07 18:32:23 +0800

    ADD

--- QUANTTOOLS/QAStockTradingDay/StockStrategySecond/train.py
+++ QUANTTOOLS/QAStockTradingDay/StockStrategySecond/train.py
@@ -60,7 +60,9 @@ def train(date, strategy_id='机器学习1号', working_dir=working_dir, ui_log 
                               test_start=(datetime.strptime(date, "%Y-%m-%d")-delta3).strftime('%Y-%m-%d'),
                               test_end=date)
     index_model.prepare_data()
-    index_model.build_model(n_estimators=200)
+    other_params = {'learning_rate': 0.1, 'n_estimators': 100, 'max_depth': 5, 'min_child_weight': 1, 'seed': 0,
+                    'subsample': 0.8, 'colsample_bytree': 0.8, 'gamma': 0, 'reg_alpha': 0, 'reg_lambda': 1}
+    index_model.build_model(other_params)
     QA_util_log_info(
         '##JOB05 Now Model Trainnig ==== {}'.format(str(date)), ui_log)
     index_model.model_running()
@@ -75,7 +77,9 @@ def train(date, strategy_id='机器学习1号', working_dir=working_dir, ui_log 
 
     index_model.set_target('INDEX_TARGET', mark = 0.3, type = 'percent')
     index_model.prepare_data()
-    index_model.build_model(n_estimators=200)
+    other_params = {'learning_rate': 0.1, 'n_estimators': 100, 'max_depth': 5, 'min_child_weight': 1, 'seed': 0,
+                    'subsample': 0.8, 'colsample_bytree': 0.8, 'gamma': 0, 'reg_alpha': 0, 'reg_lambda': 1}
+    index_model.build_model(other_params)
     index_model.model_running()
     index_model.model_check()
     index_model.save_model('safe',working_dir = working_dir)
