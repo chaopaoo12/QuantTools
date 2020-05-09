ac4275fe281228e42bd4fdb7e4f169890640bdeb
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-05-10 07:08:54 +0800

    ADD

--- QUANTTOOLS/QAIndexTradingDay/IndexModel/IndexStrategyOne.py
+++ QUANTTOOLS/QAIndexTradingDay/IndexModel/IndexStrategyOne.py
@@ -69,34 +69,43 @@ class model():
     def model_check(self):
         if self.info['rng_report']['1']['precision'] <0.75:
             print("精确率不足,模型需要优化")
-            self.info['rng_report']['precision'] = False
+            self.info['rng_status']['precision'] = False
         elif self.info['rng_report']['1']['recall'] < 0.3:
             print("召回率不足,模型需要优化")
-            self.info['rng_report']['recall'] = False
+            self.info['rng_status']['recall'] = False
+        else:
+            self.info['rng_status']['precision'] = True
+            self.info['rng_status']['recall'] = True
+
+        if self.info['train_report']['1']['precision'] <0.75:
+            print("精确率不足,模型需要优化")
+            self.info['train_status']['precision'] = False
+        elif self.info['train_report']['1']['recall'] < 0.3:
+            print("召回率不足,模型需要优化")
+            self.info['train_status']['recall'] = False
         else:
             self.info['train_status']['precision'] = True
             self.info['train_status']['recall'] = True
 
-
         if abs(self.info['rng_report']['1']['precision'] - self.info['rng_report']['1']['precision']) > 0.1:
             print("过拟合:精确率差异过大")
-            self.info['rng_report']['precision'] = False
+            self.info['rng_status']['precision'] = False
         elif abs(self.info['train_report']['1']['recall'] - self.info['rng_report']['1']['recall']) > 0.05:
             print("过拟合:召回差异过大")
-            self.info['rng_report']['recall'] = False
+            self.info['rng_status']['recall'] = False
         else:
-            self.info['rng_report']['precision'] = True
-            self.info['rng_report']['recall'] = True
+            self.info['rng_status']['precision'] = True
+            self.info['rng_status']['recall'] = True
 
         if self.info['train_status']['precision'] == False or self.info['train_status']['recall'] == False:
             self.info['train_status']['status'] = False
         else:
             self.info['train_status']['status'] = True
 
-        if self.info['rng_report']['precision'] == False or self.info['rng_report']['recall'] == False:
-            self.info['rng_report']['status'] = False
+        if self.info['rng_status']['precision'] == False or self.info['rng_status']['recall'] == False:
+            self.info['rng_status']['status'] = False
         else:
-            self.info['rng_report']['status'] = True
+            self.info['rng_status']['status'] = True
 
 
     def save_model(self, name, working_dir = 'D:\\model\\current'):
