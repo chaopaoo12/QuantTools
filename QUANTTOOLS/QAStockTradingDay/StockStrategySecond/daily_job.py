98a25400fed3caac9bd65273660420ad0771dc1b
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-05-17 18:49:03 +0800

    ADD

--- QUANTTOOLS/QAStockTradingDay/StockStrategySecond/daily_job.py
+++ QUANTTOOLS/QAStockTradingDay/StockStrategySecond/daily_job.py
@@ -5,7 +5,13 @@ from QUANTTOOLS.QAStockTradingDay.StockStrategySecond.setting import working_dir
 from QUANTAXIS.QAUtil.QADate_trade import QA_util_if_trade,QA_util_get_real_date,QA_util_get_last_day
 
 def job111(trading_date):
+
     if datetime.strptime(trading_date, "%Y-%m-%d").weekday() == 4:
+        if QA_util_if_trade(trading_date):
+            pass
+        else:
+            trading_date = QA_util_get_last_day(trading_date, 1)
+
         predict(QA_util_get_last_day(trading_date))
         train(trading_date, working_dir=working_dir)
     else:
--- QUANTTOOLS/QAStockTradingDay/StockStrategySecond/train.py
+++ QUANTTOOLS/QAStockTradingDay/StockStrategySecond/train.py
@@ -8,18 +8,12 @@ import pandas as pd
 from QUANTAXIS.QAUtil import (QA_util_log_info)
 from QUANTTOOLS.message_func.wechat import send_actionnotice
 from datetime import datetime,timedelta
-from QUANTAXIS.QAUtil.QADate_trade import QA_util_if_trade,QA_util_get_real_date,QA_util_get_last_day
 delta = timedelta(days=6)
 delta1 = timedelta(days=1)
 delta3 = timedelta(days=7)
 delta4 = timedelta(days=8)
 
 def train(date, strategy_id='机器学习1号', working_dir=working_dir, ui_log = None):
-    if QA_util_if_trade(date):
-        date = QA_util_get_last_day(date)
-    else:
-        date = QA_util_get_last_day(date, 2)
-
     QA_util_log_info(
         '##JOB01 Now Model Init ==== {}'.format(str(date)), ui_log)
     stock_model = StockModel()
@@ -48,7 +42,6 @@ def train(date, strategy_id='机器学习1号', working_dir=working_dir, ui_log 
     important = stock_model.model_important()
     stock_model.save_model('stock',working_dir = working_dir)
     body1 = build_table(pd.DataFrame(stock_model.info['train_report']), '个股模型训练集情况')
-    body2 = build_table(pd.DataFrame(stock_model.info['rng_report']), '个股模型测试集情况')
     body3 = build_table(important.head(50), '个股模型特征重要性')
 
     msg1 = '模型训练日期:{model_date}'.format(model_date=stock_model.info['date'])
@@ -78,7 +71,6 @@ def train(date, strategy_id='机器学习1号', working_dir=working_dir, ui_log 
     important = index_model.model_important()
     index_model.save_model('index',working_dir = working_dir)
     body4 = build_table(pd.DataFrame(index_model.info['train_report']), '指数模型训练集情况')
-    body5 = build_table(pd.DataFrame(index_model.info['rng_report']), '指数模型测试集情况')
     body6 = build_table(important.head(50), '指数模型特征重要性')
 
     index_model.set_target('INDEX_TARGET', mark = 0.3, type = 'percent')
@@ -90,12 +82,11 @@ def train(date, strategy_id='机器学习1号', working_dir=working_dir, ui_log 
     index_model.save_model('safe',working_dir = working_dir)
 
     body7 = build_table(pd.DataFrame(index_model.info['train_report']), '安全模型训练集情况')
-    body8 = build_table(pd.DataFrame(index_model.info['rng_report']), '安全模型测试集情况')
     body9 = build_table(important.head(50), '安全模型特征重要性')
 
     QA_util_log_info(
         '##JOB06 Now Model Trainning Report ==== {}'.format(str(date)), ui_log)
-    msg = build_email(build_head(),msg1,body1,body2,body3,body4,body5,body6,body7,body8,body9)
+    msg = build_email(build_head(),msg1,body1,body3,body4,body6,body7,body9)
 
     send_email('模型训练报告', msg, 'date')
     send_actionnotice(strategy_id,
