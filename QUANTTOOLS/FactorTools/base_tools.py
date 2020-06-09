81eea7f3d03fb2e4e9291b58af64a63adb417a06
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-06-09 16:21:23 +0800

    ADD

--- QUANTTOOLS/FactorTools/base_tools.py
+++ QUANTTOOLS/FactorTools/base_tools.py
@@ -125,14 +125,15 @@ def combine_model(index_d, stock_d, safe_d, start, end):
                 try:
                     c = stock_d.loc[(i,find_stock(j)),:].sort_values(by='O_PROB', ascending=False).head(num).reset_index()
                 except:
-                    if safe_res is not None and safe_res.shape[0] > 0:
-                        c = stock_d.loc[i].sort_values(by='O_PROB', ascending=False).head(num).reset_index()
-                    else:
-                        c = pd.DataFrame()
+                    c = pd.DataFrame()
 
                 if c is not None and c.shape[0] > 0:
                     res = res.reset_index().append(c,ignore_index=True).set_index(['date','code'])
-        else:
-            pass
+
+        elif safe_res is not None and safe_res.shape[0] > 0:
+            c = stock_d.loc[i].sort_values(by='O_PROB', ascending=False).head(num).reset_index()
+
+            if c is not None and c.shape[0] > 0:
+                res = res.reset_index().append(c,ignore_index=True).set_index(['date','code'])
 
     return(res.drop_duplicates())
