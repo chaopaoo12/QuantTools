c4f914a7f24ce0fe80bbcf5f6950a8d7a5df8703
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-06-25 10:57:11 +0800

    ADD

--- config/aotu_morning.py
+++ config/aotu_morning.py
@@ -1,6 +1,9 @@
 from QUANTTOOLS.QAStockTradingDay.StockStrategySecond.trading_morning import trading
-from QUANTAXIS.QAUtil import QA_util_today_str,QA_util_get_last_day
+from QUANTAXIS.QAUtil import QA_util_today_str,QA_util_get_last_day,QA_util_get_real_date
 
 if __name__ == '__main__':
-    mark_day = QA_util_get_last_day(QA_util_today_str())
+    if QA_util_get_last_day(QA_util_today_str()) == 'wrong date':
+        mark_day = QA_util_get_real_date(QA_util_today_str())
+    else:
+        mark_day = QA_util_get_last_day(QA_util_today_str())
     trading(mark_day)
--- config/aotu_trading.py
+++ config/aotu_trading.py
@@ -1,7 +1,10 @@
 from QUANTTOOLS.QAStockTradingDay.StockStrategySecond.trading import trading
-from QUANTAXIS.QAUtil import QA_util_today_str,QA_util_get_last_day
+from QUANTAXIS.QAUtil import QA_util_today_str,QA_util_get_last_day,QA_util_get_real_date
 
 
 if __name__ == '__main__':
-    mark_day = QA_util_get_last_day(QA_util_today_str())
+    if QA_util_get_last_day(QA_util_today_str()) == 'wrong date':
+        mark_day = QA_util_get_real_date(QA_util_today_str())
+    else:
+        mark_day = QA_util_get_last_day(QA_util_today_str())
     trading(mark_day)
