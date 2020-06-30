60adac516ff90fcad6886a9bba0d1cfb8fa51d45
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-06-30 23:15:13 +0800

    ADD

--- QUANTTOOLS/QAStockTradingDay/StockStrategySecond/train.py
+++ QUANTTOOLS/QAStockTradingDay/StockStrategySecond/train.py
@@ -17,11 +17,13 @@ def train(date, strategy_id='机器学习1号', working_dir=working_dir, ui_log 
     QA_util_log_info('##JOB01 Now Model Init ==== {}'.format(str(date)), ui_log)
     stock_model = StockModel()
     index_model = IndexModel()
-    QA_util_log_info('##JOB02 Now Prepare Date ==== {}'.format(str(date)), ui_log)
+    safe_model = IndexModel()
+
+    QA_util_log_info('##JOB02 Now Stock Prepare Date ==== {}'.format(str(date)), ui_log)
     stock_model.get_data(start=str(int(date[0:4])-2)+"-01-01", end=date, block=True, sub_block=True)
-    QA_util_log_info('##JOB03 Now Set Target ==== {}'.format(str(date)), ui_log)
+    QA_util_log_info('##JOB03 Now Stock Set Target ==== {}'.format(str(date)), ui_log)
     stock_model.set_target(mark =0.42, type = 'percent')
-    QA_util_log_info('##JOB04 Now Set Train time range ==== {}'.format(str(date)), ui_log)
+    QA_util_log_info('##JOB04 Now Set Stock Train time range ==== {}'.format(str(date)), ui_log)
     stock_model.set_train_rng(train_start=str(int(date[0:4])-3)+"-01-01",
                         train_end=(datetime.strptime(date, "%Y-%m-%d")-delta1).strftime('%Y-%m-%d'))
     stock_model.prepare_data()
@@ -29,50 +31,62 @@ def train(date, strategy_id='机器学习1号', working_dir=working_dir, ui_log 
                     'subsample': 0.9, 'colsample_bytree': 0.6, 'gamma': 0, 'reg_alpha': 0.05, 'reg_lambda': 3}
     stock_model.build_model(other_params)
     QA_util_log_info(
-        '##JOB05 Now Model Trainnig ==== {}'.format(str(date)), ui_log)
+        '##JOB05 Now Stock Model Trainnig ==== {}'.format(str(date)), ui_log)
     stock_model.model_running()
     QA_util_log_info(
-        '##JOB06 Now Save Model ==== {}'.format(str(date)), ui_log)
+        '##JOB06 Now Save Stock Model ==== {}'.format(str(date)), ui_log)
     important = stock_model.model_important()
     stock_model.save_model('stock',working_dir = working_dir)
-    body1 = build_table(pd.DataFrame(stock_model.info['train_report']), '个股模型训练集情况')
-    body3 = build_table(important.head(50), '个股模型特征重要性')
+    stock_train_report = build_table(pd.DataFrame(stock_model.info['train_report']), '个股模型训练集情况')
+    stock_ft_importance = build_table(important.head(50), '个股模型特征重要性')
 
     msg1 = '模型训练日期:{model_date}'.format(model_date=stock_model.info['date'])
     del stock_model
 
-    QA_util_log_info('##JOB02 Now Prepare Date ==== {}'.format(str(date)), ui_log)
+    QA_util_log_info('##JOB02 Now Prepare Index Date ==== {}'.format(str(date)), ui_log)
     index_model.get_data(start=str(int(date[0:4])-3)+"-01-01", end=date)
-    QA_util_log_info('##JOB03 Now Set Target ==== {}'.format(str(date)), ui_log)
+    QA_util_log_info('##JOB03 Now Set Index Target ==== {}'.format(str(date)), ui_log)
     index_model.set_target('INDEX_TARGET5', mark = 0.3, type = 'percent')
-    QA_util_log_info('##JOB04 Now Set Train time range ==== {}'.format(str(date)), ui_log)
+    QA_util_log_info('##JOB04 Now Set Index Train time range ==== {}'.format(str(date)), ui_log)
     index_model.set_train_rng(train_start=str(int(date[0:4])-3)+"-01-01",
                               train_end=(datetime.strptime(date, "%Y-%m-%d")-delta1).strftime('%Y-%m-%d'))
     index_model.prepare_data()
     other_params = {'learning_rate': 0.1, 'n_estimators': 100, 'max_depth': 3, 'min_child_weight': 3, 'seed': 0,
                     'subsample': 0.75, 'colsample_bytree': 0.65, 'gamma': 0.1, 'reg_alpha': 0.05, 'reg_lambda': 1}
     index_model.build_model(other_params)
-    QA_util_log_info('##JOB05 Now Model Trainnig ==== {}'.format(str(date)), ui_log)
+    QA_util_log_info('##JOB05 Now Index Model Trainnig ==== {}'.format(str(date)), ui_log)
     index_model.model_running()
-    QA_util_log_info('##JOB06 Now Save Model ==== {}'.format(str(date)), ui_log)
+    QA_util_log_info('##JOB06 Now Save Index Model ==== {}'.format(str(date)), ui_log)
     important = index_model.model_important()
     index_model.save_model('index',working_dir = working_dir)
-    body4 = build_table(pd.DataFrame(index_model.info['train_report']), '指数模型训练集情况')
-    body6 = build_table(important.head(50), '指数模型特征重要性')
+    index_train_report = build_table(pd.DataFrame(index_model.info['train_report']), '指数模型训练集情况')
+    index_ft_importance = build_table(important.head(50), '指数模型特征重要性')
+    del stock_model
 
-    index_model.set_target('INDEX_TARGET', mark = 0.3, type = 'percent')
-    index_model.prepare_data()
+    QA_util_log_info('##JOB02 Now Prepare Safe Date ==== {}'.format(str(date)), ui_log)
+    index_model.get_data(start=str(int(date[0:4])-3)+"-01-01", end=date)
+    QA_util_log_info('##JOB03 Now Set Safe Target ==== {}'.format(str(date)), ui_log)
+    safe_model.set_target('INDEX_TARGET', mark = 0.3, type = 'percent')
+    QA_util_log_info('##JOB04 Now Set Safe Train time range ==== {}'.format(str(date)), ui_log)
+    index_model.set_train_rng(train_start=str(int(date[0:4])-3)+"-01-01",
+                              train_end=(datetime.strptime(date, "%Y-%m-%d")-delta1).strftime('%Y-%m-%d'))
+    safe_model.prepare_data()
     other_params = {'learning_rate': 0.1, 'n_estimators': 100, 'max_depth': 5, 'min_child_weight': 1, 'seed': 0,
                     'subsample': 0.8, 'colsample_bytree': 0.8, 'gamma': 0, 'reg_alpha': 0, 'reg_lambda': 1}
-    index_model.build_model(other_params)
-    index_model.model_running()
-    index_model.save_model('safe',working_dir = working_dir)
+    safe_model.build_model(other_params)
+    QA_util_log_info('##JOB05 Now Safe Model Trainnig ==== {}'.format(str(date)), ui_log)
+    safe_model.model_running()
+    QA_util_log_info('##JOB06 Now Save Index Model ==== {}'.format(str(date)), ui_log)
+    safe_model.save_model('safe',working_dir = working_dir)
 
-    body7 = build_table(pd.DataFrame(index_model.info['train_report']), '安全模型训练集情况')
-    body9 = build_table(important.head(50), '安全模型特征重要性')
+    safe_train_report = build_table(pd.DataFrame(safe_model.info['train_report']), '安全模型训练集情况')
+    safe_ft_importance = build_table(important.head(50), '安全模型特征重要性')
 
     QA_util_log_info('##JOB06 Now Model Trainning Report ==== {}'.format(str(date)), ui_log)
-    msg = build_email(build_head(),msg1,body1,body3,body4,body6,body7,body9)
+    msg = build_email(build_head(),msg1,
+                      stock_train_report,stock_ft_importance,
+                      index_train_report,index_ft_importance,
+                      safe_train_report,safe_ft_importance)
 
     send_email('模型训练报告', msg, 'date')
     send_actionnotice(strategy_id,
