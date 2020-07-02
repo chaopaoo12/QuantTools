0afb711397545d00b1baf9e915199bf504239d68
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-07-02 15:25:08 +0800

    ADD

--- QUANTTOOLS/account_manage/trading.py
+++ QUANTTOOLS/account_manage/trading.py
@@ -4,9 +4,6 @@ from QUANTTOOLS.message_func.wechat import send_actionnotice
 from QUANTTOOLS.QAStockETL.QAFetch import QA_fetch_stock_fianacial_adv
 from QUANTAXIS.QAFetch.QAQuery_Advance import QA_fetch_stock_day_adv
 import pandas as pd
-import logging
-import strategyease_sdk
-from QUANTTOOLS.account_manage.setting import yun_ip, yun_port, easytrade_password
 import time
 import datetime
 from QUANTTOOLS.account_manage.trading_message import send_trading_message
@@ -81,9 +78,9 @@ def re_build(target, positions, sub_accounts, trading_date, percent, exceptions,
         r1['股票余额'] = r1['股票余额'].fillna(0)
         realtm = QA_fetch_get_stock_realtime('tdx', code=[x for x in list(r1.index) if x in list(QA_fetch_stock_list().index)]).reset_index('datetime')[['ask1','ask_vol1','bid1','bid_vol1']]
         close = QA_fetch_stock_day_adv(list(r1.index),QA_util_get_last_day(trading_date,60),trading_date).data.loc[trading_date].reset_index('date')['close']
-        info = QA_fetch_stock_fianacial_adv(list(r1.index), QA_util_get_last_day(trading_date,7), trading_date).data.reset_index('date')[['NAME','INDUSTRY']]
+        #info = QA_fetch_stock_fianacial_adv(list(r1.index), QA_util_get_last_day(trading_date,7), trading_date).data.reset_index('date')[['NAME','INDUSTRY']]
 
-        res = r1.join(info[~info.index.duplicated()],how='left').join(realtm,how='left').join(close,how='left')
+        res = r1.join(realtm,how='left').join(close,how='left')
         avg_account = (sub_accounts * percent)/target['double'].sum()
         res = res.assign(tar=avg_account)
         res.ix[res['RANK'].isnull(),'tar'] = 0
