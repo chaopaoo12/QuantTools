85f3dc5dfc97c56e83928157286f42a70ca9db9d
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-07-07 20:02:14 +0800

    QA

--- config/aotu_morning.py
+++ config/aotu_morning.py
@@ -1,9 +1,11 @@
 from QUANTTOOLS.QAStockTradingDay.StockStrategySecond.trading_morning import trading
-from QUANTAXIS.QAUtil import QA_util_today_str,QA_util_get_last_day,QA_util_get_real_date
+from QUANTAXIS.QAUtil import QA_util_today_str,QA_util_get_last_day,QA_util_get_real_date,QA_util_if_trade
 
 if __name__ == '__main__':
     if QA_util_get_last_day(QA_util_today_str()) == 'wrong date':
         mark_day = QA_util_get_real_date(QA_util_today_str())
     else:
         mark_day = QA_util_get_last_day(QA_util_today_str())
-    trading(mark_day)
+
+    if QA_util_if_trade(QA_util_today_str()):
+        trading(mark_day)
