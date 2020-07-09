3d9f1ad9d071c3d7b01d6538a3a6db83ba7d1363
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-07-09 18:04:49 +0800

    QA

--- QUANTTOOLS/account_manage/trading.py
+++ QUANTTOOLS/account_manage/trading.py
@@ -86,8 +86,8 @@ def re_build(target, positions, sub_accounts, trading_date, percent, exceptions,
         r1['股票余额'] = r1['股票余额'].fillna(0)
         realtm = QA_fetch_get_stock_realtime('tdx', code=[x for x in list(r1.index) if x in list(QA_fetch_stock_list().index)]).reset_index('datetime')[['ask1','ask_vol1','bid1','bid_vol1']]
         close = QA_fetch_stock_day_adv(list(r1.index),QA_util_get_last_day(trading_date,60),trading_date).data.loc[trading_date].reset_index('date')['close']
-
         res = r1.join(realtm,how='left').join(close,how='left')
+        res.ix[res['ask1']==0,'double'] = 0
         avg_account = (sub_accounts * percent)/target['double'].sum()
         res = res.assign(tar=avg_account)
         res.ix[res['RANK'].isnull(),'tar'] = 0
