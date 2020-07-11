6427a9448e0cb897711ef711791526029e00b931
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-07-11 13:02:00 +0800

    QA

--- QUANTTOOLS/FactorTools/base_func.py
+++ QUANTTOOLS/FactorTools/base_func.py
@@ -8,7 +8,7 @@ import numpy as np
 import QUANTAXIS as QA
 from QUANTTOOLS.FactorTools.base_tools import find_stock
 
-def get_quant_data(start_date, end_date, type = 'crawl', block = False, sub_block= True):
+def get_quant_data(start_date, end_date, type = 'crawl', block = False, sub_block= True, method = 'value'):
     if block is True:
         data = QA.QA_fetch_stock_block()
         codes = list(data[data.blockname.isin(['上证50','沪深300','创业300','上证180','上证380','深证100','深证300','中证100','中证200'])]['code'].drop_duplicates())
@@ -22,7 +22,7 @@ def get_quant_data(start_date, end_date, type = 'crawl', block = False, sub_bloc
         res = QA_fetch_stock_quant_pre_adv(codes,start_date,end_date, block = sub_block).data
     if type == 'model':
         res = QA_fetch_get_quant_data(codes, start_date, end_date, block = sub_block).set_index(['date','code']).drop(['date_stamp'], axis=1)
-        target = QA_fetch_stock_target(codes, start_date, end_date)
+        target = QA_fetch_stock_target(codes, start_date, end_date, method=method)
         res = res.join(target)
     #res = res[(res['RNG_L_O'] <= 5 & res['LAG_TOR_O'] < 1)]
     #dummy_industry = pd.get_dummies(res['INDUSTRY']).astype(float)
@@ -30,7 +30,7 @@ def get_quant_data(start_date, end_date, type = 'crawl', block = False, sub_bloc
     #res = pd.concat([res[[col for col in list(res.columns) if col != 'INDUSTRY']],dummy_industry],axis = 1)
     return(res)
 
-def get_index_quant_data(start_date, end_date, type = 'crawl'):
+def get_index_quant_data(start_date, end_date, type = 'crawl', method = 'value'):
     #stock = QA.QA_fetch_stock_block()
     #index_list = QA.QA_fetch_index_list_adv()
     #res = pd.merge(index_list.reset_index(drop=True), stock.groupby('blockname').count().reset_index()[['blockname','source']],
@@ -59,7 +59,7 @@ def get_index_quant_data(start_date, end_date, type = 'crawl'):
         res = QA_fetch_index_quant_pre_adv(codes,start_date,end_date).data
     if type == 'model':
         res = QA_fetch_index_quant_data(codes, start_date, end_date).set_index(['date','code']).drop(['date_stamp'], axis=1)
-        target = QA_fetch_index_target(codes, start_date, end_date)
+        target = QA_fetch_index_target(codes, start_date, end_date, method=method)
         res = res.join(target)
     return(res)
 
