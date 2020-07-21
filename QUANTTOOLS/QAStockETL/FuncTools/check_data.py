549073ee338eb90117145b1154125bbb42970594
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-07-21 11:29:16 +0800

    QA

--- QUANTTOOLS/QAStockETL/FuncTools/check_data.py
+++ QUANTTOOLS/QAStockETL/FuncTools/check_data.py
@@ -187,56 +187,56 @@ def check_index_data(func = None, mark_day = None, title = None, ui_log = None):
 
 
 def check_stock_day(mark_day = None, ui_log = None):
-    check_stock_data(func = QA_fetch_stock_day_adv, mark_day = mark_day, title = 'Stock Day', ui_log = ui_log)
+    return(check_stock_data(func = QA_fetch_stock_day_adv, mark_day = mark_day, title = 'Stock Day', ui_log = ui_log))
 
 def check_stock_fianacial(mark_day = None, ui_log = None):
-    check_stock_data(func = QA_fetch_stock_fianacial_adv, mark_day = mark_day, title = 'Stock Finance', ui_log = ui_log)
+    return(check_stock_data(func = QA_fetch_stock_fianacial_adv, mark_day = mark_day, title = 'Stock Finance', ui_log = ui_log))
 
 def check_stock_finper(mark_day = None, ui_log = None):
-    check_stock_data(func = QA_fetch_stock_financial_percent_adv, mark_day = mark_day, title = 'PE水位', ui_log = ui_log)
+    return(check_stock_data(func = QA_fetch_stock_financial_percent_adv, mark_day = mark_day, title = 'PE水位', ui_log = ui_log))
 
 def check_stock_alpha191(mark_day = None, ui_log = None):
-    check_stock_data(func = QA_fetch_stock_alpha_adv, mark_day = mark_day, title = 'Stock Alpha191', ui_log = ui_log)
+    return(check_stock_data(func = QA_fetch_stock_alpha_adv, mark_day = mark_day, title = 'Stock Alpha191', ui_log = ui_log))
 
 def check_stock_alpha101(mark_day = None, ui_log = None):
-    check_stock_data(func = QA_fetch_stock_alpha101_adv, mark_day = mark_day, title = 'Stock Alpha101', ui_log = ui_log)
+    return(check_stock_data(func = QA_fetch_stock_alpha101_adv, mark_day = mark_day, title = 'Stock Alpha101', ui_log = ui_log))
 
 def QA_fetch_stock_techindex_adv(code, start, end):
     return(QA_fetch_stock_technical_index_adv(code, start, end, type='day'))
 
 def check_stock_techindex(mark_day = None, ui_log = None):
-    check_stock_data(func = QA_fetch_stock_techindex_adv, mark_day = mark_day, title = 'Stock TechIndex', ui_log = ui_log)
+    return(check_stock_data(func = QA_fetch_stock_techindex_adv, mark_day = mark_day, title = 'Stock TechIndex', ui_log = ui_log))
 
 def QA_fetch_stock_techweek_adv(code, start, end):
     return(QA_fetch_stock_technical_index_adv(code, start, end, type='week'))
 
 def check_stock_techweek(mark_day = None, ui_log = None):
-    check_stock_data(func = QA_fetch_stock_techweek_adv, mark_day = mark_day, title = 'Stock TechWeek', ui_log = ui_log)
+    return(check_stock_data(func = QA_fetch_stock_techweek_adv, mark_day = mark_day, title = 'Stock TechWeek', ui_log = ui_log))
 
 def check_stock_quant(mark_day = None, ui_log = None):
-    check_stock_data(func = QA_fetch_stock_quant_data_adv, mark_day = mark_day, title = 'Stock Quant', ui_log = ui_log)
+    return(check_stock_data(func = QA_fetch_stock_quant_data_adv, mark_day = mark_day, title = 'Stock Quant', ui_log = ui_log))
 
 
 def check_index_day(mark_day = None, ui_log = None):
-    check_index_data(func = QA_fetch_index_day_adv, mark_day = mark_day, title = 'Index Day', ui_log = ui_log)
+    return(check_index_data(func = QA_fetch_index_day_adv, mark_day = mark_day, title = 'Index Day', ui_log = ui_log))
 
 def check_index_alpha191(mark_day = None, ui_log = None):
-    check_index_data(func = QA_fetch_index_alpha_adv, mark_day = mark_day, title = 'Index Alpha191', ui_log = ui_log)
+    return(check_index_data(func = QA_fetch_index_alpha_adv, mark_day = mark_day, title = 'Index Alpha191', ui_log = ui_log))
 
 def check_index_alpha101(mark_day = None, ui_log = None):
-    check_index_data(func = QA_fetch_index_alpha101_adv, mark_day = mark_day, title = 'Index Alpha101', ui_log = ui_log)
+    return(check_index_data(func = QA_fetch_index_alpha101_adv, mark_day = mark_day, title = 'Index Alpha101', ui_log = ui_log))
 
 def QA_fetch_index_techindex_adv(code, start, end):
     return(QA_fetch_index_technical_index_adv(code, start, end, type='day'))
 
 def check_index_techindex(mark_day = None,  ui_log = None):
-    check_index_data(func = QA_fetch_index_techindex_adv, mark_day = mark_day, title = 'Index TechIndex', ui_log = ui_log)
+    return(check_index_data(func = QA_fetch_index_techindex_adv, mark_day = mark_day, title = 'Index TechIndex', ui_log = ui_log))
 
 def QA_fetch_index_techweek_adv(code, start, end):
     return(QA_fetch_index_technical_index_adv(code, start, end, type='week'))
 
 def check_index_techweek(mark_day = None, ui_log = None):
-    check_index_data(func = QA_fetch_index_techweek_adv, mark_day = mark_day, title = 'Index TechWeek', ui_log = ui_log)
+    return(check_index_data(func = QA_fetch_index_techweek_adv, mark_day = mark_day, title = 'Index TechWeek', ui_log = ui_log))
 
 def check_index_quant(mark_day = None, ui_log = None):
-    check_index_data(func = QA_fetch_index_quant_data_adv, mark_day = mark_day, title = 'Index Quant', ui_log = ui_log)
+    return(check_index_data(func = QA_fetch_index_quant_data_adv, mark_day = mark_day, title = 'Index Quant', ui_log = ui_log))
