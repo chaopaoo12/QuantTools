dfc591c2e97cda2de8906f42de3e57efbfb9c1dc
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-07-22 07:39:45 +0800

    QA

--- QUANTTOOLS/QAStockTradingDay/StockStrategySecond/running.py
+++ QUANTTOOLS/QAStockTradingDay/StockStrategySecond/running.py
@@ -37,7 +37,7 @@ def predict(trading_date, strategy_id='机器学习1号', account='name:client-1
         tar2 = tar1[['NAME','INDUSTRY','Z_PROB','O_PROB','RANK']]
         close = QA_fetch_stock_day_adv(list(tar1.index),QA_util_get_last_day(trading_date,60),trading_date).to_qfq().data.loc[trading_date].reset_index('date')['close']
         res = tar2.join(close, how = 'left')
-        avg_account = sub_accounts/tar1.shape[0]
+        avg_account = (sub_accounts - frozen)/tar1.shape[0]
         res = res.assign(tar=avg_account[0]*percent)
         res['cnt'] = (res['tar']/res['close']/100).apply(lambda x:round(x,0)*100)
         res['real'] = res['cnt'] * res['close']
