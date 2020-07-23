c5f0933f8f40e87d004acafe162e790aec3a110a
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-07-23 15:09:32 +0800

    QA

--- QUANTTOOLS/account_manage/trading.py
+++ QUANTTOOLS/account_manage/trading.py
@@ -37,7 +37,7 @@ def trade_roboot(target_tar, account, trading_date, percent, strategy_id, type='
         target_af = int(time.strftime("%H%M%S", time.strptime("15:00:00", "%H:%M:%S")))
 
         if tm < target_ea or tm >= target_af:
-            QA_util_log_info('不在交易时段 {hour} ==================== {data}'.format(hour = tm, date = trading_date), ui_log=None)
+            QA_util_log_info('不在交易时段 {hour} ==================== {date}'.format(hour = tm, date = trading_date), ui_log=None)
             send_actionnotice(strategy_id,'交易报告:{}'.format(trading_date),'已过交易时段',direction = 'HOLD',offset='HOLD',volume=None)
             break
 
