9fa55f03e1ce5735a61fd05dd6b3e1d85b6d3b40
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-07-23 21:34:32 +0800

    QA

---
+++ QUANTTOOLS/FactorTools/QuantMk.py
@@ -0,0 +1,63 @@
+from QUANTTOOLS.QAStockETL.QAFetch import (QA_fetch_stock_target,QA_fetch_get_quant_data,QA_fetch_index_target,
+                                           QA_fetch_index_quant_data,QA_fetch_get_index_quant_data,
+                                           QA_fetch_stock_quant_pre_adv,QA_fetch_index_quant_pre_adv)
+import QUANTAXIS as QA
+from QUANTTOOLS.FactorTools.base_tools import find_stock
+
+def get_quant_data(start_date, end_date, type = 'crawl', block = False, sub_block= True, method = 'value'):
+    if block is True:
+        data = QA.QA_fetch_stock_block()
+        codes = list(data[data.blockname.isin(['上证50','沪深300','创业300','上证180','上证380','深证100','深证300','中证100','中证200'])]['code'].drop_duplicates())
+        #codes = [i for i in codes if i.startswith('300') == False]
+    else:
+        codes = list(QA.QA_fetch_stock_list_adv()['code'])
+        codes = [i for i in codes if i.startswith('688') == False]
+        codes = [i for i in codes if i.startswith('787') == False]
+        codes = [i for i in codes if i.startswith('789') == False]
+    if type == 'crawl':
+        res = QA_fetch_stock_quant_pre_adv(codes,start_date,end_date, block = sub_block, method=method).data
+    if type == 'model':
+        res = QA_fetch_get_quant_data(codes, start_date, end_date, block = sub_block).set_index(['date','code']).drop(['date_stamp'], axis=1)
+        target = QA_fetch_stock_target(codes, start_date, end_date, method=method)
+        res = res.join(target)
+    #res = res[(res['RNG_L_O'] <= 5 & res['LAG_TOR_O'] < 1)]
+    #dummy_industry = pd.get_dummies(res['INDUSTRY']).astype(float)
+    #dummy_industry.columns = ['I_' + i for i in list(dummy_industry.columns)]
+    #res = pd.concat([res[[col for col in list(res.columns) if col != 'INDUSTRY']],dummy_industry],axis = 1)
+    return(res)
+
+def get_index_quant_data(start_date, end_date, type = 'crawl', method = 'value'):
+
+    codes = list(QA.QA_fetch_index_list_adv()['code'])
+    codes = [i for i in codes if i.startswith('880') == True]
+    codes = [i for i in codes if i.startswith('8800') == False]
+    codes = [i for i in codes if i.startswith('8807') == False]
+    codes = [i for i in codes if i.startswith('8808') == False]
+    #codes = [i for i in codes if i.startswith('88098') == False]
+    #codes = [i for i in codes if i.startswith('88099') == False]
+
+    if type == 'crawl':
+        res = QA_fetch_index_quant_pre_adv(codes,start_date,end_date, method=method).data
+    if type == 'model':
+        res = QA_fetch_index_quant_data(codes, start_date, end_date).set_index(['date','code']).drop(['date_stamp'], axis=1)
+        target = QA_fetch_index_target(codes, start_date, end_date, method=method)
+        res = res.join(target)
+    return(res)
+
+def get_index_quant_data_norm(start_date, end_date, type = 'crawl', method = 'value'):
+
+    codes = list(QA.QA_fetch_index_list_adv()['code'])
+    codes = [i for i in codes if i.startswith('880') == True]
+    codes = [i for i in codes if i.startswith('8800') == False]
+    codes = [i for i in codes if i.startswith('8807') == False]
+    codes = [i for i in codes if i.startswith('8808') == False]
+    #codes = [i for i in codes if i.startswith('88098') == False]
+    #codes = [i for i in codes if i.startswith('88099') == False]
+
+    if type == 'crawl':
+        res = QA_fetch_index_quant_pre_adv(codes,start_date,end_date, method=method).data
+    if type == 'model':
+        res = QA_fetch_get_index_quant_data(codes, start_date, end_date, type='normalization').set_index(['date','code']).drop(['date_stamp'], axis=1)
+        target = QA_fetch_index_target(codes, start_date, end_date, method=method)
+        res = res.join(target)
+    return(res)
