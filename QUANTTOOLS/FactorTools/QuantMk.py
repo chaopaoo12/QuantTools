2ac5f76ce9001f5007fcfdc3ff96c7daca8b07d7
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-07-23 22:58:58 +0800

    QA

--- QUANTTOOLS/FactorTools/QuantMk.py
+++ QUANTTOOLS/FactorTools/QuantMk.py
@@ -39,7 +39,7 @@ def get_index_quant_data(start_date, end_date, type = 'crawl', method = 'value')
     if type == 'crawl':
         res = QA_fetch_index_quant_pre_adv(codes,start_date,end_date, method=method).data
     if type == 'model':
-        res = QA_fetch_index_quant_data(codes, start_date, end_date).set_index(['date','code']).drop(['date_stamp'], axis=1)
+        res = QA_fetch_index_quant_data(codes, start_date, end_date)
         target = QA_fetch_index_target(codes, start_date, end_date, method=method)
         res = res.join(target)
     return(res)
