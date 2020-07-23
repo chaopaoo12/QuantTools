808b9749af63ab9cc6103dadf68b4a8577e4c34a
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-07-23 15:48:58 +0800

    QA

--- QUANTTOOLS/account_manage/BuildTradingFrame.py
+++ QUANTTOOLS/account_manage/BuildTradingFrame.py
@@ -89,9 +89,8 @@ def build(target, positions, sub_accounts, percent, Zbreak, k=100):
             k = k + 100
 
     QA_util_log_info('##JOB Caculate Deal Position', ui_log = None)
-    print('res',res[['NAME','INDUSTRY','目标持股数','测算持股金额']])
     res['deal'] = (res['目标持股数'] - res['股票余额'].apply(lambda x:float(x))).apply(lambda x:math.floor(x/100)*100)
-    print('res',res[['NAME','INDUSTRY','目标持股数','测算持股金额','deal']])
+
     if Zbreak == True:
         QA_util_log_info('##JOB Dislodge Holding Position', ui_log = None)
         res = res[(res.deal> 0) & (res.deal < 0)]
