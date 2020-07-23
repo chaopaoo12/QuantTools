b54f722155d869bab7612823f97c1222e31830db
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-07-23 11:41:59 +0800

    QA

--- QUANTTOOLS/QAStockTradingDay/StockModel/StrategyOne.py
+++ QUANTTOOLS/QAStockTradingDay/StockModel/StrategyOne.py
@@ -21,17 +21,23 @@ class model():
         self.info['rng_status']=dict()
 
     def get_data(self, start, end, block=True, sub_block=True, type ='crawl'):
+        QA_util_log_info('##JOB Got Data by {type}, block: {}, sub_block: {} ==== from {_from} to {_to}'.format(type=type, block=block,sub_block=sub_block, _from=start, _to=end), ui_log = None)
         self.data = get_quant_data(start, end, type = type, block = block, sub_block = sub_block)
         self.data = self.data[(self.data.DAYSO>= 90)&(self.data.next_date == self.data.PRE_DATE)]
         print(self.data.shape)
 
     def set_target(self, mark, type = 'value'):
+        QA_util_log_info('##JOB Set Train Target by {type} at {mark} ===== {date}'.format(type = type, mark=mark,
+                                                                                          date = self.info['date']),
+                         ui_log = None)
         if type == 'value':
             self.data['star'] = self.data['TARGET5'].apply(lambda x : 1 if x >= mark else 0)
         elif type == 'percent':
             self.data['star'] = self.data['TARGET5'].groupby('date').apply(lambda x: x.rank(ascending=False,pct=True)).apply(lambda x :1 if x <= mark else 0)
         else:
-            print("target type must be in ['value','percent']")
+            QA_util_log_info('##target type must be in [value,percent] ===== {}'.format(self.info['date']), ui_log = None)
+
+        QA_util_log_info('##save used columns ==== {}'.format(self.info['date']), ui_log = None)
         self.cols = [i for i in self.data.columns if i not in ['moon','star','mars','venus','sun','MARK','DAYSO','RNG_LO',
                                                                'LAG_TORO','OPEN_MARK','PASS_MARK','TARGET','TARGET3',
                                                                'TARGET4','TARGET5','TARGET10','AVG_TARGET','INDEX_TARGET',
@@ -40,20 +46,25 @@ class model():
         self.info['cols'] = self.cols
 
     def set_train_rng(self, train_start, train_end):
+        QA_util_log_info('##JOB Set Train Range from {_from} to {_to} ===== {date}'.format(_from=train_start,_to=train_end, date=self.info['date']), ui_log = None)
         self.TR_RNG = pd.Series(pd.date_range(train_start, train_end, freq='D')).apply(lambda x: str(x)[0:10])
         self.info['train_rng'] = [train_start,train_end]
 
     def prepare_data(self):
-
+        QA_util_log_info('##JOB Split Train Data ===== {}'.format(self.info['date']), ui_log = None)
         self.X_train, self.Y_train = self.data.loc[self.TR_RNG][self.cols].fillna(0),self.data.loc[self.TR_RNG]['star'].fillna(0)
 
     def build_model(self, other_params):
+        QA_util_log_info('##JOB Set Model Params ===== {}'.format(self.info['date']), ui_log = None)
         #self.model = XGBClassifier(n_estimators = n_estimators, max_depth = max_depth, subsample= subsample,seed=seed)
         other_params = other_params
         self.model = XGBClassifier(**other_params)
 
     def model_running(self):
+        QA_util_log_info('##JOB Now Model Traning ===== {}'.format(self.info['date']), ui_log = None)
         self.model.fit(self.X_train,self.Y_train)
+
+        QA_util_log_info('##JOB Now Model Scoring ===== {}'.format(self.info['date']), ui_log = None)
         y_pred = self.model.predict(self.X_train)
 
         accuracy_train = accuracy_score(self.Y_train,y_pred)
@@ -64,6 +75,8 @@ class model():
         self.info['train_report'] = self.train_report
 
     def model_check(self):
+
+        QA_util_log_info('##JOB Now Model Checking ===== {}'.format(self.info['date']), ui_log = None)
 
         if self.info['train_report']['1']['precision'] <0.75:
             print("精确率不足,模型需要优化")
@@ -83,6 +96,8 @@ class model():
             self.info['train_status']['status'] = True
 
     def save_model(self, name, working_dir = 'D:\\model\\current'):
+        QA_util_log_info('##JOB Now Model Saving ===== {}'.format(self.info['date']), ui_log = None)
+
         if mkdir(working_dir):
             try:
                 joblib.dump(self.model, working_dir+"\\{name}.joblib.dat".format(name=name))
@@ -94,16 +109,21 @@ class model():
                 return(False)
 
     def model_important(self):
+        QA_util_log_info('##JOB Now Got Model Importance ===== {}'.format(self.info['date']), ui_log = None)
         importance = pd.DataFrame({'featur' :list(self.X_train.columns),'value':list(self.model.feature_importances_)}).sort_values(by='value',ascending=False)
         return(importance)
 
 def load_model(name, working_dir= 'D:\\model\\current'):
+    QA_util_log_info('##JOB Now Model Loading', ui_log = None)
     model = joblib.load(working_dir+"\\{name}.joblib.dat".format(name=name))
     info = joblib.load(working_dir+"\\{name}_info.joblib.dat".format(name=name))
     return(model, info)
 
 def model_predict(model, start, end, cols, block = False, sub_block= False, type='crawl'):
+    QA_util_log_info('##JOB Now Got Prediction Data ===== from {_from} to {_to}'.format(_from=start,_to = end), ui_log = None)
     data = get_quant_data(start, end, type= type,block = block, sub_block=sub_block)
+
+    QA_util_log_info('##JOB Now Reshape Different Columns ===== from {_from} to {_to}'.format(_from=start,_to = end), ui_log = None)
     cols1 = [i for i in data.columns if i not in [ 'moon','star','mars','venus','sun','MARK','DAYSO','RNG_LO',
                                                    'LAG_TORO','OPEN_MARK','PASS_MARK','TARGET','TARGET3',
                                                    'TARGET4','TARGET5','TARGET10','AVG_TARGET','INDEX_TARGET',
@@ -118,7 +138,10 @@ def model_predict(model, start, end, cols, block = False, sub_block= False, type
             train[i] = 0
             n_cols.append(i)
     train.index = data.index
+    QA_util_log_info('##JOB Now Got Different Columns ===== from {_from} to {_to}'.format(_from=start,_to = end), ui_log = None)
     print(n_cols)
+
+    QA_util_log_info('##JOB Now Got Prediction Result ===== from {_from} to {_to}'.format(_from=start,_to = end), ui_log = None)
     b = data[['PASS_MARK','TARGET','TARGET3','TARGET4','TARGET5','TARGET10','AVG_TARGET','INDEX_TARGET','INDEX_TARGET3','INDEX_TARGET4','INDEX_TARGET5','INDEX_TARGET10']]
     b = b.assign(y_pred = model.predict(train[cols].fillna(0)))
     bina = pd.DataFrame(model.predict_proba(train[cols].fillna(0)))[[0,1]]
