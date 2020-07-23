c2c0191b303028baca60659ca0bd33efd591f3c1
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-07-23 15:41:20 +0800

    QA

--- QUANTTOOLS/account_manage/BuildTradingFrame.py
+++ QUANTTOOLS/account_manage/BuildTradingFrame.py
@@ -23,9 +23,6 @@ def floor_round(x):
 def build(target, positions, sub_accounts, percent, Zbreak, k=100):
     QA_util_log_info('##JOB Now Check Sub Accounts', ui_log = None)
     sub_accounts= float(sub_accounts) - 10000
-    print('sub_accounts',sub_accounts)
-    print('target',target.shape)
-    print('positions',positions.shape)
     if target is None:
         QA_util_log_info('##JOB Target is None', ui_log = None)
         res = positions.set_index('证券代码')
@@ -98,5 +95,4 @@ def build(target, positions, sub_accounts, percent, Zbreak, k=100):
     if Zbreak == True:
         QA_util_log_info('##JOB Dislodge Holding Position', ui_log = None)
         res = res[(res.deal> 0) & (res.deal < 0)]
-        print(res.shape)
     return(res)
