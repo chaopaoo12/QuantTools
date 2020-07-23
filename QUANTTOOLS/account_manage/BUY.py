ee92d90e8a0d50636e8b843e1750c449c6d5cb51
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-07-23 15:21:20 +0800

    QA

--- QUANTTOOLS/account_manage/BUY.py
+++ QUANTTOOLS/account_manage/BUY.py
@@ -23,10 +23,10 @@ def BUY(client, account, strategy_id, account_info,trading_date, code, name, ind
         QA_util_log_info('##JOB Check Account Usefull Capital Before {code} Buying ===== {date}'.format(code = code, date=trading_date), ui_log = None)
         UseCapital = get_UseCapital(client, account)
         while (price * deal_pos) > UseCapital:
-            QA_util_log_info('##JOB {name}({code}){INDUSTRY} 交易资金不足 目标买入{deal_pos}股 预估资金{target} 实际资金{capital}===={date}'.format(date=trading_date,
+            QA_util_log_info('##JOB {name}({code}){industry} 交易资金不足 目标买入{deal_pos}股 预估资金{target} 实际资金{capital}===={date}'.format(date=trading_date,
                                                                                                                                  code=code,
-                                                                                                                                 NAME= name,
-                                                                                                                                 INDUSTRY=industry,
+                                                                                                                                 name= name,
+                                                                                                                                 industry=industry,
                                                                                                                                  deal_pos=abs(deal_pos),
                                                                                                                                  target=(price * deal_pos),
                                                                                                                                  capital=UseCapital), ui_log=None)
@@ -38,9 +38,9 @@ def BUY(client, account, strategy_id, account_info,trading_date, code, name, ind
                               volume=(price * deal_pos) - UseCapital)
             time.sleep(5)
 
-        QA_util_log_info('买入 {code}({NAME},{INDUSTRY}) {deal_pos}股, 目标持仓:{target_pos},单价:{price},总金额:{target}'.format(code=code,
-                                                                                                                      NAME= name,
-                                                                                                                      INDUSTRY=industry,
+        QA_util_log_info('买入 {code}({name},{industry}) {deal_pos}股, 目标持仓:{target_pos},单价:{price},总金额:{target}'.format(code=code,
+                                                                                                                      name= name,
+                                                                                                                      industry=industry,
                                                                                                                       deal_pos=abs(deal_pos),
                                                                                                                       target_pos=target_pos,
                                                                                                                       price=price,
@@ -52,9 +52,9 @@ def BUY(client, account, strategy_id, account_info,trading_date, code, name, ind
     elif type == 'morning':
         QA_util_log_info('##JOB Get Down Price Before {code} Selling ===== {date}'.format(code = code, date=trading_date), ui_log = None)
         price = round(float(close*(1-0.0995)),2)
-        QA_util_log_info('早盘挂单买入 {code}({NAME},{INDUSTRY}) {deal_pos}股, 目标持仓:{target_pos},单价:{price},总金额:{target}'.format(code=code,
-                                                                                                                          NAME= name,
-                                                                                                                          INDUSTRY=industry,
+        QA_util_log_info('早盘挂单买入 {code}({name},{industry}) {deal_pos}股, 目标持仓:{target_pos},单价:{price},总金额:{target}'.format(code=code,
+                                                                                                                          name= name,
+                                                                                                                          industry=industry,
                                                                                                                           deal_pos=abs(deal_pos),
                                                                                                                           target_pos=target_pos,
                                                                                                                           price=price,
