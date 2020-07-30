f3c99ff291ca561551287c4ca8efd344eaa619d3
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-07-30 15:09:22 +0800

    QA

--- QUANTTOOLS/account_manage/Client.py
+++ QUANTTOOLS/account_manage/Client.py
@@ -36,6 +36,9 @@ def get_StockPos(code, client, account):
 
 def get_Position(client, account):
     positions = client.get_positions(account)['positions'][['证券代码','证券名称','市值','股票余额','可用余额','冻结数量','参考盈亏','盈亏比例(%)']]
+    positions['市值'] = positions['市值'].astype(float)
+    positions['股票余额'] = positions['股票余额'].astype(float)
+    positions['可用余额'] = positions['可用余额'].astype(float)
     positions = positions[positions['股票余额'].astype(float) > 0]
     positions['上市时间'] = positions['证券代码'].apply(lambda x:QA_util_get_days_to_today(str(QA_fetch_stock_to_market_date(x))))
     positions['INDUSTRY'] = positions['证券代码'].apply(lambda x:QA_fetch_stock_industry(x))
@@ -67,6 +70,9 @@ def check_Client(client, account, strategy_id, trading_date, exceptions=exceptio
     try:
         QA_util_log_info('##JOB Now Get Positions ==== {}'.format(str(trading_date)), ui_log)
         positions = res['positions'][['证券代码','证券名称','股票余额','可用余额','冻结数量','参考盈亏','成本价','市价','市值','盈亏比例(%)']]
+        positions['市值'] = positions['市值'].astype(float)
+        positions['股票余额'] = positions['股票余额'].astype(float)
+        positions['可用余额'] = positions['可用余额'].astype(float)
         positions = positions[positions['股票余额'].astype(float) > 0]
         positions['上市时间'] = positions['证券代码'].apply(lambda x:QA_util_get_days_to_today(str(QA_fetch_stock_to_market_date(x))))
         positions['INDUSTRY'] = positions['证券代码'].apply(lambda x:QA_fetch_stock_industry(x))
