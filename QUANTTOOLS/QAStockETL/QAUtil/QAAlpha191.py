40f46d125e7d5719184f268a7dbdbecec947fa64
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-08-03 01:44:41 +0800

    QA

--- QUANTTOOLS/QAStockETL/QAUtil/QAAlpha191.py
+++ QUANTTOOLS/QAStockETL/QAUtil/QAAlpha191.py
@@ -280,6 +280,8 @@ class Alpha_191:
         A=self.close.rolling(6).mean().iloc[-6:,:]
         B=np.arange(1,7)   #等差Sequence 1:6
         temp=A.apply(lambda x:sp.stats.linregress(x,B) ,axis=0)  #linear regression
+        for i in temp:
+            print(i)
         print(temp)
         alpha = pd.Series([np.nan if i[3] > 0.05 else i[0] for i in temp],index=temp.index)
         print(alpha)
