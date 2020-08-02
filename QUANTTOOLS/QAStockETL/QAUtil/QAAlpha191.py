121e31f638edfbbb1fe7b125eeaded68cf9e2666
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-08-03 01:53:37 +0800

    QA

--- QUANTTOOLS/QAStockETL/QAUtil/QAAlpha191.py
+++ QUANTTOOLS/QAStockETL/QAUtil/QAAlpha191.py
@@ -282,7 +282,8 @@ class Alpha_191:
         temp=A.apply(lambda x:(sp.stats.linregress(x,B)) ,axis=0).T  #linear regression
         for i in temp.iterrows():
             print(i)
-        alpha = pd.Series([np.nan if i[3] > 0.05 else i[0] for i in temp.iterrows()],index=temp.index)
+        print(temp.index)
+        alpha = pd.Series([np.nan if i[3] > 0.05 else i[0] for i in temp.iterrows()])
         print(alpha)
         return alpha
 
