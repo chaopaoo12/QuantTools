e9a29f3eda08233ccf324017efd24229bd909be6
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-08-03 13:23:26 +0800

    QA

--- QUANTTOOLS/account_manage/BuildTradingFrame.py
+++ QUANTTOOLS/account_manage/BuildTradingFrame.py
@@ -102,9 +102,10 @@ def build(target, positions, sub_accounts, percent, Zbreak, k=100):
             res['目标持股数'] = res.apply(lambda x: x['目标持股数'] - x['trim'], axis=1)
             res['测算持股金额'] = res.apply(lambda x: x['目标持股数'] * x['买卖价'], axis=1)
             k = k + 100
-
+    print(res[['NAME','target','测算持股金额','目标持股数','可用余额']])
     QA_util_log_info('##JOB Caculate Deal Position', ui_log = None)
     res['deal'] = (res['目标持股数'] - res['股票余额'].apply(lambda x:float(x))).apply(lambda x:math.floor(x/100)*100)
+    print(res[['NAME','target','测算持股金额','目标持股数','可用余额','deal']])
     res['deal'] = res.apply(lambda x: x['deal'] if abs(x['deal']) < x['可用余额'] else x['可用余额'], axis=1)
 
     if Zbreak == True:
