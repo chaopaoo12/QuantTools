7bcc257f437f3e5b84a2ca3c3a181018edb42b62
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-08-03 12:37:09 +0800

    QA

--- QUANTTOOLS/account_manage/BuildTradingFrame.py
+++ QUANTTOOLS/account_manage/BuildTradingFrame.py
@@ -77,7 +77,8 @@ def build(target, positions, sub_accounts, percent, Zbreak, k=100):
 
         QA_util_log_info('##JOB Refreash Result Frame', ui_log = None)
         ##实时修正
-        res = res.assign(position=res.apply(lambda x:0 if x.ask1 == 0 else x.position))
+        res['position'] = res[['ask1','position']].apply(lambda x:0 if x.ask1 == 0 else x.position)
+        #res = res.assign(position=res.apply(lambda x:0 if x.ask1 == 0 else x.position))
         avg_account = (sub_accounts * percent)/res['position'].sum()
         res = res.assign(target=avg_account)
         res['target'] = res['target'] * res['position']
