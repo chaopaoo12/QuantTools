bb19f89da4acbaa23d33ac6d726bab06f15c9db6
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-08-04 15:13:52 +0800

    QA

--- QUANTTOOLS/account_manage/BuildTradingFrame.py
+++ QUANTTOOLS/account_manage/BuildTradingFrame.py
@@ -101,7 +101,7 @@ def build(target, positions, sub_accounts, percent, Zbreak, k=100):
             res['目标持股数'] = res.apply(lambda x: x['目标持股数'] + x['trim'], axis=1)
             res['测算持股金额'] = res.apply(lambda x: x['目标持股数'] * x['买卖价'], axis=1)
             k = k + 100
-
+        QA_util_log_info(res[['NAME','target','position','股票余额','sort_gp','买卖价','sort','目标持股数','测算持股金额']])
         while res['测算持股金额'].sum() > sub_accounts:
             QA_util_log_info('##JOB Budget Larger than Capital k: {}'.format(k), ui_log = None)
             res['trim'] = list(res['sort'].apply(lambda x:k if x == 1 else 0))
