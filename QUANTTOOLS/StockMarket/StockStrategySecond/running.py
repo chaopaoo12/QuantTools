eb54099ab37382a6f383746d487875cbee0d138f
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-08-06 05:39:12 +0800

    QA

--- QUANTTOOLS/StockMarket/StockStrategySecond/running.py
+++ QUANTTOOLS/StockMarket/StockStrategySecond/running.py
@@ -48,7 +48,7 @@ def predict(trading_date, strategy_id='机器学习1号', account='name:client-1
     QA_util_log_info('##JOB05 Now Current Report ==== {}'.format(str(trading_date)), ui_log)
     #table1 = tar[tar['RANK']<=5].groupby('date').mean()
     if tar is not None and tar.shape[0] > 0:
-        table1 = tar.groupby('date').mean()
+        table1 = tar.groupby('date').mean()[['Z_PROB','O_PROB','RANK','TARGET','TARGET3','TARGET4','TARGET5','PASS_MARK']]
     else:
         table1 = pd.DataFrame()
         tar = pd.DataFrame()
