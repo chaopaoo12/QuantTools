90ad1c72fc0f2c01599b171b57c71c0f896c73f3
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-08-10 15:19:50 +0800

    QA

--- QUANTTOOLS/QAStockETL/QAFetch/QAQuantFactor.py
+++ QUANTTOOLS/QAStockETL/QAFetch/QAQuantFactor.py
@@ -44,11 +44,11 @@ def QA_fetch_get_index_quant_data(codes, start_date, end_date, type='standardize
         '##JOB got Data index alpha101 data ============== from {from_} to {to_} '.format(from_= start_date,to_=end_date), ui_log)
     alpha101 = QA_fetch_index_alpha101_adv(codes,start,end_date).data.loc[rng1]
     alphas = alpha.join(alpha101)
-    for columnname in alphas.columns:
-        if alphas[columnname].dtype == 'float64':
-            alphas[columnname]=alphas[columnname].astype('float16')
-        if alphas[columnname].dtype == 'int64':
-            alphas[columnname]=alphas[columnname].astype('int8')
+    #for columnname in alphas.columns:
+    #    if alphas[columnname].dtype == 'float64':
+    #        alphas[columnname]=alphas[columnname].astype('float16')
+    #    if alphas[columnname].dtype == 'int64':
+    #        alphas[columnname]=alphas[columnname].astype('int8')
     QA_util_log_info(
         '##JOB got Data index tech data ============== from {from_} to {to_} '.format(from_= start_date,to_=end_date), ui_log)
     technical = QA_fetch_index_technical_index_adv(codes,start,end_date).data.drop(['PBX1','PBX1_C','PBX2','PBX2_C','PBX3','PBX3_C','PBX4','PBX4_C','PBX5','PBX5_C','PBX6','PBX6_C','PBX_STD','PVT','PVT_C'], axis=1).loc[rng1]
@@ -57,11 +57,11 @@ def QA_fetch_get_index_quant_data(codes, start_date, end_date, type='standardize
     tech_week = QA_fetch_index_technical_index_adv(codes,start,end_date, 'week').data.drop(['PBX1','PBX1_C','PBX2','PBX2_C','PBX3','PBX3_C','PBX4','PBX4_C','PBX5','PBX5_C','PBX6','PBX6_C','PBX_STD','PVT','PVT_C'], axis=1).loc[rng1]
     tech_week.columns = [x + '_WK' for x in tech_week.columns]
     technical = technical.join(tech_week)
-    for columnname in technical.columns:
-        if technical[columnname].dtype == 'float64':
-            technical[columnname]=technical[columnname].astype('float16')
-        if technical[columnname].dtype == 'int64':
-            technical[columnname]=technical[columnname].astype('int8')
+    #for columnname in technical.columns:
+    #    if technical[columnname].dtype == 'float64':
+    #        technical[columnname]=technical[columnname].astype('float16')
+    #    if technical[columnname].dtype == 'int64':
+    #        technical[columnname]=technical[columnname].astype('int8')
     QA_util_log_info(
         '##JOB index quant data combine ============== from {from_} to {to_} '.format(from_= start_date,to_=end_date), ui_log)
     res = technical.join(alphas)
