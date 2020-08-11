449cf2ce91acc614e057f727198bf3fd1b2698f8
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-08-11 12:18:28 +0800

    QA

--- QUANTTOOLS/account_manage/BuildTradingFrame.py
+++ QUANTTOOLS/account_manage/BuildTradingFrame.py
@@ -88,7 +88,7 @@ def build(target, positions, sub_accounts, percent, Zbreak, k=100):
         avg_account = (sub_accounts * percent)/res['position'].sum()
         res = res.assign(target=avg_account)
         res['target'] = res['target'] * res['position']
-
+        QA_util_log_info(res[['NAME','target','买卖价','ask1','bid1']])
         QA_util_log_info('##JOB Caculate Target Position', ui_log = None)
         res['目标持股数'] = res.apply(lambda x: math.floor(x['target'] / x['买卖价'] / 100)*100, axis=1)
         res['测算持股金额'] = res.apply(lambda x: x['目标持股数'] * x['买卖价'], axis=1)
