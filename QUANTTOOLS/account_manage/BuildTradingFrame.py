02199e828093f8fc435b2b19e7f2aca953e26ee4
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-08-11 14:51:30 +0800

    QA

--- QUANTTOOLS/account_manage/BuildTradingFrame.py
+++ QUANTTOOLS/account_manage/BuildTradingFrame.py
@@ -97,18 +97,18 @@ def build(target, positions, sub_accounts, percent, Zbreak, k=100):
 
         QA_util_log_info('##JOB Refresh Final Result', ui_log = None)
         k = 100
-        while (res['测算持股金额'].sum() - sub_accounts) <= 10000:
+        while (res['测算持股金额'].sum() - (sub_accounts * percent)) <= 10000:
             QA_util_log_info('##JOB Budget {budget} Less than Capital {capital} k: {k}'.format(k=k,
                                                                                                budget=res['测算持股金额'].sum(),
-                                                                                               capital = sub_accounts), ui_log = None)
+                                                                                               capital = (sub_accounts * percent)), ui_log = None)
             res['trim'] = list(res['sort'].apply(lambda x:k if x == 1 else 0))
             res['目标持股数'] = res.apply(lambda x: x['目标持股数'] + x['trim'], axis=1)
             res['测算持股金额'] = res.apply(lambda x: x['目标持股数'] * x['买卖价'], axis=1)
 
-        while res['测算持股金额'].sum() > sub_accounts:
+        while res['测算持股金额'].sum() > (sub_accounts * percent):
             QA_util_log_info('##JOB Budget {budget} Larger than Capital {capital} k: {k}'.format(k=k,
                                                                              budget=res['测算持股金额'].sum(),
-                                                                             capital = sub_accounts), ui_log = None)
+                                                                             capital = (sub_accounts * percent)), ui_log = None)
             res['trim'] = list(res['sort'].apply(lambda x:k if x == 1 else 0))
             res['目标持股数'] = res.apply(lambda x: x['目标持股数'] - x['trim'], axis=1)
             res['测算持股金额'] = res.apply(lambda x: x['目标持股数'] * x['买卖价'], axis=1)
