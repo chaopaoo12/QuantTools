607194daf171152cccdafc3515bcc1af03892681
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-08-15 22:31:39 +0800

    QA

--- QUANTTOOLS/StockMarket/StockStrategySecond/daily_job.py
+++ QUANTTOOLS/StockMarket/StockStrategySecond/daily_job.py
@@ -2,22 +2,13 @@ from datetime import datetime
 from QUANTTOOLS.StockMarket.StockStrategySecond.running import predict
 from QUANTAXIS.QAUtil.QADate_trade import QA_util_if_trade,QA_util_get_real_date,QA_util_get_last_day
 
-
 def daily_run(trading_date):
-
-    if datetime.strptime(trading_date, "%Y-%m-%d").weekday() == 4:
-        if QA_util_if_trade(trading_date):
-            pass
-        else:
-            trading_date = QA_util_get_real_date(trading_date)
-
-        predict(QA_util_get_last_day(trading_date))
-    else:
-        pass
 
     if QA_util_if_trade(trading_date):
         predict(trading_date)
-    else:
+    elif QA_util_if_trade((datetime.datetime.strptime(trading_date,'%Y-%m-%d')+datetime.timedelta(days=1)).strftime("%Y-%m-%d")):
         predict(QA_util_get_real_date(trading_date))
+    else:
+        pass
 
 
