ef6d0f4ce2dda76fab02fcff039b4099a9b2cd2d
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-08-15 16:13:29 +0800

    QA

--- QUANTTOOLS/QAStockETL/QAUtil/QASQLStockAlpha101.py
+++ QUANTTOOLS/QAStockETL/QAUtil/QASQLStockAlpha101.py
@@ -105,4 +105,4 @@ def QA_Sql_Stock_Alpha101(from_ , to_, sql_text = sql_text, ui_log= None):
     conn = cx_Oracle.connect(ORACLE_PATH2)
     data = pd.read_sql(sql=sql_text, con=conn)
     conn.close()
-    return(data.drop_duplicates((['CODE', 'ORDER_DATE'])).set_index(['ORDER_DATE','CODE']))
+    return(data.drop_duplicates((['CODE', 'DATE'])))
--- QUANTTOOLS/QAStockETL/QAUtil/QASQLStockIndexWeek.py
+++ QUANTTOOLS/QAStockETL/QAUtil/QASQLStockIndexWeek.py
@@ -232,4 +232,4 @@ def QA_Sql_Stock_IndexWeek(from_ , to_, sql_text = sql_text, ui_log= None):
     conn = cx_Oracle.connect(ORACLE_PATH2)
     data = pd.read_sql(sql=sql_text, con=conn)
     conn.close()
-    return(data.drop_duplicates((['CODE', 'ORDER_DATE'])))
+    return(data.drop_duplicates((['CODE', 'DATE'])))
