749d6a84fb77656ba93735ebc0de503bc1a2d7b6
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-08-15 22:23:16 +0800

    QA

--- QUANTTOOLS/StockMarket/StockStrategyThird/daily_job.py
+++ QUANTTOOLS/StockMarket/StockStrategyThird/daily_job.py
@@ -3,7 +3,7 @@ from QUANTTOOLS.StockMarket.StockStrategyThird.running import predict
 from QUANTAXIS.QAUtil.QADate_trade import QA_util_if_trade,QA_util_get_real_date,QA_util_get_last_day
 
 
-def job111(trading_date):
+def daily_run(trading_date):
 
     if datetime.strptime(trading_date, "%Y-%m-%d").weekday() == 4:
         if QA_util_if_trade(trading_date):
--- QUANTTOOLS/StockMarket/StockStrategyThird/train_job.py
+++ QUANTTOOLS/StockMarket/StockStrategyThird/train_job.py
@@ -5,7 +5,7 @@ from QUANTTOOLS.StockMarket.StockStrategyThird.setting import working_dir
 from QUANTAXIS.QAUtil.QADate_trade import QA_util_if_trade,QA_util_get_real_date,QA_util_get_last_day
 
 
-def job111(trading_date):
+def daily_train(trading_date):
 
     if datetime.strptime(trading_date, "%Y-%m-%d").weekday() == 5:
         if QA_util_if_trade(trading_date):
