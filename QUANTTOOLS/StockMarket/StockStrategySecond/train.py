7ac43b97a19405034e6ffb2c19c717c427e425d1
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-08-15 21:02:27 +0800

    QA

--- QUANTTOOLS/StockMarket/StockStrategySecond/train.py
+++ QUANTTOOLS/StockMarket/StockStrategySecond/train.py
@@ -16,11 +16,11 @@ def train(date, strategy_id='机器学习1号', working_dir=working_dir, ui_log 
     safe_model = IndexModel()
 
     QA_util_log_info('##JOB02 Now Stock Prepare Model Data ==== {}'.format(str(date)), ui_log)
-    stock_model.get_data(start=str(int(date[0:4])-2)+"-01-01", end=date, block=False, sub_block=False)
+    stock_model.get_data(start=str(int(date[0:4])-3)+"-01-01", end=date, block=False, sub_block=False)
     QA_util_log_info('##JOB03 Now Set Stock Model Target ==== {}'.format(str(date)), ui_log)
     stock_model.set_target(mark =0.42, type = 'percent')
     QA_util_log_info('##JOB04 Now Set Stock Model Train time range ==== {}'.format(str(date)), ui_log)
-    stock_model.set_train_rng(train_start=str(int(date[0:4])-2)+"-01-01",
+    stock_model.set_train_rng(train_start=str(int(date[0:4])-3)+"-01-01",
                               train_end=QA_util_get_last_day(QA_util_get_real_date(date), 5))
     stock_model.prepare_data()
     other_params = {'learning_rate': 0.1, 'n_estimators': 200, 'max_depth': 5, 'min_child_weight': 5, 'seed': 0,
