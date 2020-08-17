4965bed291f7b233be60a60a23275c540dfec6b4
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-08-17 15:36:57 +0800

    QA

--- QUANTTOOLS/FactorTools/QuantMk.py
+++ QUANTTOOLS/FactorTools/QuantMk.py
@@ -66,7 +66,7 @@ def get_quant_data_norm(start_date, end_date, type = 'crawl', block = False, sub
     if type == 'crawl':
         res = QA_fetch_stock_quant_pre_adv(codes,start_date,end_date, block = sub_block, method=method).data
     if type == 'model':
-        res = QA_fetch_get_quant_data(codes, start_date, end_date, type='normalization').set_index(['date','code']).drop(['date_stamp'], axis=1)
+        res = QA_fetch_get_quant_data(codes, start_date, end_date, type=None).set_index(['date','code']).drop(['date_stamp'], axis=1)
         target = QA_fetch_stock_target(codes, start_date, end_date, method=method)
         res = res.join(target)
     #res = res[(res['RNG_L_O'] <= 5 & res['LAG_TOR_O'] < 1)]
