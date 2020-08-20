4be58fbcc0682acbaf8d75ee74cb09cdc2962835
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-08-20 09:39:56 +0800

    QA

--- QUANTTOOLS/QAStockETL/QASU/QAMySQL.py
+++ QUANTTOOLS/QAStockETL/QASU/QAMySQL.py
@@ -396,28 +396,7 @@ def QA_etl_stock_financial_percent_day(start_date = QA_util_today_str(), end_dat
     QA_util_log_info(
         '##JOB Now ETL STOCK FINANCIAL PERCENT ==== from {from_} to {to_}'.format(from_=start_date,to_=end_date), ui_log)
     codes = list(QA_fetch_stock_list_adv()['code'])
-    data = QA_fetch_stock_financial_percent_adv(codes, start_date, end_date).data[['PE_10PCT','PE_10VAL','PEEGL_10PCT','PEEGL_10VAL','PB_10PCT','PB_10VAL',
-                                                                                   #'PEG_10PCT','PEG_10VAL',
-                                                                                   'PS_10PCT','PS_10VAL',
-                                                                                   'PE_20PCT','PE_20VAL','PEEGL_20PCT','PEEGL_20VAL','PB_20PCT','PB_20VAL',
-                                                                                   #'PEG_20PCT','PEG_20VAL',
-                                                                                   'PS_20PCT','PS_20VAL',
-                                                                                   'PE_30PCT','PE_30VAL','PE_30DN','PE_30UP',
-                                                                                   'PEEGL_30PCT','PEEGL_30VAL','PEEGL_30DN','PEEGL_30UP',
-                                                                                   'PB_30PCT','PB_30VAL','PB_30DN','PB_30UP',
-                                                                                   #'PEG_30PCT','PEG_30VAL','PEG_30DN','PEG_30UP',
-                                                                                   'PS_30PCT','PS_30VAL','PS_30DN','PS_30UP',
-                                                                                   'PE_60PCT','PE_60VAL','PE_60DN','PE_60UP',
-                                                                                   'PEEGL_60PCT','PEEGL_60VAL','PEEGL_60DN','PEEGL_60UP',
-                                                                                   'PB_60PCT','PB_60VAL','PB_60DN','PB_60UP',
-                                                                                   #'PEG_60PCT','PEG_60VAL','PEG_60DN','PEG_60UP',
-                                                                                   'PS_60PCT','PS_60VAL','PS_60DN','PS_60UP',
-                                                                                   'PE_90PCT','PE_90VAL','PE_90DN','PE_90UP',
-                                                                                   'PEEGL_90PCT','PEEGL_90VAL','PEEGL_90DN','PEEGL_90UP',
-                                                                                   'PB_90PCT','PB_90VAL','PB_90DN','PB_90UP',
-                                                                                   #'PEG_90PCT','PEG_90VAL','PEG_90DN','PEG_90UP',
-                                                                                   #'PS_90PCT','PS_90VAL','PS_90DN','PS_90UP'
-                                                                                   ]]
+    data = QA_fetch_stock_financial_percent_adv(codes, start_date, end_date).data
     if data is None:
         QA_util_log_info(
             '##JOB NO STOCK FINANCIAL PERCENT HAS BEEN SAVED ==== from {from_} to {to_}'.format(from_=start_date,to_=end_date), ui_log)
