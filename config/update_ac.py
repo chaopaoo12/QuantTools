ebcc61348c5f147fccd3fe61bf7c9400e5b5ad72
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-08-27 06:03:45 +0800

    QA

--- config/update_ac.py
+++ config/update_ac.py
@@ -28,26 +28,8 @@
 """对应于save x
 """
 from QUANTAXIS.QASU.main import (QA_SU_save_financialfiles_fromtdx)
-from QUANTAXIS.QASU.main import (QA_SU_save_etf_day, QA_SU_save_etf_min,
-                                 QA_SU_save_financialfiles,
-                                 QA_SU_save_index_day, QA_SU_save_index_min,
-                                 QA_SU_save_stock_block, QA_SU_save_stock_day,
-                                 QA_SU_save_stock_info,
-                                 QA_SU_save_stock_info_tushare,
-                                 QA_SU_save_stock_list, QA_SU_save_stock_min,
-                                 QA_SU_save_stock_xdxr)
-from QUANTTOOLS.QAStockETL import (QA_SU_save_report_calendar_his, QA_SU_save_stock_divyield_his, QA_SU_save_fianacialTTM_momgo,
-                                   QA_SU_save_stock_divyield_day,QA_SU_save_report_calendar_day,
-                                   QA_SU_save_stock_fianacial_momgo,QA_SU_save_stock_fianacial_momgo_his,
-                                   QA_SU_save_stock_financial_sina_day,QA_SU_save_stock_financial_sina_his,
-                                   QA_SU_save_stock_shares_sina_day,QA_SU_save_stock_shares_sina_his,
-                                   QA_SU_save_stock_financial_wy_day,QA_SU_save_stock_financial_wy_his)
-from QUANTTOOLS.QAStockETL import (QA_etl_stock_list, QA_etl_stock_info,
-                                   QA_etl_stock_xdxr, QA_etl_stock_day,
-                                   QA_etl_stock_financial, QA_etl_stock_calendar,
-                                   QA_etl_stock_block, QA_etl_stock_divyield,
-                                   QA_etl_process_financial_day,QA_etl_stock_shares,
-                                   QA_util_process_stock_financial,QA_etl_stock_financial_wy,QA_SU_save_usstock_list_day)
+from QUANTTOOLS.QAStockETL import (QA_etl_stock_financial, QA_SU_save_fianacialTTM_momgo,QA_SU_save_stock_financial_wy_day,
+                                   QA_util_process_stock_financial,QA_etl_stock_financial_wy)
 
 if __name__ == '__main__':
     print("write tdx financial data into sqldatabase")
