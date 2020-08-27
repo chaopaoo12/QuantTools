16c6291c898ca7b8d652770b1a44cf7aaa6d2a20
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-08-27 21:06:53 +0800

    QA

--- QUANTTOOLS/QAStockETL/QAFetch/QAQuery.py
+++ QUANTTOOLS/QAStockETL/QAFetch/QAQuery.py
@@ -1521,16 +1521,16 @@ def QA_fetch_stock_delist(collections=DATABASE.stock_delist):
     )
 
 def QA_fetch_stock_om_all():
-    tushare_list = pd.DataFrame(QA.QAFetch.QAQuery.QA_fetch_stock_basic_info_tushare())['code','name']
+    tushare_list = pd.DataFrame(QA.QAFetch.QAQuery.QA_fetch_stock_basic_info_tushare())[['code','name']]
     code_list = QA_fetch_stock_list()[['code','name']]
     code_list = code_list.append(tushare_list)
     return(code_list)
 
 
 def QA_fetch_stock_all():
-    stock_delist = QA_fetch_stock_delist()[['code','name','sse']]
-    tushare_list = pd.DataFrame(QA.QAFetch.QAQuery.QA_fetch_stock_basic_info_tushare())['code','name']
-    code_list = QA_fetch_stock_list()[['code','name','sse']]
+    stock_delist = QA_fetch_stock_delist()[['code','name']]
+    tushare_list = pd.DataFrame(QA.QAFetch.QAQuery.QA_fetch_stock_basic_info_tushare())[['code','name']]
+    code_list = QA_fetch_stock_list()[['code','name']]
     code_list = code_list.append(stock_delist).append(tushare_list)
     return(code_list)
 
