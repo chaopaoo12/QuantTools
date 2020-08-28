a2f5a1143ae5e397164f455bca7438089759ffb4
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-08-28 17:49:54 +0800

    QA

--- QUANTTOOLS/QAStockETL/QAFetch/QAAlpha.py
+++ QUANTTOOLS/QAStockETL/QAFetch/QAAlpha.py
@@ -4,44 +4,58 @@ from QUANTAXIS.QAUtil import QA_util_date_stamp,QA_util_if_trade,QA_util_log_inf
 def QA_fetch_get_stock_alpha(code, date, ui_log = None):
     if QA_util_if_trade(date) == True:
         data = stock_alpha(code, date).reset_index()
-        names = list(data.columns)
-        names[0] = 'code'
-        data.columns = names
-        data = data.assign(date_stamp=data['date'].apply(lambda x: QA_util_date_stamp(str(x)[0:10])))
-        return(data)
+        if data is not None:
+            names = list(data.columns)
+            names[0] = 'code'
+            data.columns = names
+            data = data.assign(date_stamp=data['date'].apply(lambda x: QA_util_date_stamp(str(x)[0:10])))
+            return(data)
+        else:
+            QA_util_log_info(
+                '##JOB Non Data Stock Alpha191 for ============== {}'.format(date), ui_log)
     else:
         QA_util_log_info(
-            '##JOB Non Data Stock Alpha191 for ============== {date}'.format(date), ui_log)
+            '##JOB Non Data Stock Alpha191 for ============== {}'.format(date), ui_log)
 
 def QA_fetch_get_index_alpha(code, date, ui_log = None):
     if QA_util_if_trade(date) == True:
         data = index_alpha(code, date).reset_index()
-        names = list(data.columns)
-        names[0] = 'code'
-        data.columns = names
-        data = data.assign(date_stamp=data['date'].apply(lambda x: QA_util_date_stamp(str(x)[0:10])))
-        return(data)
+        if data is not None:
+            names = list(data.columns)
+            names[0] = 'code'
+            data.columns = names
+            data = data.assign(date_stamp=data['date'].apply(lambda x: QA_util_date_stamp(str(x)[0:10])))
+            return(data)
+        else:
+            QA_util_log_info(
+                '##JOB Non Data Index Alpha191 for ============== {}'.format(date), ui_log)
     else:
         QA_util_log_info(
-            '##JOB Non Data Index Alpha191 for ============== {date}'.format(date), ui_log)
+            '##JOB Non Data Index Alpha191 for ============== {}'.format(date), ui_log)
 
 def QA_fetch_get_stock_alpha101(code, start, end, ui_log = None):
     deal_date_list = QA_util_get_trade_range(start, end)
     if deal_date_list is not None:
         data = stock_alpha101(code, start, end)
-        names = list(data.columns)
-        data = data.assign(date_stamp=data['date'].apply(lambda x: QA_util_date_stamp(str(x)[0:10])))
-        return(data)
+        if data is not None:
+            data = data.assign(date_stamp=data['date'].apply(lambda x: QA_util_date_stamp(str(x)[0:10])))
+            return(data)
+        else:
+            QA_util_log_info(
+                '##JOB Non Data Stock Alpha101 ============== from {_from} to {_to}'.format(_from=start, _to=end), ui_log)
     else:
         QA_util_log_info(
-            '##JOB Non Data Stock Alpha101 ============== from {_from} to {_to}'.format(start, end), ui_log)
+            '##JOB Non Data Stock Alpha101 ============== from {_from} to {_to}'.format(_from=start, _to=end), ui_log)
 
 def QA_fetch_get_index_alpha101(code, start, end, ui_log = None):
     deal_date_list = QA_util_get_trade_range(start, end)
     if deal_date_list is not None:
         data = index_alpha101(code, start, end)
-        data = data.assign(date_stamp=data['date'].apply(lambda x: QA_util_date_stamp(str(x)[0:10])))
-        return(data)
+        if data is not None:
+            data = data.assign(date_stamp=data['date'].apply(lambda x: QA_util_date_stamp(str(x)[0:10])))
+            return(data)
+        QA_util_log_info(
+            '##JOB Non Data Index Alpha101 ============== from {_from} to {_to}'.format(_from=start, _to=end), ui_log)
     else:
         QA_util_log_info(
-            '##JOB Non Data Index Alpha101 ============== from {_from} to {_to}'.format(start, end), ui_log)
+            '##JOB Non Data Index Alpha101 ============== from {_from} to {_to}'.format(_from=start, _to=end), ui_log)
