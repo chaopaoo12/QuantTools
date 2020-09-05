de97d695909098d9b0933d05a190fd8266059ee5
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-09-05 11:57:53 +0800

    QA

--- QUANTTOOLS/StockMarket/StockStrategyForth/train_job.py
+++ QUANTTOOLS/StockMarket/StockStrategyForth/train_job.py
@@ -3,20 +3,21 @@ from QUANTTOOLS.StockMarket.StockStrategyForth.train import train
 from QUANTTOOLS.StockMarket.StockStrategyForth.running import predict
 from QUANTTOOLS.StockMarket.StockStrategyForth.setting import working_dir
 
-from QUANTAXIS.QAUtil.QADate_trade import QA_util_if_trade,QA_util_get_real_date,QA_util_get_last_day
+from QUANTAXIS.QAUtil.QADate_trade import QA_util_if_trade,QA_util_get_real_date
 
 from datetime import datetime
 
 def daily_train(trading_date):
 
-    if datetime.strptime(trading_date, "%Y-%m-%d").weekday() == 5:
+    if datetime.strptime(trading_date, "%Y-%m-%d").weekday() == 4:
+
         if QA_util_if_trade(trading_date):
             pass
         else:
             trading_date = QA_util_get_real_date(trading_date)
 
         train(trading_date, working_dir=working_dir)
-        predict(QA_util_get_last_day(trading_date))
+        predict(trading_date)
     else:
         pass
 
