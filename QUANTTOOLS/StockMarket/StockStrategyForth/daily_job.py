0234071d816c235871b96d7b9e29b6220d91e65b
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-09-07 10:55:08 +0800

    QA

--- QUANTTOOLS/StockMarket/StockStrategyForth/daily_job.py
+++ QUANTTOOLS/StockMarket/StockStrategyForth/daily_job.py
@@ -1,15 +1,9 @@
 
 from QUANTTOOLS.StockMarket.StockStrategyForth.running import predict
-from QUANTAXIS.QAUtil.QADate_trade import QA_util_if_trade,QA_util_get_real_date,QA_util_get_last_day
-from datetime import datetime
+
 
 def daily_run(trading_date):
 
-    if QA_util_if_trade(trading_date):
-        predict(trading_date)
-    elif QA_util_if_trade((datetime.datetime.strptime(trading_date,'%Y-%m-%d')+datetime.timedelta(days=1)).strftime("%Y-%m-%d")):
-        predict(QA_util_get_real_date(trading_date))
-    else:
-        pass
+    predict(trading_date)
 
 
