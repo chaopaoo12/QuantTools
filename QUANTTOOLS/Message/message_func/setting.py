aa8110a8269f54490868fe5254ff9ca463cea218
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-09-16 13:44:18 +0800

    QA

--- QUANTTOOLS/Message/message_func/setting.py
+++ QUANTTOOLS/Message/message_func/setting.py
@@ -1,7 +1,10 @@
-# todo 需要保密转移
-QAPRO_ID='oL-C4w1_ETCiWO42fo_kakd6Sq_Q'
-smtpserver = "smtp.qq.com"
-smtpport = "465"
-msg_from ='738105084@qq.com'                                 #发送方邮箱
-passwd ='fzeyhazfiztnbfbj'                                   #填入发送方邮箱的授权码
-msg_to =['738105084@qq.com']                                 #收件人邮箱
+from QUANTAXIS.QAUtil.QASetting import QA_Setting
+
+QASETTING = QA_Setting()
+
+QAPRO_ID = QASETTING.get_config('WECHAT','QAPRO_ID')
+smtpserver = QASETTING.get_config('EMAIL','smtpserver')
+smtpport = QASETTING.get_config('EMAIL','smtpport')
+msg_from = QASETTING.get_config('EMAIL','msg_from')        #发送方邮箱
+passwd = QASETTING.get_config('EMAIL','passwd')            #填入发送方邮箱的授权码
+msg_to = QASETTING.get_config('EMAIL','msg_to').split(',') #收件人邮箱
