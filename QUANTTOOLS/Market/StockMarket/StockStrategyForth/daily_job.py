62ac3e57f274b1c2e922f2d95ad4467457f20238
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-09-16 12:02:17 +0800

    QA

---
+++ QUANTTOOLS/Market/StockMarket/StockStrategyForth/daily_job.py
@@ -0,0 +1,9 @@
+
+from QUANTTOOLS.Market.StockMarket.StockStrategyForth.running import predict
+
+
+def daily_run(trading_date):
+
+    predict(trading_date)
+
+
---
+++ QUANTTOOLS/Market/StockMarket/StockStrategyForth/train_job.py
@@ -0,0 +1,24 @@
+
+from QUANTTOOLS.Market.StockMarket.StockStrategyForth import train
+from QUANTTOOLS.Market.StockMarket.StockStrategyForth.running import predict
+from QUANTTOOLS.Market.StockMarket.StockStrategyForth.setting import working_dir
+
+from QUANTAXIS.QAUtil.QADate_trade import QA_util_if_trade,QA_util_get_real_date
+
+from datetime import datetime
+
+def daily_train(trading_date):
+
+    if datetime.strptime(trading_date, "%Y-%m-%d").weekday() == 4:
+
+        if QA_util_if_trade(trading_date):
+            pass
+        else:
+            trading_date = QA_util_get_real_date(trading_date)
+
+        train(trading_date, working_dir=working_dir)
+        predict(trading_date)
+    else:
+        pass
+
+
