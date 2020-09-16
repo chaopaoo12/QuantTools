dd0eecf7417d304c2858c587ecd0ac35a8f6d64c
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-09-16 10:51:23 +0800

    QA

--- QUANTTOOLS/QAStockETL/QAFetch/QATdx.py
+++ QUANTTOOLS/QAStockETL/QAFetch/QATdx.py
@@ -1,40 +1,39 @@
-from QUANTAXIS import QA_fetch_get_future_day,QA_fetch_get_stock_realtime
+from QUANTAXIS import QA_fetch_get_future_day
 from QUANTTOOLS.QAStockETL.QAData.database_settings import tdx_dir
+import easyquotation
 import pandas as pd
 import akshare as ak
 
 QA_fetch_get_usstock_day = QA_fetch_get_future_day
 
+def QA_fetch_get_stock_realtime(code):
+    quotation = easyquotation.use('sina')
+    values = pd.DataFrame(quotation.stocks(code)).T
+    values.index.name = 'code'
+    return(values)
+
 def QA_fetch_get_stock_close(code):
-    return(float(QA_fetch_get_stock_realtime('tdx', code).reset_index('datetime')['last_close']))
+    return(float(QA_fetch_get_stock_realtime(code)['close']))
 
 def QA_fetch_get_stock_realtm_ask(code):
-    return(float(QA_fetch_get_stock_realtime('tdx', code).reset_index('datetime')['ask1']))
+    return(float(QA_fetch_get_stock_realtime(code)['ask1']))
 
 def QA_fetch_get_stock_realtm_askvol(code):
-    return(float(QA_fetch_get_stock_realtime('tdx', code).reset_index('datetime')['ask_vol1']))
+    return(float(QA_fetch_get_stock_realtime(code)['ask_vol1']))
 
 def QA_fetch_get_stock_realtm_askvol5(code):
-    aks_vol1 = float(QA_fetch_get_stock_realtime('tdx', code).reset_index('datetime')['aks_vol1'])
-    aks_vol2 = float(QA_fetch_get_stock_realtime('tdx', code).reset_index('datetime')['aks_vol2'])
-    aks_vol3 = float(QA_fetch_get_stock_realtime('tdx', code).reset_index('datetime')['aks_vol3'])
-    aks_vol4 = float(QA_fetch_get_stock_realtime('tdx', code).reset_index('datetime')['aks_vol4'])
-    aks_vol5 = float(QA_fetch_get_stock_realtime('tdx', code).reset_index('datetime')['aks_vol5'])
-    return(aks_vol1+aks_vol2+aks_vol3+aks_vol4+aks_vol5)
+    res = float(QA_fetch_get_stock_realtime(code)[['aks1_volume','aks2_volume','aks3_volume','aks4_volume','aks5_volume']])
+    return(float(res.aks1_volume + res.aks2_volume + res.aks3_volume + res.aks4_volume + res.aks5_volume))
 
 def QA_fetch_get_stock_realtm_bid(code):
-    return(float(QA_fetch_get_stock_realtime('tdx', code).reset_index('datetime')['bid1']))
+    return(float(QA_fetch_get_stock_realtime(code)['bid1']))
 
 def QA_fetch_get_stock_realtm_bidvol(code):
-    return(float(QA_fetch_get_stock_realtime('tdx', code).reset_index('datetime')['bid_vol1']))
+    return(float(QA_fetch_get_stock_realtime(code)['bid_vol1']))
 
 def QA_fetch_get_stock_realtm_bidvol5(code):
-    bid_vol1 = float(QA_fetch_get_stock_realtime('tdx', code).reset_index('datetime')['bid_vol1'])
-    bid_vol2 = float(QA_fetch_get_stock_realtime('tdx', code).reset_index('datetime')['bid_vol2'])
-    bid_vol3 = float(QA_fetch_get_stock_realtime('tdx', code).reset_index('datetime')['bid_vol3'])
-    bid_vol4 = float(QA_fetch_get_stock_realtime('tdx', code).reset_index('datetime')['bid_vol4'])
-    bid_vol5 = float(QA_fetch_get_stock_realtime('tdx', code).reset_index('datetime')['bid_vol5'])
-    return(bid_vol1+bid_vol2+bid_vol3+bid_vol4+bid_vol5)
+    res = float(QA_fetch_get_stock_realtime(code)[['bid1_volume','bid2_volume','bid3_volume','bid4_volume','bid5_volume']])
+    return(float(res.bid1_volume + res.bid2_volume + res.bid3_volume + res.bid4_volume + res.bid5_volume))
 
 def QA_fetch_get_usstock_adj():
     pass
