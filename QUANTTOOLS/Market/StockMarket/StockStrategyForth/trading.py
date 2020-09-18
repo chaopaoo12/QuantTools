07baa50d2a4b9d54400b913fef6dd45ec965b33d
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-09-18 11:19:48 +0800

    QA

--- QUANTTOOLS/Market/StockMarket/StockStrategyForth/trading.py
+++ QUANTTOOLS/Market/StockMarket/StockStrategyForth/trading.py
@@ -40,9 +40,9 @@ def trading(trading_date, percent=percent, strategy_id= '机器学习1号', acco
     QA_util_log_info('##JOB04 Now Timing Control ==== {}'.format(str(trading_date)), ui_log)
     tm = int(datetime.datetime.now().strftime("%H%M%S"))
     target_tm = int(time.strftime("%H%M%S", time.strptime("14:50:00", "%H:%M:%S")))
-    #while tm < target_tm:
-    #    tm = int(datetime.datetime.now().strftime("%H%M%S"))
-    #    time.sleep(15)
+    while tm < target_tm:
+        tm = int(datetime.datetime.now().strftime("%H%M%S"))
+        time.sleep(15)
 
     QA_util_log_info(
         '##JOB04 Now Trading ==== {}'.format(str(trading_date)), ui_log)
