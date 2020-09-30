d604dd145291b3ab4fcdcc6c81ef9e469ada9c68
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-09-30 13:24:39 +0800

    QA

--- QUANTTOOLS/QAStockETL/QASU/__init__.py
+++ QUANTTOOLS/QAStockETL/QASU/__init__.py
@@ -1,42 +1,26 @@
 
 
-from QUANTTOOLS.QAStockETL.QASU.QAMySQL import (QA_etl_stock_list, QA_etl_stock_info,
-                                                QA_etl_stock_xdxr, QA_etl_stock_day,
-                                                QA_etl_stock_financial, QA_etl_stock_calendar,
-                                                QA_etl_stock_block, QA_etl_stock_divyield,
-                                                QA_etl_stock_shares,
-                                                QA_etl_stock_financial_wy,
-                                                QA_etl_process_financial_day,
-                                                QA_etl_stock_alpha_day,
-                                                QA_etl_stock_technical_day,
-                                                QA_etl_stock_financial_day,
-                                                QA_etl_stock_technical_week,
-                                                QA_etl_stock_alpha101_day,
-                                                QA_etl_stock_alpha101half_day,
-                                                QA_etl_stock_alpha191half_day,
-                                                QA_etl_stock_financial_percent_day,
-                                                QA_etl_index_alpha_day,
-                                                QA_etl_index_alpha101_day,
-                                                QA_etl_index_technical_day,
-                                                QA_etl_index_technical_week
-                                                )
+###Stock Basic data
+from QUANTTOOLS.QAStockETL.QASU.main import (QA_SU_save_stock_day,QA_SU_save_stock_xdxr,
+                                             QA_SU_save_stock_half,QA_SU_save_stock_info,
+                                             QA_SU_save_stock_delist,QA_SU_save_stock_industryinfo)
 
-from QUANTTOOLS.QAStockETL.QASU.main import (QA_SU_save_stock_day,QA_SU_save_stock_xdxr,QA_SU_save_stock_info,
-                                             QA_SU_save_stock_delist,QA_SU_save_stock_industryinfo,
-                                             QA_SU_save_index_info,
-                                             QA_SU_save_index_week,QA_SU_save_index_month,QA_SU_save_index_year,
+###index basic data
+from QUANTTOOLS.QAStockETL.QASU.main import (QA_SU_save_index_info,
+                                             QA_SU_save_index_week,QA_SU_save_index_month,QA_SU_save_index_year)
 
-                                             QA_SU_save_report_calendar_day, QA_SU_save_report_calendar_his,
+###Stock crawl
+from QUANTTOOLS.QAStockETL.QASU.main import (QA_SU_save_report_calendar_day, QA_SU_save_report_calendar_his,
                                              QA_SU_save_stock_divyield_day, QA_SU_save_stock_divyield_his,
                                              QA_SU_save_financialfiles,QA_SU_save_interest_rate,
                                              QA_SU_save_stock_financial_ths_day,QA_SU_save_stock_financial_ths_his,
                                              QA_SU_save_stock_financial_sina_day,QA_SU_save_stock_financial_sina_his,
                                              QA_SU_save_stock_shares_sina_day,QA_SU_save_stock_shares_sina_his,
-                                             QA_SU_save_stock_financial_wy_day,QA_SU_save_stock_financial_wy_his,
+                                             QA_SU_save_stock_financial_wy_day,QA_SU_save_stock_financial_wy_his)
 
-                                             QA_SU_save_stock_half,QA_SU_save_fianacialTTM_momgo,
+###Stock indicator
+from QUANTTOOLS.QAStockETL.QASU.main import (QA_SU_save_fianacialTTM_momgo,
                                              QA_SU_save_stock_fianacial_momgo, QA_SU_save_stock_fianacial_momgo_his,
-
                                              QA_SU_save_stock_alpha_day, QA_SU_save_stock_alpha_his,
                                              QA_SU_save_stock_alpha101_day,QA_SU_save_stock_alpha101_his,
                                              QA_SU_save_stock_technical_index_day,QA_SU_save_stock_technical_index_his,
@@ -45,15 +29,51 @@ from QUANTTOOLS.QAStockETL.QASU.main import (QA_SU_save_stock_day,QA_SU_save_sto
                                              QA_SU_save_stock_technical_month_day,QA_SU_save_stock_technical_month_his,
                                              QA_SU_save_stock_quant_data_day,QA_SU_save_stock_quant_data_his,
                                              QA_SU_save_stock_alpha101half_day,QA_SU_save_stock_alpha101half_his,
-                                             QA_SU_save_stock_alpha191half_day,QA_SU_save_stock_alpha191half_his,
+                                             QA_SU_save_stock_alpha191half_day,QA_SU_save_stock_alpha191half_his)
 
+###Index indicator
+from QUANTTOOLS.QAStockETL.QASU.main import (
                                              QA_SU_save_index_alpha_day,QA_SU_save_index_alpha_his,
                                              QA_SU_save_index_technical_index_day,QA_SU_save_index_technical_index_his,
                                              QA_SU_save_index_technical_week_day,QA_SU_save_index_technical_week_his,
                                              QA_SU_save_index_technical_month_day,QA_SU_save_index_technical_month_his,
                                              QA_SU_save_index_quant_data_day,QA_SU_save_index_quant_data_his,
-                                             QA_SU_save_index_alpha101_day,QA_SU_save_index_alpha101_his,
+                                             QA_SU_save_index_alpha101_day,QA_SU_save_index_alpha101_his
 
-                                             QA_SU_save_usstock_list_day,QA_SU_save_usstock_list_day,
+                                             )
+
+###US Stock basic data
+from QUANTTOOLS.QAStockETL.QASU.main import (
+                                             QA_SU_save_usstock_list_day,QA_SU_save_usstock_list,
                                              QA_SU_save_usstock_day,QA_SU_save_usstock_adj,
-                                             QA_SU_save_stock_industryinfo)
+                                             )
+
+from QUANTTOOLS.QAStockETL.QASU.QAMySQL import (QA_etl_stock_list, QA_etl_stock_info,
+                                                QA_etl_stock_xdxr, QA_etl_stock_day,
+                                                QA_etl_stock_financial, QA_etl_stock_calendar,
+                                                QA_etl_stock_block, QA_etl_stock_divyield,
+                                                QA_etl_stock_shares,
+                                                QA_etl_stock_financial_wy
+                                                )
+
+from QUANTTOOLS.QAStockETL.QASU.QAMySQL import (
+                                                QA_etl_process_financial_day
+                                                )
+
+from QUANTTOOLS.QAStockETL.QASU.QAMySQL import (
+                                                QA_etl_stock_alpha_day,
+                                                QA_etl_stock_technical_day,
+                                                QA_etl_stock_financial_day,
+                                                QA_etl_stock_technical_week,
+                                                QA_etl_stock_alpha101_day,
+                                                QA_etl_stock_alpha101half_day,
+                                                QA_etl_stock_alpha191half_day,
+                                                QA_etl_stock_financial_percent_day
+                                                )
+
+from QUANTTOOLS.QAStockETL.QASU.QAMySQL import (
+                                                QA_etl_index_alpha_day,
+                                                QA_etl_index_alpha101_day,
+                                                QA_etl_index_technical_day,
+                                                QA_etl_index_technical_week
+                                                )
--- QUANTTOOLS/QAStockETL/__init__.py
+++ QUANTTOOLS/QAStockETL/__init__.py
@@ -1,78 +1,101 @@
-
-
+###Stock Basic data
 from QUANTTOOLS.QAStockETL.QASU import (QA_SU_save_report_calendar_day, QA_SU_save_report_calendar_his,
                                         QA_SU_save_stock_divyield_day, QA_SU_save_stock_divyield_his,
-                                        QA_SU_save_fianacialTTM_momgo,
+                                        QA_SU_save_interest_rate,QA_SU_save_stock_day,
+                                        QA_SU_save_financialfiles,
+                                        QA_SU_save_stock_financial_ths_day,QA_SU_save_stock_financial_ths_his,
+                                        QA_SU_save_stock_financial_sina_day,QA_SU_save_stock_financial_sina_his,
+                                        QA_SU_save_stock_shares_sina_day,QA_SU_save_stock_shares_sina_his,
+                                        QA_SU_save_stock_financial_wy_day,QA_SU_save_stock_financial_wy_his
+                                        )
+
+###Stock Indicator
+from QUANTTOOLS.QAStockETL.QASU import (
                                         QA_SU_save_stock_fianacial_momgo, QA_SU_save_stock_fianacial_momgo_his,
+                                        QA_SU_save_fianacialTTM_momgo,
+                                        QA_SU_save_stock_fianacial_percent_day,QA_SU_save_stock_fianacial_percent_his,
+                                        QA_SU_save_stock_alpha_day,QA_SU_save_stock_alpha_his,
+                                        QA_SU_save_stock_technical_index_day,QA_SU_save_stock_technical_index_his,
+                                        QA_SU_save_stock_technical_week_day,QA_SU_save_stock_technical_week_his,
+                                        QA_SU_save_stock_technical_month_day,QA_SU_save_stock_technical_month_his,
+                                        QA_SU_save_stock_alpha101_day,QA_SU_save_stock_alpha101_his,
+                                        QA_SU_save_stock_alpha101half_day,QA_SU_save_stock_alpha101half_his,
+                                        QA_SU_save_stock_alpha191half_day,QA_SU_save_stock_alpha191half_his,
+                                        QA_SU_save_stock_quant_data_day,QA_SU_save_stock_quant_data_his,
+                                        )
+
+###Index Indicator
+from QUANTTOOLS.QAStockETL.QASU import (
+                                        QA_SU_save_index_technical_index_day,QA_SU_save_index_technical_index_his,
+                                        QA_SU_save_index_technical_week_day,QA_SU_save_index_technical_week_his,
+                                        QA_SU_save_index_technical_month_day,QA_SU_save_index_technical_month_his,
+                                        QA_SU_save_index_alpha_day,QA_SU_save_index_alpha_his,
+                                        QA_SU_save_index_alpha101_day,QA_SU_save_index_alpha101_his,
+                                        QA_SU_save_index_quant_data_day,QA_SU_save_index_quant_data_his,
+
+                                        QA_SU_save_index_info,QA_SU_save_stock_industryinfo,QA_SU_save_stock_delist,
+                                        QA_SU_save_index_week,QA_SU_save_index_month,QA_SU_save_index_year
+                                        )
+
+###US Stock Basic data
+from QUANTTOOLS.QAStockETL.QASU import (
+                                        QA_SU_save_usstock_list_day,QA_SU_save_usstock_list
+                                        )
+###Stock Save Result
+from QUANTTOOLS.QAStockETL.QASU import (
                                         QA_etl_stock_list, QA_etl_stock_info,
                                         QA_etl_stock_xdxr, QA_etl_stock_day,
                                         QA_etl_stock_financial, QA_etl_stock_calendar,
                                         QA_etl_stock_block, QA_etl_stock_divyield,
                                         QA_etl_process_financial_day,QA_etl_stock_shares,
                                         QA_etl_stock_financial_wy,QA_SU_save_stock_xdxr,QA_SU_save_stock_info,
-                                        QA_etl_stock_alpha_day,QA_SU_save_stock_day,
-                                        QA_etl_stock_technical_day,
-                                        QA_etl_stock_financial_day,
-                                        QA_etl_stock_technical_week,
-                                        QA_etl_stock_alpha101_day,
-                                        QA_etl_stock_alpha101half_day,
-                                        QA_etl_stock_financial_percent_day,
-                                        QA_etl_index_alpha_day,
+
+                                        QA_etl_stock_financial_day,QA_etl_stock_financial_percent_day,
+
+                                        QA_etl_stock_technical_day,QA_etl_stock_technical_week,
+                                        QA_etl_stock_alpha101_day,QA_etl_stock_alpha101half_day,
+                                        QA_etl_stock_alpha_day,QA_etl_stock_alpha191half_day
+                                        )
+
+###Index Save Result
+from QUANTTOOLS.QAStockETL.QASU import (QA_etl_index_alpha_day,
                                         QA_etl_index_alpha101_day,
                                         QA_etl_index_technical_day,
-                                        QA_etl_index_technical_week,
-                                        QA_etl_stock_alpha191half_day,
-                                        QA_SU_save_financialfiles,
-                                        QA_SU_save_stock_alpha_day,
-                                        QA_SU_save_stock_alpha_his,
-                                        QA_SU_save_stock_financial_ths_day,
-                                        QA_SU_save_stock_financial_ths_his,
-                                        QA_SU_save_stock_financial_sina_day,
-                                        QA_SU_save_stock_financial_sina_his,
-                                        QA_SU_save_stock_shares_sina_day,
-                                        QA_SU_save_stock_shares_sina_his,
-                                        QA_SU_save_stock_financial_wy_day,
-                                        QA_SU_save_stock_financial_wy_his,
-                                        QA_SU_save_stock_technical_index_day,
-                                        QA_SU_save_stock_technical_index_his,
-                                        QA_SU_save_interest_rate,
-                                        QA_SU_save_stock_fianacial_percent_day,
-                                        QA_SU_save_stock_fianacial_percent_his,
-                                        QA_SU_save_stock_quant_data_day,
-                                        QA_SU_save_stock_quant_data_his,
-                                        QA_SU_save_stock_technical_week_day,QA_SU_save_stock_technical_week_his,
-                                        QA_SU_save_stock_technical_month_day,QA_SU_save_stock_technical_month_his,
-                                        QA_SU_save_usstock_list_day,QA_SU_save_index_alpha_day,QA_SU_save_index_alpha_his,
-                                        QA_SU_save_index_technical_index_day,QA_SU_save_index_technical_index_his,
-                                        QA_SU_save_index_technical_week_day,QA_SU_save_index_technical_week_his,
-                                        QA_SU_save_index_technical_month_day,QA_SU_save_index_technical_month_his,
-                                        QA_SU_save_index_quant_data_day,QA_SU_save_index_quant_data_his,
-                                        QA_SU_save_index_alpha101_day,QA_SU_save_index_alpha101_his,
-                                        QA_SU_save_stock_alpha101_day,QA_SU_save_stock_alpha101_his,
-                                        QA_SU_save_index_info,QA_SU_save_stock_industryinfo,QA_SU_save_stock_delist,
-                                        QA_SU_save_index_week,QA_SU_save_index_month,QA_SU_save_index_year,
-                                        QA_SU_save_stock_alpha101half_day,QA_SU_save_stock_alpha101half_his,
-                                        QA_SU_save_stock_alpha191half_day,QA_SU_save_stock_alpha191half_his)
+                                        QA_etl_index_technical_week
+                                        )
 
+###SQL database ETL Stock data
 from QUANTTOOLS.QAStockETL.QAUtil import (QA_util_process_financial,QA_util_etl_financial_TTM,
                                           QA_util_etl_stock_quant,QA_util_sql_store_mysql,QA_util_process_stock_financial)
+###Crawl/Caculate Stock data
+from QUANTTOOLS.QAStockETL.QAFetch import (QA_fetch_get_stock_alpha,QA_fetch_get_financial_calendar, QA_fetch_get_stock_divyield)
 
-from QUANTTOOLS.QAStockETL.QAFetch import (QA_fetch_financial_report_adv, QA_fetch_stock_financial_calendar_adv, QA_fetch_stock_divyield_adv,
-                                           QA_fetch_financial_TTM_adv, QA_fetch_stock_fianacial_adv, QA_fetch_financial_report,
-                                           QA_fetch_stock_financial_calendar, QA_fetch_stock_divyield, QA_fetch_financial_TTM,
-                                           QA_fetch_stock_fianacial, QA_fetch_get_financial_calendar, QA_fetch_get_stock_divyield,
-                                           QA_fetch_stock_alpha,QA_fetch_stock_alpha_adv,QA_fetch_get_stock_alpha,
+###Query Index data
+from QUANTTOOLS.QAStockETL.QAFetch import (QA_fetch_index_info,
+                                           QA_fetch_index_week,QA_fetch_index_month,QA_fetch_index_year,
+                                           QA_fetch_index_week_adv,QA_fetch_index_month_adv,QA_fetch_index_year_adv)
+
+###Query Stock data
+from QUANTTOOLS.QAStockETL.QAFetch import (
+                                           QA_fetch_financial_report_adv, QA_fetch_stock_financial_calendar_adv, QA_fetch_stock_divyield_adv,
+                                           QA_fetch_financial_TTM_adv, QA_fetch_stock_fianacial_adv,
+                                           QA_fetch_financial_report,QA_fetch_stock_financial_calendar, QA_fetch_stock_divyield,
+                                           QA_fetch_stock_fianacial, QA_fetch_financial_TTM,
+                                           QA_fetch_stock_alpha,QA_fetch_stock_alpha_adv,
                                            QA_fetch_stock_technical_index,QA_fetch_stock_technical_index_adv,
                                            QA_fetch_stock_financial_percent,QA_fetch_stock_financial_percent_adv,
                                            QA_fetch_stock_quant_data,QA_fetch_stock_quant_data_adv,
                                            QA_fetch_stock_quant_pre,QA_fetch_stock_quant_pre_adv,
-                                           QA_fetch_stock_target,QA_fetch_stock_target_adv,QA_fetch_stock_industry,
-                                           QA_fetch_financial_code_wy,QA_fetch_financial_code_tdx,QA_fetch_financial_code_new,QA_fetch_financial_code_ttm,
-                                           QA_fetch_index_info,QA_fetch_stock_industryinfo,
-                                           QA_fetch_stock_all,QA_fetch_stock_delist,QA_fetch_stock_week_adv,QA_fetch_stock_month_adv,
-                                           QA_fetch_stock_year_adv,QA_fetch_index_week_adv,QA_fetch_index_month_adv,
-                                           QA_fetch_index_year_adv,QA_fetch_stock_week,QA_fetch_stock_month,
-                                           QA_fetch_stock_year,QA_fetch_index_week,QA_fetch_index_month,QA_fetch_index_year)
+                                           QA_fetch_stock_target,QA_fetch_stock_target_adv,
+
+                                           QA_fetch_stock_industry,QA_fetch_stock_industryinfo,
+                                           QA_fetch_financial_code_wy,QA_fetch_financial_code_tdx,
+                                           QA_fetch_financial_code_new,QA_fetch_financial_code_ttm,
+                                           QA_fetch_stock_all,QA_fetch_stock_delist,
+
+                                           QA_fetch_stock_week_adv,QA_fetch_stock_month_adv,QA_fetch_stock_year_adv,
+                                           QA_fetch_stock_week,QA_fetch_stock_month,QA_fetch_stock_year)
+
 
 from QUANTTOOLS.QAStockETL.Check import (check_index_day,check_stock_day,
                                          check_stock_fianacial,check_stock_adj,
