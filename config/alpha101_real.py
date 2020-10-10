bbb3b32cc0af0a16d7f38a82a2ce88b76a1a5bc3
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-10-10 22:53:38 +0800

    QA

--- config/alpha101_real.py
+++ config/alpha101_real.py
@@ -1,11 +1,18 @@
 from  QUANTAXIS.QAUtil import QA_util_today_str,QA_util_if_trade
-from QUANTTOOLS.QAStockETL.Check import check_stock_alpha101real
-from QUANTTOOLS.QAStockETL.QASU import QA_SU_save_stock_alpha101half_real
+from QUANTTOOLS.QAStockETL.Check import check_stock_alpha101real,check_stock_real
+from QUANTTOOLS.QAStockETL.QASU import QA_SU_save_stock_alpha101half_real,QA_SU_save_stock_real
 
 if __name__ == '__main__':
     mark_day = QA_util_today_str()
 
     if QA_util_if_trade(mark_day):
+
+        res = check_stock_real(mark_day)
+        while res is None or res > 20:
+            QA_SU_save_stock_real()
+            res = check_stock_real(mark_day)
+
+
         res = check_stock_alpha101real(mark_day)
         while res is None or res > 20:
             QA_SU_save_stock_alpha101half_real()
--- config/alpha191_real.py
+++ config/alpha191_real.py
@@ -1,11 +1,17 @@
 from  QUANTAXIS.QAUtil import QA_util_today_str,QA_util_if_trade
-from QUANTTOOLS.QAStockETL.Check import check_stock_alpha191real
-from QUANTTOOLS.QAStockETL.QASU import QA_SU_save_stock_alpha191half_real
+from QUANTTOOLS.QAStockETL.Check import check_stock_alpha191real, check_stock_real
+from QUANTTOOLS.QAStockETL.QASU import QA_SU_save_stock_alpha191half_real,QA_SU_save_stock_real
 
 if __name__ == '__main__':
     mark_day = QA_util_today_str()
 
     if QA_util_if_trade(mark_day):
+
+        res = check_stock_real(mark_day)
+        while res is None or res > 20:
+            QA_SU_save_stock_real()
+            res = check_stock_real(mark_day)
+
         res = check_stock_alpha191real(mark_day)
         while res is None or res > 20:
             QA_SU_save_stock_alpha191half_real()
