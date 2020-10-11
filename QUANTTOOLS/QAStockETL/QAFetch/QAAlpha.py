f1f72fd37c13588c1b5fe0276360e6094cfcd406
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-10-12 02:47:55 +0800

    QA

--- QUANTTOOLS/QAStockETL/QAFetch/QAAlpha.py
+++ QUANTTOOLS/QAStockETL/QAFetch/QAAlpha.py
@@ -1,4 +1,7 @@
-from QUANTTOOLS.QAStockETL.QAFetch.AlphaTools import (stock_alpha, index_alpha, stock_alpha101, index_alpha101,hedge_alpha,
+from QUANTTOOLS.QAStockETL.QAFetch.AlphaTools import (stock_alpha, stock_alpha101,
+                                                      index_alpha, index_alpha101,
+                                                      hedge_alpha,
+                                                      usstock_alpha, usstock_alpha101,
                                                       stock_alpha101_half, stock_alpha101_half_realtime,
                                                       stock_alpha191_half, stock_alpha191_half_realtime)
 from QUANTAXIS.QAUtil import QA_util_date_stamp,QA_util_if_trade,QA_util_log_info,QA_util_get_trade_range,QA_util_today_str,QA_util_get_real_date
@@ -156,3 +159,33 @@ def QA_fetch_get_hedge_alpha(index_code, date, ui_log = None):
     else:
         QA_util_log_info(
             '##JOB Non Data Stock Alpha191 for ============== {}'.format(date), ui_log)
+
+def QA_fetch_get_usstock_alpha(code, date, ui_log = None):
+    if QA_util_if_trade(date) == True:
+        data = stock_alpha(code, date).reset_index()
+        if data is not None:
+            names = list(data.columns)
+            names[0] = 'code'
+            data.columns = names
+            data = data.assign(date_stamp=data['date'].apply(lambda x: QA_util_date_stamp(str(x)[0:10])))
+            return(data)
+        else:
+            QA_util_log_info(
+                '##JOB Non Data UsStock Alpha191 for ============== {}'.format(date), ui_log)
+    else:
+        QA_util_log_info(
+            '##JOB Non Data UsStock Alpha191 for ============== {}'.format(date), ui_log)
+
+def QA_fetch_get_usstock_alpha101(code, start, end, ui_log = None):
+    deal_date_list = QA_util_get_trade_range(start, end)
+    if deal_date_list is not None:
+        data = stock_alpha101(code, start, end)
+        if data is not None:
+            data = data.assign(date_stamp=data['date'].apply(lambda x: QA_util_date_stamp(str(x)[0:10])))
+            return(data)
+        else:
+            QA_util_log_info(
+                '##JOB Non Data UsStock Alpha101 ============== from {_from} to {_to}'.format(_from=start, _to=end), ui_log)
+    else:
+        QA_util_log_info(
+            '##JOB Non Data UsStock Alpha101 ============== from {_from} to {_to}'.format(_from=start, _to=end), ui_log)
--- QUANTTOOLS/QAStockETL/QAFetch/__init__.py
+++ QUANTTOOLS/QAStockETL/QAFetch/__init__.py
@@ -1,37 +1,66 @@
 
-from QUANTTOOLS.QAStockETL.QAFetch.QAQuery_Advance import (QA_fetch_financial_report_adv, QA_fetch_stock_financial_calendar_adv,
-                                                           QA_fetch_stock_divyield_adv,QA_fetch_stock_shares_adv,QA_fetch_index_week_adv,
-                                                           QA_fetch_financial_TTM_adv, QA_fetch_stock_fianacial_adv,QA_fetch_stock_alpha_adv,
-                                                           QA_fetch_financial_report_wy_adv,QA_fetch_stock_technical_index_adv,
-                                                           QA_fetch_stock_financial_percent_adv,QA_fetch_interest_rate_adv,
-                                                           QA_fetch_stock_quant_data_adv,QA_fetch_stock_quant_pre_adv,QA_fetch_stock_target_adv,
-                                                           QA_fetch_index_alpha_adv,QA_fetch_index_technical_index_adv,QA_fetch_index_quant_data_adv,
-                                                           QA_fetch_index_quant_pre_adv,QA_fetch_stock_alpha101_adv,QA_fetch_index_alpha101_adv,
+from QUANTTOOLS.QAStockETL.QAFetch.QAQuery_Advance import (QA_fetch_interest_rate_adv,
+
+                                                           QA_fetch_financial_report_adv, QA_fetch_stock_financial_calendar_adv,
+                                                           QA_fetch_stock_divyield_adv,QA_fetch_stock_shares_adv,
+                                                           QA_fetch_financial_report_wy_adv,
+
+                                                           QA_fetch_financial_TTM_adv,QA_fetch_stock_fianacial_adv,QA_fetch_stock_financial_percent_adv,
                                                            QA_fetch_stock_week_adv,QA_fetch_stock_month_adv,QA_fetch_stock_year_adv,
-                                                           QA_fetch_index_month_adv,QA_fetch_index_year_adv,QA_fetch_stock_alpha101half_adv,
-                                                           QA_fetch_stock_quant_pre_train_adv,QA_fetch_stock_half_adv,
-                                                           QA_fetch_usstock_day_adv,QA_fetch_stock_alpha191half_adv,
+                                                           QA_fetch_stock_half_adv,
+                                                           QA_fetch_stock_alpha191half_adv,QA_fetch_stock_alpha101half_adv,
                                                            QA_fetch_stock_alpha101real_adv,QA_fetch_stock_alpha191real_adv,
+                                                           QA_fetch_stock_alpha_adv,QA_fetch_stock_alpha101_adv,QA_fetch_stock_technical_index_adv,
+
+                                                           QA_fetch_stock_quant_data_adv,QA_fetch_stock_quant_pre_adv,QA_fetch_stock_quant_pre_train_adv,
+                                                           QA_fetch_stock_target_adv,
+
+                                                           QA_fetch_index_week_adv,QA_fetch_index_month_adv,QA_fetch_index_year_adv,
+
+                                                           QA_fetch_index_alpha_adv,QA_fetch_index_alpha101_adv,QA_fetch_index_technical_index_adv,
+
+                                                           QA_fetch_index_quant_data_adv,QA_fetch_index_quant_pre_adv,
+
+                                                           QA_fetch_usstock_day_adv,
                                                            QA_fetch_usstock_xq_day_adv)
 
-from QUANTTOOLS.QAStockETL.QAFetch.QAQuery import (QA_fetch_financial_report, QA_fetch_stock_financial_calendar, QA_fetch_stock_divyield,
-                                                   QA_fetch_financial_TTM, QA_fetch_stock_fianacial,QA_fetch_stock_alpha,QA_fetch_stock_shares,
-                                                   QA_fetch_financial_report_wy,QA_fetch_stock_technical_index,QA_fetch_stock_financial_percent,
-                                                   QA_fetch_stock_quant_data,QA_fetch_stock_quant_pre,QA_fetch_stock_target,
-                                                   QA_fetch_stock_industry,QA_fetch_stock_name,QA_fetch_index_name,QA_fetch_index_cate,
-                                                   QA_fetch_financial_code_wy, QA_fetch_financial_code_tdx,QA_fetch_financial_code_new,
-                                                   QA_fetch_financial_code_ttm,QA_fetch_stock_half,
-                                                   QA_fetch_interest_rate,QA_fetch_index_alpha,QA_fetch_index_technical_index,
-                                                   QA_fetch_index_target,QA_fetch_index_quant_data,QA_fetch_index_quant_data,
-                                                   QA_fetch_index_quant_pre,QA_fetch_stock_alpha101,QA_fetch_index_alpha101,
-                                                   QA_fetch_index_info,QA_fetch_stock_industryinfo,QA_fetch_stock_delist,
-                                                   QA_fetch_stock_week,QA_fetch_stock_month,QA_fetch_stock_year,
-                                                   QA_fetch_index_week,QA_fetch_index_month,QA_fetch_index_year,
-                                                   QA_fetch_stock_om_all,QA_fetch_stock_all,QA_fetch_stock_alpha101half,
-                                                   QA_fetch_stock_quant_pre_train,QA_fetch_stock_alpha191half,
-                                                   QA_fetch_usstock_day,QA_fetch_usstock_pb,QA_fetch_usstock_pe,QA_fetch_usstock_adj,
+from QUANTTOOLS.QAStockETL.QAFetch.QAQuery import (QA_fetch_interest_rate,
+
+                                                   QA_fetch_stock_real,
                                                    QA_fetch_stock_alpha101_real,QA_fetch_stock_alpha_real,
-                                                   QA_fetch_stock_real,QA_fetch_usstock_xq_day)
+
+                                                   QA_fetch_financial_report, QA_fetch_financial_report_wy, QA_fetch_stock_financial_calendar,
+                                                   QA_fetch_stock_shares,QA_fetch_stock_divyield,
+
+                                                   QA_fetch_financial_TTM, QA_fetch_stock_fianacial,QA_fetch_stock_financial_percent,
+                                                   QA_fetch_stock_industry,QA_fetch_stock_name,
+
+                                                   QA_fetch_stock_half,QA_fetch_stock_week,QA_fetch_stock_month,QA_fetch_stock_year,
+
+                                                   QA_fetch_financial_code_wy, QA_fetch_financial_code_tdx,
+                                                   QA_fetch_financial_code_new,QA_fetch_financial_code_ttm,
+                                                   QA_fetch_stock_om_all,QA_fetch_stock_all,QA_fetch_stock_delist,
+
+                                                   QA_fetch_stock_alpha,QA_fetch_stock_alpha101,QA_fetch_stock_technical_index,
+                                                   QA_fetch_stock_alpha191half, QA_fetch_stock_alpha101half,
+
+                                                   QA_fetch_stock_quant_data,QA_fetch_stock_quant_pre,QA_fetch_stock_quant_pre_train,
+                                                   QA_fetch_stock_target,
+
+                                                   QA_fetch_index_name,QA_fetch_index_cate,QA_fetch_index_info,QA_fetch_stock_industryinfo,
+
+                                                   QA_fetch_index_week,QA_fetch_index_month,QA_fetch_index_year,
+
+                                                   QA_fetch_index_alpha,QA_fetch_index_alpha101,QA_fetch_index_technical_index,
+
+                                                   QA_fetch_index_quant_data,QA_fetch_index_quant_pre,
+                                                   QA_fetch_index_target,
+
+                                                   QA_fetch_usstock_list,QA_fetch_usstock_adj,
+                                                   QA_fetch_usstock_day,QA_fetch_usstock_xq_day,
+
+                                                   QA_fetch_usstock_pb,QA_fetch_usstock_pe
+                                                   )
 
 from QUANTTOOLS.QAStockETL.QAFetch.QAcalendar import QA_fetch_get_financial_calendar
 
@@ -39,13 +68,16 @@ from QUANTTOOLS.QAStockETL.QAFetch.QAdivyield import QA_fetch_get_stock_divyield
 
 from QUANTTOOLS.QAStockETL.QAFetch.QAFinancial import QA_fetch_get_stock_report_ths, QA_fetch_get_stock_report_sina, QA_fetch_get_stock_report_wy
 
-from QUANTTOOLS.QAStockETL.QAFetch.QAAlpha import (QA_fetch_get_stock_alpha,QA_fetch_get_index_alpha,
-                                                   QA_fetch_get_stock_alpha101,QA_fetch_get_index_alpha101,
-                                                   QA_fetch_get_stock_alpha101_half,
-                                                   QA_fetch_get_stock_alpha101half_realtime,
-                                                   QA_fetch_get_stock_alpha191_half,
-                                                   QA_fetch_get_stock_alpha191half_realtime,
-                                                   QA_fetch_get_hedge_alpha)
+from QUANTTOOLS.QAStockETL.QAFetch.QAAlpha import (QA_fetch_get_stock_alpha,QA_fetch_get_stock_alpha101,
+                                                   QA_fetch_get_stock_alpha191_half,QA_fetch_get_stock_alpha101_half,
+                                                   QA_fetch_get_stock_alpha191half_realtime,QA_fetch_get_stock_alpha101half_realtime,
+
+                                                   QA_fetch_get_hedge_alpha,
+
+                                                   QA_fetch_get_index_alpha,QA_fetch_get_index_alpha101,
+
+                                                   QA_fetch_get_usstock_alpha,QA_fetch_get_usstock_alpha101
+                                                   )
 
 from QUANTTOOLS.QAStockETL.QAFetch.QAShares import QA_fetch_get_stock_shares_sina
 
@@ -66,7 +98,11 @@ from QUANTTOOLS.QAStockETL.QAFetch.QATdx import (QA_fetch_get_stock_close,
                                                  QA_fetch_get_stock_realtm_ask,QA_fetch_get_stock_realtm_askvol,
                                                  QA_fetch_get_stock_realtm_bid,QA_fetch_get_stock_realtm_bidvol,
                                                  QA_fetch_get_stock_realtm_askvol5,QA_fetch_get_stock_realtm_bidvol5,
+
                                                  QA_fetch_get_stock_industryinfo,QA_fetch_get_index_info,
-                                                 QA_fetch_get_stock_delist,QA_fetch_get_stock_half,
-                                                 QA_fetch_get_stock_half_realtime,
+
+                                                 QA_fetch_get_stock_delist,
+
+                                                 QA_fetch_get_stock_half,QA_fetch_get_stock_half_realtime,
+
                                                  QA_fetch_get_usstock_pe,QA_fetch_get_usstock_pb)
