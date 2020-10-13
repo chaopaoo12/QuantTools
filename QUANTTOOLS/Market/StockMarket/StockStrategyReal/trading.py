50d68272de25e0793dae737ae09d1338ac913b4e
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-10-14 04:21:45 +0800

    QA

--- QUANTTOOLS/Market/StockMarket/StockStrategyReal/trading.py
+++ QUANTTOOLS/Market/StockMarket/StockStrategyReal/trading.py
@@ -4,13 +4,16 @@ from .concat_predict import concat_predict,concat_predict_real,concat_predict_he
 from QUANTTOOLS.Market.MarketTools import trading_base
 
 def trading(trading_date, func = concat_predict, model_name = 'stock_xg', file_name = 'prediction', percent = percent, account= 'name:client-1', working_dir = working_dir, exceptions = exceptions):
-    trading_base(trading_date, func, model_name, file_name, percent = percent, account= account, working_dir = working_dir, exceptions = exceptions)
+    res = trading_base(trading_date, func, model_name, file_name, percent = percent, account= account, working_dir = working_dir, exceptions = exceptions)
+    return(res)
 
 def trading_real(trading_date, func = concat_predict_real, model_name = 'stock_xg_real', file_name = 'prediction_real', percent = percent, account= 'name:client-1', working_dir = working_dir, exceptions = exceptions):
-    trading_base(trading_date, func, model_name, file_name, percent = percent, account= account, working_dir = working_dir, exceptions = exceptions)
+    res = trading_base(trading_date, func, model_name, file_name, percent = percent, account= account, working_dir = working_dir, exceptions = exceptions)
+    return(res)
 
 def trading_hedge(trading_date, func = concat_predict_hedge, model_name = 'hedge_xg', file_name = 'prediction_hedge', percent = percent, account= 'name:client-1', working_dir = working_dir, exceptions = exceptions):
-    trading_base(trading_date, func, model_name, file_name, percent = percent, account= account, working_dir = working_dir, exceptions = exceptions)
+    res = trading_base(trading_date, func, model_name, file_name, percent = percent, account= account, working_dir = working_dir, exceptions = exceptions)
+    return(res)
 
 if __name__ == '__main__':
     pass
