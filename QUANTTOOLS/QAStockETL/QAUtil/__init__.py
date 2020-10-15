008b3944ab9ccb02b42930ffdf6fddc0cfc9f1aa
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-10-15 23:39:06 +0800

    QA

--- QUANTTOOLS/QAStockETL/QAUtil/__init__.py
+++ QUANTTOOLS/QAStockETL/QAUtil/__init__.py
@@ -5,7 +5,7 @@ from QUANTTOOLS.QAStockETL.QAUtil.QAEtlQuant import QA_util_etl_stock_quant
 from QUANTTOOLS.QAStockETL.QAUtil.QAEtlFinancial import QA_util_etl_financial_TTM,QA_util_process_stock_financial
 
 #SQL
-from QUANTTOOLS.QAStockETL.QAUtil.QASql import (QA_util_sql_store_mysql,ASCENDING ,DESCENDING ,QA_util_sql_mongo_sort_ASCENDING ,QA_util_sql_mongo_sort_DESCENDING)
+from QUANTTOOLS.QAStockETL.QAUtil.QASql import (QA_util_sql_store_mysql)
 
 from QUANTTOOLS.QAStockETL.QAUtil.QADate import QA_util_add_days, QA_util_add_years, QA_util_getBetweenYear, QA_util_get_days_to_today
 
