e47966ef8b15d3ba652ce204d2e557a2f4c69179
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-10-16 00:10:17 +0800

    QA

--- config/update_c.py
+++ config/update_c.py
@@ -34,12 +34,14 @@ from QUANTTOOLS.QAStockETL import (QA_etl_stock_list, QA_etl_stock_info, QA_etl_
                                    QA_SU_save_stock_xdxr, QA_SU_save_stock_info,QA_SU_save_stock_financial_wy_day,
                                    QA_SU_save_stock_fianacial_percent_day, QA_util_process_stock_financial,
                                    QA_SU_save_stock_fianacial_momgo, QA_SU_save_fianacialTTM_momgo,
-                                   QA_SU_save_stock_industryinfo, QA_SU_save_stock_day)
+                                   QA_SU_save_stock_industryinfo, QA_SU_save_stock_day,
+                                   QA_SU_save_single_stock_xdxr)
 from QUANTTOOLS.QAStockETL import (QA_etl_stock_financial_day,
                                    QA_etl_stock_financial_percent_day)
 from QUANTAXIS.QASU.main import (QA_SU_save_financialfiles_fromtdx)
 from QUANTTOOLS.QAStockETL.Check import (check_stock_day, check_stock_fianacial, check_stock_adj, check_stock_finper,
-                                         check_sinastock_day, check_wy_financial, check_tdx_financial, check_ttm_financial)
+                                         check_sinastock_day, check_sinastock_adj,
+                                         check_wy_financial, check_tdx_financial, check_ttm_financial)
 from  QUANTAXIS.QAUtil import QA_util_today_str,QA_util_if_trade
 
 if __name__ == '__main__':
@@ -67,6 +69,12 @@ if __name__ == '__main__':
         while res is None or (len(res[0]) + len(res[1])) > 20:
             QA_SU_save_stock_xdxr()
             res = check_stock_adj(mark_day)
+
+        res = check_sinastock_adj(mark_day)
+        while res is None or (len(res[0]) + len(res[1])) > 0:
+            for i in res[0] + res[1]:
+                QA_SU_save_single_stock_xdxr(i)
+            res = check_sinastock_adj(mark_day)
 
         print("done")
         print("write data into sqldatabase")
--- config/update_min.py
+++ config/update_min.py
@@ -1,8 +1,14 @@
 from QUANTAXIS.QAUtil import QA_util_today_str,QA_util_if_trade
-from QUANTAXIS.QASU.save_tdx import  QA_SU_save_single_stock_min
-from QUANTTOOLS.QAStockETL.Check import check_stock_60min, check_sinastock_60min, check_sinastock_half, check_stock_alpha101half,check_stock_half,check_stock_alpha191half
-from QUANTTOOLS.QAStockETL.QASU import QA_SU_save_stock_half,QA_SU_save_stock_min
-from QUANTTOOLS.QAStockETL import (QA_SU_save_stock_alpha101half_day,QA_SU_save_stock_alpha191half_day, QA_etl_stock_alpha101half_day,QA_etl_stock_alpha191half_day)
+from QUANTTOOLS.QAStockETL.Check import (check_stock_60min, check_sinastock_60min,
+                                         check_stock_half, check_sinastock_half,
+                                         check_stock_adj,check_sinastock_adj,
+                                         check_stock_alpha101half,
+                                         check_stock_alpha191half)
+from QUANTTOOLS.QAStockETL.QASU import (QA_SU_save_stock_min,QA_SU_save_single_stock_min,
+                                        QA_SU_save_stock_half,
+                                        QA_SU_save_stock_xdxr,QA_SU_save_single_stock_xdxr)
+from QUANTTOOLS.QAStockETL import (QA_SU_save_stock_alpha101half_day,QA_SU_save_stock_alpha191half_day,
+                                   QA_etl_stock_alpha101half_day,QA_etl_stock_alpha191half_day)
 
 if __name__ == '__main__':
     mark_day = QA_util_today_str()
@@ -14,10 +20,32 @@ if __name__ == '__main__':
             QA_SU_save_stock_min()
             res = check_stock_60min(mark_day)
 
+        res = check_sinastock_60min(mark_day)
+        while res is None or (len(res[0]) + len(res[1])) > 0:
+            for i in res[0] + res[1]:
+                QA_SU_save_single_stock_min(i)
+            res = check_sinastock_60min(mark_day)
+
         res = check_stock_half(mark_day)
         while res is None or (len(res[0]) + len(res[1])) > 20:
             QA_SU_save_stock_half()
             res = check_stock_half(mark_day)
+
+        res = check_sinastock_half(mark_day)
+        while res is None or (len(res[0]) + len(res[1])) > 0:
+            QA_SU_save_stock_half()
+            res = check_sinastock_half(mark_day)
+
+        res = check_stock_adj(mark_day)
+        while res is None or (len(res[0]) + len(res[1])) > 20:
+            QA_SU_save_stock_xdxr()
+            res = check_stock_adj(mark_day)
+
+        res = check_sinastock_adj(mark_day)
+        while res is None or (len(res[0]) + len(res[1])) > 0:
+            for i in res[0] + res[1]:
+                QA_SU_save_single_stock_xdxr(i)
+            res = check_sinastock_adj(mark_day)
 
         res = check_stock_alpha101half(mark_day)
         while res is None or (len(res[0]) + len(res[1])) > 20:
