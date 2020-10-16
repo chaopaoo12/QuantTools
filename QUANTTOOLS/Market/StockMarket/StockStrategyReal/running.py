7498822c8bb1c8cb7433b6a39284302d1e6e2cb6
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-10-16 14:21:42 +0800

    QA

--- QUANTTOOLS/Market/StockMarket/StockStrategyReal/running.py
+++ QUANTTOOLS/Market/StockMarket/StockStrategyReal/running.py
@@ -10,7 +10,7 @@ def predict_real(trading_date, top_num=top, working_dir=working_dir, exceptions=
     predict_base(trading_date, concat_predict_real, 'stock_xg_real', 'prediction_real', top_num=top_num, percent=percent, working_dir=working_dir, exceptions=exceptions)
 
 def predict_crawl(trading_date, top_num=top, working_dir=working_dir, exceptions=exceptions):
-    predict_base(trading_date, concat_predict_crawl, 'stock_xg_real', 'prediction_real', top_num=top_num, percent=percent, working_dir=working_dir, exceptions=exceptions)
+    predict_base(trading_date, concat_predict_crawl, 'stock_xg_real', 'prediction_crawl', top_num=top_num, percent=percent, working_dir=working_dir, exceptions=exceptions)
 
 def predict_hedge(trading_date, top_num=top, working_dir=working_dir, exceptions=exceptions):
     predict_base(trading_date, concat_predict_hedge, 'hedge_xg', 'prediction_hedge', top_num=top_num, percent=percent, working_dir=working_dir, exceptions=exceptions)
