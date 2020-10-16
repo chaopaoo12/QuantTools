a85d74d1422a55a303689b8221f304b090f7da9f
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-10-17 01:54:31 +0800

    QA

--- QUANTTOOLS/Market/StockMarket/StockStrategyReal/train.py
+++ QUANTTOOLS/Market/StockMarket/StockStrategyReal/train.py
@@ -8,7 +8,7 @@ from QUANTTOOLS.Market.MarketTools.train_tools import prepare_train, start_train
 
 def train(date, working_dir=working_dir):
     stock_model = QAStockXGBoostReal()
-    stock_model = prepare_train(stock_model, date, col = 'TARGET5',k = 3, start = "-01-01", shift=5)
+    stock_model = prepare_train(stock_model, date, col = 'TARGET5',k = 0, start = "-10-01", shift=5)
 
     other_params = {'learning_rate': 0.1, 'n_estimators': 200, 'max_depth': 5, 'min_child_weight': 1, 'seed': 1,
                     'subsample': 0.8, 'colsample_bytree': 0.8, 'gamma': 0, 'reg_alpha': 0, 'reg_lambda': 1}
