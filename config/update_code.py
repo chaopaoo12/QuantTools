0f508e4b7ddbbd260d052387c9ef383f7956b9af
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-10-16 16:44:30 +0800

    QA

---
+++ config/update_code.py
@@ -0,0 +1,15 @@
+from  QUANTAXIS.QAUtil import QA_util_today_str,QA_util_if_trade
+from QUANTTOOLS.QAStockETL.Check import check_stock_code
+from QUANTTOOLS.QAStockETL.QASU import QA_SU_save_stock_aklist
+from QUANTAXIS.QASU.main import (QA_SU_save_stock_list,QA_SU_save_stock_info_tushare)
+
+if __name__ == '__main__':
+    mark_day = QA_util_today_str()
+
+    if QA_util_if_trade(mark_day):
+
+        QA_SU_save_stock_list('tdx')
+        QA_SU_save_stock_info_tushare()
+        QA_SU_save_stock_aklist()
+        #QA_SU_save_stock_industryinfo()
+        res = check_stock_code()
