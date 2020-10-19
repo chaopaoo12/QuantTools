4a1145223200413ec72630b3cd233412b4ccad0c
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-10-19 11:22:37 +0800

    QA

--- config/update_quant.py
+++ config/update_quant.py
@@ -64,7 +64,7 @@ if __name__ == '__main__':
             check = check_stock_alpha191real(mark_day)
 
         check = check_stock_alpha101real(mark_day)
-        while check is None or (len(check[0]) + len(check[1])) > 20:
+        while check is None or (len(check[0]) + len(check[1])) > 3000:
             time.sleep(180)
             check = check_stock_alpha101real(mark_day)
 
--- config/update_trading.py
+++ config/update_trading.py
@@ -29,7 +29,7 @@
 """
 
 from QUANTTOOLS.QAStockETL.Check import (check_stock_quant,check_stock_code)
-from QUANTAXIS.QAUtil import QA_util_today_str,QA_util_if_trade,QA_util_get_pre_trade_date
+from QUANTAXIS.QAUtil import QA_util_today_str,QA_util_if_trade,QA_util_get_pre_trade_date,QA_util_get_real_date
 from QUANTTOOLS.Market.StockMarket.StockStrategyReal.daily_job import daily_run, daily_run_hedge, daily_run_crawl
 from QUANTAXIS.QASU.main import (QA_SU_save_stock_list,QA_SU_save_stock_info_tushare)
 from QUANTTOOLS.QAStockETL import QA_SU_save_stock_aklist
@@ -41,18 +41,22 @@ if __name__ == '__main__':
 
     if QA_util_if_trade(mark_day):
         check_day = QA_util_get_pre_trade_date(mark_day,1)
-        QA_SU_save_stock_aklist()
+
+    else:
+        check_day = QA_util_get_real_date(mark_day)
+
+    QA_SU_save_stock_aklist()
+    res = check_stock_code()
+    while len(res) > 0:
+        QA_SU_save_stock_list('tdx')
+        QA_SU_save_stock_info_tushare()
+        #QA_SU_save_stock_industryinfo()
         res = check_stock_code()
-        while len(res) > 0:
-            QA_SU_save_stock_list('tdx')
-            QA_SU_save_stock_info_tushare()
-            #QA_SU_save_stock_industryinfo()
-            res = check_stock_code()
 
-        res = check_stock_quant(mark_day)
-        while res is None or (len(res[0]) + len(res[1])) > 10:
-            time.sleep(180)
+    res = check_stock_quant(mark_day)
+    while res is None or (len(res[0]) + len(res[1])) > 10:
+        time.sleep(180)
 
-        daily_run(mark_day)
-        daily_run_crawl(check_day)
-        #daily_run_hedge(check_day)
+    daily_run(mark_day)
+    daily_run_crawl(check_day)
+    #daily_run_hedge(check_day)
