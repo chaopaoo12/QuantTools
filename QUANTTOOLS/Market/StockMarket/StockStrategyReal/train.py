d9b1e86db061deeb778fef636fb3874bf9d25fa3
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-10-22 17:54:58 +0800

    QA

--- QUANTTOOLS/Market/StockMarket/StockStrategyReal/train.py
+++ QUANTTOOLS/Market/StockMarket/StockStrategyReal/train.py
@@ -4,11 +4,16 @@ from QUANTTOOLS.Model.StockModel.StrategyXgboostReal import QAStockXGBoostReal
 from QUANTTOOLS.Model.StockModel.StrategyXgboostHedge import QAStockModelHedgeReal
 from QUANTTOOLS.Model.IndexModel.IndexXGboost import QAIndexXGBoost
 from .setting import working_dir, data_set, datareal_set
-from QUANTTOOLS.Market.MarketTools.train_tools import prepare_train, start_train, save_report
+from QUANTTOOLS.Market.MarketTools.train_tools import prepare_train, start_train, save_report, load_data, prepare_data
 
 def train(date, working_dir=working_dir):
     stock_model = QAStockXGBoostReal()
-    stock_model = prepare_train(stock_model, date, col = 'TARGET5',k = 3, start = "-01-01", shift=5)
+
+    stock_model = load_data(stock_model, date, k = 3, start = "-01-01", shift=5)
+
+    stock_model = prepare_data(stock_model, date, col = 'TARGET5', k = 3, start = "-01-01", shift=5)
+
+    #stock_model = prepare_train(stock_model, date, col = 'TARGET5', k = 3, start = "-01-01", shift=5)
 
     other_params = {'learning_rate': 0.1, 'n_estimators': 200, 'max_depth': 5, 'min_child_weight': 1, 'seed': 1,
                     'subsample': 0.8, 'colsample_bytree': 0.8, 'gamma': 0, 'reg_alpha': 0, 'reg_lambda': 1}
