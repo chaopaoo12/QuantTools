b0f4bf83021951d74b1b9c1d17330034158b040c
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-10-23 15:03:36 +0800

    QA

--- QUANTTOOLS/Trader/account_manage/BuildTradingFrame.py
+++ QUANTTOOLS/Trader/account_manage/BuildTradingFrame.py
@@ -138,6 +138,7 @@ def balance(res, k = 100):
         ###调增
         #调整范围确认 行动为多买
         for i in range(len(list(res[res.sort_gp == 0].index)), 0, -1):
+            print(i)
             if res[(res.sort_gp == 0) & (res.price_rank <= i)]['买卖价'].apply(lambda x :x*100).sum() <= (res['测算持股金额'].sum() - res['target'].sum()):
                 trim_code = list(res[(res.sort_gp == 0) & (res.price_rank <= i)].index)
                 res.loc[trim_code,'trim'] = res.loc[trim_code,'trim'] + k
