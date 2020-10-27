2de851a8248adf40d271220150788b916d759fb5
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-10-27 18:51:54 +0800

    QA

--- QUANTTOOLS/Market/MarketTools/predict_tools/concat_predict.py
+++ QUANTTOOLS/Market/MarketTools/predict_tools/concat_predict.py
@@ -25,9 +25,9 @@ def make_prediction(Model, trading_date, name, working_dir, type='model', ui_log
                           volume=None
                           )
     if datetime.strptime(trading_date, "%Y-%m-%d").weekday() >= 4:
-        start = (datetime.strptime(trading_date, "%Y-%m-%d") + relativedelta(weekday=FR(-2))).strftime('%Y-%m-%d')
+        start = (datetime.strptime(trading_date, "%Y-%m-%d") + relativedelta(weekday=FR(-1))).strftime('%Y-%m-%d')
     else:
-        start = (datetime.strptime(trading_date, "%Y-%m-%d") - timedelta(days=datetime.strptime(trading_date, "%Y-%m-%d").weekday())).strftime('%Y-%m-%d')
+        start = (datetime.strptime(trading_date, "%Y-%m-%d") + relativedelta(weekday=FR(-2))).strftime('%Y-%m-%d')
     end = trading_date
     rng = pd.Series(pd.date_range(start, end, freq='D')).apply(lambda x: str(x)[0:10])
     QA_util_log_info('##JOB Now Model Predict from {start} to {end} ==== {s}'.format(start = start, end = end, s = str(trading_date)), ui_log)
