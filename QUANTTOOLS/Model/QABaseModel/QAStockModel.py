0030385756bd839eca58baa4d2f1e991da07e19b
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-10-30 01:59:49 +0800

    QA

--- QUANTTOOLS/Model/QABaseModel/QAStockModel.py
+++ QUANTTOOLS/Model/QABaseModel/QAStockModel.py
@@ -68,6 +68,7 @@ class QAStockModel(QAModel):
         train.index = data.index
         QA_util_log_info('##JOB Now Got Different Columns ===== from {_from} to {_to}'.format(_from=start,_to = end), ui_log = None)
         QA_util_log_info(n_cols)
+        QA_util_log_info(train.shape)
 
         if self.thresh is None:
             train = train[self.cols]
@@ -101,6 +102,7 @@ class QAStockModel(QAModel):
                               volume=None
                               )
 
+        QA_util_log_info(train.shape)
         train = train.join(data[['INDUSTRY','PASS_MARK','TARGET','TARGET3','TARGET4','TARGET5','TARGET10','AVG_TARGET','INDEX_TARGET','INDEX_TARGET3','INDEX_TARGET4','INDEX_TARGET5','INDEX_TARGET10']])
 
         QA_util_log_info('##JOB Now Got Prediction Result ===== from {_from} to {_to}'.format(_from=start,_to = end), ui_log = None)
