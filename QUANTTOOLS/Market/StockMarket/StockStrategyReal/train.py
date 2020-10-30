a4d682b838e0dec0b4ac7574c6c29fa49b837fe5
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-10-31 05:48:09 +0800

    QA

--- QUANTTOOLS/Market/StockMarket/StockStrategyReal/train.py
+++ QUANTTOOLS/Market/StockMarket/StockStrategyReal/train.py
@@ -9,7 +9,7 @@ from QUANTTOOLS.Market.MarketTools.train_tools import prepare_train, start_train
 def train(date, working_dir=working_dir):
     stock_model = QAStockXGBoostReal()
 
-    stock_model = load_data(stock_model, date, k = 3, start = "-01-01", shift=5)
+    stock_model = load_data(stock_model, date, k = 3, start = "-01-01", shift=5, norm_type=None)
 
     stock_model = prepare_data(stock_model, date, col = 'TARGET5', k = 3, start = "-01-01", shift=5)
 
@@ -24,24 +24,28 @@ def train(date, working_dir=working_dir):
     stock_model = start_train(stock_model, datareal_set,  other_params, 0, 0.99)
     save_report(stock_model, 'stock_xg_real', working_dir)
 
-    stock_model = QAStockModelHedgeReal()
-    stock_model = prepare_train(stock_model, date, col = 'TARGET5',k = 3, start = "-01-01", shift=5)
+    hedge_model = QAStockModelHedgeReal()
+    hedge_model = load_data(hedge_model, date, k = 3, start = "-01-01", shift=5, norm_type=None)
+
+    hedge_model = prepare_data(hedge_model, date, col = 'TARGET5', k = 3, start = "-01-01", shift=5)
 
     other_params = {'learning_rate': 0.1, 'n_estimators': 200, 'max_depth': 5, 'min_child_weight': 1, 'seed': 1,
                     'subsample': 0.8, 'colsample_bytree': 0.8, 'gamma': 0, 'reg_alpha': 0, 'reg_lambda': 1}
 
-    stock_model = start_train(stock_model, datareal_set, other_params, 0, 0.99)
-    save_report(stock_model, 'hedge_xg', working_dir)
+    hedge_model = start_train(hedge_model, datareal_set, other_params, 0, 0.99)
+    save_report(hedge_model, 'hedge_xg', working_dir)
 
 def train_hedge(date, working_dir=working_dir):
-    stock_model = QAStockModelHedgeReal()
-    stock_model = prepare_train(stock_model, date, col = 'TARGET',k = 3, start = "-01-01", shift=5)
+    hedge_model = QAStockModelHedgeReal()
+    hedge_model = load_data(hedge_model, date, k = 3, start = "-01-01", shift=5, norm_type=None)
+
+    hedge_model = prepare_data(hedge_model, date, col = 'TARGET5', k = 3, start = "-01-01", shift=5)
 
     other_params = {'learning_rate': 0.1, 'n_estimators': 200, 'max_depth': 5, 'min_child_weight': 1, 'seed': 1,
                     'subsample': 0.8, 'colsample_bytree': 0.8, 'gamma': 0, 'reg_alpha': 0, 'reg_lambda': 1}
 
-    stock_model = start_train(stock_model, None, other_params, 0, 0.99)
-    save_report(stock_model, 'hedge_xg', working_dir)
+    hedge_model = start_train(hedge_model, datareal_set, other_params, 0, 0.99)
+    save_report(hedge_model, 'hedge_xg', working_dir)
 
 def train_index(date, working_dir=working_dir):
     index_model = QAIndexXGBoost()
