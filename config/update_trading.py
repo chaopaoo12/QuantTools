cf297f7c842a8506bcab9ee0cd8e34c2fe3d45b8
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-11-03 02:23:42 +0800

    QA

--- config/update_trading.py
+++ config/update_trading.py
@@ -28,7 +28,10 @@
 """对应于save x
 """
 
-from QUANTTOOLS.QAStockETL.Check import (check_stock_quant,check_stock_code,check_stock_alpha191half)
+from QUANTTOOLS.QAStockETL.Check import (check_stock_quant,check_stock_code,
+                                         check_stock_finper,check_stock_alpha191,
+                                         check_stock_techweek,
+                                         check_stock_alpha191half,check_stock_alpha101half)
 from QUANTAXIS.QAUtil import QA_util_today_str,QA_util_if_trade,QA_util_get_pre_trade_date,QA_util_get_real_date
 from QUANTTOOLS.Market.StockMarket.StockStrategyReal.daily_job import daily_run, daily_run_hedge, daily_run_crawl
 from QUANTAXIS.QASU.main import (QA_SU_save_stock_list,QA_SU_save_stock_info_tushare)
@@ -58,12 +61,32 @@ if __name__ == '__main__':
         time.sleep(180)
         res = check_stock_quant(mark_day)
 
+    res = check_stock_techweek(mark_day)
+    while res is None or (len(res[0]) + len(res[1])) > 20:
+        time.sleep(180)
+        res = check_stock_techweek(mark_day)
+
+    res = check_stock_alpha191(mark_day)
+    while res is None or (len(res[0]) + len(res[1])) > 20:
+        time.sleep(180)
+        res = check_stock_alpha191(mark_day)
+
+    res = check_stock_finper(mark_day)
+    while res is None or (len(res[0]) + len(res[1])) > 20:
+        time.sleep(180)
+        res = check_stock_finper(mark_day)
+
     daily_run(mark_day)
 
     res = check_stock_alpha191half(mark_day)
     while res is None or (len(res[0]) + len(res[1])) > 20:
         time.sleep(180)
         res = check_stock_alpha191half(mark_day)
+
+    res = check_stock_alpha101half(mark_day)
+    while res is None or (len(res[0]) + len(res[1])) > 20:
+        time.sleep(180)
+        res = check_stock_alpha101half(mark_day)
 
     daily_run_crawl(check_day)
     daily_run_hedge(check_day)
