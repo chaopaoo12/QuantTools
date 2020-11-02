46457aa0586200d81c934b8bd78c2cfcd238c438
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-11-02 15:02:59 +0800

    QA

--- QUANTTOOLS/Market/StockMarket/StockStrategyReal/trading.py
+++ QUANTTOOLS/Market/StockMarket/StockStrategyReal/trading.py
@@ -37,9 +37,7 @@ def trading_summary(trading_date,percent = percent, account= 'name:client-1',exc
     buy_code = []
     for i in list(r_tar.index):
         if QA_fetch_get_stock_realtm_ask(i) > 0:
-            print(QA_fetch_get_stock_realtm_ask(i))
             buy_code.append(i)
-            print(buy_code)
 
     #if buy_code is None or len(buy_code) == 0:
     #    r_tar, prediction_tar = load_data(concat_predict, trading_date, working_dir, 'stock_xg_real', 'prediction_real')
