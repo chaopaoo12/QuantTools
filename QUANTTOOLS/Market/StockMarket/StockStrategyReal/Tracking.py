b6d95eb405558d8ee9432613e2cc99bd6881da1b
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-11-07 00:42:09 +0800

    QA

--- QUANTTOOLS/Market/StockMarket/StockStrategyReal/Tracking.py
+++ QUANTTOOLS/Market/StockMarket/StockStrategyReal/Tracking.py
@@ -5,7 +5,7 @@ from QUANTTOOLS.Market.MarketTools import tracking_morning, tracking_afternon, t
 
 def tracker1(trading_date, func = concat_predict, model_name = 'stock_xg', file_name = 'prediction', percent = percent, account = 'name:client-1', working_dir = working_dir, exceptions = exceptions):
     start_time = '09:25:00'
-    end_time = '11:30:00'
+    end_time = '15:00:00'
     tracking_base(trading_date, func, model_name, file_name, start_time, end_time, percent, account, working_dir, exceptions)
 
 def tracker2(trading_date, func = concat_predict_real, model_name = 'stock_xg_real', file_name = 'prediction_real', percent = percent, account = 'name:client-1', working_dir = working_dir, exceptions = exceptions):
@@ -15,7 +15,7 @@ def tracker2(trading_date, func = concat_predict_real, model_name = 'stock_xg_re
 
 def Tracking(trading_date):
     tracker1(trading_date)
-    tracker2(trading_date)
+    #tracker2(trading_date)
 
 
 if __name__ == '__main__':
