e69592c2092af5f3554f88eee4aeb1d0752fa759
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-11-08 04:33:00 +0800

    QA

--- QUANTTOOLS/Market/MarketTools/predict_tools/runnig_base.py
+++ QUANTTOOLS/Market/MarketTools/predict_tools/runnig_base.py
@@ -1,6 +1,7 @@
 from QUANTTOOLS.Market.MarketTools.predict_tools import save_prediction, prediction_report
+from QUANTAXIS.QAUtil import QA_util_today_str,QA_util_get_real_date
 
 def predict_base(trading_date, predict_func, model_name, file_name, top_num, percent, working_dir, exceptions):
     stock_tar, stock_b, start, end, model_date = predict_func(trading_date, model_name = model_name,  working_dir=working_dir)
     save_prediction({'date': trading_date, 'target_pool':stock_tar, 'prediction':stock_b}, file_name, working_dir)
-    prediction_report(trading_date, stock_tar, stock_b, model_date, top_num, exceptions, percent, account='name:client-1', ui_log = None)
+    prediction_report(QA_util_get_real_date(trading_date), stock_tar, stock_b, model_date, top_num, exceptions, percent, account='name:client-1', ui_log = None)
