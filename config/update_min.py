bebd4a5c07d3b3e8928ea9414c08a4a6f972dc57
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-11-27 10:43:42 +0800

    QA

--- config/update_min.py
+++ config/update_min.py
@@ -51,23 +51,3 @@ if __name__ == '__main__':
         while res is None or (len(res[0]) + len(res[1])) > 0:
             QA_SU_save_stock_half()
             res = check_sinastock_half(mark_day)
-
-        res = check_stock_alpha101half(mark_day)
-        while res is None or (len(res[0]) + len(res[1])) > 20:
-            QA_SU_save_stock_alpha101half_day(start_date = mark_day,  end_date = mark_day)
-            res = check_stock_alpha101half(mark_day)
-
-        res = check_sinastock_alpha101half(mark_day)
-        if res is None or (len(res[0]) + len(res[1])) > 0:
-            for i in res[0] + res[1]:
-                QA_SU_save_stock_alpha101half_day(code=i, start_date = mark_day,  end_date = mark_day)
-            check_sinastock_alpha101half(mark_day)
-
-        res = check_stock_alpha191half(mark_day)
-        while res is None or (len(res[0]) + len(res[1])) > 20:
-            QA_SU_save_stock_alpha191half_day(start_date = mark_day,  end_date = mark_day)
-            res = check_stock_alpha191half(mark_day)
-        check_sinastock_alpha191half(mark_day)
-
-        QA_etl_stock_alpha101half_day(mark_day, mark_day)
-        QA_etl_stock_alpha191half_day(mark_day, mark_day)
