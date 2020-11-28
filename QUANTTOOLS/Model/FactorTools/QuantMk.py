fc7b2919eb556b12354ed73a5975a7c3b6839e10
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-11-28 14:53:49 +0800

    QA

--- QUANTTOOLS/Model/FactorTools/QuantMk.py
+++ QUANTTOOLS/Model/FactorTools/QuantMk.py
@@ -88,6 +88,9 @@ def get_index_quant_data(start_date, end_date, code=None, type = 'crawl', method
         codes = list(code_list[code_list.cate != '5'].code)
     else:
         codes = list(code_list[code_list.code.isin(code)].code)
+    codes = codes + ['000001','399001','399006']
+
+    codes = [i for i in codes if i not in ['880602','880604']]
 
     if type == 'crawl':
         res = QA_fetch_index_quant_pre_adv(codes,start_date,end_date, method=method,norm_type=norm_type).data
@@ -106,7 +109,8 @@ def get_index_quant_data_norm(start_date, end_date, code=None, type = 'crawl', m
         codes = list(code_list[code_list.cate != '5'].code)
     else:
         codes = list(code_list[code_list.code.isin(code)].code)
-
+    codes = codes + ['000001','399001','399006']
+    codes = [i for i in codes if i not in ['880602','880604']]
     if type == 'crawl':
         res = QA_fetch_index_quant_pre_adv(codes,start_date,end_date, method=method,norm_type=norm_type).data
     if type == 'model':
--- QUANTTOOLS/QAStockETL/QAFetch/QAQuantFactor.py
+++ QUANTTOOLS/QAStockETL/QAFetch/QAQuantFactor.py
@@ -39,41 +39,6 @@ def QA_fetch_get_index_quant_data(codes, start_date, end_date, type='standardize
         '##JOB got index quant data date range ============== from {from_} to {to_} '.format(from_=start,to_=end_date), ui_log)
     rng1 = QA_util_get_trade_range(start_date, end_date)
     QA_util_log_info(
-        '##JOB got Data index alpha191 data ============== from {from_} to {to_} '.format(from_= start_date,to_=end_date), ui_log)
-    alpha = QA_fetch_index_alpha_adv(codes,start,end_date).data[['alpha_001','alpha_002','alpha_003','alpha_004','alpha_005','alpha_006',
-                                                                 'alpha_007','alpha_008','alpha_009','alpha_010','alpha_011','alpha_012',
-                                                                 'alpha_013','alpha_014','alpha_015','alpha_016','alpha_018','alpha_019',
-                                                                 'alpha_020','alpha_021','alpha_022','alpha_023','alpha_024','alpha_025',
-                                                                 'alpha_026','alpha_028','alpha_029','alpha_031','alpha_032','alpha_033',
-                                                                 'alpha_034','alpha_035','alpha_036','alpha_037','alpha_038','alpha_039',
-                                                                 'alpha_040','alpha_041','alpha_042','alpha_043','alpha_044','alpha_045',
-                                                                 'alpha_046','alpha_047','alpha_048','alpha_049','alpha_052','alpha_053',
-                                                                 'alpha_054','alpha_056','alpha_057','alpha_058','alpha_059','alpha_060',
-                                                                 'alpha_061','alpha_062','alpha_063','alpha_064','alpha_065','alpha_066',
-                                                                 'alpha_067','alpha_068','alpha_070','alpha_071','alpha_072','alpha_074',
-                                                                 'alpha_076','alpha_077','alpha_078','alpha_079','alpha_080','alpha_081',
-                                                                 'alpha_082','alpha_083','alpha_084','alpha_085','alpha_086','alpha_087',
-                                                                 'alpha_088','alpha_089','alpha_090','alpha_091','alpha_092','alpha_093',
-                                                                 'alpha_094','alpha_095','alpha_096','alpha_097','alpha_098','alpha_099',
-                                                                 'alpha_100','alpha_101','alpha_102','alpha_103','alpha_104','alpha_105',
-                                                                 'alpha_106','alpha_107','alpha_108','alpha_109','alpha_111','alpha_112',
-                                                                 'alpha_113','alpha_114','alpha_115','alpha_116','alpha_117','alpha_119',
-                                                                 'alpha_120','alpha_122','alpha_123','alpha_124','alpha_125','alpha_126',
-                                                                 'alpha_127','alpha_128','alpha_129','alpha_130','alpha_132','alpha_133',
-                                                                 'alpha_134','alpha_135','alpha_136','alpha_137','alpha_138','alpha_139',
-                                                                 'alpha_141','alpha_142','alpha_145','alpha_148','alpha_150','alpha_152',
-                                                                 'alpha_153','alpha_154','alpha_155','alpha_156','alpha_157','alpha_158',
-                                                                 'alpha_160','alpha_161','alpha_162','alpha_163','alpha_164','alpha_167',
-                                                                 'alpha_168','alpha_169','alpha_170','alpha_171','alpha_172','alpha_173',
-                                                                 'alpha_174','alpha_175','alpha_176','alpha_177','alpha_178','alpha_179',
-                                                                 'alpha_180','alpha_184','alpha_185','alpha_186','alpha_187','alpha_188',
-                                                                 'alpha_189','alpha_191'
-                                                                 ]]
-    QA_util_log_info(
-        '##JOB got Data index alpha101 data ============== from {from_} to {to_} '.format(from_= start_date,to_=end_date), ui_log)
-    alpha101 = QA_fetch_index_alpha101_adv(codes,start,end_date).data
-    alphas = alpha.join(alpha101).groupby('code').fillna(method='ffill')
-    QA_util_log_info(
         '##JOB got Data index tech data ============== from {from_} to {to_} '.format(from_= start_date,to_=end_date), ui_log)
     technical = QA_fetch_index_technical_index_adv(codes,start,end_date).data.drop(['PBX1','PBX1_C','PBX2','PBX2_C','PBX3','PBX3_C','PBX4','PBX4_C','PBX5','PBX5_C','PBX6','PBX6_C','PBX_STD','PVT','PVT_C'], axis=1)
     QA_util_log_info(
@@ -83,7 +48,7 @@ def QA_fetch_get_index_quant_data(codes, start_date, end_date, type='standardize
     technical = technical.join(tech_week).groupby('code').fillna(method='ffill')
     QA_util_log_info(
         '##JOB index quant data combine ============== from {from_} to {to_} '.format(from_= start_date,to_=end_date), ui_log)
-    res = technical.join(alphas).loc[rng1]
+    res = technical.loc[rng1]
     QA_util_log_info(
         '##JOB index quant data trans ============== from {from_} to {to_} '.format(from_= start_date,to_=end_date), ui_log)
     if type == 'standardize':
