69be6327e0858729458196e5fa20ee9435662413
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-12-07 12:10:27 +0800

    QA

--- config/update_min.py
+++ config/update_min.py
@@ -1,18 +1,12 @@
 from QUANTAXIS.QAUtil import QA_util_today_str,QA_util_if_trade
 from QUANTTOOLS.QAStockETL.Check import (check_stock_15min, check_sinastock_15min,
-                                         check_stock_half, check_sinastock_half,
                                          check_stock_adj,check_sinastock_adj,
-                                         check_stock_alpha101half,check_sinastock_alpha101half,
-                                         check_stock_alpha191half,check_sinastock_alpha191half,
                                          check_stock_tech15min
                                          )
 from QUANTTOOLS.QAStockETL.QASU import (QA_SU_save_stock_15min,QA_SU_save_single_stock_15min,
-                                        QA_SU_save_stock_half,QA_SU_save_stock_aklist,
-                                        QA_SU_save_stock_xdxr,QA_SU_save_single_stock_xdxr)
-from QUANTTOOLS.QAStockETL import (QA_SU_save_stock_alpha101half_day,QA_SU_save_stock_alpha191half_day,
-                                   QA_SU_save_stock_technical_15min_day,
-                                   QA_etl_stock_alpha101half_day,QA_etl_stock_alpha191half_day,
-                                   QA_etl_stock_half,QA_etl_stock_technical_15min)
+                                        QA_SU_save_stock_aklist)
+from QUANTTOOLS.QAStockETL import (QA_SU_save_stock_technical_15min_day,
+                                   QA_etl_stock_technical_15min)
 import time
 
 if __name__ == '__main__':
