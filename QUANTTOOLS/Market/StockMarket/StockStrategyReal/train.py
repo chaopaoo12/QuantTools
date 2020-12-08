e214c9e393147e28a3632df605353757a43b4664
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-12-08 12:14:06 +0800

    QA

--- QUANTTOOLS/Market/StockMarket/StockStrategyReal/train.py
+++ QUANTTOOLS/Market/StockMarket/StockStrategyReal/train.py
@@ -6,7 +6,7 @@ from QUANTTOOLS.Model.StockModel.StrategyXgboost15Min import QAStockXGBoost15Min
 from QUANTTOOLS.Model.IndexModel.IndexXGboost import QAIndexXGBoost
 from QUANTTOOLS.Model.IndexModel.IndexXGboostHour import QAIndexXGBoostHour
 from QUANTTOOLS.Model.IndexModel.IndexXGboost15Min import QAIndexXGBoost15Min
-from .setting import working_dir, stock_day_set, stock_hour_set, index_day_set, index_hour_set
+from .setting import working_dir, stock_day_set, stock_hour_set, stock_min_set, index_day_set, index_hour_set
 from QUANTTOOLS.Market.MarketTools.train_tools import prepare_train, start_train, save_report, load_data, prepare_data
 
 def train(date, working_dir=working_dir):
@@ -23,6 +23,16 @@ def train(date, working_dir=working_dir):
     stock_model = start_train(stock_model, stock_day_set, other_params, 0, 0.99)
     save_report(stock_model, 'stock_mars_day', working_dir)
 
+    min_model = QAStockXGBoost15Min()
+    min_model = load_data(min_model, date, k = 1, start = "-01-01", norm_type=None)
+
+    min_model = prepare_data(min_model, date, mark = -5, col = 'TERNS_15M', type='shift', k = 3, start = "-01-01", shift=2)
+
+    other_params = {'learning_rate': 0.1, 'n_estimators': 200, 'max_depth': 5, 'min_child_weight': 1, 'seed': 1,
+                    'subsample': 0.8, 'colsample_bytree': 0.8, 'gamma': 0, 'reg_alpha': 0, 'reg_lambda': 1}
+
+    min_model = start_train(min_model, stock_min_set, other_params, 0, 0.99)
+    save_report(min_model, 'stock_mars_min', working_dir)
 
     hour_model = QAStockXGBoostHour()
     hour_model = load_data(hour_model, date, k = 1, start = "-01-01", norm_type=None)
@@ -34,17 +44,6 @@ def train(date, working_dir=working_dir):
 
     hour_model = start_train(hour_model, stock_hour_set, other_params, 0, 0.99)
     save_report(hour_model, 'stock_mars_hour', working_dir)
-
-    min_model = QAStockXGBoost15Min()
-    min_model = load_data(min_model, date, k = 1, start = "-01-01", norm_type=None)
-
-    min_model = prepare_data(min_model, date, mark = -5, col = 'TERNS_15M', type='shift', k = 3, start = "-01-01", shift=2)
-
-    other_params = {'learning_rate': 0.1, 'n_estimators': 200, 'max_depth': 5, 'min_child_weight': 1, 'seed': 1,
-                    'subsample': 0.8, 'colsample_bytree': 0.8, 'gamma': 0, 'reg_alpha': 0, 'reg_lambda': 1}
-
-    min_model = start_train(min_model, stock_hour_set, other_params, 0, 0.99)
-    save_report(min_model, 'stock_mars_min', working_dir)
 
 def train_hedge(date, working_dir=working_dir):
     hedge_model = QAStockXGBoost()
