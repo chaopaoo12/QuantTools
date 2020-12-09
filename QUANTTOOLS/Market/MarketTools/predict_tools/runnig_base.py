b9beb50c94f120ac8556fad34f25748eace00f34
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-12-09 15:42:15 +0800

    QA

--- QUANTTOOLS/Market/MarketTools/predict_tools/runnig_base.py
+++ QUANTTOOLS/Market/MarketTools/predict_tools/runnig_base.py
@@ -8,7 +8,7 @@ def predict_base(trading_date, predict_func, model_name, file_name, top_num, per
     save_prediction({'date': trading_date, 'target_pool':stock_tar, 'prediction':stock_b}, file_name, working_dir)
     prediction_report(QA_util_get_real_date(trading_date), stock_tar, stock_b, model_date, top_num, exceptions, percent, account='name:client-1', ui_log = None)
 
-def predict_index_base(trading_date, predict_func, model_name, file_name, top_num, working_dir, exceptions):
+def predict_index_base(trading_date, predict_func, model_name, file_name, top_num, working_dir):
     stock_tar, stock_b, start, end, model_date = predict_func(trading_date, model_name = model_name,  working_dir=working_dir)
     save_prediction({'date': trading_date, 'target_pool':stock_tar, 'prediction':stock_b}, file_name, working_dir)
-    Index_Report(QA_util_get_real_date(trading_date), stock_tar, stock_b, model_date, top_num, exceptions)
+    Index_Report(QA_util_get_real_date(trading_date), stock_tar, stock_b, model_date, top_num)
