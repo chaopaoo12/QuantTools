3eda3d0714ab659b547c347453a1d4cfc5fb7d44
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-12-09 15:40:41 +0800

    QA

--- QUANTTOOLS/Market/MarketTools/predict_tools/prediction_report.py
+++ QUANTTOOLS/Market/MarketTools/predict_tools/prediction_report.py
@@ -138,6 +138,102 @@ def prediction_report(trading_date, target_pool, prediction, model_date, top_num
 
     return(target_fd)
 
+def Index_Reporter(trading_date, target_pool, top_num=5):
+    QA_util_log_info('##JOB## Now Current Report ==== {}'.format(str(trading_date)))
+    if target_pool is not None and target_pool.shape[0] > 0:
+        if top_num is None or top_num == 0:
+            current_score = target_pool[target_pool.OPEN_MARK == 0].groupby('date').mean()[['Z_PROB','O_PROB','RANK','INDEX_TARGET','INDEX_TARGET3','INDEX_TARGET4','INDEX_TARGET5','PASS_MARK']]
+            current_details = target_pool[['NAME','Z_PROB','O_PROB','RANK','INDEX_TARGET','INDEX_TARGET3','INDEX_TARGET4','INDEX_TARGET5','PASS_MARK']]
+        else:
+            current_score = target_pool[(target_pool['RANK'] <= top_num) & (target_pool.OPEN_MARK == 0)].groupby('date').mean()[['Z_PROB','O_PROB','RANK','INDEX_TARGET','INDEX_TARGET3','INDEX_TARGET4','INDEX_TARGET5','PASS_MARK']]
+            current_details = target_pool[(target_pool['RANK'] <= top_num) & (target_pool.OPEN_MARK == 0)][['NAME','Z_PROB','O_PROB','RANK','INDEX_TARGET','INDEX_TARGET3','INDEX_TARGET4','INDEX_TARGET5','PASS_MARK']]
+    else:
+        current_score = pd.DataFrame()
+        current_details = pd.DataFrame()
+
+    return(current_score, current_details)
+
+def Index_Report(trading_date, target_pool, prediction, model_date, top_num,  ui_log = None):
+    QA_util_log_info('##JOB## Now Got Account Info ==== {}'.format(str(trading_date)), ui_log)
+
+    ###预测为正TOP
+    current_score, current_details = Index_Reporter(trading_date, target_pool, top_num)
+
+    ###预测为正T
+    currentall_score, currentall_details = Index_Reporter(trading_date, target_pool)
+
+    ###预测TOP
+    top_score, top_details = Index_Reporter(trading_date, prediction, top_num)
+
+    ####预测的最终结果
+    target_fd = current_details.loc[trading_date]
+
+    QA_util_log_info('##JOB## Now Message Building ==== {}'.format(str(trading_date)), ui_log)
+
+    try:
+        err_msg = '模型训练日期:{model_date}'.format(model_date=model_date)
+    except:
+        send_email('交易报告:'+ trading_date, "模型训练日期获取运算失败", trading_date)
+
+    try:
+        target_body = build_table(target_fd, '目标持仓')
+    except:
+        send_email('交易报告:'+ trading_date, "消息组件运算失败:目标持仓", trading_date)
+
+    #########
+    try:
+        model_score = build_table(current_score, '模型周期内交易成绩')
+    except:
+        send_email('交易报告:'+ trading_date, "消息组件运算失败:模型周期内交易成绩", trading_date)
+
+    try:
+        stock_socre = build_table(currentall_score, '选股模型周期内交易成绩')
+    except:
+        send_email('交易报告:'+ trading_date, "消息组件运算失败:选股模型周期内交易成绩", trading_date)
+
+    #########
+    try:
+        modelhis_body = build_table(current_details, '模型周期内选股记录')
+    except:
+        send_email('交易报告:'+ trading_date, "消息组件运算失败:模型周期内选股记录", trading_date)
+
+    try:
+        allstock_socre = build_table(top_score, '选股模型周期内TOP交易成绩')
+    except:
+        send_email('交易报告:'+ trading_date, "消息组件运算失败:选股模型周期内TOP交易成绩", trading_date)
+
+    try:
+        modeltophis_body = build_table(top_details, '模型周期内TOP选股记录')
+    except:
+        send_email('交易报告:'+ trading_date, "消息组件运算失败:模型周期内选股记录", trading_date)
 
 
+    if target_fd is not None:
+        title = '交易报告'
+    else:
+        title = '空仓交易报告'
 
+    try:
+        msg = build_email(build_head(),err_msg,
+                          target_body,
+                          model_score,  stock_socre, allstock_socre,
+                          modelhis_body, modeltophis_body)
+        send_actionnotice("prediction_report",
+                          '交易报告:{}'.format(trading_date),
+                          '模型运行完毕',
+                          direction = 'HOLD',
+                          offset='HOLD',
+                          volume=None
+                          )
+        send_email(title + trading_date, msg, trading_date)
+    except:
+        send_email('交易报告:'+ trading_date, "消息构建失败", trading_date)
+        send_actionnotice("prediction_report",
+                          '交易报告:{}'.format(trading_date),
+                          '模型运行完毕 Email过程失败',
+                          direction = 'HOLD',
+                          offset='HOLD',
+                          volume=None
+                          )
+
+    return(target_fd)
--- QUANTTOOLS/Market/StockMarket/StockStrategyReal/running.py
+++ QUANTTOOLS/Market/StockMarket/StockStrategyReal/running.py
@@ -2,7 +2,7 @@
 
 from .concat_predict import concat_predict,concat_predict_real,concat_predict_crawl,concat_predict_hedge,concat_predict_index
 from .setting import working_dir, percent, exceptions, top
-from QUANTTOOLS.Market.MarketTools import predict_base
+from QUANTTOOLS.Market.MarketTools import predict_base, predict_index_base
 
 def predict(trading_date, top_num=top, working_dir=working_dir, exceptions=exceptions):
     predict_base(trading_date, concat_predict, model_name = 'stock_xg', file_name = 'prediction', top_num=top_num, percent=percent, working_dir=working_dir, exceptions=exceptions)
@@ -16,8 +16,8 @@ def predict_crawl(trading_date, top_num=top, working_dir=working_dir, exceptions
 def predict_hedge(trading_date, top_num=top, working_dir=working_dir, exceptions=exceptions):
     predict_base(trading_date, concat_predict_hedge, model_name = 'hedge_xg', file_name = 'prediction_hedge', top_num=top_num, percent=percent, working_dir=working_dir, exceptions=exceptions)
 
-def predict_index(trading_date, top_num=top, working_dir=working_dir, exceptions=exceptions):
-    predict_base(trading_date, concat_predict_index, model_name = 'index_mars_day', file_name = 'prediction_index', top_num=10, percent=percent, working_dir=working_dir, exceptions=exceptions)
+def predict_index(trading_date, working_dir=working_dir, exceptions=exceptions):
+    predict_index_base(trading_date, concat_predict_index, model_name = 'index_mars_day', file_name = 'prediction_index', top_num=10, percent=percent, working_dir=working_dir, exceptions=exceptions)
 
 def predict_daily(trading_date, top_num=top, working_dir=working_dir, exceptions=exceptions):
     predict_base(trading_date, concat_predict, model_name = 'stock_mars_day', file_name = 'prediction_stock_mars_day', top_num=top_num, percent=percent, working_dir=working_dir, exceptions=exceptions)
