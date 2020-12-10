4dd6afbafb138abf22a0b8bf8861e8d8b4d83f3c
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-12-10 13:53:40 +0800

    QA

--- QUANTTOOLS/Model/QABaseModel/QAStockModelHour.py
+++ QUANTTOOLS/Model/QABaseModel/QAStockModelHour.py
@@ -66,10 +66,10 @@ class QAStockModelHour(QAModel):
                 train = train[self.cols].dropna(thresh=(len(self.cols) - self.thresh))
 
         QA_util_log_info(train.shape[0])
-        train = train.join(data[['INDUSTRY','OPEN_MARK','PASS_MARK','TARGET','TARGET3','TARGET4','TARGET5','TARGET10']])
+        train = train.join(data[['PASS_MARK','TARGET','TARGET3','TARGET4','TARGET5','TARGET10']])
 
         QA_util_log_info('##JOB Now Got Prediction Result ===== from {_from} to {_to}'.format(_from=start,_to = end), ui_log = None)
-        b = train[['INDUSTRY','OPEN_MARK','PASS_MARK','TARGET','TARGET3','TARGET4','TARGET5','TARGET10']]
+        b = train[['PASS_MARK','TARGET','TARGET3','TARGET4','TARGET5','TARGET10']]
         b = b.assign(y_pred = self.model.predict(train[self.cols]))
         bina = pd.DataFrame(self.model.predict_proba(train[self.cols]))[[0,1]]
         bina.index = b.index
