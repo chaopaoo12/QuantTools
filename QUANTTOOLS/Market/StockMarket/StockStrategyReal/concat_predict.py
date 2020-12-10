62608457306195276de428baff8c86b7855d0660
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-12-10 16:54:42 +0800

    QA

--- QUANTTOOLS/Market/StockMarket/StockStrategyReal/concat_predict.py
+++ QUANTTOOLS/Market/StockMarket/StockStrategyReal/concat_predict.py
@@ -10,19 +10,19 @@ from QUANTTOOLS.Model.IndexModel.IndexXGboostHour import QAIndexXGBoostHour
 from QUANTTOOLS.Model.IndexModel.IndexXGboost15Min import QAIndexXGBoost15Min
 from QUANTTOOLS.Market.MarketTools import make_prediction,make_stockprediction,make_indexprediction
 
-def concat_predict(trading_date, working_dir, type = 'crawl', model_name = 'stock_xg'):
+def concat_predict(trading_date, working_dir, code = None, type = 'crawl', model_name = 'stock_xg'):
     Stock = QAStockXGBoost()
-    target_pool,prediction,start,end,Model_Date = make_stockprediction(Stock, trading_date, model_name, working_dir, index='date', type=type)
+    target_pool,prediction,start,end,Model_Date = make_stockprediction(Stock, trading_date, model_name, working_dir, code, index='date', type=type)
     return(target_pool,prediction,start,end,Model_Date)
 
-def concat_predict_hour(trading_date, working_dir, type = 'crawl', model_name = 'stock_xg'):
+def concat_predict_hour(trading_date, working_dir, code = None, type = 'crawl', model_name = 'stock_xg'):
     Stock = QAStockXGBoostHour()
-    target_pool,prediction,start,end,Model_Date = make_stockprediction(Stock, trading_date, model_name, working_dir, index='datetime', type=type)
+    target_pool,prediction,start,end,Model_Date = make_stockprediction(Stock, trading_date, model_name, working_dir, code, index='datetime', type=type)
     return(target_pool,prediction,start,end,Model_Date)
 
-def concat_predict_15min(trading_date, working_dir, type = 'crawl', model_name = 'stock_xg'):
+def concat_predict_15min(trading_date, working_dir, code = None, type = 'crawl', model_name = 'stock_xg'):
     Stock = QAStockXGBoost15Min()
-    target_pool,prediction,start,end,Model_Date = make_stockprediction(Stock, trading_date, model_name, working_dir, index='datetime', type=type)
+    target_pool,prediction,start,end,Model_Date = make_stockprediction(Stock, trading_date, model_name, working_dir, code, index='datetime', type=type)
     return(target_pool,prediction,start,end,Model_Date)
 
 def concat_predict_index(trading_date, working_dir, type = 'crawl', model_name = 'index_xg'):
