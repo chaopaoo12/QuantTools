2fb5d9a070d1ccbb8f40b7aacf4f0663f383b607
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-12-10 14:16:14 +0800

    QA

--- QUANTTOOLS/Market/MarketTools/predict_tools/runnig_base.py
+++ QUANTTOOLS/Market/MarketTools/predict_tools/runnig_base.py
@@ -39,7 +39,7 @@ def predict_index_dev(trading_date, predict_func1, predict_func2, predict_func3,
     Index_Report(QA_util_get_real_date(trading_date), stock_tar, stock_b, model_date, top_num)
 
 
-def predict_stock_dev(trading_date, predict_func1, predict_func2, predict_func3, day_moel, hour_model, min_model, file_name, top_num, working_dir):
+def predict_stock_dev(trading_date, predict_func1, predict_func2, predict_func3, day_moel, hour_model, min_model, file_name, top_num, percent, working_dir, exceptions):
     stock_b = pd.DataFrame()
     if predict_func1 is not None:
         day_tar, day_b, start, end, model_date = predict_func1(trading_date, model_name = day_moel,  working_dir=working_dir)
@@ -60,4 +60,4 @@ def predict_stock_dev(trading_date, predict_func1, predict_func2, predict_func3,
 
     stock_tar = stock_b[(stock_b.DAY_PROB > 0.5)]
     save_prediction({'date': trading_date, 'target_pool':stock_tar, 'prediction':stock_b}, file_name, working_dir)
-    prediction_report(QA_util_get_real_date(trading_date), stock_tar, stock_b, model_date, top_num)
+    prediction_report(QA_util_get_real_date(trading_date), stock_tar, stock_b, model_date, top_num, exceptions, percent, account='name:client-1', ui_log = None)
