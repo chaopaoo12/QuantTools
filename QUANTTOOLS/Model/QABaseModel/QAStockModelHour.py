2993749af9ad190e68be893faa9cfc7dff5d0877
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-12-10 17:43:48 +0800

    QA

--- QUANTTOOLS/Model/QABaseModel/QAStockModelHour.py
+++ QUANTTOOLS/Model/QABaseModel/QAStockModelHour.py
@@ -20,6 +20,7 @@ class QAStockModelHour(QAModel):
             self.code = code
         QA_util_log_info('##JOB Got Data by {type}, block: {block}, sub_block: {sub_block} ==== from {_from} to {_to}'.format(type=type, block=self.block,sub_block=self.sub_block, _from=start, _to=end), ui_log = None)
         data = get_quant_data_hour(start, end, code = self.code, type= type,block = self.block, sub_block=self.sub_block, norm_type=self.norm_type)
+        print(data)
         print(data[self.cols])
         QA_util_log_info('##JOB Now Reshape Different Columns ===== from {_from} to {_to}'.format(_from=start,_to = end), ui_log = None)
         cols1 = [i for i in data.columns if i not in [ 'moon','star','mars','venus','sun','MARK','date','datetime',
