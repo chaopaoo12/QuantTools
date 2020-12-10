5fbc12199333ba00e481bfd407d203c809b8581d
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-12-10 15:58:47 +0800

    QA

--- QUANTTOOLS/Market/MarketTools/trading_tools/load_data.py
+++ QUANTTOOLS/Market/MarketTools/trading_tools/load_data.py
@@ -2,7 +2,7 @@ from QUANTTOOLS.Market.MarketTools import load_prediction,check_prediction
 from QUANTTOOLS.Message import send_email
 from QUANTAXIS.QAUtil import QA_util_log_info
 
-def load_data(predict_func, trading_date, working_dir, model_name, file_name):
+def load_data(trading_date, working_dir, file_name):
     QA_util_log_info('##JOB## Now Predict ==== {}'.format(str(trading_date)))
     try:
         prediction = load_prediction(file_name, working_dir)
@@ -10,7 +10,7 @@ def load_data(predict_func, trading_date, working_dir, model_name, file_name):
         target_pool = prediction['target_pool']
         prediction = prediction['prediction']
     except:
-        target_pool, prediction, start, end, model_date = predict_func(trading_date, model_name=model_name,  working_dir=working_dir)
+        pass
 
     try:
         r_tar = target_pool.loc[trading_date]
