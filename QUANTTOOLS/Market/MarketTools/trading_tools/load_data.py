229c9b99035a077b9940e059b6d6719f6b399a70
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-12-11 01:29:46 +0800

    QA

--- QUANTTOOLS/Market/MarketTools/trading_tools/load_data.py
+++ QUANTTOOLS/Market/MarketTools/trading_tools/load_data.py
@@ -8,18 +8,17 @@ def load_data(func, trading_date, working_dir, file_name):
         prediction = load_prediction(file_name, working_dir)
         check_prediction(prediction, trading_date)
         target_pool = prediction['target_pool']
-        prediction = prediction['prediction']
+        prediction_tar = prediction['prediction']
     except:
         func(trading_date, top_num=10, working_dir=working_dir)
         prediction = load_prediction(file_name, working_dir)
         target_pool = prediction['target_pool']
-        prediction = prediction['prediction']
+        prediction_tar = prediction['prediction']
 
     try:
         r_tar = target_pool.loc[trading_date]
-        prediction_tar = prediction
     except:
         r_tar = None
         prediction_tar =  None
         send_email('交易报告:'+ trading_date, "空仓状态", 'date')
-    return(r_tar, prediction_tar)
+    return(r_tar, prediction_tar, prediction)
