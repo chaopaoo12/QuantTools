a19c604c1e795e188b0c80db3a926ebf19a7a70c
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-12-16 22:18:47 +0800

    QA

--- QUANTTOOLS/Model/FactorTools/QuantMk.py
+++ QUANTTOOLS/Model/FactorTools/QuantMk.py
@@ -260,7 +260,7 @@ def get_quant_data_hour(start_date, end_date, code=None, type = 'model', block =
     elif type == 'model':
         res = QA_fetch_stock_quant_hour(codes, start_date, end_date, block = sub_block, norm_type =norm_type)
     elif type == 'real':
-        res = QA_fetch_get_stock_quant_hour(codes, start_date, end_date).drop(['date','date_stamp','time_stamp'],axis=1)
+        res = QA_fetch_get_stock_quant_hour(codes, start_date, end_date).drop(['date','time_stamp'],axis=1)
         res.columns = [x.upper() + '_HR' for x in res.columns]
     return(res)
 
@@ -314,7 +314,7 @@ def get_quant_data_15min(start_date, end_date, code=None, type = 'model', block 
     elif type == 'model':
         res = QA_fetch_stock_quant_min(codes, start_date, end_date, block = sub_block, norm_type =norm_type)
     elif type == 'real':
-        res = QA_fetch_get_stock_quant_min(codes, start_date, end_date).drop(['date','date_stamp','time_stamp'],axis=1)
+        res = QA_fetch_get_stock_quant_min(codes, start_date, end_date).drop(['date','time_stamp'],axis=1)
         res.columns = [x.upper() + '_15M' for x in res.columns]
     return(res)
 
