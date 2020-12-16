8ad929008e32d0e4eb112fd017c228f52aa8e1bb
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-12-16 23:41:05 +0800

    QA

--- QUANTTOOLS/Model/QABaseModel/QAStockModel.py
+++ QUANTTOOLS/Model/QABaseModel/QAStockModel.py
@@ -21,8 +21,6 @@ class QAStockModel(QAModel):
             self.code = code
         QA_util_log_info('##JOB Got Data by {type}, block: {block}, sub_block: {sub_block} ==== from {_from} to {_to}'.format(type=type, block=self.block,sub_block=self.sub_block, _from=start, _to=end), ui_log = None)
         data = get_quant_data(start, end, code = self.code, type= type,block = self.block, sub_block=self.sub_block, norm_type=self.norm_type)
-        print(data)
-        print(data[['RNG_90', 'AMT_90', 'MAMT_90', 'NEG90_RATE', 'VR', 'VRSI', 'VRSI_C', 'VSTD', 'BOLL', 'UB', 'LB', 'WIDTH', 'WR', 'MR', 'SR', 'WS', 'MS', 'SS', 'MIKE_WRSC', 'MIKE_WRJC', 'MIKE_WSSC', 'MIKE_WSJC', 'MIKE_TR', 'MIKE_BOLL', 'ASI', 'ASIT', 'OBV', 'OBV_C', 'KDJ_K', 'KDJ_D', 'KDJ_J', 'KDJ_CROSS1', 'KDJ_CROSS2', 'WR1', 'WR2', 'WR_CROSS1', 'WR_CROSS2', 'ROC', 'ROCMA', 'RSI1', 'RSI2', 'RSI3', 'RSI1_C', 'RSI2_C', 'RSI3_C', 'RSI_CROSS1', 'RSI_CROSS2', 'CCI', 'CCI_CROSS1', 'CCI_CROSS2', 'CCI_CROSS3', 'CCI_CROSS4', 'BIAS1', 'BIAS2', 'BIAS3', 'BIAS_CROSS1', 'BIAS_CROSS2', 'OSC', 'MAOSC', 'OSC_CROSS1', 'OSC_CROSS2', 'OSC_CROSS3', 'OSC_CROSS4', 'ADTM', 'MAADTM', 'ADTM_CROSS1', 'ADTM_CROSS2', 'DIF', 'DEA', 'MACD', 'CROSS_JC', 'CROSS_SC', 'MACD_TR', 'DI1', 'DI2', 'ADX', 'ADXR', 'ADX_C', 'DI_M', 'DI_CROSS1', 'DI_CROSS2', 'ADX_CROSS1', 'ADX_CROSS2', 'DDD', 'AMA', 'DMA_CROSS1', 'DMA_CROSS2', 'MTM', 'MTMMA', 'MTM_CROSS1', 'MTM_CROSS2', 'MTM_CROSS3', 'MTM_CROSS4', 'MA1', 'MA2', 'MA3', 'MA4', 'BBI', 'MFI', 'MFI_C', 'TR', 'ATR', 'ATRR', 'RSV', 'SKDJ_K', 'SKDJ_D', 'SKDJ_CROSS1', 'SKDJ_CROSS2', 'DDI', 'ADDI', 'AD', 'DDI_C', 'AD_C', 'ADDI_C', 'SHA_LOW', 'SHA_UP', 'BODY', 'BODY_ABS', 'PRICE_PCG', 'MA5', 'MA10', 'MA20', 'MA60', 'SHORT10', 'SHORT20', 'SHORT60', 'LONG60', 'SHORT_CROSS1', 'SHORT_CROSS2', 'LONG_CROSS1', 'LONG_CROSS2', 'LONG_AMOUNT', 'SHORT_AMOUNT', 'CDL2CROWS', 'CDL3BLACKCROWS', 'CDL3INSIDE', 'CDL3LINESTRIKE', 'CDL3OUTSIDE', 'CDL3STARSINSOUTH', 'CDL3WHITESOLDIERS', 'CDLABANDONEDBABY', 'CDLADVANCEBLOCK', 'CDLBELTHOLD', 'CDLBREAKAWAY', 'CDLCLOSINGMARUBOZU', 'CDLCONCEALBABYSWALL', 'CDLCOUNTERATTACK', 'CDLDARKCLOUDCOVER', 'CDLDOJI', 'CDLDOJISTAR', 'CDLDRAGONFLYDOJI', 'CDLENGULFING', 'CDLEVENINGDOJISTAR', 'CDLEVENINGSTAR', 'CDLGAPSIDESIDEWHITE', 'CDLGRAVESTONEDOJI', 'CDLHAMMER', 'CDLHANGINGMAN', 'CDLHARAMI', 'CDLHARAMICROSS', 'CDLHIGHWAVE', 'CDLHIKKAKE', 'CDLHIKKAKEMOD', 'CDLHOMINGPIGEON', 'CDLIDENTICAL3CROWS', 'CDLINNECK', 'CDLINVERTEDHAMMER', 'CDLKICKING', 'CDLKICKINGBYLENGTH', 'CDLLADDERBOTTOM', 'CDLLONGLEGGEDDOJI', 'CDLLONGLINE', 'CDLMARUBOZU', 'CDLMATCHINGLOW', 'CDLMATHOLD', 'CDLMORNINGDOJISTAR', 'CDLMORNINGSTAR', 'CDLONNECK', 'CDLPIERCING', 'CDLRICKSHAWMAN', 'CDLRISEFALL3METHODS', 'CDLSEPARATINGLINES', 'CDLSHOOTINGSTAR', 'CDLSHORTLINE', 'CDLSPINNINGTOP', 'CDLSTALLEDPATTERN', 'CDLSTICKSANDWICH', 'CDLTAKURI', 'CDLTASUKIGAP', 'CDLTHRUSTING', 'CDLTRISTAR', 'CDLUNIQUE3RIVER', 'CDLUPSIDEGAP2CROWS', 'CDLXSIDEGAP3METHODS', 'MA5_C', 'MA10_C', 'MA20_C', 'MA60_C', 'CCI_JC', 'CCI_SC', 'CCI_TR', 'TERNS', 'CCI_TR_HR', 'ALPHA_061', 'ALPHA_123']])
         code_all = QA_fetch_get_stockcode_real(QA_fetch_stock_all().code.unique().tolist())
         code_old = QA_fetch_code_old().code.unique().tolist()
         code_new = QA_fetch_code_new().code.unique().tolist()
@@ -77,7 +75,6 @@ class QAStockModel(QAModel):
                 train[i] = 0
                 n_cols.append(i)
         train.index = data.index
-        print(train)
         QA_util_log_info('##JOB Now Got Different Columns ===== from {_from} to {_to}'.format(_from=start,_to = end), ui_log = None)
         QA_util_log_info(n_cols)
         QA_util_log_info(train.shape[0])
@@ -107,7 +104,6 @@ class QAStockModel(QAModel):
                                   offset='WARNING',
                                   volume=None
                                   )
-        print(train)
         short_of_data = [i for i in target_code if i not in train.loc[end].reset_index().code.unique().tolist()]
 
         if len(short_of_data) > 0:
