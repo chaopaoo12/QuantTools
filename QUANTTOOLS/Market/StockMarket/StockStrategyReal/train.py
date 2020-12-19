a0359cc9f7c866a6155df3ab9ca470416ebb4101
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2020-12-19 14:53:17 +0800

    QA

--- QUANTTOOLS/Market/StockMarket/StockStrategyReal/train.py
+++ QUANTTOOLS/Market/StockMarket/StockStrategyReal/train.py
@@ -25,7 +25,7 @@ def choose_model(date, working_dir=working_dir):
     other_params = {'learning_rate': 0.1, 'n_estimators': 200, 'max_depth': 5, 'min_child_weight': 1, 'seed': 1,
                     'subsample': 0.8, 'colsample_bytree': 0.8, 'gamma': 0, 'reg_alpha': 0, 'reg_lambda': 1}
 
-    stock_model = start_train(stock_model, stock_xg_set, other_params, 0, 0.99)
+    stock_model = start_train(stock_model, None, other_params, 0, 0.99)
     save_report(stock_model, 'stock_xg', working_dir)
 
 def daymodel_train(date, working_dir=working_dir):
