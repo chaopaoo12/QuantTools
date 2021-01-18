ee1515f4fe7d87209b899e1e4fa436e1285c26fa
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2021-01-18 23:31:38 +0800

    QA

--- QUANTTOOLS/Ananlysis/Trends/trends.py
+++ QUANTTOOLS/Ananlysis/Trends/trends.py
@@ -5,25 +5,29 @@ def btc_daily(BTC):
     day, week = trends_btc(BTC)
     day_check = check(day).loc[BTC]
     week_check = check(week).loc[BTC]
-    return(day_check, week_check)
+    incr = day.iloc[-1:].reset_index()['MA15_C']/10
+    return(day_check, week_check, incr)
 
 def money_daily(MONEY, date):
     day, week = trends_money(MONEY, date)
     day_check = check(day).loc[MONEY]
     week_check = check(week).loc[MONEY]
-    return(day_check, week_check)
+    incr = day.iloc[-1:].reset_index()['MA15_C']/10
+    return(day_check, week_check, incr)
 
 def gold_daily(GOLD, date):
     day, week = trends_gold(GOLD, date)
     day_check = check(day).loc[GOLD]
     week_check = check(week).loc[GOLD]
-    return(day_check, week_check)
+    incr = day.iloc[-1:].reset_index()['MA15_C']/10
+    return(day_check, week_check, incr)
 
 def stock_daily(stock, start_date, end_date):
     day, week = trends_stock(stock,start_date,end_date)
     day_check = check(day).loc[stock]
     week_check = check(week).loc[stock]
-    return(day_check, week_check)
+    incr = day.iloc[-1:].reset_index()['MA15_C']/10
+    return(day_check, week_check, incr)
 
 def stock_hourly(stock, start_date, end_date, date_type):
     hour = trends_stock_hour(stock,start_date,end_date)
--- QUANTTOOLS/Market/MarketReport/JOB/daily_job.py
+++ QUANTTOOLS/Market/MarketReport/JOB/daily_job.py
@@ -8,23 +8,23 @@ def aotu_report(trading_date):
     BTC_RES = pd.DataFrame(columns=('code', 'daily', 'weekly'))
     for code in BTC:
         res = btc_daily(code)
-        BTC_RES = BTC_RES.append({'code':code, 'daily':res[0], 'weekly':res[1]}, ignore_index=True)
+        BTC_RES = BTC_RES.append({'code':code, 'daily':res[0], 'weekly':res[1], 'speed':res[2]}, ignore_index=True)
 
     for code in GOLD:
         res = gold_daily(code, trading_date)
-        BTC_RES = BTC_RES.append({'code':code, 'daily':res[0], 'weekly':res[1]}, ignore_index=True)
+        BTC_RES = BTC_RES.append({'code':code, 'daily':res[0], 'weekly':res[1], 'speed':res[2]}, ignore_index=True)
 
     for code in MONEY:
         res = money_daily(code, trading_date)
-        BTC_RES = BTC_RES.append({'code':code, 'daily':res[0], 'weekly':res[1]}, ignore_index=True)
+        BTC_RES = BTC_RES.append({'code':code, 'daily':res[0], 'weekly':res[1], 'speed':res[2]}, ignore_index=True)
 
     for code in CN_INDEX:
         res = stock_daily(code, trading_date, trading_date)
-        BTC_RES = BTC_RES.append({'code':code, 'daily':res[0], 'weekly':res[1]}, ignore_index=True)
+        BTC_RES = BTC_RES.append({'code':code, 'daily':res[0], 'weekly':res[1], 'speed':res[2]}, ignore_index=True)
 
     for code in US_INDEX:
         res = stock_daily(code, trading_date, trading_date)
-        BTC_RES = BTC_RES.append({'code':code, 'daily':res[0], 'weekly':res[1]}, ignore_index=True)
+        BTC_RES = BTC_RES.append({'code':code, 'daily':res[0], 'weekly':res[1], 'speed':res[2]}, ignore_index=True)
     target_body = build_table(BTC_RES, '目标持仓')
     msg = build_email(build_head(),target_body)
     send_email('金融产品价格趋势' + trading_date, msg, trading_date)
