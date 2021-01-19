60dd2b0930806f2989cf644e41b400d4de51321a
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2021-01-19 22:02:51 +0800

    QA

--- QUANTTOOLS/Market/MarketReport/JOB/daily_job.py
+++ QUANTTOOLS/Market/MarketReport/JOB/daily_job.py
@@ -25,7 +25,7 @@ def aotu_report(trading_date):
     for code in US_INDEX:
         res = stock_daily(code, trading_date, trading_date)
         BTC_RES = BTC_RES.append({'code':code, 'daily':res[0], 'weekly':res[1], '日线斜率':res[2], '斜率变动':res[3], '五日线偏离幅度':res[4], '十五日线偏离幅度':res[5]}, ignore_index=True)
-    BTC_RES = BTC_RES.rename(columns={'code':'标的', 'daily':'日线走势', 'weekly':'周线走势'}, inplace = True)
+    #BTC_RES = BTC_RES.rename(columns={'code':'标的', 'daily':'日线走势', 'weekly':'周线走势'}, inplace = True)
     target_body = build_table(BTC_RES, '目标持仓')
     msg = build_email(build_head(),target_body)
     send_email('金融产品价格趋势' + trading_date, msg, trading_date)
