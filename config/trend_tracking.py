89b1fd73f796fc19db529103df69325f01c480f3
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2021-02-08 14:14:10 +0800

    QA

--- config/trend_tracking.py
+++ config/trend_tracking.py
@@ -1,8 +1,22 @@
-from QUANTTOOLS.Market.MarketReport.JOB.hourly_job import daily_job
-from QUANTAXIS.QAUtil import QA_util_today_str,QA_util_if_trade
+#from QUANTTOOLS.Market.MarketReport.JOB.hourly_job import daily_job
+#from QUANTAXIS.QAUtil import QA_util_today_str,QA_util_if_trade
+#
+#if __name__ == '__main__':
+#    mark_day = QA_util_today_str()
+#
+#    if QA_util_if_trade(mark_day):
+#        daily_job(mark_day)
+
+
+from QUANTTOOLS.Market.StockMarket.StockStrategyReal import Tracking
+from QUANTAXIS.QAUtil import QA_util_today_str,QA_util_get_last_day,QA_util_get_real_date,QA_util_if_trade
+
 
 if __name__ == '__main__':
-    mark_day = QA_util_today_str()
+    if QA_util_get_last_day(QA_util_today_str()) == 'wrong date':
+        mark_day = QA_util_get_real_date(QA_util_today_str())
+    else:
+        mark_day = QA_util_get_last_day(QA_util_today_str())
 
-    if QA_util_if_trade(mark_day):
-        daily_job(mark_day)
+    if QA_util_if_trade(QA_util_today_str()):
+        Tracking(mark_day)
