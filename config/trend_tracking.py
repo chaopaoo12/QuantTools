0c885be24742772be9dbc4d84863863a878fbe9c
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2021-02-09 18:20:51 +0800

    QA

--- config/trend_tracking.py
+++ config/trend_tracking.py
@@ -13,10 +13,7 @@ from QUANTAXIS.QAUtil import QA_util_today_str,QA_util_get_last_day,QA_util_get_
 
 
 if __name__ == '__main__':
-    if QA_util_get_last_day(QA_util_today_str()) == 'wrong date':
-        mark_day = QA_util_get_real_date(QA_util_today_str())
-    else:
-        mark_day = QA_util_get_last_day(QA_util_today_str())
+    mark_day = QA_util_today_str()
 
-    if QA_util_if_trade(QA_util_today_str()):
+    if QA_util_if_trade(mark_day):
         Tracking(mark_day)
