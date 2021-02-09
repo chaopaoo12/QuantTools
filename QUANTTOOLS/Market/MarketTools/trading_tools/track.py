c6a2760832967707d2a05f25e177516f72c71178
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2021-02-09 18:31:47 +0800

    QA

--- QUANTTOOLS/Market/MarketTools/trading_tools/track.py
+++ QUANTTOOLS/Market/MarketTools/trading_tools/track.py
@@ -42,7 +42,6 @@ def track_roboot(target_tar, account, trading_date, percent, strategy_id,  excep
                     QA_util_log_info('##JOB Now Time ==== {}'.format(str(mark_tm)), ui_log = None)
                     ####job1 小时级报告 指数小时级跟踪
                     for code in res:
-                        QA_util_log_info(code)
                         name = QA_fetch_stock_name(code)
                         QA_util_log_info('##JOB Now Code ==== {} {}'.format(str(code),str(name)), ui_log = None)
 
@@ -51,7 +50,7 @@ def track_roboot(target_tar, account, trading_date, percent, strategy_id,  excep
                         elif code[0:3] in ['000','002','300']:
                             code = 'SZ' + code
                         res1 = stock_daily(code, trading_date, trading_date)
-                        QA_util_log_info('{code}{name}-{trading_date}:daily: {daily}; weekly: {weekly}'.format(code=code,name=name,trading_date=trading_date,daily=res1[0],weekly=res[1]))
+                        QA_util_log_info('{code}{name}-{trading_date}:daily: {daily}; weekly: {weekly}'.format(code=code,name=name,trading_date=trading_date,daily=res1[0],weekly=res1[1]))
                         res2 = stock_hourly(code, trading_date, trading_date, mark_tm)
                         QA_util_log_info(res2, ui_log = None)
                         QA_util_log_info('{code}{name}-{trading_date}:hourly: {hourly}'.format(code=code,name=name,trading_date=trading_date,hourly=res2[0]))
