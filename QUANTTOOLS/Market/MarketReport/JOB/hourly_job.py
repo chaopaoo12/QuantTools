55f9494974439abd86f3050367fc11d62621d7d8
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2021-02-10 14:57:02 +0800

    QA

--- QUANTTOOLS/Market/MarketReport/JOB/hourly_job.py
+++ QUANTTOOLS/Market/MarketReport/JOB/hourly_job.py
@@ -21,53 +21,6 @@ def INDEX_hourly(trading_date, hour, strategy_id):
         if res == False:
             send_actionnotice(strategy_id,'{code}:{trading_date}'.format(code=code,trading_date=trading_date),'60min线趋势下跌',direction = 'SELL',offset='SELL',volume=None)
 
-
-def daily_job(trading_date, account = 'name:client-1', strategy_id = '趋势跟踪'):
-    tm = int(datetime.datetime.now().strftime("%H%M%S"))
-
-    morning_begin = "09:30:00"
-    morning_end = "11:30:00"
-    afternoon_begin = "13:00:00"
-    afternoon_end = "15:00:00"
-
-    while tm < int(time.strftime(morning_begin, time.strptime("09:30:00", "%H:%M:%S"))):
-        time.sleep(15)
-        tm = int(datetime.datetime.now().strftime("%H%M%S"))
-
-    mark = 0
-    mark_tm = morning_begin
-
-    while tm < int(time.strftime("%H%M%S", time.strptime(afternoon_end, "%H:%M:%S"))):
-
-        if tm >= int(time.strftime("%H%M%S", time.strptime(mark_tm))):
-
-            if mark_tm in ["10:30:00", "11:30:00", "14:00:00", "14:50:00"]:
-                ####job1 小时级报告 指数小时级跟踪
-                daily(trading_date, mark_tm, account, strategy_id)
-                INDEX_hourly(trading_date, mark_tm, strategy_id)
-                pass
-
-            time.sleep(5)
-            ###15分钟级程序 1 爬虫 2 分析
-
-            if tm > int(time.strftime("%H%M%S", time.strptime(morning_end, "%H:%M:%S"))):
-                mark = 0
-                mark_tm = (datetime.datetime.strptime(afternoon_begin, "%H:%M:%S") + datetime.timedelta(minutes=mark*15)).strftime("%H:%M:%S")
-            else:
-                mark += 1
-                mark_tm = (datetime.datetime.strptime(morning_begin, "%H:%M:%S") + datetime.timedelta(minutes=mark*15)).strftime("%H:%M:%S")
-
-        while tm > int(time.strftime("%H%M%S", time.strptime(morning_end, "%H:%M:%S"))) and tm < int(time.strftime("%H%M%S", time.strptime(afternoon_begin, "%H:%M:%S"))):
-            time.sleep(60)
-            tm = int(datetime.datetime.now().strftime("%H%M%S"))
-
-        time.sleep(60)
-        tm = int(datetime.datetime.now().strftime("%H%M%S"))
-
-    if tm > int(time.strftime("%H%M%S", time.strptime(afternoon_begin, "%H:%M:%S"))):
-        ###time out
-        pass
-
 def auto_btc_tracking(trading_date, strategy_id='BTC数据跟踪'):
     QA_util_log_info('##JOB Now Check Timing ==== {}'.format(str(trading_date)), ui_log = None)
 
