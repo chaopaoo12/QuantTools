e595cb5d28b4fcac7fbe4348a79a0c02386384e6
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2021-02-18 14:09:59 +0800

    QA

--- QUANTTOOLS/Market/MarketTools/trading_tools/tracking_base.py
+++ QUANTTOOLS/Market/MarketTools/trading_tools/tracking_base.py
@@ -7,7 +7,7 @@ import datetime
 def tracking_base(trading_date, strategy_id, func, model_name, file_name, percent, account, working_dir, exceptions):
 
     r_tar, prediction_tar, prediction = load_data(func, QA_util_get_last_day(trading_date), working_dir, model_name, file_name)
-    r_tar = prediction_tar[(prediction_tar.RANK <= 20)&(prediction_tar.TARGET5.isnull())].reset_index(level=0, drop=True).drop_duplicates(subset='NAME')
+    r_tar = prediction_tar[(prediction_tar.RANK <= 5)&(prediction_tar.TARGET5.isnull())].reset_index(level=0, drop=True).drop_duplicates(subset='NAME')
 
     QA_util_log_info('##JOB## Now Tracking ===== {}'.format(str(trading_date)))
 
