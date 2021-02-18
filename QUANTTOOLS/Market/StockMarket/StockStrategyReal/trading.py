dec2b256ae55d5018145d797803505b9beac3243
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2021-02-18 11:17:08 +0800

    QA

--- QUANTTOOLS/Market/StockMarket/StockStrategyReal/trading.py
+++ QUANTTOOLS/Market/StockMarket/StockStrategyReal/trading.py
@@ -2,17 +2,15 @@
 from .setting import working_dir, percent, exceptions
 from .concat_predict import concat_predict,concat_predict_real,concat_predict_hedge
 from .running import predict_stock_summary
-from QUANTTOOLS.Market.MarketTools import trading_base, load_data
+from QUANTTOOLS.Market.MarketTools import trading_base, load_data, trading_base2
 from QUANTTOOLS.QAStockETL.QAFetch import QA_fetch_get_stock_realtm_ask, QA_fetch_get_stock_indicator_realtime
 
 def trading(trading_date, func = concat_predict, model_name = 'stock_xg', file_name = 'prediction', percent = percent, account= 'name:client-1', working_dir = working_dir, exceptions = exceptions):
 
     r_tar, prediction_tar, prediction = load_data(func, trading_date, working_dir, model_name, file_name)
     r_tar = prediction_tar[(prediction_tar.RANK <= 20)&(prediction_tar.TARGET5.isnull())].reset_index(level=0, drop=True).drop_duplicates(subset='NAME')
-    mark = QA_fetch_get_stock_indicator_realtime(list(r_tar.index), trading_date, trading_date, type = 'hour')
-    mark = mark[mark.SKDJ_CROSS2 == 1]
-    mark = mark[mark.SKDJ_TR == 1]
-    res = trading_base(trading_date, r_tar.loc[mark.index], percent = percent, account= account, title = model_name, exceptions = exceptions)
+
+    res = trading_base2(trading_date, r_tar, percent = percent, account= account, title = model_name, exceptions = exceptions)
 
     return(res)
 
