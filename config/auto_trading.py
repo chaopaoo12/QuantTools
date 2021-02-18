ba569d5502afbbec9bf73416d87ebac83197ac91
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2021-02-18 11:19:12 +0800

    QA

--- config/auto_trading.py
+++ config/auto_trading.py
@@ -3,10 +3,6 @@ from QUANTAXIS.QAUtil import QA_util_today_str,QA_util_get_last_day,QA_util_get_
 
 
 if __name__ == '__main__':
-    if QA_util_get_last_day(QA_util_today_str()) == 'wrong date':
-        mark_day = QA_util_get_real_date(QA_util_today_str())
-    else:
-        mark_day = QA_util_get_last_day(QA_util_today_str())
 
     if QA_util_if_trade(QA_util_today_str()):
-        trading(mark_day)
+        trading(QA_util_today_str())
