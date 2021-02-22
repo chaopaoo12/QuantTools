a09ce96d7b44027d77fef30784b6e600fae2727c
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2021-02-22 19:10:16 +0800

    QA

--- QUANTTOOLS/Market/MarketTools/trading_tools/trading.py
+++ QUANTTOOLS/Market/MarketTools/trading_tools/trading.py
@@ -221,7 +221,7 @@ def trade_roboot2(target_tar, account, trading_date, percent, strategy_id, type=
                                     time.sleep(1)
 
                         if code in [i for i in list(target_tar.index) if i not in positions.code.tolist()]:
-                            if (res2.SKDJ_CROSS2_HR == True and res2.MA5_HR >= 0):
+                            if res2.SKDJ_CROSS2_HR == True and res2.MA5_HR >= 0:
                                 msg = 'SKDJ金叉'
                             elif res2.MACD_JC_HR == True:
                                 msg = 'MACD金叉'
