d7bdc06de28e816b2df78012b01074a9eb8c787c
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2021-02-23 11:38:05 +0800

    QA

--- QUANTTOOLS/Market/MarketTools/trading_tools/trading.py
+++ QUANTTOOLS/Market/MarketTools/trading_tools/trading.py
@@ -198,15 +198,13 @@ def trade_roboot2(target_tar, account, trading_date, percent, strategy_id, type=
                     for code in code_list:
                         name = QA_fetch_stock_name(code)
                         QA_util_log_info('##JOB Now Code {stm} ==== {code}({name})'.format(stm=str(stm),code=str(code),name=str(name)), ui_log = None)
-                        print(data)
-                        print(data.loc[(stm, code)][['SKDJ_TR_HR','SKDJ_CROSS1_HR','MACD_JC_HR','SKDJ_CROSS2_HR','MA5_HR','MA10_HR','MA60_HR']])
                         try:
                             if mark_tm == "09:30:00":
-                                res2 = data.loc[(stm, code)][['SKDJ_CROSS1','SKDJ_TR_HR','SKDJ_CROSS1_HR','MACD_JC_HR','SKDJ_CROSS2_HR','MA5_HR','MA10_HR','MA60_HR']]
+                                res2 = data.loc[(stm, code)][['SKDJ_CROSS1','SKDJ_TR_HR','SKDJ_CROSS1_HR','CROSS_JC_HR','SKDJ_CROSS2_HR','MA5_HR','MA10_HR','MA60_HR']]
                                 QA_util_log_info('{code}{name}-{stm}:hourly: {hourly}'.format(code=code,name=name,stm=stm,hourly=res2.SKDJ_TR_HR))
                             else:
                                 QA_util_log_info('error B ')
-                                res2 = data.loc[(stm, code)][['SKDJ_TR_HR','SKDJ_CROSS1_HR','MACD_JC_HR','SKDJ_CROSS2_HR','MA5_HR','MA10_HR','MA60_HR']]
+                                res2 = data.loc[(stm, code)][['SKDJ_TR_HR','SKDJ_CROSS1_HR','CROSS_JC_HR','SKDJ_CROSS2_HR','MA5_HR','MA10_HR','MA60_HR']]
                                 QA_util_log_info('{code}{name}-{stm}:hourly: {hourly}'.format(code=code,name=name,stm=stm,hourly=res2.SKDJ_TR_HR))
                                 res2.SKDJ_CROSS1 = None
                         except:
@@ -237,7 +235,7 @@ def trade_roboot2(target_tar, account, trading_date, percent, strategy_id, type=
                             if code in [i for i in list(target_tar.index) if i not in positions.code.tolist()]:
                                 if (res2.SKDJ_CROSS2_HR == True) and (res2.MA5_HR >= 0):
                                     msg = 'SKDJ金叉'
-                                #elif res2.MACD_JC_HR == True:
+                                #elif res2.CROSS_JC_HR == True:
                                 #    msg = 'MACD金叉'
                                 else:
                                     msg = None
