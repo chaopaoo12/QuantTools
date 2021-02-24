2d785a9bb04ec87c2ae5096315cbbbf47c5c0614
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2021-02-24 09:51:58 +0800

    QA

--- QUANTTOOLS/Market/StockMarket/StockStrategyReal/trading.py
+++ QUANTTOOLS/Market/StockMarket/StockStrategyReal/trading.py
@@ -3,14 +3,14 @@ from .setting import working_dir, percent, exceptions
 from .concat_predict import concat_predict,concat_predict_real,concat_predict_hedge
 from .running import predict_stock_summary
 from QUANTTOOLS.Market.MarketTools import trading_base, load_data, trading_base2
-from QUANTAXIS.QAUtil import QA_util_today_str,QA_util_get_last_day,QA_util_get_real_date,QA_util_if_trade
+from QUANTAXIS.QAUtil import QA_util_today_str,QA_util_get_last_day,QA_util_get_real_date,QA_util_if_trade,QA_util_log_info
 
 def trading(trading_date, func = concat_predict, model_name = 'stock_xg', file_name = 'prediction', percent = percent, account= 'name:client-1', working_dir = working_dir, exceptions = exceptions):
 
     r_tar, prediction_tar, prediction = load_data(func, QA_util_get_last_day(trading_date), working_dir, model_name, file_name)
     r_tar = prediction_tar[(prediction_tar.RANK <= 20)&(prediction_tar.TARGET5.isnull())].reset_index(level=0, drop=True).drop_duplicates(subset='NAME')
     per = round(r_tar[(r_tar.PASS_MARK.isnull())&(r_tar.O_PROB > 0.5)].shape[0]/20,1)
-
+    QA_util_log_info(r_tar[(r_tar.PASS_MARK.isnull())&(r_tar.O_PROB > 0.5)].shape[0])
     if per < 0.2:
         per = 0.2
     elif per >= 0.6:
