4e0b805a394f2e188e088e6a19beefa17ac89c19
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2021-03-01 13:21:25 +0800

    QA

--- config/setting.py
+++ config/setting.py
@@ -1,7 +1,5 @@
 
 from QUANTTOOLS.Market.StockMarket.StockStrategyReal.train_job import daily_train
 from QUANTTOOLS.Market.StockMarket.StockStrategyReal.daily_job import daily_run
-from QUANTTOOLS.Market.StockMarket.StockStrategyReal.daily_job import daily_run_real
 from QUANTTOOLS.Market.StockMarket.StockStrategyReal import Tracking
 from QUANTTOOLS.Market.StockMarket.StockStrategyReal import trading
-from QUANTTOOLS.Market.StockMarket.StockStrategyReal import morning_trading
