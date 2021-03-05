a9456bdfc69ed867f787d1e8205476165f42d00a
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2021-03-05 20:49:27 +0800

    QA

--- QUANTTOOLS/Ananlysis/Trends/trends.py
+++ QUANTTOOLS/Ananlysis/Trends/trends.py
@@ -9,7 +9,9 @@ def btc_daily(BTC):
     incr15 = day.iloc[-1:]['MA15'].values[0]
     incr = day.iloc[-1:]['MA15_C'].values[0]
     incrs = day.iloc[-1:]['MA15_D'].values[0]
-    return(day_check, week_check, incr, incrs,incr5,incr15)
+    SKDJ_MARK = day.iloc[-1:]['SKDJ_CROSS2'].values[0] + day.iloc[-1:]['SKDJ_CROSS1'].values[0] * -1
+    CCI_MARK = day.iloc[-1:]['CCI_CROSS1'].values[0] + day.iloc[-1:]['CCI_CROSS2'].values[0] * -1
+    return(day_check, week_check, incr, incrs,incr5,incr15,SKDJ_MARK,CCI_MARK)
 
 def money_daily(MONEY, date):
     day, week = trends_money(MONEY, date)
@@ -19,7 +21,9 @@ def money_daily(MONEY, date):
     incr15 = day.iloc[-1:]['MA15'].values[0]
     incr = day.iloc[-1:]['MA15_C'].values[0]
     incrs = day.iloc[-1:]['MA15_D'].values[0]
-    return(day_check, week_check, incr, incrs,incr5,incr15)
+    SKDJ_MARK = day.iloc[-1:]['SKDJ_CROSS2'].values[0] + day.iloc[-1:]['SKDJ_CROSS1'].values[0] * -1
+    CCI_MARK = day.iloc[-1:]['CCI_CROSS1'].values[0] + day.iloc[-1:]['CCI_CROSS2'].values[0] * -1
+    return(day_check, week_check, incr, incrs,incr5,incr15,SKDJ_MARK,CCI_MARK)
 
 def gold_daily(GOLD, date):
     day, week = trends_gold(GOLD, date)
@@ -29,7 +33,9 @@ def gold_daily(GOLD, date):
     incr15 = day.iloc[-1:]['MA15'].values[0]
     incr = day.iloc[-1:]['MA15_C'].values[0]
     incrs = day.iloc[-1:]['MA15_D'].values[0]
-    return(day_check, week_check, incr, incrs,incr5,incr15)
+    SKDJ_MARK = day.iloc[-1:]['SKDJ_CROSS2'].values[0] + day.iloc[-1:]['SKDJ_CROSS1'].values[0] * -1
+    CCI_MARK = day.iloc[-1:]['CCI_CROSS1'].values[0] + day.iloc[-1:]['CCI_CROSS2'].values[0] * -1
+    return(day_check, week_check, incr, incrs,incr5,incr15,SKDJ_MARK,CCI_MARK)
 
 def stock_daily(stock, start_date, end_date):
     day, week = trends_stock(stock,start_date,end_date)
@@ -39,7 +45,9 @@ def stock_daily(stock, start_date, end_date):
     incr15 = day.iloc[-1:]['MA15'].values[0]
     incr = day.iloc[-1:]['MA15_C'].values[0]
     incrs = day.iloc[-1:]['MA15_D'].values[0]
-    return(day_check, week_check, incr, incrs, incr5, incr15)
+    SKDJ_MARK = day.iloc[-1:]['SKDJ_CROSS2'].values[0] + day.iloc[-1:]['SKDJ_CROSS1'].values[0] * -1
+    CCI_MARK = day.iloc[-1:]['CCI_CROSS1'].values[0] + day.iloc[-1:]['CCI_CROSS2'].values[0] * -1
+    return(day_check, week_check, incr, incrs, incr5, incr15,SKDJ_MARK,CCI_MARK)
 
 def stock_hourly(stock, start_date, end_date, date_type):
     hour = trends_stock_hour(stock,start_date,end_date)
--- QUANTTOOLS/Market/MarketReport/JOB/daily_job.py
+++ QUANTTOOLS/Market/MarketReport/JOB/daily_job.py
@@ -8,23 +8,23 @@ def aotu_report(trading_date):
     BTC_RES = pd.DataFrame(columns=('code', 'daily', 'weekly'))
     for code in BTC:
         res = btc_daily(code)
-        BTC_RES = BTC_RES.append({'code':code, 'daily':res[0], 'weekly':res[1], '日线斜率':res[2], '斜率变动':res[3], '五日线偏离幅度':res[4], '十五日线偏离幅度':res[5]}, ignore_index=True)
+        BTC_RES = BTC_RES.append({'code':code, 'daily':res[0], 'weekly':res[1], '日线斜率':res[2], '斜率变动':res[3], '五日线偏离幅度':res[4], '十五日线偏离幅度':res[5], 'SKDJ_MARK':res[6], 'CCI_MARK':res[7]}, ignore_index=True)
 
     for code in GOLD:
         res = gold_daily(code, trading_date)
-        BTC_RES = BTC_RES.append({'code':code, 'daily':res[0], 'weekly':res[1], '日线斜率':res[2], '斜率变动':res[3], '五日线偏离幅度':res[4], '十五日线偏离幅度':res[5]}, ignore_index=True)
+        BTC_RES = BTC_RES.append({'code':code, 'daily':res[0], 'weekly':res[1], '日线斜率':res[2], '斜率变动':res[3], '五日线偏离幅度':res[4], '十五日线偏离幅度':res[5], 'SKDJ_MARK':res[6], 'CCI_MARK':res[7]}, ignore_index=True)
 
     for code in MONEY:
         res = money_daily(code, trading_date)
-        BTC_RES = BTC_RES.append({'code':code, 'daily':res[0], 'weekly':res[1], '日线斜率':res[2], '斜率变动':res[3], '五日线偏离幅度':res[4], '十五日线偏离幅度':res[5]}, ignore_index=True)
+        BTC_RES = BTC_RES.append({'code':code, 'daily':res[0], 'weekly':res[1], '日线斜率':res[2], '斜率变动':res[3], '五日线偏离幅度':res[4], '十五日线偏离幅度':res[5], 'SKDJ_MARK':res[6], 'CCI_MARK':res[7]}, ignore_index=True)
 
     for code in CN_INDEX:
         res = stock_daily(code, trading_date, trading_date)
-        BTC_RES = BTC_RES.append({'code':code, 'daily':res[0], 'weekly':res[1], '日线斜率':res[2], '斜率变动':res[3], '五日线偏离幅度':res[4], '十五日线偏离幅度':res[5]}, ignore_index=True)
+        BTC_RES = BTC_RES.append({'code':code, 'daily':res[0], 'weekly':res[1], '日线斜率':res[2], '斜率变动':res[3], '五日线偏离幅度':res[4], '十五日线偏离幅度':res[5], 'SKDJ_MARK':res[6], 'CCI_MARK':res[7]}, ignore_index=True)
 
     for code in US_INDEX:
         res = stock_daily(code, trading_date, trading_date)
-        BTC_RES = BTC_RES.append({'code':code, 'daily':res[0], 'weekly':res[1], '日线斜率':res[2], '斜率变动':res[3], '五日线偏离幅度':res[4], '十五日线偏离幅度':res[5]}, ignore_index=True)
+        BTC_RES = BTC_RES.append({'code':code, 'daily':res[0], 'weekly':res[1], '日线斜率':res[2], '斜率变动':res[3], '五日线偏离幅度':res[4], '十五日线偏离幅度':res[5], 'SKDJ_MARK':res[6], 'CCI_MARK':res[7]}, ignore_index=True)
     #BTC_RES = BTC_RES.rename(columns={'code':'标的', 'daily':'日线走势', 'weekly':'周线走势'}, inplace = True)
     target_body = build_table(BTC_RES, '目标持仓')
     msg = build_email(build_head(),target_body)
