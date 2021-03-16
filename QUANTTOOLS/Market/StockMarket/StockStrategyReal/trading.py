07c83ec134eb5b735573ed02cd6b25b8fb63b72f
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2021-03-17 00:43:09 +0800

    QA

--- QUANTTOOLS/Market/StockMarket/StockStrategyReal/trading.py
+++ QUANTTOOLS/Market/StockMarket/StockStrategyReal/trading.py
@@ -3,22 +3,28 @@ from .setting import working_dir, percent, exceptions
 from .concat_predict import concat_predict,concat_predict_real,concat_predict_hedge
 from .running import predict_stock_summary
 from QUANTTOOLS.Market.MarketTools import trading_base, load_data, trading_base2
-from QUANTAXIS.QAUtil import QA_util_today_str,QA_util_get_last_day,QA_util_get_real_date,QA_util_if_trade,QA_util_log_info
+from QUANTAXIS.QAUtil import QA_util_today_str,QA_util_get_last_day,QA_util_get_real_date,QA_util_if_trade,QA_util_log_info,QA_util_get_pre_trade_date
+from QUANTTOOLS.Model.FactorTools.QuantMk import get_index_quant_hour,get_index_quant_data,get_quant_data
 
 def trading(trading_date, func = concat_predict, model_name = 'stock_xg', file_name = 'prediction', percent = percent, account= 'name:client-1', working_dir = working_dir, exceptions = exceptions):
 
     r_tar, prediction_tar, prediction = load_data(func, QA_util_get_last_day(trading_date), working_dir, model_name = 'stock_xg', file_name = 'prediction')
     r_tar = prediction_tar[(prediction_tar.RANK <= 20)&(prediction_tar.TARGET5.isnull())].reset_index(level=0, drop=True).drop_duplicates(subset='NAME')
     per = round(prediction_tar[(prediction_tar.PASS_MARK.isnull())&(prediction_tar.O_PROB > 0.5)].shape[0]/20,1)
-    if per < 0.2:
-        per = 0.2
-    elif per >= 0.6:
+    if per >= 0.5:
         per = percent
+        pe_list = None
     else:
-        per = per
-    target_pool,prediction,start,end,Model_Date = func(QA_util_get_last_day(trading_date), working_dir, code = list(r_tar.index), type = 'crawl', model_name = 'stock_mars_day')
-    res = trading_base2(trading_date, target_pool.loc[QA_util_get_last_day(trading_date)], percent = per, account= account, title = model_name, exceptions = exceptions)
+        data = get_quant_data(QA_util_get_pre_trade_date(trading_date,5),trading_date,type='crawl', block=False, sub_block=False,norm_type=None)
+        pe_list = list(data[(data.ROE_RATE > 1)&(data.PE_RATE < 1)&(data.NETPROFIT_INRATE > 50)&(data.ROE_TTM >= 15)&(data.PE_TTM <= 30)].loc[trading_date].index)
+        per = percent
 
+    if pe_list is None:
+        target_pool,prediction,start,end,Model_Date = func(QA_util_get_last_day(trading_date), working_dir, code = list(r_tar.index), type = 'crawl', model_name = 'stock_mars_day')
+        res = trading_base2(trading_date, target_pool.loc[QA_util_get_last_day(trading_date)], percent = per, account= account, title = model_name, exceptions = exceptions)
+    else:
+        target_pool,prediction,start,end,Model_Date = func(QA_util_get_last_day(trading_date), working_dir, code = list(r_tar.index) + pe_list, type = 'crawl', model_name = 'stock_mars_day')
+        res = trading_base2(trading_date, target_pool.loc[QA_util_get_last_day(trading_date)], percent = per, account= account, title = model_name, exceptions = exceptions)
     return(res)
 
 def trading_real(trading_date, func = concat_predict, model_name = 'stock_xg_real', file_name = 'prediction_real', percent = percent, account= 'name:client-1', working_dir = working_dir, exceptions = exceptions):
