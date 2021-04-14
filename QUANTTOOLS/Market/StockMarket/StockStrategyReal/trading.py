2110954b5c63a824d5bde28062036ad72a4326f0
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2021-04-14 14:35:58 +0800

    QA

--- QUANTTOOLS/Market/StockMarket/StockStrategyReal/trading.py
+++ QUANTTOOLS/Market/StockMarket/StockStrategyReal/trading.py
@@ -26,7 +26,7 @@ def trading(trading_date, func = concat_predict, model_name = 'stock_xg', file_n
     if pe_list is None:
         target_pool,prediction,start,end,Model_Date = func(QA_util_get_last_day(trading_date), working_dir, code = list(r_tar.index), type = 'crawl', model_name = 'stock_mars_day')
     else:
-        target_pool,prediction,start,end,Model_Date = func(QA_util_get_last_day(trading_date), working_dir, code = list(set(pe_list + list(r_tar.index))), type = 'crawl', model_name = 'stock_mars_day')
+        target_pool,prediction,start,end,Model_Date = func(QA_util_get_last_day(trading_date), working_dir, code = pe_list, type = 'crawl', model_name = 'stock_mars_day')
     res = trading_base2(trading_date, target_pool.loc[QA_util_get_last_day(trading_date)], percent = per, account= account, title = model_name, exceptions = exceptions)
     return(res)
 
