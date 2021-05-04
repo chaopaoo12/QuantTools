325075d8cebb51b602e23a364754f3c1a7d3e14a
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2021-05-04 23:04:28 +0800

    QA

--- config/update_c.py
+++ config/update_c.py
@@ -121,7 +121,7 @@ if __name__ == '__main__':
             res = check_stock_fianacial(mark_day)
 
         res = check_stock_finper(mark_day)
-        while res is None or len(res[1]) > 30:
+        while res is None or len(res[1]) > 50:
             QA_SU_save_stock_fianacial_percent_day(start_date = mark_day, end_date = mark_day)
             res = check_stock_finper(mark_day)
 
