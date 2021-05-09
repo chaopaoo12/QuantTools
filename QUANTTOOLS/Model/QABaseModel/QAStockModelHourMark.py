d01882437b100321127e7ca25fff110b4fe82a70
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2021-05-10 00:21:49 +0800

    QA

--- QUANTTOOLS/Model/QABaseModel/QAStockModelHourMark.py
+++ QUANTTOOLS/Model/QABaseModel/QAStockModelHourMark.py
@@ -48,6 +48,7 @@ class QAStockModelHourMark(QAModel):
             self.code = code
         QA_util_log_info('##JOB Got Stock Quant hour Data by {type}, block: {block}, sub_block: {sub_block} ==== from {_from} to {_to} target:{target}'.format(type=type, block=self.block,sub_block=self.sub_block, _from=start, _to=end, target = self.target), ui_log = None)
         data = get_quant_data_hour(start, end, code = self.code, type= type,block = self.block, sub_block=self.sub_block, norm_type=self.norm_type)
+        data = data[(data.SKDJ_CROSS2_HR == 1)|(data.CROSS_JC_HR == 1)]
         QA_util_log_info('##JOB Now Reshape Different Columns ===== from {_from} to {_to}'.format(_from=start,_to = end), ui_log = None)
         cols1 = [i for i in data.columns if i not in [ 'moon','star','mars','venus','sun','MARK','date','datetime',
                                                        'OPEN_MARK','PASS_MARK','TARGET','TARGET3',
