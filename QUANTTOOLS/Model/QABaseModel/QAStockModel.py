593aad9aee771bbdcf0a330e4410721641936112
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2021-05-31 01:16:41 +0800

    QA

--- QUANTTOOLS/Model/QABaseModel/QAStockModel.py
+++ QUANTTOOLS/Model/QABaseModel/QAStockModel.py
@@ -133,7 +133,7 @@ class QAStockModel(QAModel):
         elif type == 'real':
             b = train[['y_pred','Z_PROB','O_PROB','RANK']]
 
-        b = b.join(data[['SKDJ_TR','SKDJ_K','SKDJ_TR_HR']])
+        b = b.join(data[['SKDJ_TR','SKDJ_K','SKDJ_TR_HR','SKDJ_K_HR']])
         return(b[b.y_pred==1], b)
 
 if __name__ == 'main':
