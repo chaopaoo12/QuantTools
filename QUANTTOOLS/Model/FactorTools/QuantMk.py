b3c2d032ce20a5352cb6d5e7e00676cc554c3a5e
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2021-06-02 02:54:08 +0800

    QA

--- QUANTTOOLS/Model/FactorTools/QuantMk.py
+++ QUANTTOOLS/Model/FactorTools/QuantMk.py
@@ -260,7 +260,7 @@ def get_quant_data_hour(start_date, end_date, code=None, type = 'model', block =
     elif type == 'model':
         res = QA_fetch_stock_quant_hour(codes, start_date, end_date, block = sub_block, norm_type =norm_type)
     elif type == 'real':
-        res = QA_fetch_get_stock_quant_hour(codes, start_date, end_date)
+        res = QA_fetch_get_stock_quant_hour(codes, start_date, end_date).drop(['date','time_stamp'],axis=1)
         res.columns = [x.upper() + '_HR' for x in res.columns]
     return(res)
 
