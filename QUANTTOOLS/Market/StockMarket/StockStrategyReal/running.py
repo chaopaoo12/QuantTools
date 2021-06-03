fac30100672eb90d1b442761e4b0ded735e68882
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2021-06-03 21:36:17 +0800

    QA

--- QUANTTOOLS/Market/StockMarket/StockStrategyReal/running.py
+++ QUANTTOOLS/Market/StockMarket/StockStrategyReal/running.py
@@ -58,13 +58,9 @@ def predict_stock_summary(trading_date, top_num=top, working_dir=working_dir):
 def predict_watch(trading_date, working_dir=working_dir):
     trading_date = QA_util_get_real_date(trading_date)
     data = get_quant_data(QA_util_get_pre_trade_date(trading_date,5),trading_date,type='crawl', block=False, sub_block=False,norm_type=None)
-    wk_list = data[(data.SKDJ_CROSS2_WK == 1)&(data.CCI_WK > 0)].index
-    pe_list = data[(data.ROE_RATE > 1)&(data.PE_RATE < 1)&(data.NETPROFIT_INRATE > 50)&(data.ROE_TTM >= 15)&(data.PE_TTM <= 30)].index
-    #both_list = [i for i in wk_list if i in pe_list]
-    target_pool,prediction,start,end,Model_Date = concat_predict(trading_date, working_dir, type = 'crawl', model_name = 'stock_mars_day')
-    target_pool1 = target_pool.reindex(index=wk_list).dropna(how='all')
-    target_pool2 = target_pool.reindex(index=pe_list).dropna(how='all')
-    target_pool3 = target_pool.reindex(index=pe_list).reindex(index=wk_list).dropna(how='all')
-    #target_pool1,prediction,start,end,Model_Date = concat_predict(trading_date, working_dir, code = wk_list, type = 'crawl', model_name = 'stock_mars_day')
-    #target_pool2,prediction,start,end,Model_Date = concat_predict(trading_date, working_dir, code = pe_list, type = 'crawl', model_name = 'stock_mars_day')
+    wk_list = data[data.SKDJ_K_WK <= 30][['SKDJ_K_WK','SKDJ_TR_WK','SKDJ_K','SKDJ_TR','SKDJ_K_HR','SKDJ_TR_HR','INDUSTRY','PASS_MARK','TARGET','TARGET3','TARGET4','TARGET5','TARGET10']]
+    pe_list = data[(data.ROE_RATE > 1)&(data.PE_RATE < 1)&(data.NETPROFIT_INRATE > 50)&(data.ROE_TTM >= 15)&(data.PE_TTM <= 30)][['SKDJ_K_WK','SKDJ_TR_WK','SKDJ_K','SKDJ_TR','SKDJ_K_HR','SKDJ_TR_HR','INDUSTRY','PASS_MARK','TARGET','TARGET3','TARGET4','TARGET5','TARGET10']]
+    target_pool2 = pe_list.reset_index().sort_values(by=['date','SKDJ_K'],ascending=[False,True]).set_index(['date','code'])
+    target_pool1 = wk_list.reset_index().sort_values(by=['date','SKDJ_K'],ascending=[False,True]).set_index(['date','code'])
+    target_pool3 = pe_list[pe_list.SKDJ_K_WK <= 30].reindex(index=pe_list).reindex(index=wk_list).dropna(how='all')
     base_report(trading_date, '观察报告', **{'低估值清单': target_pool2, '周线趋势清单': target_pool1, '复合清单': target_pool3})
