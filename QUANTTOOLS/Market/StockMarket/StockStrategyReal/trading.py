44df6aade7217c1a934c7fd5fefe73f8dba07e33
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2021-06-03 12:56:53 +0800

    QA

--- QUANTTOOLS/Market/StockMarket/StockStrategyReal/trading.py
+++ QUANTTOOLS/Market/StockMarket/StockStrategyReal/trading.py
@@ -9,7 +9,7 @@ from QUANTTOOLS.Model.FactorTools.QuantMk import get_index_quant_hour,get_index_
 def trading(trading_date, func = concat_predict, model_name = 'stock_xg', file_name = 'prediction', percent = percent, account= 'name:client-1', working_dir = working_dir, exceptions = exceptions):
 
     r_tar, prediction_tar, prediction = load_data(func, QA_util_get_last_day(trading_date), working_dir, model_name = 'stock_xg', file_name = 'prediction')
-    r_tar = prediction_tar[(prediction_tar.RANK <= 100)].loc[QA_util_get_last_day(trading_date)]
+    r_tar = prediction_tar[(prediction_tar.RANK <= 100)&(r_tar.SKDJ_K <= 40)].loc[QA_util_get_last_day(trading_date)]
     #r_tar = prediction_tar[(prediction_tar.RANK <= 20)&(prediction_tar.TARGET5.isnull())].drop_duplicates(subset='NAME',keep='last').reset_index().sort_values(by=['date','RANK'],ascending=[False,True]).set_index('code')
     per = prediction_tar[(prediction_tar.PASS_MARK.isnull())&(prediction_tar.O_PROB > 0.5)].shape[0]
 
@@ -19,18 +19,20 @@ def trading(trading_date, func = concat_predict, model_name = 'stock_xg', file_n
     else:
         data = get_quant_data(QA_util_get_pre_trade_date(trading_date,5),QA_util_get_last_day(trading_date),type='crawl', block=False, sub_block=False,norm_type=None)
         pe_list = data[(data.ROE_RATE > 1)&(data.PE_RATE < 1)&(data.NETPROFIT_INRATE > 50)&(data.ROE_TTM >= 15)&(data.PE_TTM <= 30)]
-        pe_list = list(pe_list[(pe_list.SKDJ_K <= 40)].loc[QA_util_get_last_day(trading_date)].index)
+        pe_list = pe_list[(pe_list.SKDJ_K <= 40)].loc[QA_util_get_last_day(trading_date)][['SKDJ_K_WK','SKDJ_TR_WK','SKDJ_K','SKDJ_TR','SKDJ_K_HR','SKDJ_TR_HR','INDUSTRY','PASS_MARK','TARGET','TARGET3','TARGET4','TARGET5','TARGET10']]
         if per > 0:
             per = 0.6
         else:
             per = 0.5
 
     if pe_list is None:
-        target_pool,prediction,start,end,Model_Date = func(QA_util_get_last_day(trading_date), working_dir, code = list(r_tar.index), type = 'crawl', model_name = 'stock_mars_day')
-        target_pool = target_pool.loc[QA_util_get_last_day(trading_date)].reindex(index=r_tar.index).dropna(how='all')
+        #target_pool,prediction,start,end,Model_Date = func(QA_util_get_last_day(trading_date), working_dir, code = list(r_tar.index), type = 'crawl', model_name = 'stock_mars_day')
+        target_pool = r_tar
+        #target_pool = target_pool.loc[QA_util_get_last_day(trading_date)].reindex(index=r_tar.index).dropna(how='all')
     else:
-        target_pool,prediction,start,end,Model_Date = func(QA_util_get_last_day(trading_date), working_dir, code = list(r_tar.index) + pe_list, type = 'crawl', model_name = 'stock_mars_day')
-        target_pool = target_pool.loc[QA_util_get_last_day(trading_date)].reindex(index=pe_list.index).dropna(how='all')
+        #target_pool,prediction,start,end,Model_Date = func(QA_util_get_last_day(trading_date), working_dir, code = list(r_tar.index) + pe_list, type = 'crawl', model_name = 'stock_mars_day')
+        target_pool = r_tar.append(pe_list).reset_index().drop_duplicates(subset=['code'],keep='first',inplace=False).set_index('code')
+        #target_pool = target_pool.loc[QA_util_get_last_day(trading_date)].reindex(index=pe_list.index).dropna(how='all')
 
     ##追涨抄底控制 K<=25 抄底 K>=75追涨
     #data = get_quant_data(QA_util_get_pre_trade_date(trading_date,5),QA_util_get_last_day(trading_date),code=list(target_pool.index),type='crawl', block=False, sub_block=False,norm_type=None)
