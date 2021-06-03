a781ec927c2c7eae919fc72bafb3629ea4d324cd
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2021-06-03 17:30:41 +0800

    QA

--- QUANTTOOLS/Market/StockMarket/StockStrategyReal/trading.py
+++ QUANTTOOLS/Market/StockMarket/StockStrategyReal/trading.py
@@ -15,10 +15,9 @@ def trading(trading_date, func = concat_predict, model_name = 'stock_xg', file_n
     data = get_quant_data(QA_util_get_pre_trade_date(trading_date,5),QA_util_get_last_day(trading_date),type='crawl', block=False, sub_block=False,norm_type=None)
     pe_list = data[(data.ROE_RATE > 1)&(data.PE_RATE < 1)&(data.NETPROFIT_INRATE > 50)&(data.ROE_TTM >= 15)&(data.PE_TTM <= 30)]
     pe_list = pe_list[(pe_list.SKDJ_K <= 40)].loc[QA_util_get_last_day(trading_date)][['SKDJ_K_WK','SKDJ_TR_WK','SKDJ_K','SKDJ_TR','SKDJ_K_HR','SKDJ_TR_HR','INDUSTRY','PASS_MARK','TARGET','TARGET3','TARGET4','TARGET5','TARGET10']]
-
+    
     if per >= 1:
         per = percent
-        pe_list = None
     else:
         per = 0.5
 
