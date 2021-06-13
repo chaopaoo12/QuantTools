0d2077757f20a612f9feabb606ff461317cc3048
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2021-06-13 20:45:06 +0800

    QA

--- QUANTTOOLS/Market/StockMarket/StockStrategyReal/train.py
+++ QUANTTOOLS/Market/StockMarket/StockStrategyReal/train.py
@@ -6,7 +6,7 @@ from QUANTTOOLS.Model.StockModel.StrategyXgboost15Min import QAStockXGBoost15Min
 from QUANTTOOLS.Model.IndexModel.IndexXGboost import QAIndexXGBoost
 from QUANTTOOLS.Model.IndexModel.IndexXGboostHour import QAIndexXGBoostHour
 from QUANTTOOLS.Model.IndexModel.IndexXGboost15Min import QAIndexXGBoost15Min
-from .setting import working_dir, stock_day_set, stock_hour_set, index_day_set, index_hour_set, stock_xg_set
+from .setting import working_dir, stock_day_set, stock_hour_set, index_day_set, index_hour_set, stock_xg_set, index_xg_set
 from QUANTTOOLS.Market.MarketTools.train_tools import prepare_train, start_train, save_report, load_data, prepare_data
 from QUANTTOOLS.QAStockETL.QAUtil.QADate_trade import QA_util_get_real_date,QA_util_get_last_day
 from QUANTAXIS.QAUtil import QA_util_add_months
@@ -92,6 +92,16 @@ def train_index(date, working_dir=working_dir):
     end_date = date
 
     index_model = load_data(index_model, start_date, end_date, norm_type=None)
+
+    index_model = load_data(index_model, start_date, end_date, norm_type=None)
+
+    index_model = prepare_data(index_model, start_date, QA_util_get_last_day(QA_util_get_real_date(date), 6), mark = 0.3, col = 'TARGET5', type='percent')
+
+    other_params = {'learning_rate': 0.1, 'n_estimators': 200, 'max_depth': 5, 'min_child_weight': 1, 'seed': 1,
+                    'subsample': 0.8, 'colsample_bytree': 0.8, 'gamma': 0, 'reg_alpha': 0, 'reg_lambda': 1}
+
+    index_model = start_train(index_model, index_xg_set, other_params, 0, 0.95)
+    save_report(index_model, 'index_xg', working_dir)
 
     index_model = prepare_data(index_model, start_date, QA_util_get_last_day(QA_util_get_real_date(date), 6), mark = 0, col = 'SKDJ_TR', type='value', shift = -2)
 
