c514d71842d032f9a47facf61991e6d7f719af66
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2021-06-18 17:44:45 +0800

    QA

--- QUANTTOOLS/QAStockETL/QAFetch/QAFinper.py
+++ QUANTTOOLS/QAStockETL/QAFetch/QAFinper.py
@@ -1,58 +1,49 @@
 from QUANTTOOLS.QAStockETL.QAFetch.QAQuery_Advance import QA_fetch_stock_fianacial_adv
 from QUANTAXIS.QAUtil import (QA_util_date_stamp,QA_util_get_pre_trade_date,QA_util_log_info)
 from QUANTTOOLS.QAStockETL.QAUtil import (QA_util_get_trade_range)
+import numpy as np
 
+def rolling_calc(data,N=5):
+    data[['PB_M', 'PE_TTM_M', 'PEEGL_TTM_M', 'PEG_M', 'PS_M']] = data[['PB', 'PE_TTM', 'PEEGL_TTM', 'PEG', 'PS']].rolling(window=N).mean()
+    data[['PB_2', 'PE_TTM_2', 'PEEGL_TTM_2', 'PEG_2', 'PS_2']] = data[['PB', 'PE_TTM', 'PEEGL_TTM', 'PEG', 'PS']].rolling(window=N).agg(lambda x:np.percentile(x, 75))
+    data[['PB_7', 'PE_TTM_7', 'PEEGL_TTM_7', 'PEG_7', 'PS_7']] = data[['PB', 'PE_TTM', 'PEEGL_TTM', 'PEG', 'PS']].rolling(window=N).agg(lambda x:np.percentile(x, 25))
+    data = data.assign(PB_VAL = data.PB / data.PB_M - 1,
+                       PB_DN = data.PB / data.PB_2 - 1,
+                       PB_UP = data.PB / data.PB_7 - 1,
 
-def rolling_rank(data):
-    return(data.rank(pct=True).iloc[-1])
+                       PE_TTM_VAL = data.PE_TTM / data.PE_TTM_M - 1,
+                       PE_TTM_DN = data.PE_TTM / data.PE_TTM_2 - 1,
+                       PE_TTM_UP = data.PE_TTM / data.PE_TTM_7 - 1,
 
-def rolling_median(data):
-    return((data/data.median()-1).iloc[-1])
+                       PEEGL_VAL = data.PEEGL_TTM / data.PEEGL_TTM_M - 1,
+                       PEEGL_DN = data.PEEGL_TTM / data.PEEGL_TTM_2 - 1,
+                       PEEGL_UP = data.PEEGL_TTM / data.PEEGL_TTM_7 - 1,
 
-def rolling_down(data):
-    return((data/data.quantile(0.25)-1).iloc[-1])
+                       PEG_VAL = data.PEG / data.PEG_M - 1,
+                       PEG_DN = data.PEG / data.PEG_2 - 1,
+                       PEG_UP = data.PEG / data.PEG_7 - 1,
 
-def rolling_up(data):
-    return((data/data.quantile(0.75)-1).iloc[-1])
+                       PS_VAL = data.PS / data.PS_M - 1,
+                       PS_DN = data.PS / data.PS_2 - 1,
+                       PS_UP = data.PS / data.PS_7 - 1,
+                       )
 
-def rolling_calc(data,N):
-    res1 = data.groupby('code').rolling(window=N)
-    data[['PE_VAL','PEEGL_VAL','PB_VAL','PEG_VAL','PS_VAL']] = \
-        res1.agg({'PE_TTM':rolling_median,
-                  'PEEGL_TTM':rolling_median,
-                  'PB':rolling_median,
-                  'PEG':rolling_median,
-                  'PS':rolling_median}).reset_index(level=0,drop=True)
-    return(data[['PE_VAL','PEEGL_VAL','PB_VAL','PEG_VAL','PS_VAL']])
-
-def rolling_calc1(data,N):
-    res1 = data.groupby('code').rolling(window=N)
-    data[['PE_VAL','PE_DN','PE_UP',
-          'PEEGL_VAL','PEEGL_DN','PEEGL_UP',
-          'PB_VAL','PB_DN','PB_UP',
-          'PEG_VAL','PEG_DN','PEG_UP',
-          'PS_VAL','PS_DN','PS_UP']] = res1.agg({'PE_TTM':[ rolling_median,rolling_down,rolling_up],
-                                                  'PEEGL_TTM':[ rolling_median,rolling_down,rolling_up],
-                                                  'PB':[ rolling_median,rolling_down,rolling_up],
-                                                  'PEG':[ rolling_median,rolling_down,rolling_up],
-                                                  'PS':[ rolling_median,rolling_down,rolling_up]}).reset_index(level=0,drop=True)
-    return(data[['PE_VAL','PE_DN','PE_UP',
-                 'PEEGL_VAL','PEEGL_DN','PEEGL_UP',
-                 'PB_VAL','PB_DN','PB_UP',
-                 'PEG_VAL','PEG_DN','PEG_UP',
-                 'PS_VAL','PS_DN','PS_UP']])
+    return(data[['PB_VAL','PB_DN','PB_UP'
+        ,'PE_TTM_VAL','PE_TTM_DN','PE_TTM_UP'
+        ,'PEEGL_VAL','PEEGL_DN','PEEGL_UP'
+        ,'PEG_VAL','PEG_DN','PEG_UP'
+        ,'PS_VAL','PS_DN','PS_UP']])
 
 def perank(data):
-    data[['PE_10VAL','PEEGL_10VAL','PB_10VAL','PEG_10VAL','PS_10VAL']] = rolling_calc(data, 10)
-    data[['PE_20VAL','PEEGL_20VAL','PB_20VAL','PEG_20VAL','PS_20VAL']] = rolling_calc(data, 20)
-    data[['PE_30VAL','PE_30DN','PE_30UP','PEEGL_30VAL','PEEGL_30DN','PEEGL_30UP','PB_30VAL','PB_30DN','PB_30UP','PEG_30VAL','PEG_30DN','PEG_30UP','PS_30VAL','PS_30DN','PS_30UP']] = rolling_calc1(data, 30)
-    data[['PE_60VAL','PE_60DN','PE_60UP','PEEGL_60VAL','PEEGL_60DN','PEEGL_60UP','PB_60VAL','PB_60DN','PB_60UP','PEG_60VAL','PEG_60DN','PEG_60UP','PS_60VAL','PS_60DN','PS_60UP']] = rolling_calc1(data, 60)
-    data[['PE_90VAL','PE_90DN','PE_90UP','PEEGL_90VAL','PEEGL_90DN','PEEGL_90UP','PB_90VAL','PB_90DN','PB_90UP','PEG_90VAL','PEG_90DN','PEG_90UP','PS_90VAL','PS_90DN','PS_90UP']] = rolling_calc1(data, 90)
-    return(data[['PE_10VAL','PEEGL_10VAL','PB_10VAL','PEG_10VAL','PS_10VAL',
-                 'PE_20VAL','PEEGL_20VAL','PB_20VAL','PEG_20VAL','PS_20VAL',
-                 'PE_30VAL','PE_30DN','PE_30UP','PEEGL_30VAL','PEEGL_30DN','PEEGL_30UP','PB_30VAL','PB_30DN','PB_30UP','PEG_30VAL','PEG_30DN','PEG_30UP','PS_30VAL','PS_30DN','PS_30UP',
-                 'PE_60VAL','PE_60DN','PE_60UP','PEEGL_60VAL','PEEGL_60DN','PEEGL_60UP','PB_60VAL','PB_60DN','PB_60UP','PEG_60VAL','PEG_60DN','PEG_60UP','PS_60VAL','PS_60DN','PS_60UP',
-                 'PE_90VAL','PE_90DN','PE_90UP','PEEGL_90VAL','PEEGL_90DN','PEEGL_90UP','PB_90VAL','PB_90DN','PB_90UP','PEG_90VAL','PEG_90DN','PEG_90UP','PS_90VAL','PS_90DN','PS_90UP']])
+
+    data[['PB_30VAL','PB_30DN','PB_30UP','PE_30VAL','PE_30DN','PE_30UP','PEEGL_30VAL','PEEGL_30DN','PEEGL_30UP','PEG_30VAL','PEG_30DN','PEG_30UP','PS30VAL','PS_30DN','PS_30UP']] = rolling_calc(data, 30)
+    data[['PB_60VAL','PB_60DN','PB_60UP','PE_60VAL','PE_60DN','PE_60UP','PEEGL_60VAL','PEEGL_60DN','PEEGL_60UP','PEG_60VAL','PEG_60DN','PEG_60UP','PS60VAL','PS_60DN','PS_60UP']] = rolling_calc(data, 30)
+    data[['PB_90VAL','PB_90DN','PB_90UP','PE_90VAL','PE_90DN','PE_90UP','PEEGL_90VAL','PEEGL_90DN','PEEGL_90UP','PEG_90VAL','PEG_90DN','PEG_90UP','PS90VAL','PS_90DN','PS_90UP']] = rolling_calc(data, 30)
+    return(data[['PB_30VAL','PB_30DN','PB_30UP','PE_30VAL','PE_30DN','PE_30UP','PEEGL_30VAL','PEEGL_30DN','PEEGL_30UP','PEG_30VAL','PEG_30DN','PEG_30UP','PS30VAL','PS_30DN','PS_30UP',
+                 'PB_60VAL','PB_60DN','PB_60UP','PE_60VAL','PE_60DN','PE_60UP','PEEGL_60VAL','PEEGL_60DN','PEEGL_60UP','PEG_60VAL','PEG_60DN','PEG_60UP','PS60VAL','PS_60DN','PS_60UP',
+                 'PB_90VAL','PB_90DN','PB_90UP','PE_90VAL','PE_90DN','PE_90UP','PEEGL_90VAL','PEEGL_90DN','PEEGL_90UP','PEG_90VAL','PEG_90DN','PEG_90UP','PS90VAL','PS_90DN','PS_90UP'
+                 ]])
+
 
 def QA_fetch_get_stock_financial_percent(code,start_date,end_date):
     start = QA_util_get_pre_trade_date(start_date,91)
