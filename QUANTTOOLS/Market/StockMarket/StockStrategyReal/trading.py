840514cf25941f6e2d05d908fed15f7df046bb70
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2021-06-26 17:05:39 +0800

    QA

--- QUANTTOOLS/Market/StockMarket/StockStrategyReal/trading.py
+++ QUANTTOOLS/Market/StockMarket/StockStrategyReal/trading.py
@@ -9,21 +9,21 @@ from QUANTTOOLS.Model.FactorTools.base_tools import find_stock
 
 def trading(trading_date, func = concat_predict, model_name = 'stock_xg', file_name = 'prediction', percent = percent, account= 'name:client-1', working_dir = working_dir, exceptions = exceptions):
 
-    data = get_index_quant_data(QA_util_get_pre_trade_date(trading_date,91),QA_util_get_last_day(trading_date),type='crawl', norm_type=None)
-    r = data[['PASS_MARK']].groupby('code').describe()
-    r.columns=['cnt','mean','std','min','p25','median','p75','max']
-    rr = r.join(data.loc[QA_util_get_last_day(trading_date)][['SKDJ_K','SKDJ_TR','SKDJ_K_WK','SKDJ_TR_WK','SKDJ_K_HR','SKDJ_TR_HR']])
-    rr['per'] = rr['p75'] / abs(rr['p25'])
-    rr1 = rr[((rr.per >= 1.5)|(rr['std'] >=1.8))&(rr.p75 >= 1)].reset_index()
-    QA_util_log_info(rr1[rr1.SKDJ_K <= 40])
+    #data = get_index_quant_data(QA_util_get_pre_trade_date(trading_date,91),QA_util_get_last_day(trading_date),type='crawl', norm_type=None)
+    #r = data[['PASS_MARK']].groupby('code').describe()
+    #r.columns=['cnt','mean','std','min','p25','median','p75','max']
+    #rr = r.join(data.loc[QA_util_get_last_day(trading_date)][['SKDJ_K','SKDJ_TR','SKDJ_K_WK','SKDJ_TR_WK','SKDJ_K_HR','SKDJ_TR_HR']])
+    #rr['per'] = rr['p75'] / abs(rr['p25'])
+    #rr1 = rr[((rr.per >= 1.5)|(rr['std'] >=1.8))&(rr.p75 >= 1)].reset_index()
+    #QA_util_log_info(rr1[rr1.SKDJ_K <= 40])
     r_tar, prediction_tar, prediction = load_data(func, QA_util_get_last_day(trading_date), working_dir, model_name = 'stock_xg', file_name = 'prediction')
 
-    r_tar = prediction_tar.loc[(QA_util_get_last_day(trading_date),find_stock(rr1[rr1.SKDJ_K <= 40].code.tolist())),]
-    r_tar = r_tar[r_tar.y_pred==1].reset_index('date')
+    #r_tar = prediction_tar.loc[(QA_util_get_last_day(trading_date),find_stock(rr1[rr1.SKDJ_K <= 40].code.tolist())),]
+    #r_tar = r_tar[r_tar.y_pred==1].reset_index('date')
 
     #r_tar = prediction_tar[prediction_tar.RANK <= 20].loc[QA_util_get_last_day(trading_date)]
-    #r_tar = prediction_tar[(prediction_tar.RANK <= 20)&(prediction_tar.TARGET5.isnull())].drop_duplicates(subset='NAME',keep='last').reset_index().sort_values(by=['date','RANK'],ascending=[False,True]).set_index('code')
-    #r_tar = prediction_tar.loc[(slice(None),list(r_tar.index)),].loc[QA_util_get_last_day(trading_date)]
+    r_tar = prediction_tar[(prediction_tar.RANK <= 20)&(prediction_tar.TARGET5.isnull())].drop_duplicates(subset='NAME',keep='last').reset_index().sort_values(by=['date','RANK'],ascending=[False,True]).set_index('code')
+    r_tar = prediction_tar.loc[(slice(None),list(r_tar.index)),].loc[QA_util_get_last_day(trading_date)]
     per = prediction_tar[(prediction_tar.PASS_MARK.isnull())&(prediction_tar.O_PROB > 0.5)].shape[0]
     #data = get_quant_data(QA_util_get_pre_trade_date(trading_date,5),QA_util_get_last_day(trading_date),type='crawl', block=False, sub_block=False,norm_type=None)
     #pe_list = data[(data.ROE_RATE > 1)&(data.PE_RATE < 1)&(data.NETPROFIT_INRATE > 50)&(data.ROE_TTM >= 15)&(data.PE_TTM <= 30)&(data.STOCK_TYPE <= 2)]
