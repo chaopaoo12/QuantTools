cb8425b3c4856e268875b99f7cde13e68c2743d1
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2021-07-08 00:11:06 +0800

    QA

--- QUANTTOOLS/Market/MarketTools/trading_tools/trading.py
+++ QUANTTOOLS/Market/MarketTools/trading_tools/trading.py
@@ -222,15 +222,18 @@ def trade_roboot2(target_tar, account, trading_date, percent, strategy_id, type=
                 else:
                     stm = trading_date + ' ' + mark_tm
                 data = get_quant_data_hour(QA_util_get_pre_trade_date(trading_date),trading_date,list(set(positions.code.tolist()+list(target_tar.index))), type= 'real')
+                res2 = data.loc[stm][['SKDJ_K_30M','SKDJ_TR_30M','SKDJ_K_HR','SKDJ_TR_HR','SKDJ_CROSS1_HR','CROSS_JC_HR','CROSS_SC_HR','SKDJ_CROSS2_HR','MA5_HR','MA5_30M','MA10_HR','MA60_HR','CCI_HR','CCI_CROSS1_HR','CCI_CROSS2_HR']].sort_values('SKDJ_K_HR')
+
+                target_list = list(target_tar.index)
                 QA_util_log_info('##JOB Now Time ==== {}'.format(str(stm)), ui_log = None)
 
                 ####job1 小时级报告 指数小时级跟踪
-                for code_list in [positions.code.tolist(), list(target_tar.index)]:
+                for code_list in [positions.code.tolist(), target_list]:
                     for code in code_list:
                         name = QA_fetch_stock_name(code)
                         QA_util_log_info('##JOB Now Code {stm} ==== {code}({name})'.format(stm=str(stm),code=str(code),name=str(name)), ui_log = None)
                         try:
-                            res2 = data.loc[(stm, code)][['SKDJ_K_30M','SKDJ_TR_HR','SKDJ_TR_30M','SKDJ_CROSS1_HR','CROSS_JC_HR','CROSS_SC_HR','SKDJ_CROSS2_HR','MA5_HR','MA5_30M','MA10_HR','MA60_HR','CCI_HR','CCI_CROSS1_HR','CCI_CROSS2_HR']]
+                            res2 = res2.loc[code]
                             QA_util_log_info('{code}{name}-{stm}:hourly: {hourly}'.format(code=code,name=name,stm=stm,hourly=res2.SKDJ_TR_HR))
                         except:
                             res2 = None
