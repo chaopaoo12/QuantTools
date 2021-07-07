9d41a93d58503a9f9821f3f1fab5d0141a066767
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2021-07-07 15:00:46 +0800

    QA

--- QUANTTOOLS/Market/MarketTools/trading_tools/trading.py
+++ QUANTTOOLS/Market/MarketTools/trading_tools/trading.py
@@ -241,11 +241,11 @@ def trade_roboot2(target_tar, account, trading_date, percent, strategy_id, type=
 
                             if code in positions.code.tolist():
                                 QA_util_log_info('##JOB Now Selling Check ==== {}'.format(code), ui_log = None)
-                                if res2.SKDJ_CROSS1_HR == True and res2.MA5_HR < 0:
+                                if res2.SKDJ_CROSS1_HR == True and res2.MA5_HR < 0 and res2.SKDJ_TR_30M < 1:
                                     msg = 'SKDJ死叉'
                                 #elif res2.MA10_HR < 0:
                                 #    msg = '打穿MA10'
-                                elif res2.SKDJ_TR_HR < 1 and res2.MA5_HR < 0:
+                                elif res2.SKDJ_TR_HR < 1 and res2.MA5_HR < 0 and res2.SKDJ_TR_30M < 1:
                                     msg = 'SKDJ止损:跌破MA5'
                                 elif res2.SKDJ_TR_30M < 1 and res2.MA5_30M < 0:
                                     ##当日错误入场之后 次日及早离场
