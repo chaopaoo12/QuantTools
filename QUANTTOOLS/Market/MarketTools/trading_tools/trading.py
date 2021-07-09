74b09f1fc89977ac6e29cb4233c6fd8e0eafd127
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2021-07-09 13:05:02 +0800

    QA

--- QUANTTOOLS/Market/MarketTools/trading_tools/trading.py
+++ QUANTTOOLS/Market/MarketTools/trading_tools/trading.py
@@ -225,7 +225,7 @@ def trade_roboot2(target_tar, account, trading_date, percent, strategy_id, type=
 
         while tm >= int(time.strftime("%H%M%S",time.strptime(morning_end, "%H:%M:%S"))) and tm <= int(time.strftime("%H%M%S",time.strptime(afternoon_begin, "%H:%M:%S"))):
             QA_util_log_info('##JOB Not Trading Time ==== {}'.format(str(trading_date)), ui_log = None)
-            time.sleep(600)
+            time.sleep(60)
             tm = int(datetime.datetime.now().strftime("%H%M%S"))
             QA_util_log_info(tm)
 
