1a84e592ca1b27d0a948d9490d4d4f61776505c7
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2021-07-15 14:49:39 +0800

    QA

--- QUANTTOOLS/Market/MarketTools/trading_tools/trading.py
+++ QUANTTOOLS/Market/MarketTools/trading_tools/trading.py
@@ -300,12 +300,14 @@ def trade_roboot2(target_tar, account, trading_date, percent, strategy_id, type=
                         QA_util_log_info('##JOB Now Buying Ckeck==== {}'.format(code), ui_log = None)
 
                         QA_util_log_info('##JOB Not On Buying ==== {} SKDJ_CROSS2_HR:{} CROSS_JC_HR:{} SKDJ_K_30M:{} SKDJ_TR_30M:{}'.format(code, res2.SKDJ_CROSS2_HR, res2.CROSS_JC_HR, res2.SKDJ_K_30M, res2.SKDJ_TR_30M))
-
-                        if (res2.SKDJ_CROSS2_30M == 1 and res2.SKDJ_K_HR < 50):
+                        QA_util_log_info(res2.SKDJ_CROSS2_30M)
+                        QA_util_log_info(res2.SKDJ_K_HR)
+                        QA_util_log_info(res2)
+                        if res2.SKDJ_CROSS2_30M == 1 and res2.SKDJ_K_HR < 50:
                             msg = 'SKDJ:30M金叉 小时线K:{}'.format(res2.SKDJ_K_HR)
                         elif res2.CROSS_JC_30M == 1:
                             msg = 'MACD金叉'
-                        elif (res2.SKDJ_CROSS2_30M == 1 and res2.SKDJ_K_TR == 1):
+                        elif res2.SKDJ_CROSS2_30M == 1 and res2.SKDJ_K_TR == 1:
                             msg = 'SKDJ金叉'
                         else:
                             msg = None
