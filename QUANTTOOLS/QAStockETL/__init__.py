9f49b1b64f0d17473cd02627ab3afcc4af5029d7
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2021-07-16 12:21:07 +0800

    QA

--- QUANTTOOLS/QAStockETL/__init__.py
+++ QUANTTOOLS/QAStockETL/__init__.py
@@ -131,3 +131,6 @@ from QUANTTOOLS.QAStockETL.Check import (check_index_day,check_stock_day,
                                          check_stock_fianacial,check_stock_adj,
                                          check_stock_quant,check_index_quant,
                                          check_stock_alpha101half)
+
+if __name__ == '__main__':
+    pass
