18f76f665778c62811d31425ff4d10a3b144eeef
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2021-07-17 14:43:52 +0800

    QA

--- QUANTTOOLS/Ananlysis/Trends/base_tools.py
+++ QUANTTOOLS/Ananlysis/Trends/base_tools.py
@@ -4,8 +4,6 @@ from QUANTTOOLS.QAStockETL.QAFetch import (QA_fetch_get_btc_day,QA_fetch_get_btc
                                            QA_fetch_get_usstock_day_xq,QA_fetch_get_stock_indicator_realtime)
 from QUANTTOOLS.QAStockETL.QAData import QA_DataStruct_Stock_day,QA_DataStruct_Stock_min,QA_DataStruct_Index_day,QA_DataStruct_Index_min
 from QUANTTOOLS.QAStockETL.QAFetch.QAIndicator import get_indicator
-import numpy as np
-import datetime
 
 def check(data):
     res = data.iloc[-1:].reset_index().set_index('code')
