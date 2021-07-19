c2e2b8111dbc8189031d2d122cdddf48e7b81887
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2021-07-19 21:20:38 +0800

    QA

--- QUANTTOOLS/QAStockETL/QAFetch/QAUsFinancial.py
+++ QUANTTOOLS/QAStockETL/QAFetch/QAUsFinancial.py
@@ -77,8 +77,8 @@ def QA_fetch_get_stock_min_sina(code, period='30', type='qfq'):
         code1 = code
     data = stock_zh_a_minute(code1, period, type).rename(columns={'day':'datetime'})
     data[['open','close','high','low','volume']] = data[['open','close','high','low','volume']].apply(pd.to_numeric)
-    data = data.assign(date_stamp=data['datetime'].apply(lambda x: QA_util_date_stamp(str(x)[0:10])))
-    data = data.assign(code=code)
+    data = data.assign(date_stamp=data['datetime'].apply(lambda x: QA_util_date_stamp(str(x)[0:10])),code=code)
+    data['datetime']=pd.to_datetime(data['datetime'],format='%m/%d/%Y %H:%M:%S')
     return(data)
 
 if __name__ == '__main__':
