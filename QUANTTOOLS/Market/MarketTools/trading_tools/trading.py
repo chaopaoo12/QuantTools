2e83fc5259a03b5519185f39a9db87db2c2ce755
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2021-07-21 22:19:32 +0800

    QA

--- QUANTTOOLS/Market/MarketTools/trading_tools/trading.py
+++ QUANTTOOLS/Market/MarketTools/trading_tools/trading.py
@@ -302,9 +302,9 @@ def trade_roboot2(target_tar, account, trading_date, percent, strategy_id, type=
                         QA_util_log_info('##JOB Now Buying Ckeck==== {}'.format(code), ui_log = None)
 
                         QA_util_log_info('##JOB Not On Buying ==== {} SKDJ_CROSS2_HR:{} CROSS_JC_HR:{} SKDJ_K_30M:{} SKDJ_TR_30M:{}'.format(code, res2.SKDJ_CROSS2_HR, res2.CROSS_JC_HR, res2.SKDJ_K_30M, res2.SKDJ_TR_30M))
-                        if res2.SKDJ_CROSS2_30M == 1 and res2.SKDJ_K_HR < 50 and res2.MA5_30M > 0:
+                        if res2.SKDJ_CROSS2_HR == 1 and res2.SKDJ_K_30M > 0 and res2.MA5_30M > 0:
                             msg = 'SKDJ:30M金叉 小时线K:{}'.format(res2.SKDJ_K_HR)
-                        elif res2.CROSS_JC_30M == 1 and res2.SKDJ_K_HR < 50 and res2.MA5_30M > 0:
+                        elif res2.CROSS_JC_HR == 1 and res2.SKDJ_K_30M > 0 and res2.MA5_30M > 0:
                             msg = 'MACD金叉'
                         #elif res2.SKDJ_CROSS2_30M == 1 and res2.SKDJ_TR_HR == 1:
                         #    msg = 'SKDJ金叉'
