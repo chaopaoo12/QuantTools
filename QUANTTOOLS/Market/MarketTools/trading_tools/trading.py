0a17e61f4e16a133c604c83d04092b81fad26a4c
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2021-07-28 11:05:40 +0800

    QA

--- QUANTTOOLS/Market/MarketTools/trading_tools/trading.py
+++ QUANTTOOLS/Market/MarketTools/trading_tools/trading.py
@@ -281,7 +281,7 @@ def trade_roboot2(target_tar, account, trading_date, percent, strategy_id, type=
             tm = int(datetime.datetime.now().strftime("%H%M%S"))
 
         if tm > int(time.strftime("%H%M%S",time.strptime(action_tm, "%H:%M:%S"))) and action_tm is not None:
-            for code in positions[positions['可用余额'] > 0].code.tolist() + target_list:
+            for code in positions[positions['股票余额'] > 0].code.tolist() + target_list:
                 name = QA_fetch_stock_name(code)
                 QA_util_log_info('##JOB Now Code {stm} ==== {code}({name})'.format(stm=str(stm),code=str(code),name=str(name)), ui_log = None)
                 try:
