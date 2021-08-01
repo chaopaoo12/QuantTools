2fa2684242801f247bf7883744ad4cfd537e820c
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2021-08-01 20:56:10 +0800

    QA

--- QUANTTOOLS/Market/StockMarket/StockStrategyReal/running.py
+++ QUANTTOOLS/Market/StockMarket/StockStrategyReal/running.py
@@ -138,7 +138,7 @@ def predict_target(trading_date, working_dir=working_dir):
 
     #out_ist = res[((res.SKDJ_CROSS1_30M == 1) | (res.SKDJ_TR_30M < 1)) & (res.MA5_30M < 0)].loc[(slice(None),target_list),][['date','SKDJ_K_HR','SKDJ_TR_HR','SKDJ_K_30M','SKDJ_TR_30M','MA5_30M','PASS_MARK','TARGET','TARGET3','TARGET5']]
 
-    base_report(trading_date, '模型汇总报告', **{'本日选股': target_pool[target_pool.RSI3 > target_pool.RSI2].sort_values('RANK').head(20),
+    base_report(trading_date, '模型汇总报告', **{'本日选股': target_pool[target_pool.RSI3 > target_pool.RSI2].sort_values('ATRR').head(20),
                                            'INDEX选股': rrr[(rrr.y_pred == 1)&(rrr.RSI3 > rrr.RSI2)],
                                            'PE选股': pe_list[(pe_list.y_pred == 1)&(pe_list.RSI3 > pe_list.RSI2)],
                                            #'进场信号':in_list,
