19ce81d66387156198f36604354278b8fee81471
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2021-08-06 12:25:07 +0800

    QA

--- QUANTTOOLS/Market/MarketTools/trading_tools/trading.py
+++ QUANTTOOLS/Market/MarketTools/trading_tools/trading.py
@@ -332,16 +332,17 @@ def trade_roboot2(target_tar, account, trading_date, percent, strategy_id, type=
                         QA_util_log_info('##JOB Now Buying Ckeck==== {}'.format(code), ui_log = None)
 
                         QA_util_log_info('##JOB Not On Buying ==== {} SKDJ_CROSS2_HR:{} CROSS_JC_HR:{} SKDJ_K_30M:{} SKDJ_TR_30M:{}'.format(code, res2.SKDJ_CROSS2_HR, res2.CROSS_JC_HR, res2.SKDJ_K_30M, res2.SKDJ_TR_30M))
-                        if res2.SKDJ_CROSS2_30M == 1 and res2.SKDJ_K_HR <= 40 and res2.SKDJ_TR_HR < 0 and round(res2.MA5_30M,2) >= 0:
-                            msg = 'SKDJ:30MIN金叉抄底 小时线K:{}'.format(res2.SKDJ_K_HR)
-                        elif res2.SKDJ_CROSS2_30M == 1 and res2.SKDJ_K_15M <= 50 and res2.SKDJ_K_15M > 0 and round(res2.MA5_30M,2) >= 0:
-                            msg = 'SKDJ:30MIN金叉抄底 30M线K:{}'.format(res2.SKDJ_K_15M)
+                        if res2.SKDJ_CROSS2_HR == 1 and res2.SKDJ_K_30M < 40 and res2.SKDJ_TR_30M > 0 and round(res2.MA5_30M,2) >= 0:
+                            msg = 'SKDJ:60MIN金叉追涨 小时线K:{}'.format(res2.SKDJ_K_HR)
+
+                        #elif res2.SKDJ_CROSS2_30M == 1 and res2.SKDJ_K_15M <= 50 and res2.SKDJ_K_15M > 0 and round(res2.MA5_30M,2) >= 0:
+                         #   msg = 'SKDJ:30MIN金叉抄底 30M线K:{}'.format(res2.SKDJ_K_15M)
                         #elif res2.SKDJ_CROSS2_30M == 1 and res2.SKDJ_K_30M <= 40 and res2.SKDJ_TR_HR < 0 and round(res2.MA5_30M,2) >= 0:
                         #    msg = 'SKDJ:30MIN金叉追涨 小时线K:{}'.format(res2.SKDJ_K_HR)
                         #elif res2.CROSS_JC_HR == 1 and res2.SKDJ_K_30M < 70 and res2.SKDJ_TR_30M > 0 and round(res2.MA5_30M,2) >= 0:
                         #    msg = 'MACD金叉'
-                        elif res2.SKDJ_CROSS2_HR == 1 and res2.SKDJ_K_30M < 40 and res2.SKDJ_TR_30M > 0 and round(res2.MA5_30M,2) >= 0:
-                            msg = 'SKDJ:60MIN金叉追涨 小时线K:{}'.format(res2.SKDJ_K_HR)
+                        #elif res2.SKDJ_CROSS2_30M == 1 and res2.SKDJ_K_HR <= 40 and res2.SKDJ_TR_HR < 0 and round(res2.MA5_30M,2) >= 0:
+                        #    msg = 'SKDJ:30MIN金叉抄底 小时线K:{}'.format(res2.SKDJ_K_HR)
                         #elif res2.SKDJ_CROSS2_30M == 1 and res2.SKDJ_TR_HR == 1:
                         #    msg = 'SKDJ金叉'
                         else:
