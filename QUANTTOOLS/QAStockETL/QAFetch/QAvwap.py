afff68059e80b315470a28a5288038dc41f20fd4
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2021-08-19 01:47:37 +0800

    QA

--- QUANTTOOLS/QAStockETL/QAFetch/QAvwap.py
+++ QUANTTOOLS/QAStockETL/QAFetch/QAvwap.py
@@ -1,5 +1,22 @@
 from QUANTTOOLS.QAStockETL.QAFetch.QAUsFinancial import QA_fetch_get_usstock_day_xq, QA_fetch_get_stock_min_sina
 from QUANTAXIS.QAUtil import QA_util_date_stamp,QA_util_get_pre_trade_date,QA_util_log_info,QA_util_get_trade_range
+from scipy import stats
+import numpy as np
+import pandas as pd
+import math
+
+def rolling_ols(y):
+    '''
+    滚动回归，返回滚动回归后的回归系数
+    rb: 因变量序列
+    '''
+    #y = pd.DataFrame.ewm(y,alpha=1.0/24,ignore_na=True).mean().values
+    model = stats.linregress(y=y, x=pd.Series(range(1,len(y)+1)))
+    return(math.atan(model.slope)*180/math.pi)
+
+def spc(data, N= 5):
+    data[['vamp_c']]= data.rolling(window=N).agg({'vamp':rolling_ols})
+    return(data)
 
 def QA_fetch_get_stock_vwap(code, start_date, end_date, period = '1'):
     try:
@@ -11,6 +28,7 @@ def QA_fetch_get_stock_vwap(code, start_date, end_date, period = '1'):
         data = data.assign(camt = data.groupby('date')['amt'].cumsum(),
                            cvolume = data.groupby('date')['volume'].cumsum())
         data['vamp'] = data['camt'] / data['cvolume']
+        data = data.apply(spc)
     except:
         QA_util_log_info("JOB No {} Minly data for {code} ======= from {start_date} to {end_date}".format(period, code=code, start_date=start_date,end_date=end_date))
         data = None
