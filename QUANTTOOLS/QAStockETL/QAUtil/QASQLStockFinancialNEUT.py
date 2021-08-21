0103700e28f0c686e1a2959d82d211f24426b3be
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2021-08-21 20:29:55 +0800

    QA

---
+++ QUANTTOOLS/QAStockETL/QAUtil/QASQLStockFinancialNEUT.py
@@ -0,0 +1,55 @@
+import cx_Oracle
+import pandas as pd
+from QUANTAXIS.QAUtil import QA_util_log_info
+from QUANTTOOLS.QAStockETL.QAData.database_settings import (Oracle_Database, Oracle_User, Oracle_Password, Oralce_Server, MongoDB_Server, MongoDB_Database)
+
+ORACLE_PATH2 = '{user}/{password}@{server}:1521/{database}'.format(database = Oracle_Database, password = Oracle_Password, server = Oralce_Server, user = Oracle_User)
+
+sql_text = '''select to_char(ORDER_DATE, 'yyyy-mm-dd') as "date",
+CODE AS "code",INDUSTRY,ln(TOTAL_MARKET) as TOTAL_MARKET,
+ln(TOTAL_MARKET*TRA_RATE+1) as TRA_RATE, 
+case
+when total_market * tra_rate / 100000000000 >= 100 then
+  0
+ when total_market * tra_rate / 100000000000 >= 10 then
+  1
+ when total_market * tra_rate / 100000000000 >= 5 then
+  2
+ when total_market * tra_rate / 100000000000 >= 3 then
+  3
+ when total_market * tra_rate / 100000000000 < 3 then
+  4
+ else
+  5
+end as stock_type,
+               DAYS,
+AVG5,AVG10,AVG20,AVG30,AVG60,
+LAG,LAG2,LAG3,LAG5,LAG10,LAG20,LAG30,LAG60,
+AVG5_TOR, AVG20_TOR,AVG30_TOR,AVG60_TOR,
+GROSSMARGIN,NETPROFIT_INRATE,OPERATINGRINRATE,NETCASHOPERATINRATE,
+PB, PBG, PC, PE_TTM, PEEGL_TTM, PEG, PM, PS,PSG,PT,
+PE_RATE,PEEGL_RATE,PB_RATE,ROE_RATE,ROE_TTM,ROA_RATE,GROSS_RATE,
+RNG,RNG_L,RNG_5,RNG_10,RNG_20, RNG_30, RNG_60, RNG_90,
+AMT_L,AMT_5,AMT_10,AMT_20,AMT_30,AMT_60,AMT_90,
+MAMT_5,MAMT_10,MAMT_20,MAMT_30,MAMT_60,MAMT_90,
+NEG5_RT,NEG5_RATE,NEG10_RT,NEG10_RATE,NEG20_RT,NEG20_RATE,
+NEG30_RT,NEG30_RATE,NEG60_RT,NEG60_RATE,NEG90_RT,NEG90_RATE,
+AVG5_RNG,AVG10_RNG,AVG20_RNG,AVG30_RNG,AVG60_RNG,ROA, ROE, 
+AVG5_CR, AVG10_CR,AVG20_CR,AVG30_CR,AVG60_CR,
+AVG5_TR,AVG10_TR,AVG20_TR,AVG30_TR,AVG60_TR,TOTALPROFITINRATE,
+AVG10_C_MARKET,AVG20_C_MARKET,AVG60_C_MARKET
+from STOCK_QUANT_FINANCIAL
+where order_Date >=
+to_date('{from_}', 'yyyy-mm-dd')
+and order_Date <=
+to_date('{to_}', 'yyyy-mm-dd')
+'''
+
+def QA_Sql_Stock_Financial_neut(from_ , to_, sql_text = sql_text, ui_log= None):
+    QA_util_log_info(
+        '##JOB01 Now Fetch Stock QuantData Financial ==== from {from_} to {to_}'.format(from_=from_,to_=to_), ui_log)
+    sql_text = sql_text.format(from_=from_,to_=to_)
+    conn = cx_Oracle.connect(ORACLE_PATH2)
+    data = pd.read_sql(sql=sql_text, con=conn)
+    conn.close()
+    return(data.drop_duplicates((['code', 'date'])).set_index(['date','code']))
---
+++ QUANTTOOLS/QAStockETL/QAUtil/QASQLStockFinancialPENEUT.py
@@ -0,0 +1,30 @@
+import cx_Oracle
+import pandas as pd
+import datetime
+from QUANTAXIS.QAUtil import QA_util_log_info
+from  QUANTAXIS.QAUtil import (QA_util_date_stamp,QA_util_today_str,
+                               QA_util_if_trade,QA_util_get_pre_trade_date)
+from QUANTTOOLS.QAStockETL.QAData.database_settings import (Oracle_Database, Oracle_User, Oracle_Password, Oralce_Server, MongoDB_Server, MongoDB_Database)
+
+ORACLE_PATH2 = '{user}/{password}@{server}:1521/{database}'.format(database = Oracle_Database, password = Oracle_Password, server = Oralce_Server, user = Oracle_User)
+
+sql_text = '''select to_char(ORDER_DATE, 'yyyy-mm-dd') as "date",
+CODE AS "code",
+PE_30VAL,PE_30DN,PE_30UP,PEEGL_30VAL,PEEGL_30DN,PEEGL_30UP,PB_30VAL,PB_30DN,PB_30UP,PS30VAL as PS_30VAL,PS_30DN,PS_30UP,
+PE_60VAL,PE_60DN,PE_60UP,PEEGL_60VAL,PEEGL_60DN,PEEGL_60UP,PB_60VAL,PB_60DN,PB_60UP,PS60VAL as PS_60VAL,PS_60DN,PS_60UP,
+PE_90VAL,PE_90DN,PE_90UP,PEEGL_90VAL,PEEGL_90DN,PEEGL_90UP,PB_90VAL,PB_90DN,PB_90UP,PS90VAL as PS_90VAL,PS_90DN,PS_90UP
+from STOCK_QUANT_FINANCIAL_PERCENT
+where order_Date >=
+to_date('{from_}', 'yyyy-mm-dd')
+and order_Date <=
+to_date('{to_}', 'yyyy-mm-dd')
+'''
+
+def QA_Sql_Stock_FinancialPercent_neut(from_ , to_, sql_text = sql_text, ui_log= None):
+    QA_util_log_info(
+        '##JOB01 Now Fetch Stock QuantData Financial Percent ==== from {from_} to {to_}'.format(from_=from_,to_=to_), ui_log)
+    sql_text = sql_text.format(from_=from_,to_=to_)
+    conn = cx_Oracle.connect(ORACLE_PATH2)
+    data = pd.read_sql(sql=sql_text, con=conn)
+    conn.close()
+    return(data.drop_duplicates((['code', 'date'])).set_index(['date','code']))
