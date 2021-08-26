50b25ee14f5743946d5f8ba98d9c2554145e32da
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2021-08-26 23:07:00 +0800

    QA

--- config/update_neut.py
+++ config/update_neut.py
@@ -63,9 +63,9 @@ if __name__ == '__main__':
             time.sleep(180)
             res =check_stock_techhour(mark_day)
 
-        res = check_stock_neut(mark_day)
+        res = check_stock_quant(mark_day)
         while res is None or len(res[1]) > 100:
             time.sleep(180)
-            res = check_stock_neut(mark_day)
+            res = check_stock_quant(mark_day)
 
         QA_SU_save_stock_neutral_day(start_date=mark_day,end_date=mark_day)
