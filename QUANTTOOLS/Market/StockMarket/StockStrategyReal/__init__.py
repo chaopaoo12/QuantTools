724301992bbc6454f7365bb9af9ef63e0dc258ea
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2021-08-26 10:59:24 +0800

    QA

--- QUANTTOOLS/Market/StockMarket/StockStrategyReal/__init__.py
+++ QUANTTOOLS/Market/StockMarket/StockStrategyReal/__init__.py
@@ -3,5 +3,5 @@ from .daily_job import daily_run, index_run, hourly_run
 from .running import predict, predict_real, predict_hedge
 from .Tracking import Tracking
 from .trading import trading, trading_real, trading_hedge
-from .train import daymodel_train, train_hedge, hourmodel_train, train_index
+from .train import daymodel_train, train_hedge, train_index
 from .train_job import daily_train, hourly_train, hedge_train, index_train
