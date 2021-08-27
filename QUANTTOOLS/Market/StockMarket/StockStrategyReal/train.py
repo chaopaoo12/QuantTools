6e7f7dc4fae1bc56c0823abddbb8b0fec473f068
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2021-08-27 17:20:26 +0800

    QA

--- QUANTTOOLS/Market/StockMarket/StockStrategyReal/train.py
+++ QUANTTOOLS/Market/StockMarket/StockStrategyReal/train.py
@@ -79,7 +79,7 @@ def daymodel_train(date, working_dir=working_dir):
 
     stock_model = set_target(stock_model, start_date, QA_util_get_last_day(QA_util_get_real_date(date), 6), mark = 0.3, col = 'TARGET5', type='percent')
 
-    stock_model = prepare_data(stock_model, stock_xg_set, 0, 0.95)
+    stock_model = prepare_data(stock_model, stock_day_set, 0, 0.95)
     other_params = {'learning_rate': 0.1, 'n_estimators': 200, 'max_depth': 5, 'min_child_weight': 1, 'seed': 1,
                     'subsample': 0.8, 'colsample_bytree': 0.8, 'gamma': 0, 'reg_alpha': 0, 'reg_lambda': 1}
 
