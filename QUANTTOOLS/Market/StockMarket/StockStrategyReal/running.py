3e4c0417a49f02c8fd750b2bec9ea7714d461284
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2021-09-05 19:23:06 +0800

    QA

--- QUANTTOOLS/Market/StockMarket/StockStrategyReal/running.py
+++ QUANTTOOLS/Market/StockMarket/StockStrategyReal/running.py
@@ -68,7 +68,7 @@ def predict_watch(trading_date, working_dir=working_dir):
     data = get_quant_data(QA_util_get_pre_trade_date(trading_date,5),trading_date,type='crawl', block=False, sub_block=False,norm_type=None)
     #r_tar, prediction_tar, prediction = load_data(concat_predict, trading_date, working_dir, 'stock_xg', 'prediction')
     #wk_list = data[data.SKDJ_K_WK <= 30][['SKDJ_K_WK','SKDJ_TR_WK','SKDJ_K','SKDJ_TR','SKDJ_K_HR','SKDJ_TR_HR','INDUSTRY','PASS_MARK','TARGET','TARGET3','TARGET4','TARGET5','TARGET10']]
-    data = data[(data.NETPROFIT_INRATE > 50)&(data.ROE_TTM >= 10)&(data.PE_TTM <= 100)&(data.SHORT10 < 0.01)&(data.SHORT20 < 0)&(data.SHORT10 > -0.01)&(data.SHORT20 > -0.01)&(data.MA60_C > 0)&(data.ATRR >= 0.02)][['INDUSTRY','NETPROFIT_INRATE','ROE_TTM','PE_TTM','SHORT10','SHORT20','MA60_C','PASS_MARK','TARGET','TARGET3','TARGET4','TARGET5','TARGET10','TARGET20','SKDJ_K','SKDJ_TR','SKDJ_K_HR','SKDJ_TR_HR','ATRR']]
+    data = data[(data.NETPROFIT_INRATE > 50)&(data.ROE_TTM >= 10)&(data.ROE >= 1)&(data.SHORT10.abs() < 0.01)&(data.SHORT20 < 0)&(data.SHORT20 > -0.01)&(data.MA60.abs() < 0.01)][['INDUSTRY','NETPROFIT_INRATE','ROE_TTM','PE_TTM','SHORT10','SHORT20','MA60_C','PASS_MARK','TARGET','TARGET3','TARGET4','TARGET5','TARGET10','TARGET20','SKDJ_K','SKDJ_TR','SKDJ_K_HR','SKDJ_TR_HR','MA60']]
     #pe_list = prediction_tar.loc[data.index]
     #target_pool1 = wk_list.reset_index().sort_values(by=['date','SKDJ_K'],ascending=[False,True]).set_index(['date','code'])
     base_report(trading_date, '观察报告', **{'初始清单':data})
