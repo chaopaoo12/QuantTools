7f62386db5918db670985226815644b770f37f22
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2021-09-29 17:04:55 +0800

    QA

--- QUANTTOOLS/Market/MarketTools/trading_tools/tracking_base.py
+++ QUANTTOOLS/Market/MarketTools/trading_tools/tracking_base.py
@@ -1,5 +1,5 @@
 from QUANTTOOLS.Market.MarketTools.trading_tools import load_data
-from QUANTTOOLS.Market.MarketTools.trading_tools.track import track_roboot,track_roboot2
+from QUANTTOOLS.Market.MarketTools.trading_tools.track import track_roboot,track_roboot2,track_roboot3
 from QUANTAXIS.QAUtil import QA_util_log_info, QA_util_get_last_day
 import time
 import datetime
@@ -9,7 +9,7 @@ def tracking_base(trading_date, strategy_id, account, exceptions):
 
     QA_util_log_info('##JOB## Now Tracking ===== {}'.format(str(trading_date)))
 
-    res = track_roboot2(account, trading_date, strategy_id, exceptions = exceptions)
+    res = track_roboot3(account, trading_date, strategy_id, exceptions = exceptions)
 
     return(res)
 
