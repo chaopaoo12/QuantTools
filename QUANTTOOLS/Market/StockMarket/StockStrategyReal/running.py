830c1072078ce53773825dd0b85212c4b839d738
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2021-09-30 18:16:46 +0800

    QA

--- QUANTTOOLS/Market/StockMarket/StockStrategyReal/running.py
+++ QUANTTOOLS/Market/StockMarket/StockStrategyReal/running.py
@@ -66,12 +66,13 @@ def predict_stock_summary(trading_date, top_num=top, working_dir=working_dir):
 def predict_watch(trading_date, working_dir=working_dir):
     trading_date = QA_util_get_real_date(trading_date)
     data = get_quant_data(QA_util_get_pre_trade_date(trading_date,5),trading_date,type='crawl', block=False, sub_block=False,norm_type=None)
-    #r_tar, prediction_tar, prediction = load_data(concat_predict, trading_date, working_dir, 'stock_xg', 'prediction')
-    #wk_list = data[data.SKDJ_K_WK <= 30][['SKDJ_K_WK','SKDJ_TR_WK','SKDJ_K','SKDJ_TR','SKDJ_K_HR','SKDJ_TR_HR','INDUSTRY','PASS_MARK','TARGET','TARGET3','TARGET4','TARGET5','TARGET10']]
-    data = data[(data.NETPROFIT_INRATE > 50)&(data.ROE_TTM >= 10)&(data.ROE >= 1)&(data.SHORT10.abs() < 0.01)&(data.SHORT20.abs() < 0.01)&(data.MA60.abs() < 0.01)][['INDUSTRY','NETPROFIT_INRATE','ROE_TTM','PE_TTM','SHORT10','SHORT20','MA60_C','PASS_MARK','TARGET','TARGET3','TARGET4','TARGET5','TARGET10','TARGET20','SKDJ_K','SKDJ_TR','SKDJ_K_HR','SKDJ_TR_HR','MA60']]
-    #pe_list = prediction_tar.loc[data.index]
-    #target_pool1 = wk_list.reset_index().sort_values(by=['date','SKDJ_K'],ascending=[False,True]).set_index(['date','code'])
-    base_report(trading_date, '观察报告', **{'初始清单':data})
+
+    codelist = data[(data.NETPROFIT_INRATE > 50)&(data.ROE_TTM >= 10)&(data.PB < 2)&(data.ROE >= 1)&(data.SHORT10.abs() < 0.01)&(data.SHORT20.abs() < 0.02)&(data.LONG60.abs() < 0.05)].loc[QA_util_get_trade_range(QA_util_get_pre_trade_date(trading_date,1),trading_date)].reset_index().code.unique().tolist()
+    res = data.loc[(trading_date,codelist),][['INDUSTRY','NETPROFIT_INRATE','ROE_TTM','PE_TTM','SHORT10','SHORT20','MA60_C','PASS_MARK','TARGET','TARGET3','TARGET4','TARGET5','TARGET10','TARGET20','SKDJ_K','SKDJ_TR','SKDJ_K_HR','SKDJ_TR_HR','MA60']]
+
+    base_report(trading_date, '观察报告', **{'爆发清单':res[~((data.SHORT10.abs() < 0.01)&(data.SHORT20.abs() < 0.02)&(data.LONG60.abs() < 0.05))&(data.AVG5 >= 5)],
+                                         '下跌清单':res[~((data.SHORT10.abs() < 0.01)&(data.SHORT20.abs() < 0.02)&(data.LONG60.abs() < 0.05))&(data.AVG5 <= -1)],
+                                         'stay清单':res[~((data.SHORT10.abs() < 0.01)&(data.SHORT20.abs() < 0.02)&(data.LONG60.abs() < 0.05))&(data.AVG5 < 5)&(data.AVG5 > -1)]})
 
 
 def index_predict_watch(trading_date, working_dir=working_dir):
