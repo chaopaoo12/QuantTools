76e484c40645ef0894c3d02a4834d67611009ce9
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2021-10-18 16:32:00 +0800

    QA

--- QUANTTOOLS/QAStockETL/QAFetch/QAQuantFactor.py
+++ QUANTTOOLS/QAStockETL/QAFetch/QAQuantFactor.py
@@ -550,19 +550,22 @@ def QA_fetch_get_index_quant_min(code, start_date, end_date, type='30min'):
         res = QA_fetch_get_index_indicator_realtime(code[0], start_date=start_date, end_date=end_date, type=type)
         return(res)
 
+def QA_fetch_get_stock_vwap_ascny(code, start_date, end_date, period):
+    return(QA_fetch_get_stock_vwap(code, start_date=start_date, end_date=end_date, period = period, type='real'))
+
 def QA_fetch_get_stock_vwap_min(code, start_date, end_date, type='30min'):
     if len(code) >= 5:
         pool = multiprocessing.Pool(5)
         with pool as p:
-            res = p.map(partial(QA_fetch_get_stock_vwap, start_date=start_date, end_date=end_date, period = type, type='real'), code)
+            res = p.map(partial(QA_fetch_get_stock_vwap_ascny, start_date=start_date, end_date=end_date, period = type), code)
         return(pd.concat(res))
     elif len(code) > 1:
         pool = multiprocessing.Pool(len(code))
         with pool as p:
-            res = p.map(partial(QA_fetch_get_stock_vwap, start_date=start_date, end_date=end_date, period = type, type='real'), code)
+            res = p.map(partial(QA_fetch_get_stock_vwap_ascny, start_date=start_date, end_date=end_date, period = type), code)
         return(pd.concat(res))
     else:
-        res = QA_fetch_get_stock_vwap(code[0], start_date=start_date, end_date=end_date, period = type, type='real')
+        res = QA_fetch_get_stock_vwap_ascny(code[0], start_date=start_date, end_date=end_date, period = type)
         return(res)
 
 if __name__ == '__main__':
