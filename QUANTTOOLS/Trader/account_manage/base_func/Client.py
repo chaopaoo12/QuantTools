b809e4d126358e514980db7aeec9bb7bf8dc8456
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2021-11-06 19:58:58 +0800

    QA

--- QUANTTOOLS/Trader/account_manage/base_func/Client.py
+++ QUANTTOOLS/Trader/account_manage/base_func/Client.py
@@ -1,6 +1,7 @@
 
 import logging
 import strategyease_sdk
+import easytrader
 from QUANTTOOLS.Trader.account_manage.Setting.setting import yun_ip, yun_port, easytrade_password
 from QUANTAXIS.QAUtil import QA_util_log_info
 from QUANTTOOLS.Message.message_func.wechat import send_actionnotice
@@ -10,31 +11,45 @@ from QUANTAXIS.QAFetch.QAQuery import QA_fetch_stock_to_market_date
 from QUANTTOOLS.QAStockETL.QAFetch import QA_fetch_stock_industry,QA_fetch_stock_name,QA_fetch_stock_industryinfo
 import pandas as pd
 
-def get_Client(host=yun_ip, port=yun_port, key=easytrade_password):
-    logging.basicConfig(level=logging.DEBUG)
-    client = strategyease_sdk.Client(host=host, port=port, key=key)
+def get_Client(host=yun_ip, port=yun_port, key=easytrade_password, trader_path=None):
+    if trader_path is None:
+        logging.basicConfig(level=logging.DEBUG)
+        client = strategyease_sdk.Client(host=host, port=port, key=key)
+        client.type = 'online'
+    else:
+        client = easytrader.use("ths")
+        client.connect(trader_path)
+        client.type = 'local'
     return(client)
 
-def get_UseCapital(client, account):
-    res = client.get_positions(account)
-    capital = float(res['sub_accounts']['可用金额'])
-    return(capital)
+def get_UseCapital(client, account=None):
+    if client.type == 'online':
+        res = client.get_positions(account)
+        capital = float(res['sub_accounts']['可用金额'])
+        return(capital)
+    elif client.type == 'local':
+        capital = client.balance['资金余额']
+        return(capital)
+    else:
+        pass
 
-def get_AllCapital(client, account):
-    res = client.get_positions(account)
-    capital = float(res['sub_accounts']['总 资 产'])
-    return(capital)
+def get_AllCapital(client, account=None):
+    if client.type == 'online':
+        res = client.get_positions(account)
+        capital = float(res['sub_accounts']['可用金额'])
+        return(capital)
+    elif client.type == 'local':
+        capital = client.balance['总资产']
+        return(capital)
+    else:
+        pass
 
-def get_StockPos(code, client, account):
-    positions = get_Position(client, account).set_index('code')
-    try:
-        res = float(positions.loc[code]['股票余额'])
-    except:
-        res = 0
-    return(res)
+def get_Position(client, account=None):
+    if client.type == 'local':
+        positions = pd.DataFrame(client.position)
+    elif client.type == 'local':
+        positions = client.get_positions(account)['positions'][['证券代码','证券名称','市值','股票余额','可用余额','冻结数量','参考盈亏','盈亏比例(%)']]
 
-def get_Position(client, account):
-    positions = client.get_positions(account)['positions'][['证券代码','证券名称','市值','股票余额','可用余额','冻结数量','参考盈亏','盈亏比例(%)']]
     positions=positions.astype({'市值':'float','股票余额':'float','可用余额':'float'})
     positions = positions[positions['股票余额'] > 0]
     positions['上市时间'] = positions['证券代码'].apply(lambda x:QA_util_get_days_to_today(str(QA_fetch_stock_to_market_date(x))))
@@ -43,22 +58,42 @@ def get_Position(client, account):
     positions =positions.rename(columns={'证券代码': 'code'})
     return(positions)
 
-def get_hold(client, account):
+def get_hold(client, account=None):
     logging.basicConfig(level=logging.DEBUG)
     try:
-        res = client.get_positions(account)
-        hold = float(res['sub_accounts']['股票市值'])/float(res['sub_accounts']['总 资 产'])
+        if client.type == 'online':
+            res = client.get_positions(account)
+            hold = float(res['sub_accounts']['股票市值'])/float(res['sub_accounts']['总 资 产'])
+        elif client.type == 'local':
+            res = client.balance
+            hold = float(res['股票市值'])/float(res['总 资 产'])
+
     except:
         hold = 0
     return(hold)
+
+def get_StockPos(code, client, account=None):
+    positions = get_Position(client, account).set_index('code')
+    try:
+        res = float(positions.loc[code]['股票余额'])
+    except:
+        res = 0
+    return(res)
 
 def check_Client(client, account, strategy_id, trading_date, exceptions, ui_log= None):
     logging.basicConfig(level=logging.DEBUG)
     try:
         QA_util_log_info(
             '##JOB Now Check Account Server ==== {}'.format(str(trading_date)), ui_log)
-        res = client.get_positions(account)
+        QA_util_log_info('##JOB Now Get Sub_Accounts ==== {}'.format(str(trading_date)), ui_log)
+        if client.type == 'online':
+            sub_accounts = get_AllCapital(client, account)
+        elif client.type == 'local':
+            sub_accounts = get_AllCapital(client)
+
     except:
+        QA_util_log_info('##JOB Now Get Sub_Accounts Failed ==== {}'.format(str(trading_date)), ui_log)
+        sub_accounts = 0
         send_email('错误报告', '云服务器错误,请检查', trading_date)
         send_actionnotice(strategy_id,
                           '错误报告:{}'.format(trading_date),
@@ -68,20 +103,8 @@ def check_Client(client, account, strategy_id, trading_date, exceptions, ui_log=
                           volume=None
                           )
     try:
-        QA_util_log_info('##JOB Now Get Sub_Accounts ==== {}'.format(str(trading_date)), ui_log)
-        sub_accounts = float(res['sub_accounts']['总 资 产'])
-    except:
-        QA_util_log_info('##JOB Now Get Sub_Accounts Failed ==== {}'.format(str(trading_date)), ui_log)
-        sub_accounts = 0
-    try:
         QA_util_log_info('##JOB Now Get Positions ==== {}'.format(str(trading_date)), ui_log)
-        positions = res['positions'][['证券代码','证券名称','股票余额','可用余额','冻结数量','参考盈亏','成本价','市价','市值','盈亏比例(%)']]
-        positions=positions.astype({'市值':'float','股票余额':'float','可用余额':'float'})
-        positions = positions[positions['股票余额'] > 0]
-        positions['上市时间'] = positions['证券代码'].apply(lambda x:QA_util_get_days_to_today(str(QA_fetch_stock_to_market_date(x))))
-        positions['INDUSTRY'] = positions['证券代码'].apply(lambda x:QA_fetch_stock_industryinfo(x).SWHY.values[0])
-        positions['NAME'] = positions['证券代码'].apply(lambda x:QA_fetch_stock_name(x).values[0])
-        positions =positions.rename(columns={'证券代码': 'code'})
+        positions = get_Position(client, account)
     except:
         QA_util_log_info('##JOB Now Get Positions Failed ==== {}'.format(str(trading_date)), ui_log)
         positions = pd.DataFrame()
--- QUANTTOOLS/Trader/account_manage/base_func/trading_message.py
+++ QUANTTOOLS/Trader/account_manage/base_func/trading_message.py
@@ -5,28 +5,48 @@ def Sell(client, account1, symbol, NAME, INDUSTRY, type, priceType, amount, pric
     if type == 'MARKET':
         QA_util_log_info('最优五档卖出 {code}({NAME},{INDUSTRY}) {cnt}股'.format(code=symbol, NAME= NAME,
                                                                           INDUSTRY= INDUSTRY,cnt=amount), ui_log=None)
-        client.sell(account1, symbol=symbol, type=type, priceType=priceType, amount=amount)
+        if client.type == 'online':
+            client.sell(account1, symbol=symbol, type=type, priceType=priceType, amount=amount)
+            message = None
+        elif client.type == 'local':
+            QA_util_log_info('not support')
+            message = None
     elif type == 'LIMIT':
         QA_util_log_info('限价卖出 {code}({NAME},{INDUSTRY}) {cnt}股 单价:{price} 总价:{tar}'.format(code=symbol, NAME= NAME,
                                                                                             INDUSTRY= INDUSTRY,cnt=amount, price=price, tar=price*amount), ui_log=None)
-        client.sell(account1, symbol=symbol, type=type, amount=amount , price = price)
+        if client.type == 'online':
+            client.sell(account1, symbol=symbol, type=type, amount=amount, price = price)
+            message = None
+        elif client.type == 'local':
+            message = client.sell(symbol=symbol, price = price, amount=amount)
     else:
         QA_util_log_info('Direction 参数错误 type{type} 必须为 [SELL, LIMIT]'.format(type=type), ui_log=None)
+
+    return(message)
 
 def Buy(client, account1, symbol, NAME, INDUSTRY, type, priceType, amount, price):
     if type == 'MARKET':
         QA_util_log_info('最优五档买入 {code}({NAME},{INDUSTRY}) {cnt}股'.format(code=symbol, NAME= NAME,
                                                                           INDUSTRY= INDUSTRY,cnt=amount), ui_log=None)
-        client.buy(account1, symbol=symbol, type=type, priceType=priceType, amount=amount)
+        if client.type == 'online':
+            client.buy(account1, symbol=symbol, type=type, priceType=priceType, amount=amount)
+            message = None
+        elif client.type == 'local':
+            QA_util_log_info('not support')
+            message = None
     elif type == 'LIMIT':
         QA_util_log_info('限价买入 {code}({NAME},{INDUSTRY}) {cnt}股 单价:{price} 总价:{tar}'.format(code=symbol, NAME= NAME,
                                                                                             INDUSTRY= INDUSTRY,cnt=amount, price=price, tar=price*amount), ui_log=None)
-        client.buy(account1, symbol=symbol, type=type, amount=amount , price = price)
+        if client.type == 'online':
+            client.buy(account1, symbol=symbol, type=type, amount=amount, price=price)
+            message = None
+        elif client.type == 'local':
+            message = client.buy(symbol=symbol, price=price, amount=amount)
     else:
         QA_util_log_info('Direction 参数错误 {type} 必须为 [SELL, LIMIT]'.format(type=type), ui_log=None)
+    return(message)
 
 def send_trading_message(account1, strategy_id, account_info, code, NAME, INDUSTRY, mark, direction, type, priceType, price, client):
-    codes = []
     try:
         send_actionnotice(strategy_id,
                           account_info,
@@ -38,9 +58,9 @@ def send_trading_message(account1, strategy_id, account_info, code, NAME, INDUST
                           )
 
         if direction == 'SELL':
-            Sell(client, account1, code, NAME, INDUSTRY, type, priceType, abs(mark), price)
+            message = Sell(client, account1, code, NAME, INDUSTRY, type, priceType, abs(mark), price)
         elif direction == 'BUY':
-            Buy(client, account1, code, NAME, INDUSTRY, type, priceType, abs(mark), price)
+            message = Buy(client, account1, code, NAME, INDUSTRY, type, priceType, abs(mark), price)
         elif direction == 'HOLD':
             QA_util_log_info('保持持股 {code}({NAME},{INDUSTRY}) {cnt}股'.format(code=code, NAME= NAME,
                                                                               INDUSTRY= INDUSTRY,cnt=abs(mark)), ui_log=None)
@@ -56,6 +76,4 @@ def send_trading_message(account1, strategy_id, account_info, code, NAME, INDUST
                           )
         QA_util_log_info('{type}交易失败 {code}({NAME},{INDUSTRY}) {cnt}股'.format(type=type, code=code, NAME= NAME,
                                                                           INDUSTRY= INDUSTRY,cnt=abs(mark)), ui_log=None)
-        print(e)
-        codes.append(code)
-    return(codes)
+    return(message)
