b989c6bb200308ff3807aa339107f513c2ccda8b
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2021-11-09 13:42:53 +0800

    QA

--- QUANTTOOLS/QAStockETL/QAFetch/QAQuantFactor.py
+++ QUANTTOOLS/QAStockETL/QAFetch/QAQuantFactor.py
@@ -572,12 +572,9 @@ def QA_fetch_get_stock_vwap_min(code, start_date, end_date, type='30min'):
 
     if len(code) > 30:
         for i in range(0, len(code), 30):
-            QA_util_log_info(i)
             res = pd.DataFrame()
             data = __acess(code[i:i+30], start_date, end_date, type)
             res.append(data)
-            QA_util_log_info(data)
-            time.sleep(10)
     else:
         res = __acess(code, start_date, end_date, type)
     return(res)
