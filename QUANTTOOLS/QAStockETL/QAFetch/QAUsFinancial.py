44ddf29b90078f618f868ea72cd192b1dc3c89f0
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2021-11-09 12:19:07 +0800

    QA

--- QUANTTOOLS/QAStockETL/QAFetch/QAUsFinancial.py
+++ QUANTTOOLS/QAStockETL/QAFetch/QAUsFinancial.py
@@ -91,9 +91,12 @@ def QA_fetch_get_index_min_sina(code,period='30'):
         code1 = code
     print('code',code,'code1',code1)
     data = stock_zh_a_minute(code1, period).rename(columns={'day':'datetime'})
-    data[['open','close','high','low','volume']] = data[['open','close','high','low','volume']].apply(pd.to_numeric)
-    data = data.assign(date_stamp=data['datetime'].apply(lambda x: QA_util_date_stamp(str(x)[0:10])),code=code)
-    data['datetime']=pd.to_datetime(data['datetime'],format='%Y-%m-%d %H:%M:%S')
+    try:
+        data[['open','close','high','low','volume']] = data[['open','close','high','low','volume']].apply(pd.to_numeric)
+        data = data.assign(date_stamp=data['datetime'].apply(lambda x: QA_util_date_stamp(str(x)[0:10])),code=code)
+        data['datetime']=pd.to_datetime(data['datetime'],format='%Y-%m-%d %H:%M:%S')
+    except:
+        data = None
     return(data)
 
 if __name__ == '__main__':
