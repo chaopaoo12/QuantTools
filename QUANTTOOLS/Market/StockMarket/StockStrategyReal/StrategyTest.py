70a88ebe3e361d7d9f034d5ba7ca78dcf301313d
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2021-11-25 23:12:29 +0800

    QA

--- QUANTTOOLS/Market/StockMarket/StockStrategyReal/StrategyTest.py
+++ QUANTTOOLS/Market/StockMarket/StockStrategyReal/StrategyTest.py
@@ -57,8 +57,8 @@ def balance(data, position, sub_account, percent):
         data = data[(data.signal == 1)]
         data = data.assign(市值=0, 可用余额=0)
 
-    data = data.assign(target_position=1 / data.signal.sum(),
-                     target_capital=data.target_position * sub_account * percent)
+    data = data.assign(target_position=1 / data.signal.sum())
+    data = data.assign(target_capital=data.target_position * sub_account * percent)
 
     # 方案2
     # data = pd.assign(target_position=1 / data.signal.sum(),
