84ae61b3d351ba7fa5c6ddbce4a50f4257129f8a
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2021-11-25 18:08:59 +0800

    QA

--- QUANTTOOLS/Market/MarketTools/__init__.py
+++ QUANTTOOLS/Market/MarketTools/__init__.py
@@ -1,4 +1,5 @@
-from .train_tools import prepare_train, start_train, save_report, load_data, prepare_data, norm_data, set_target
-from .predict_tools import make_prediction, save_prediction, load_prediction, check_prediction,make_stockprediction,make_indexprediction
-from .predict_tools import prediction_report, predict_base, predict_index_base, Index_Report, predict_index_dev, predict_stock_dev, base_report
-from .trading_tools import load_data, trading_base, tracking_base, trading_base2
+from .TrainTools import prepare_train, start_train, save_report, load_data, prepare_data, norm_data, set_target
+from .PredictTools import make_prediction, save_prediction, load_prediction, check_prediction,make_stockprediction,make_indexprediction
+from .PredictTools import prediction_report, predict_base, predict_index_base, Index_Report, predict_index_dev, predict_stock_dev, base_report
+from .TradingTools import load_data, trading_base, tracking_base, trading_base2, TradeRobotBase
+from .StrategyTools import StrategyBase
