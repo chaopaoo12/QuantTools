9c1c1fe3594a6c36f41223ef090fe251dd0f25a2
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2022-02-03 19:33:13 +0800

    QA

--- QUANTTOOLS/Model/QABaseModel/QAStockModelNeut.py
+++ QUANTTOOLS/Model/QABaseModel/QAStockModelNeut.py
@@ -22,9 +22,9 @@ class QAStockModelNeut(QAModel):
         self.code = code
         QA_util_log_info('##JOB Got Stock Data by {type}, block: {block}, sub_block: {sub_block} ==== from {_from} to {_to} target:{target}'.format(type=type, block=self.block,sub_block=self.sub_block, _from=start, _to=end, target = self.target), ui_log = None)
         data = get_quant_data_neut(start, end, code = self.code, type= type,block = self.block, sub_block=self.sub_block)
-        code_all = QA_fetch_get_stockcode_real(QA_fetch_stock_all().code.unique().tolist())
-        code_old = QA_fetch_code_old().code.unique().tolist()
-        code_new = QA_fetch_code_new().code.unique().tolist()
+        code_all = QA_fetch_get_stockcode_real(QA_fetch_stock_all()['code'].unique().tolist())
+        code_old = QA_fetch_code_old()['code'].unique().tolist()
+        code_new = QA_fetch_code_new()['code'].unique().tolist()
         codes = QA_fetch_stock_om_all()
         if self.code is None:
             codes = codes
