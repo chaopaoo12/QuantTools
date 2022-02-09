4e0d09853edb367d0785da5de9d2a83ccbf5df3d
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2022-02-09 10:53:49 +0800

    QA

--- QUANTTOOLS/Market/StockMarket/StockStrategyReal/Trading.py
+++ QUANTTOOLS/Market/StockMarket/StockStrategyReal/Trading.py
@@ -1,6 +1,6 @@
 from .setting import working_dir, percent, exceptions, strategy_id
 from .concat_predict import concat_predict, concat_predict_neut
-from QUANTTOOLS.Market.MarketTools import load_data, StrategyRobotBase, StrategyBase
+from QUANTTOOLS.Market.MarketTools import load_data, StrategyRobotBase, StrategyBase, on_bar
 from QUANTAXIS.QAUtil import QA_util_get_last_day,QA_util_get_real_date, QA_util_get_pre_trade_date
 from .StrategyOne import signal, balance, tracking_signal, track_balance
 
@@ -18,56 +18,7 @@ def trading_new(trading_date, working_dir=working_dir):
                          #+ prediction_tar3[prediction_tar3.y_pred == 1].loc[QA_util_get_last_day(trading_date)].reset_index().code.unique().tolist()
                          ))
 
-    time_list = ['09:30:00',
-                 '09:35:00',
-                 '09:40:00',
-                 '09:45:00',
-                 '09:50:00',
-                 '09:55:00',
-                 '10:00:00',
-                 '10:05:00',
-                 '10:10:00',
-                 '10:15:00',
-                 '10:20:00',
-                 '10:25:00',
-                 '10:30:00',
-                 '10:35:00',
-                 '10:40:00',
-                 '10:45:00',
-                 '10:50:00',
-                 '10:55:00',
-                 '11:00:00',
-                 '11:05:00',
-                 '11:10:00',
-                 '11:15:00',
-                 '11:20:00',
-                 '11:25:00',
-                 '11:30:00',
-                 '13:00:00',
-                 '13:05:00',
-                 '13:10:00',
-                 '13:15:00',
-                 '13:20:00',
-                 '13:25:00',
-                 '13:30:00',
-                 '13:35:00',
-                 '13:40:00',
-                 '13:45:00',
-                 '13:50:00',
-                 '13:55:00',
-                 '14:00:00',
-                 '14:05:00',
-                 '14:10:00',
-                 '14:15:00',
-                 '14:20:00',
-                 '14:25:00',
-                 '14:30:00',
-                 '14:35:00',
-                 '14:40:00',
-                 '14:45:00',
-                 '14:50:00',
-                 '14:55:00',
-                 '15:00:00']
+    time_list = on_bar('09:30:00', '15:00:00', 5, [['11:30:00', '13:00:00']])
 
     robot = StrategyRobotBase(code_list, time_list, trading_date)
     robot.set_account(strategy_id)
@@ -88,56 +39,7 @@ def tracking_new(trading_date):
     code_list = None
 
     #[i.strftime('%H:%M:%S') for i in pd.date_range(start='2019-01-09 09:30:00',end = '2019-01-09 15:00:00',freq='5T')]
-    time_list = ['09:30:00',
-                 '09:35:00',
-                 '09:40:00',
-                 '09:45:00',
-                 '09:50:00',
-                 '09:55:00',
-                 '10:00:00',
-                 '10:05:00',
-                 '10:10:00',
-                 '10:15:00',
-                 '10:20:00',
-                 '10:25:00',
-                 '10:30:00',
-                 '10:35:00',
-                 '10:40:00',
-                 '10:45:00',
-                 '10:50:00',
-                 '10:55:00',
-                 '11:00:00',
-                 '11:05:00',
-                 '11:10:00',
-                 '11:15:00',
-                 '11:20:00',
-                 '11:25:00',
-                 '11:30:00',
-                 '13:00:00',
-                 '13:05:00',
-                 '13:10:00',
-                 '13:15:00',
-                 '13:20:00',
-                 '13:25:00',
-                 '13:30:00',
-                 '13:35:00',
-                 '13:40:00',
-                 '13:45:00',
-                 '13:50:00',
-                 '13:55:00',
-                 '14:00:00',
-                 '14:05:00',
-                 '14:10:00',
-                 '14:15:00',
-                 '14:20:00',
-                 '14:25:00',
-                 '14:30:00',
-                 '14:35:00',
-                 '14:40:00',
-                 '14:45:00',
-                 '14:50:00',
-                 '14:55:00',
-                 '15:00:00']
+    time_list = time_list = on_bar('09:30:00', '15:00:00', 1, [['11:30:00', '13:00:00']])
 
     robot = StrategyRobotBase(code_list, time_list, trading_date)
     robot.set_account(strategy_id)
