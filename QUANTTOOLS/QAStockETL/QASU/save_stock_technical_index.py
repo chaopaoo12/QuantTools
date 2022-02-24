be4ab014c42f839db0b5459acc519ff372a0e30b
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2022-02-24 23:28:45 +0800

    QA

--- QUANTTOOLS/QAStockETL/QASU/save_stock_technical_index.py
+++ QUANTTOOLS/QAStockETL/QASU/save_stock_technical_index.py
@@ -2,7 +2,7 @@
 import pymongo
 from QUANTAXIS.QAUtil import (DATABASE, QA_util_getBetweenQuarter, QA_util_log_info, QA_util_add_months,
                               QA_util_to_json_from_pandas, QA_util_today_str,QA_util_get_pre_trade_date,
-                              QA_util_datetime_to_strdate,QA_util_code_tolist)
+                              QA_util_datetime_to_strdate,QA_util_code_tolist,QA_util_get_trade_range)
 from QUANTTOOLS.QAStockETL.QAFetch import QA_fetch_stock_all,QA_fetch_stock_om_all
 from QUANTTOOLS.QAStockETL.QAFetch import QA_fetch_get_stock_indicator,QA_fetch_get_index_indicator
 from QUANTAXIS.QAFetch.QAQuery_Advance import QA_fetch_index_list_adv
@@ -45,16 +45,29 @@ def QA_SU_save_stock_technical_index_day(codes=None, start_date=None,end_date=No
             print(error0)
             err.append(str(code))
 
-    for item in codes:
+    date_rng = QA_util_get_trade_range(start_date, end_date)
 
-        QA_util_log_info('The {} of Total {}'.format
-                         ((codes.index(item) +1), len(codes)))
+    if len(date_rng) > 10:
+        for item in codes:
 
-        strProgressToLog = 'DOWNLOAD PROGRESS {}'.format(str(float((codes.index(item) +1) / len(codes) * 100))[0:4] + '%', ui_log)
-        intProgressToLog = int(float((codes.index(item) +1) / len(codes) * 100))
-        QA_util_log_info(strProgressToLog, ui_log= ui_log, ui_progress= ui_progress, ui_progress_int_value= intProgressToLog)
+            QA_util_log_info('The {} of Total {}'.format
+                             ((codes.index(item) +1), len(codes)))
 
-        __saving_work( item,start_date,end_date)
+            strProgressToLog = 'DOWNLOAD PROGRESS {}'.format(str(float((codes.index(item) +1) / len(codes) * 100))[0:4] + '%', ui_log)
+            intProgressToLog = int(float((codes.index(item) +1) / len(codes) * 100))
+            QA_util_log_info(strProgressToLog, ui_log= ui_log, ui_progress= ui_progress, ui_progress_int_value= intProgressToLog)
+
+            __saving_work( item,start_date,end_date)
+    else:
+        k=100
+        for i in range(0, len(codes), k):
+            code = codes[i:i+k]
+            QA_util_log_info('The {} of Total {}'.format
+                             ((i +k ), len(codes)))
+            strProgressToLog = 'DOWNLOAD PROGRESS {}'.format(str(float((i + k) / len(codes) * 100))[0:4] + '%', ui_log)
+            intProgressToLog = int(float((i + k ) / len(codes) * 100 ))
+            QA_util_log_info(strProgressToLog, ui_log= ui_log, ui_progress= ui_progress, ui_progress_int_value= intProgressToLog)
+            __saving_work(code, start_date, end_date)
 
     if len(err) < 1:
         QA_util_log_info('SUCCESS save stock_technical_index ^_^',  ui_log)
