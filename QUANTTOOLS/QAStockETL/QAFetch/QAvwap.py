0fc1a24dff9c10d37efab6ca612572c2e58b8e3d
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2022-03-05 22:35:56 +0800

    QA

--- QUANTTOOLS/QAStockETL/QAFetch/QAvwap.py
+++ QUANTTOOLS/QAStockETL/QAFetch/QAvwap.py
@@ -47,7 +47,7 @@ def rolling_atan(y):
     return(round(math.atan(model.slope),4))
 
 def spc(data, N= 240):
-    data[['VAMP_C']]= data.rolling(window=N,min_periods=5).agg({'VAMP':rolling_ols})
+    data[['VAMPC_DEGRESS','VAMPC_SLOPE','VAMPC_ATAN']]= data.rolling(window=N,min_periods=5).agg({'VAMP':[rolling_ols,rolling_slope,rolling_atan]})
     return(data)
 
 #def spc5(data, N= 5):
