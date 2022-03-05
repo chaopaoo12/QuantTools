47622b7c5cc036804e5573b0a34b12b9ee82d08b
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2022-03-05 20:02:53 +0800

    QA

--- QUANTTOOLS/QAStockETL/QAFetch/QAQuantFactor.py
+++ QUANTTOOLS/QAStockETL/QAFetch/QAQuantFactor.py
@@ -505,21 +505,6 @@ def QA_fetch_get_stock_quant_hour(code, start_date, end_date):
         res = QA_fetch_get_stock_indicator_realtime(code[0], start_date=start_date, end_date=end_date, type='hour')
         return(res)
 
-def QA_fetch_get_stock_quant_min(code, start_date, end_date, type='30min'):
-    if len(code) >= 5:
-        pool = multiprocessing.Pool(5)
-        with pool as p:
-            res = p.map(partial(QA_fetch_get_stock_indicator_realtime, start_date=start_date, end_date=end_date, type=type), code)
-        return(pd.concat(res))
-    elif len(code) > 1:
-        pool = multiprocessing.Pool(len(code))
-        with pool as p:
-            res = p.map(partial(QA_fetch_get_stock_indicator_realtime, start_date=start_date, end_date=end_date, type=type), code)
-        return(pd.concat(res))
-    else:
-        res = QA_fetch_get_stock_indicator_realtime(code[0], start_date=start_date, end_date=end_date, type=type)
-        return(res)
-
 def QA_fetch_get_index_quant_hour(code, start_date, end_date):
     if len(code) > 5:
         pool = multiprocessing.Pool(5)
@@ -549,6 +534,27 @@ def QA_fetch_get_index_quant_min(code, start_date, end_date, type='30min'):
     else:
         res = QA_fetch_get_index_indicator_realtime(code[0], start_date=start_date, end_date=end_date, type=type)
         return(res)
+
+def QA_fetch_get_stock_quant_min(code, start_date, end_date, type='30min'):
+
+    pool = multiprocessing.Pool(15)
+
+    k = 1
+    while k <= 5:
+        try:
+            with pool as p:
+                res = p.map(partial(QA_fetch_get_stock_indicator_realtime, start_date=start_date, end_date=end_date, type=type), code)
+            QA_util_log_info('##JOB QA_fetch_get_stock_quant_min finish with {k} times  ===================='.format(k=k), ui_log=None)
+            break
+        except:
+            k += 1
+            QA_util_log_info('##JOB QA_fetch_get_stock_quant_min failed with {k} times ===================='.format(k=k), ui_log=None)
+            time.sleep(5)
+
+    if k > 5:
+        return None
+    else:
+        return(pd.concat(res))
 
 def QA_fetch_get_stock_vwap_min(code, start_date, end_date, type='30min'):
 
