fa50411172e3ce15b480a178eebc6cc2bbf3bda5
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2022-03-08 01:54:23 +0800

    QA

--- config/update_neut.py
+++ config/update_neut.py
@@ -63,7 +63,6 @@ if __name__ == '__main__':
         res = check_stock_quant(mark_day)
         while res is None or len(res[1]) > 100:
             time.sleep(180)
-            QA_SU_save_stock_quant_data_day(start_date=mark_day,end_date=mark_day)
             res = check_stock_quant(mark_day)
 
         QA_SU_save_stock_neutral_day(start_date=mark_day,end_date=mark_day)
