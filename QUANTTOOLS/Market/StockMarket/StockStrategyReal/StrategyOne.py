8fd717f5dd25d072286bbecb9ffd80dfe7e27771
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2022-04-13 06:12:16 +0800

    QA

--- QUANTTOOLS/Market/StockMarket/StockStrategyReal/StrategyOne.py
+++ QUANTTOOLS/Market/StockMarket/StockStrategyReal/StrategyOne.py
@@ -116,6 +116,10 @@ def signal(buy_list, tmp_list, position, trading_date, mark_tm):
             data.loc[(data.DISTANCE < -0.03) & (data.VAMP_K > -0.03) & (data.CLOSE_K > 0) & (data.VAMP < data.yes_close), "signal"] = 1
             data.loc[(data.DISTANCE < -0.03) & (data.VAMP_K > -0.03) & (data.CLOSE_K > 0) & (data.VAMP < data.yes_close), "msg"] = '水线下VMAP超跌'
 
+            # 强制止损
+            data.loc[(data.pct_chg <= 5) & (data.CLOSE_K < 0) & (data.VAMP_K < 0.01), "signal"] = 0
+            data.loc[(data.pct_chg <= 5) & (data.CLOSE_K < 0) & (data.VAMP_K < 0.01), "msg"] = '强制止损'
+
         elif time_check_after('09:33:00') is True:
             data.loc[(data.VAMPC_K >= 0.2) & (data.DISTANCE < 0.02) & (data.VAMP > data.yes_close * 0.95), "signal"] = 1
             data.loc[(data.VAMPC_K >= 0.2) & (data.DISTANCE < 0.02) & (data.VAMP > data.yes_close * 0.95), "msg"] = '早盘追涨:水线上VMAP上升通道'
