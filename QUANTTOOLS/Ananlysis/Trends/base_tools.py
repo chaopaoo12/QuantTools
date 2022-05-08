ba9d8f52605b568f480494ff8f5470f32dd12f3d
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2022-05-08 16:12:36 +0800

    QA

--- QUANTTOOLS/Ananlysis/Trends/base_tools.py
+++ QUANTTOOLS/Ananlysis/Trends/base_tools.py
@@ -43,7 +43,7 @@ def trends_func(func, code, date):
     data_index = QA_DataStruct_Stock_day(day.drop('date_stamp',axis=1).set_index(['date','code']))
     data_ind = indicator(data_index, 'day')
     data_ind[['mean','per25','per75','perc']] = day['close'].rolling(1800).agg(['mean', per25, per75, perc])
-    data_ind['close'] = day['close']
+    data_ind[['mean','per25','per75','perc','close']] = day.set_index(['date','code'])[['mean','per25','per75','perc','close']]
 
     week = day.drop('date_stamp',axis=1).set_index(['date']).resample('W').agg({'code':'last','open':'first','high':'max','low':'min','close':'last'})
     week_index = QA_DataStruct_Stock_day(week.reset_index().set_index(['date','code']))
@@ -55,7 +55,7 @@ def trends_func1(func, code):
     data_index = QA_DataStruct_Stock_day(day.drop('date_stamp',axis=1).set_index(['date','code']))
     data_ind = indicator(data_index, 'day')
     data_ind[['mean','per25','per75','perc']] = day['close'].rolling(1800).agg(['mean', per25, per75, perc])
-    data_ind['close'] = day['close']
+    data_ind[['mean','per25','per75','perc','close']] = day.set_index(['date','code'])[['mean','per25','per75','perc','close']]
 
     week = day.drop('date_stamp',axis=1).set_index(['date']).resample('W').agg({'code':'last','open':'first','high':'max','low':'min','close':'last'})
     week_index = QA_DataStruct_Stock_day(week.reset_index().set_index(['date','code']))
