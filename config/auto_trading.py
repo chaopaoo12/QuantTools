d8396c1908e58f349723dfcda07eba96c8d91162
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2022-05-11 19:01:03 +0800

    QA

--- config/auto_trading.py
+++ config/auto_trading.py
@@ -6,13 +6,14 @@ from QUANTTOOLS.Message.message_func.wechat import send_actionnotice
 if __name__ == '__main__':
 
     if QA_util_if_trade(QA_util_today_str()):
-        try:
-            trading_new(QA_util_today_str())
-        except:
-            send_actionnotice('自动交易错误报告',
-                              '错误报告:{}'.format(QA_util_today_str()),
-                              '交易程序执行中断,请检查',
-                              direction = 'HOLD',
-                              offset='HOLD',
-                              volume=None
-                              )
+        trading_new(QA_util_today_str())
+        #try:
+        #    trading_new(QA_util_today_str())
+        #except:
+        #    send_actionnotice('自动交易错误报告',
+        #                      '错误报告:{}'.format(QA_util_today_str()),
+        #                      '交易程序执行中断,请检查',
+        #                      direction = 'HOLD',
+        #                      offset='HOLD',
+        #                      volume=None
+        #                      )
