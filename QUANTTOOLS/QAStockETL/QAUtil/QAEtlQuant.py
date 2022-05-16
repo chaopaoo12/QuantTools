9c8c9de6327e5ed25d9bd389eb37382e4eb33eb9
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2022-05-16 23:38:31 +0800

    QA

--- QUANTTOOLS/QAStockETL/QAUtil/QAEtlQuant.py
+++ QUANTTOOLS/QAStockETL/QAUtil/QAEtlQuant.py
@@ -572,7 +572,6 @@ def QA_util_etl_stock_quant(deal_date = None,ui_log= None):
        pb_rank
   from stock_analysis_data a
  where order_date = to_date('{start_date}', 'yyyy-mm-dd')
- and (turnoverRatio * 1000 >= 10 or order_Date - market_day >= 15)
 '''
     if deal_date is None:
         QA_util_log_info('Must Have A DATE')
