934cb6565042035ad44a03a0970fcf87aecd1ef0
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2022-05-31 16:00:13 +0800

    QA

--- QUANTTOOLS/Market/StockMarket/StockStrategyReal/train.py
+++ QUANTTOOLS/Market/StockMarket/StockStrategyReal/train.py
@@ -93,7 +93,7 @@ def daymodel_train(date, working_dir=working_dir):
     stock_model = start_train(stock_model, other_params)
     save_report(stock_model, 'stock_mars_day', working_dir)
 
-    res_a, res_b, res_c, res_d = watch_func(end_date)
+    res_a, res_b, res_c, res_d = watch_func(start_date, end_date)
 
     stock_model.data = stock_model.data.reindex(res_b.index)
     stock_model = set_target(stock_model, start_date, QA_util_get_last_day(QA_util_get_real_date(date), 6), mark = 3, col = 'TARGET', type='value')
