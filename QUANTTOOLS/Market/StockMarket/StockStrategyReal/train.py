65552ddbaf4a7322630ac039bb124caeba95fc0a
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2022-06-16 17:13:55 +0800

    QA

--- QUANTTOOLS/Market/StockMarket/StockStrategyReal/train.py
+++ QUANTTOOLS/Market/StockMarket/StockStrategyReal/train.py
@@ -52,12 +52,7 @@ def daymodel_train(date, working_dir=working_dir):
 
     stock_model = set_target(stock_model, start_date, QA_util_get_last_day(QA_util_get_real_date(date), 6), mark = 0.3, col = 'TARGET5', type='percent')
 
-    tock_model = prepare_data(stock_model, None, 0, 0.95)
-    other_params = {'learning_rate': 0.1, 'n_estimators': 200, 'max_depth': 5, 'min_child_weight': 1, 'seed': 1,
-                    'subsample': 0.8, 'colsample_bytree': 0.8, 'gamma': 0, 'reg_alpha': 0, 'reg_lambda': 1}
-
-    stock_model = start_train(stock_model, other_params)
-
+    stock_model = prepare_data(stock_model, stock_xg_nn, 0, 0.95)
     other_params = {'learning_rate': 0.1, 'n_estimators': 200, 'max_depth': 5, 'min_child_weight': 1, 'seed': 1,
                     'subsample': 0.8, 'colsample_bytree': 0.8, 'gamma': 0, 'reg_alpha': 0, 'reg_lambda': 1}
 
@@ -67,13 +62,8 @@ def daymodel_train(date, working_dir=working_dir):
     stock_model.data = stock_model.data[stock_model.data.OPEN_MARK == 0]
     stock_model = set_target(stock_model, start_date, QA_util_get_last_day(QA_util_get_real_date(date), 6), mark = 0.3, col = 'TARGET', type='percent')
 
-    stock_model = prepare_data(stock_model, None, 0, 0.95)
-    other_params = {'learning_rate': 0.1, 'n_estimators': 200, 'max_depth': 5, 'min_child_weight': 1, 'seed': 1,
-                    'subsample': 0.8, 'colsample_bytree': 0.8, 'gamma': 0, 'reg_alpha': 0, 'reg_lambda': 1}
 
-    stock_model = start_train(stock_model, other_params)
-
-    stock_model = prepare_data(stock_model, stock_model.info['importance'].head(100).featur.tolist(), None, 0.01)
+    stock_model = prepare_data(stock_model, stock_day_nn, None, 0.01)
     other_params = {'learning_rate': 0.1, 'n_estimators': 200, 'max_depth': 5, 'min_child_weight': 1, 'seed': 1,
                     'subsample': 0.8, 'colsample_bytree': 0.8, 'gamma': 0, 'reg_alpha': 0, 'reg_lambda': 1}
 
@@ -88,13 +78,7 @@ def daymodel_train(date, working_dir=working_dir):
 
     stock_model.data = stock_model.data[stock_model.data.OPEN_MARK == 0]
 
-    stock_model = prepare_data(stock_model, None, 0, 0.95)
-    other_params = {'learning_rate': 0.1, 'n_estimators': 200, 'max_depth': 5, 'min_child_weight': 1, 'seed': 1,
-                    'subsample': 0.8, 'colsample_bytree': 0.8, 'gamma': 0, 'reg_alpha': 0, 'reg_lambda': 1}
-
-    stock_model = start_train(stock_model, other_params)
-
-    stock_model = prepare_data(stock_model, stock_model.info['importance'].head(100).featur.tolist(), None, 0.01)
+    stock_model = prepare_data(stock_model, stock_xg_set, None, 0.01)
     other_params = {'learning_rate': 0.1, 'n_estimators': 200, 'max_depth': 5, 'min_child_weight': 1, 'seed': 1,
                     'subsample': 0.8, 'colsample_bytree': 0.8, 'gamma': 0, 'reg_alpha': 0, 'reg_lambda': 1}
 
@@ -103,13 +87,7 @@ def daymodel_train(date, working_dir=working_dir):
 
     stock_model = set_target(stock_model, start_date, QA_util_get_last_day(QA_util_get_real_date(date), 6), mark = 0.3, col = 'TARGET', type='percent')
 
-    stock_model = prepare_data(stock_model, None, 0, 0.95)
-    other_params = {'learning_rate': 0.1, 'n_estimators': 200, 'max_depth': 5, 'min_child_weight': 1, 'seed': 1,
-                    'subsample': 0.8, 'colsample_bytree': 0.8, 'gamma': 0, 'reg_alpha': 0, 'reg_lambda': 1}
-
-    stock_model = start_train(stock_model, other_params)
-
-    stock_model = prepare_data(stock_model, stock_model.info['importance'].head(100).featur.tolist(), None, 0.01)
+    stock_model = prepare_data(stock_model, stock_day_set, None, 0.01)
     other_params = {'learning_rate': 0.1, 'n_estimators': 200, 'max_depth': 5, 'min_child_weight': 1, 'seed': 1,
                     'subsample': 0.8, 'colsample_bytree': 0.8, 'gamma': 0, 'reg_alpha': 0, 'reg_lambda': 1}
 
