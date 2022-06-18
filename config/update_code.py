08a103330b35c853d0de52676e80900d0c07b0c5
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2022-06-18 14:55:06 +0800

    QA

--- config/update_code.py
+++ config/update_code.py
@@ -3,10 +3,12 @@ from QUANTTOOLS.QAStockETL.Check import check_stock_code
 from QUANTTOOLS.QAStockETL.QASU import QA_SU_save_stock_aklist,QA_SU_save_stock_info
 from QUANTAXIS.QASU.main import (QA_SU_save_stock_list)
 from QUANTTOOLS.Market.MarketReport.JOB.daily_job import aotu_report
+import chromedriver_autoinstaller
 
 if __name__ == '__main__':
     mark_day = QA_util_today_str()
 
+    chromedriver_autoinstaller.install()
     if QA_util_if_trade(mark_day):
         QA_SU_save_stock_list('tdx')
         #QA_SU_save_stock_info_tushare()
