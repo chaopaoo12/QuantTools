d7d160292b3f7b5c2bb85ad807bcaca864df7c30
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2022-06-18 15:32:22 +0800

    QA

--- QUANTTOOLS/QAStockETL/Crawly/sina_btc_simulation_web.py
+++ QUANTTOOLS/QAStockETL/Crawly/sina_btc_simulation_web.py
@@ -20,7 +20,6 @@ def get_btc_day_sina(symbol):
     headers = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
                'Accept-Language': 'zh-CN,zh;q=0.9',
                'Cache-Control': 'max-age=0',
-               'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36',
                'Connection': 'keep-alive'
                }
     url = 'https://quotes.sina.cn/fx/api/openapi.php/BtcService.getDayKLine?symbol={symbol}&callback=var1='
