eac75cf1c13c9cef38f0cbc9a516d3c3b378c2f9
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2022-06-20 23:33:57 +0800

    QA

--- config/update_cbond.py
+++ config/update_cbond.py
@@ -1,9 +1,14 @@
 from QUANTAXIS.QAUtil import QA_util_today_str,QA_util_if_trade
 from QUANTTOOLS.Market.MarketReport.JOB.daily_job import aotu_bond
-
+from QUANTTOOLS.QAStockETL.Check import (check_stock_quant)
+import time
 
 if __name__ == '__main__':
     mark_day = QA_util_today_str()
 
     if QA_util_if_trade(mark_day):
+        res = check_stock_quant(mark_day)
+        while res is None or len(res[1]) > 500:
+            time.sleep(180)
+            res = check_stock_quant(mark_day)
         aotu_bond(mark_day)
