194115df8d1ce452afcaa3bbfc5e7b3f53cf0c5c
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2022-06-25 23:04:53 +0800

    QA

--- QUANTTOOLS/Ananlysis/Trends/base_tools.py
+++ QUANTTOOLS/Ananlysis/Trends/base_tools.py
@@ -42,7 +42,7 @@ def trends_func(func, code, date):
     day = func(code, date)
     data_index = QA_DataStruct_Stock_day(day.drop('date_stamp',axis=1).set_index(['date','code']))
     data_ind = indicator(data_index, 'day')
-    data_ind[['mean','per25','per75','perc']] = data_ind['RRNG'].rolling(1800,min_periods=50).agg(['mean', per25, per75, perc])
+    data_ind[['mean','per25','per75','perc']] = data_ind['RRNG'].rolling(1800,min_periods=60).agg(['mean', per25, per75, perc])
 
     week = day.drop('date_stamp',axis=1).set_index(['date']).resample('W').agg({'code':'last','open':'first','high':'max','low':'min','close':'last'})
     week_index = QA_DataStruct_Stock_day(week.reset_index().set_index(['date','code']))
@@ -53,7 +53,7 @@ def trends_func1(func, code):
     day = func(code)
     data_index = QA_DataStruct_Stock_day(day.drop('date_stamp',axis=1).set_index(['date','code']))
     data_ind = indicator(data_index, 'day')
-    data_ind[['mean','per25','per75','perc']] = data_ind['RRNG'].rolling(1800,min_periods=50).agg(['mean', per25, per75, perc])
+    data_ind[['mean','per25','per75','perc']] = data_ind['RRNG'].rolling(1800,min_periods=60).agg(['mean', per25, per75, perc])
 
     week = day.drop('date_stamp',axis=1).set_index(['date']).resample('W').agg({'code':'last','open':'first','high':'max','low':'min','close':'last'})
     week_index = QA_DataStruct_Stock_day(week.reset_index().set_index(['date','code']))
@@ -86,7 +86,7 @@ def trends_stock(code, start_date, end_date, period='day', type='before'):
     data_index = QA_DataStruct_Stock_day(day.drop('date_stamp',axis=1).set_index(['date','code']))
     week_index = QA_DataStruct_Stock_day(week.reset_index().set_index(['date','code']))
     data_index = get_indicator_short(data_index,'day')
-    data_index[['mean','per25','per75','perc']] = data_index['RRNG'].rolling(1800).agg(['mean', per25, per75, perc])
+    data_index[['mean','per25','per75','perc']] = data_index['RRNG'].rolling(1800,min_periods = 60).agg(['mean', per25, per75, perc])
     week_index = get_indicator_short(week_index,'week')
     return(data_index, week_index)
 
