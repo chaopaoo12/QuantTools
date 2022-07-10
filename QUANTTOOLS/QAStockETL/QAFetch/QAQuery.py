cfe83ec4d7ab497b7a37d3aad70f5bd0ba20b344
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2022-07-11 07:53:20 +0800

    QA

--- QUANTTOOLS/QAStockETL/QAFetch/QAQuery.py
+++ QUANTTOOLS/QAStockETL/QAFetch/QAQuery.py
@@ -828,18 +828,24 @@ def QA_fetch_stock_quant_data(code, start, end=None, block = True, norm_type='no
                                           ]].groupby('code').fillna(method='ffill').fillna(0)
         financial_res = financial(start_date,end_date,code=code)
         financial_res = financial_res#[financial_res.DAYS >= 90]
+        print('financial_res',financial_res)
+        print('pe_res',pe_res)
         QA_util_log_info(
             'JOB Get Stock Tech Index data start=%s end=%s' % (start, end))
         index_res = index(start_date,end_date,code=code)
+        print('index_res',index_res)
         QA_util_log_info(
             'JOB Get Stock Tech Hour data start=%s end=%s' % (start, end))
         hour_res = hour(start_date,end_date,code=code, type= 'day')
+        print('hour_res',hour_res)
         QA_util_log_info(
             'JOB Get Stock Tech Week data start=%s end=%s' % (start, end))
         week_res = week(QA_util_get_pre_trade_date(start,90),end_date,code=code)
+        print('week_res',week_res)
         QA_util_log_info(
             'JOB Get Stock Alpha191 data start=%s end=%s' % (start, end))
         alpha_res = alpha(start_date,end_date,code=code)
+        print('alpha_res',alpha_res)
         res = financial_res.join(index_res).join(week_res).join(alpha_res).join(hour_res).join(pe_res).groupby('code').fillna(method='ffill').loc[(rng,code),]
         try:
             for columnname in res.columns:
