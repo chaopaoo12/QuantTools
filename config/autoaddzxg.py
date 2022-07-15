e1f134ea16fdc9e9cf0c715bae6880226860c13e
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2022-07-15 08:52:07 +0800

    QA

--- config/autoaddzxg.py
+++ config/autoaddzxg.py
@@ -9,6 +9,9 @@ if __name__ == '__main__':
     if QA_util_if_trade(QA_util_today_str()):
         check_day = QA_util_get_pre_trade_date(QA_util_today_str(),1)
         htmlbody=reademail('目标股池{}'.format(check_day))
-        data = pd.read_html(htmlbody[0], encoding='gbk', header=0)[1]
-        data=data[data.RANK.notna()].rename(columns={'Unnamed: 0':'code'})
-        putzxgfile(data.code.unique().tolist())
+        data1 = pd.read_html(htmlbody[0], encoding='gbk', header=0)[1]
+        htmlbody=reademail('综合选股报告 一{}'.format(check_day))
+        data2 = pd.read_html(htmlbody[0], encoding='gbk', header=0)[1]
+        data1=data1[data1.RANK.notna()].rename(columns={'Unnamed: 0':'code'})
+        data2=data2[data2.RANK.notna()].rename(columns={'Unnamed: 1':'code'})
+        putzxgfile(data1.code.unique().tolist() + data2.code.unique().tolist())
