46854bbc76713608db729c5b630dc06702db6a55
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2022-07-19 08:43:04 +0800

    QA

--- config/autoaddzxg.py
+++ config/autoaddzxg.py
@@ -7,11 +7,13 @@ from QUANTAXIS.QAUtil import QA_util_today_str,QA_util_get_last_day,QA_util_get_
 if __name__ == '__main__':
 
     if QA_util_if_trade(QA_util_today_str()):
-        check_day = QA_util_get_pre_trade_date(QA_util_today_str(),1)
+        check_day ='2022-07-17'
         htmlbody=reademail('目标股池{}'.format(check_day))
-        data1 = pd.read_html(htmlbody[0], encoding='gbk', header=0)[1]
+        data1 = pd.read_html(htmlbody[0], encoding='gbk', header=0)[0]
+        data1=data1[data1.RANK.notna()].rename(columns={'Unnamed: 0':'date','Unnamed: 1':'code'})
+        data1=data1[data1.date == QA_util_get_real_date(check_day)]
         htmlbody=reademail('综合选股报告 一{}'.format(check_day))
         data2 = pd.read_html(htmlbody[0], encoding='gbk', header=0)[0]
-        data1=data1[data1.RANK.notna()].rename(columns={'Unnamed: 0':'code'})
-        data2=data2[data2.RANK.notna()].rename(columns={'Unnamed: 1':'code'})
+        data2=data2[data2.RANK.notna()].rename(columns={'Unnamed: 0':'date','Unnamed: 1':'code'})
+        data2=data2[data2.date == QA_util_get_real_date(check_day)]
         putzxgfile(data1.code.unique().tolist() + data2.code.unique().tolist())
