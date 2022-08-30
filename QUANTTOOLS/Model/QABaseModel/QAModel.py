03a71bca0db0648e99fadd57938810e58d117957
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2022-08-30 21:04:41 +0800

    QA

--- QUANTTOOLS/Model/QABaseModel/QAModel.py
+++ QUANTTOOLS/Model/QABaseModel/QAModel.py
@@ -38,11 +38,10 @@ class QAModel():
         self.info['target'] = self.target
         QA_util_log_info('##save used columns ==== {}'.format(self.info['date']), ui_log = None)
 
-    def shuffle(self, n_in = None):
+    def shuffle(self, cols, n_in = None):
         QA_util_log_info('##JOB01 Now Data shuffle {}'.format(n_in))
         if n_in is not None:
-            self.data = self.data[[i for i in self.data.columns if i not in ['next_date','PRE_DATE','PASS_MARK','TARGET',
-                                                                             'TARGET3','TARGET4','TARGET5','TARGET10','TARGET20']]].groupby('code').apply(series_to_supervised, n_in = n_in).join(self.data[['next_date','PRE_DATE','PASS_MARK','TARGET','TARGET3','TARGET4','TARGET5','TARGET10','TARGET20']])
+            self.data = self.data[cols].groupby('code').apply(series_to_supervised, n_in = n_in).join(self.data[['next_date','PRE_DATE','PASS_MARK','TARGET','TARGET3','TARGET4','TARGET5','TARGET10','TARGET20']])
         self.info['n_in'] = n_in
         QA_util_log_info('##JOB01 Now Data shuffle Finish')
         QA_util_log_info(self.data.shape)
