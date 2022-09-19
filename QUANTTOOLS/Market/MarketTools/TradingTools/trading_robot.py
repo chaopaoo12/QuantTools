2d590779c673e72d1570c714bec158b48634813e
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2022-09-19 21:51:02 +0800

    QA

--- QUANTTOOLS/Market/MarketTools/TradingTools/trading_robot.py
+++ QUANTTOOLS/Market/MarketTools/TradingTools/trading_robot.py
@@ -22,7 +22,10 @@ def trading_robot(client, account, account_info, signal_data, trading_date, mark
         mark_tm), ui_log=None)
     if signal_data is not None and test is False:
         if client.type in ['yun_ease','loacl']:
-            client.client.cancel_all(account)
+            try:
+                client.client.cancel_all(account)
+            except:
+                QA_util_log_info('JOB Cancel Last Mark Time Trading Action Failed')
         elif client.type == 'sim_myquant':
             order_cancel_all()
         else:
