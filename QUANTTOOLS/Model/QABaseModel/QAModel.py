141bba67ad765b7a211d28f150dd2843a1700925
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2022-10-26 17:40:32 +0800

    QA

--- QUANTTOOLS/Model/QABaseModel/QAModel.py
+++ QUANTTOOLS/Model/QABaseModel/QAModel.py
@@ -194,9 +194,9 @@ class QAModel():
     def thresh_check(self):
         nan_num = (self.data[self.cols].isnull().sum(axis=1)> 0).sum()
         loss_rate = nan_num/self.data.shape[0]
-        QA_util_log_info('##JOB Clean Data With per ({NAN_NUM}/{shape})  Contain NAN ==== '.format(
+        QA_util_log_info('##JOB Clean Data With {per} ({NAN_NUM}/{shape})  Contain NAN ==== '.format(
             NAN_NUM=nan_num, per = loss_rate, shape=self.data.shape[0]), ui_log = None)
-        if self.thresh == 0:
+        if self.thresh == 0 or self.thresh is None:
             self.data = self.data[self.cols].dropna()
         else:
             self.data = self.data[self.cols].dropna(thresh=(len(self.cols) - self.thresh))
