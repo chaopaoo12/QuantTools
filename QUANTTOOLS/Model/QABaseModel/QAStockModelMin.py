efb8704f2ccb6e806dbc0485065ef87a18dee902
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2022-10-26 23:52:18 +0800

    QA

--- QUANTTOOLS/Model/QABaseModel/QAStockModelMin.py
+++ QUANTTOOLS/Model/QABaseModel/QAStockModelMin.py
@@ -12,7 +12,11 @@ class QAStockModelMin(QAModel):
     def get_data(self, start, end, code =None, block=False, sub_block=False, type = 'crawl', norm_type=None, ST=True):
         QA_util_log_info('##JOB Got Data by {type}, block: {block}, sub_block: {sub_block} ==== from {_from} to {_to}'.format(type=type, block=block,sub_block=sub_block, _from=start, _to=end), ui_log = None)
         data = QA_fetch_get_stock_vwap(code, start, end, period = '1', type = type)
-        data_15min = get_quant_data_15min(QA_util_get_pre_trade_date(start,1), end, code, type=type)
+        if type in ['crawl','model']:
+            type_15min = 'model'
+        else:
+            type_15min = type
+        data_15min = get_quant_data_15min(QA_util_get_pre_trade_date(start,1), end, code, type=type_15min)
         last = data.groupby(['date','code'])['close'].agg([('last1','last')])
         last = last.assign(yes_close=last.groupby('code').shift())
 
