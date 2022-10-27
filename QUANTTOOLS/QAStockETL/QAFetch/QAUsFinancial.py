7f3878d49d8029f49964e51c3fc2d7bc2561e875
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2022-10-27 08:22:47 +0800

    QA

--- QUANTTOOLS/QAStockETL/QAFetch/QAUsFinancial.py
+++ QUANTTOOLS/QAStockETL/QAFetch/QAUsFinancial.py
@@ -83,23 +83,21 @@ def QA_fetch_get_stock_min_sina(code, period='30', type=''):
     else:
         data=None
 
-    try:
-        data = data.rename(columns={'时间':'datetime',
-                                    '开盘':'open',
-                                    '收盘':'close',
-                                    '最高':'high',
-                                    '最低':'low',
-                                    '成交量':'volume',
-                                    '成交额':'amount',
-                                    '最新价':'price',})
-        print('a',data)
-        data[['open','close','high','low','volume','amount']] = data[['open','close','high','low','volume','amount']].apply(pd.to_numeric)
-        print('b',data)
-        data = data.assign(date_stamp=data['datetime'].apply(lambda x: QA_util_date_stamp(str(x)[0:10])),code=code)
-        print('c',data)
-        data['datetime']=pd.to_datetime(data['datetime'],format='%Y-%m-%d %H:%M:%S')
-    except:
-        data = None
+    #try:
+    data = data.rename(columns={'时间':'datetime',
+                                '开盘':'open',
+                                '收盘':'close',
+                                '最高':'high',
+                                '最低':'low',
+                                '成交量':'volume',
+                                '成交额':'amount',
+                                '最新价':'price',})
+    data[['open','close','high','low','volume','amount']] = data[['open','close','high','low','volume','amount']].apply(pd.to_numeric)
+    data = data.assign(date_stamp=data['datetime'].apply(lambda x: QA_util_date_stamp(str(x)[0:10])), code=code)
+    print('c',data)
+    data['datetime']=pd.to_datetime(data['datetime'],format='%Y-%m-%d %H:%M:%S')
+    #except:
+    #    data = None
 
     try:
         data[['price']] = data[['price']].apply(pd.to_numeric)
