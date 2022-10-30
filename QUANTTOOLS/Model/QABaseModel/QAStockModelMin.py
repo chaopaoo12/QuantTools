15ed01d95223561362067bcfebb51513527971da
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2022-10-30 23:04:05 +0800

    QA

--- QUANTTOOLS/Model/QABaseModel/QAStockModelMin.py
+++ QUANTTOOLS/Model/QABaseModel/QAStockModelMin.py
@@ -25,7 +25,7 @@ class QAStockModelMin(QAModel):
                            pct= data.day_close/data.yes_close-1)
         self.data = data[['date','duration','open_pct', 'high_pct', 'low_pct', 'VAMP_JC', 'VAMP_SC', 'VAMPC_K', 'VAMP_K', 'CLOSE_K', 'TARGET', 'pct', 'AMT_UP', 'DISTANCE', 'camt_vol', 'camt_k']].join(data_15min)
         self.data = self.data.groupby('code').fillna(method='ffill')
-        self.data = self.data.drop(['DATE_15M', 'TIME_STAMP_15M', 'DATE_30M', 'TIME_STAMP_30M'], axis=1)
+        self.data = self.data.drop(['DATE_15M', 'DATE_30M'], axis=1)
         self.info['code'] = code
         self.info['norm_type'] = norm_type
         self.info['block'] = block
