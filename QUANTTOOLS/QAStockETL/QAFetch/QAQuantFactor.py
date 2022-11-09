cad0ef1d747a70ad0c683be9c6877bc3778bcd69
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2022-11-09 12:15:46 +0800

    QA

--- QUANTTOOLS/QAStockETL/QAFetch/QAQuantFactor.py
+++ QUANTTOOLS/QAStockETL/QAFetch/QAQuantFactor.py
@@ -553,26 +553,29 @@ def QA_fetch_get_stock_quant_min(code, start_date, end_date, type='30min', keep 
     else:
         return(pd.concat(res))
 
-def QA_fetch_get_stock_vwap_min(code, start_date, end_date, type='30min'):
+def QA_fetch_get_stock_vwap_min(code, start_date, end_date, type='1'):
+    if isinstance(code, list):
+        if len(code) > 10:
+            code = [code[i:i+10] for i in range(0, len(code), 10)]
+            pool = multiprocessing.Pool(15)
+            k = 1
+            while k <= 5:
+                try:
+                    with pool as p:
+                        res = p.map(partial(QA_fetch_get_stock_vwap, start_date=start_date, end_date=end_date, period = type,type='real'), code)
+                    QA_util_log_info('##JOB QA_fetch_get_stock_vwap_min finish with {k} times  ===================='.format(k=k), ui_log=None)
+                    break
+                except:
+                    k += 1
+                    QA_util_log_info('##JOB QA_fetch_get_stock_vwap_min failed with {k} times ===================='.format(k=k), ui_log=None)
+                    time.sleep(5)
 
-    pool = multiprocessing.Pool(15)
-
-    k = 1
-    while k <= 5:
-        try:
-            with pool as p:
-                res = p.map(partial(QA_fetch_get_stock_vwap, start_date=start_date, end_date=end_date, period = type,type='real'), code)
-            QA_util_log_info('##JOB QA_fetch_get_stock_vwap_min finish with {k} times  ===================='.format(k=k), ui_log=None)
-            break
-        except:
-            k += 1
-            QA_util_log_info('##JOB QA_fetch_get_stock_vwap_min failed with {k} times ===================='.format(k=k), ui_log=None)
-            time.sleep(5)
-
-    if k > 5:
-        return None
+            if k > 5:
+                return None
+            else:
+                return(pd.concat(res))
     else:
-        return(pd.concat(res))
+        return(QA_fetch_get_stock_vwap(code, start_date, end_date, period = type, type='real'))
 
 
 if __name__ == '__main__':
