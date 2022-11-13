71939c0421f4f970d584366a9379d2df7a212039
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2022-11-13 21:37:25 +0800

    QA

--- QUANTTOOLS/QAStockETL/Crawly/xq_simulation_web.py
+++ QUANTTOOLS/QAStockETL/Crawly/xq_simulation_web.py
@@ -109,6 +109,7 @@ def read_stock_day(code, start_date, end_date, proxies, period='day', type='norm
     if len(lose_code) > 0:
         lose_data = xq_base_func(lose_code, cnt, period=period, type=type, headers=headers,proxy=proxies[1])
         data = pd.concat([data, lose_data])
+    print(data)
     data = data.assign(timestamp = data.timestamp.apply(lambda x:x/1000))
     data = data.assign(datetime = pd.to_datetime(data.timestamp.apply(lambda x:str(datetime.datetime.fromtimestamp(x)))))
     data = data.assign(date = pd.to_datetime(data.timestamp.apply(lambda x:str(datetime.datetime.fromtimestamp(x))[0:10])))
