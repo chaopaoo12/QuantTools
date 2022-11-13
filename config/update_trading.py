6d86aefd03ae0233eb97c7a1b37941cec385825d
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2022-11-13 09:01:48 +0800

    QA

--- config/update_trading.py
+++ config/update_trading.py
@@ -63,10 +63,10 @@ if __name__ == '__main__':
         time.sleep(180)
         res = check_stock_finper(mark_day)
 
-    res = check_stock_vwap(mark_day)
-    while res is None or len(res[1]) > 50:
-        time.sleep(180)
-        res = check_stock_vwap(mark_day)
+    #res = check_stock_vwap(mark_day)
+    #while res is None or len(res[1]) > 50:
+    #    time.sleep(180)
+    #    res = check_stock_vwap(mark_day)
 
     daily_run(mark_day)
 
