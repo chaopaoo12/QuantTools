0099e469aa533ff5daf2d0ffb31df64612320321
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2022-11-14 21:34:02 +0800

    QA

--- QUANTTOOLS/QAStockETL/Crawly/IP_Proxy.py
+++ QUANTTOOLS/QAStockETL/Crawly/IP_Proxy.py
@@ -1,14 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 
-def get_ip_poll(url):
+def get_ip_poll(url='http://www.ip3366.net/free/?stype=1&page=1'):
     headers = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
                'Accept-Language': 'zh-CN,zh;q=0.9',
                'Cache-Control': 'max-age=0',
                'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36',
                'Connection': 'keep-alive'
                }
-    response = requests.get(url='http://www.ip3366.net/free/?stype=1&page=2', headers=headers)
+    response = requests.get(url=url, headers=headers)
     soup = BeautifulSoup(response.text, "html.parser").body
     port = [i.find_all('td')[1].text for i in soup.find_all('tr')[1:]]
     ip = [i.find_all('td')[0].text for i in soup.find_all('tr')[1:]]
