3e1663e07415ad468d6bad6a863bcc22df494bed
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2022-12-12 08:37:58 +0800

    QA

--- QUANTTOOLS/Market/StockMarket/StockStrategyReal/running.py
+++ QUANTTOOLS/Market/StockMarket/StockStrategyReal/running.py
@@ -332,11 +332,11 @@ def summary_func(trading_date):
     stock_target = get_quant_data(start_date, end_date, type='crawl', block=False, sub_block=False,norm_type=None)[['RRNG','RRNG_HR','MA60','MA60_C','MA60_D','RRNG_WK','TAR','MA60_C_WK','SHORT10','SHORT20','LONG60','AVG5','MA60_C','SHORT10_WK','SHORT20_WK','LONG60_WK','MA60_C_WK','PASS_MARK','TARGET','TARGET3','TARGET4','TARGET5','TARGET10','SKDJ_K','SKDJ_K_WK']]
     stock_res = stock_target[['RRNG','RRNG_HR','MA60','MA60_C','MA60_D','TAR','RRNG_WK','MA60_C_WK','SHORT10','SHORT20','LONG60','AVG5','MA60_C','SHORT10_WK','SHORT20_WK','LONG60_WK','MA60_C_WK','SKDJ_K','SKDJ_K_WK']]
     cols_name = ['RRNG','RRNG_WK','SKDJ_K','SKDJ_K_WK','PASS_MARK', 'TARGET', 'TARGET3', 'TARGET4', 'TARGET5','TARGET10', 'y_pred', 'model', 'RANK']
-    xg = xg.join(stock_res['RRNG']).assign(model='xg')
+    xg = xg.join(stock_res[['RRNG','RRNG_WK']]).assign(model='xg')
     xg_nn = xg_nn.join(stock_res).assign(model='xg_nn')
     mars_nn = mars_nn.join(stock_res).assign(model='mars_nn')
-    mars_day = mars_day.join(stock_res['RRNG']).assign(model='mars_day')
-    xg_sh = xg_sh.join(stock_res['RRNG']).assign(model='xg_sh')
+    mars_day = mars_day.join(stock_res[['RRNG','RRNG_WK']]).assign(model='mars_day')
+    xg_sh = xg_sh.join(stock_res[['RRNG','RRNG_WK']]).assign(model='xg_sh')
 
     res = pd.concat([mars_day[(mars_day.RANK<=20)&(mars_day.SKDJ_K < 50)][cols_name],
                      xg_sh[(xg_sh.RANK<=20)&(xg_sh.SKDJ_K < 50)][cols_name],
