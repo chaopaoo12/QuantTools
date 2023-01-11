b8cd725f6771678e8dc5e9aa273f90ec1cd59d0b
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2023-01-11 08:00:15 +0800

    QA

--- QUANTTOOLS/Market/StockMarket/StockStrategyReal/running.py
+++ QUANTTOOLS/Market/StockMarket/StockStrategyReal/running.py
@@ -336,16 +336,16 @@ def summary_func(trading_date):
     r_tar, xg_nn, prediction = load_data(concat_predict_neut, QA_util_get_pre_trade_date(trading_date,1), working_dir, 'stock_xg_nn', 'prediction_stock_xg_nn')
     r_tar, mars_nn, prediction = load_data(concat_predict_neut, QA_util_get_pre_trade_date(trading_date,1), working_dir, 'stock_mars_nn', 'prediction_stock_mars_nn')
     r_tar, mars_day, prediction = load_data(concat_predict, QA_util_get_pre_trade_date(trading_date,1), working_dir, 'stock_mars_day', 'prediction_stock_mars_day')
-    r_tar, xg_sh, prediction = load_data(concat_predict, trading_date, working_dir, 'stock_sh', 'prediction_sh')
+    r_tar, xg_sh, prediction = load_data(concat_predict, QA_util_get_pre_trade_date(trading_date,1), working_dir, 'stock_sh', 'prediction_sh')
 
     stock_target = get_quant_data(start_date, end_date, type='crawl', block=False, sub_block=False,norm_type=None)[['TOTAL_MARKET','RRNG','RRNG_HR','MA60','MA60_C','MA60_D','RRNG_WK','TAR','MA60_C_WK','SHORT10','SHORT20','LONG60','AVG5','MA60_C','SHORT10_WK','SHORT20_WK','LONG60_WK','MA60_C_WK','PASS_MARK','TARGET','TARGET3','TARGET4','TARGET5','TARGET10','SKDJ_K','SKDJ_K_WK']]
     stock_res = stock_target[['TOTAL_MARKET','RRNG','RRNG_HR','MA60','MA60_C','MA60_D','TAR','RRNG_WK','MA60_C_WK','SHORT10','SHORT20','LONG60','AVG5','MA60_C','SHORT10_WK','SHORT20_WK','LONG60_WK','MA60_C_WK','SKDJ_K','SKDJ_K_WK']]
     cols_name = ['NAME','INDUSTRY','TOTAL_MARKET','RRNG','RRNG_WK','SKDJ_K','SKDJ_K_WK','PASS_MARK', 'TARGET', 'TARGET3', 'TARGET4', 'TARGET5','TARGET10', 'y_pred', 'model', 'RANK']
-    xg = xg.join(stock_res[['RRNG','RRNG_WK','TOTAL_MARKET']]).assign(model='xg')
+    xg = xg.join(stock_res[['RRNG','RRNG_WK']]).assign(model='xg')
     xg_nn = xg_nn.join(stock_res[['RRNG','RRNG_WK','SKDJ_K_WK', 'SKDJ_K','TOTAL_MARKET']]).assign(model='xg_nn')
     mars_nn = mars_nn.join(stock_res[['RRNG','RRNG_WK','SKDJ_K_WK', 'SKDJ_K','TOTAL_MARKET']]).assign(model='mars_nn')
-    mars_day = mars_day.join(stock_res[['RRNG','RRNG_WK','TOTAL_MARKET']]).assign(model='mars_day')
-    xg_sh = xg_sh.join(stock_res[['RRNG','RRNG_WK','TOTAL_MARKET']]).assign(model='xg_sh')
+    mars_day = mars_day.join(stock_res[['RRNG','RRNG_WK']]).assign(model='mars_day')
+    xg_sh = xg_sh.join(stock_res[['RRNG','RRNG_WK']]).assign(model='xg_sh')
 
     code_list = list(set(xg_sh[(xg_sh.RANK <= 20)&(xg_sh.TARGET3.isnull())].reset_index().code.tolist()
                          + xg[(xg.RANK <= 20)&(~xg.INDUSTRY.isin(['银行']))&(xg.TARGET3.isnull())].reset_index().code.tolist()
