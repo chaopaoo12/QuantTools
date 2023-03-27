9c3835b1478f1f9b713114de9d62ad9abee43cda
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2023-03-27 22:34:54 +0800

    ADD

--- QUANTTOOLS/QAStockETL/QAUtil/QASQLBlockAnalystic.py
+++ QUANTTOOLS/QAStockETL/QAUtil/QASQLBlockAnalystic.py
@@ -23,6 +23,7 @@ sql_text = '''select *
           from (select code as index_code, stock as code
                   from index_stock
                  where code not like '8802%'
+                 and cate <> '5'
                    and stock is not null) a
           left join (
                     select a.code,
