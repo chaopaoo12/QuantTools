3dec600e0dc5109b31c11b177545771d7ccec190
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2023-03-29 08:20:59 +0800

    ADD

--- QUANTTOOLS/Market/MarketTools/StrategyTools/StrategyRobot.py
+++ QUANTTOOLS/Market/MarketTools/StrategyTools/StrategyRobot.py
@@ -130,8 +130,10 @@ class StrategyRobotBase:
                                                              })
 
             # prepare signal.
-
-            signal_data = self.strategy.strategy_run(mark_tm)
+            try:
+                signal_data = self.strategy.strategy_run(mark_tm)
+            except:
+                signal_data = {'sell': None, 'buy': None}
             QA_util_log_info('##Sell List ==== {}'.format(str(self.trading_date)), ui_log=None)
             QA_util_log_info(signal_data['sell'], ui_log=None)
 
