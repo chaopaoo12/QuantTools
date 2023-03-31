efd454b4e72bc32ff6358c9e2515e349ae2106a0
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2023-03-31 08:21:13 +0800

    ADD

--- QUANTTOOLS/Market/StockMarket/StockStrategyReal/running.py
+++ QUANTTOOLS/Market/StockMarket/StockStrategyReal/running.py
@@ -175,6 +175,7 @@ def divv(x, y):
 def block_func(trading_date):
     trading_date = QA_util_get_real_date(trading_date)
     data = QA_Sql_BlockAnalystic(trading_date,trading_date)
+    print(data)
     res = data.groupby(['index','BLN'])[['TOTAL_MARKET','GROSSMARGIN','TURNOVERRATIOOFTOTALASSETS','OPERATINGRINRATE','PB','PE_TTM','ROE_TTM']].agg(f).rename(
         columns={'TOTAL_MARKET':'I_TM','GROSSMARGIN':'I_GM','TURNOVERRATIOOFTOTALASSETS':'I_TURNR','OPERATINGRINRATE':'I_OPINR','ROE_TTM':'I_ROE','PE_TTM':'I_PE','PB':'I_PB'})
     data = data.set_index(['index','BLN']).join(res).reset_index()
