da80ec6965db0fe79308a38f44077f4887498d63
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2023-04-05 10:24:54 +0800

    ADD

--- QUANTTOOLS/QAStockETL/QAFetch/QABtc.py
+++ QUANTTOOLS/QAStockETL/QAFetch/QABtc.py
@@ -3,6 +3,7 @@ from QUANTAXIS.QAUtil import QA_util_date_stamp
 
 def QA_fetch_get_btc_day(code):
     data = get_btc_day_sina(code)
+    print(data)
     data = data.assign(date_stamp=data['date'].apply(lambda x: QA_util_date_stamp(str(x)[0:10])),
                        code=code)
     return(data)
