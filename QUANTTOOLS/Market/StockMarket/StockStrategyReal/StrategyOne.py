761e28cc96a378d28e6ad7f4d7eb5fd949df2cc2
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2023-04-18 21:13:15 +0800

    ADD

--- QUANTTOOLS/Market/StockMarket/StockStrategyReal/StrategyOne.py
+++ QUANTTOOLS/Market/StockMarket/StockStrategyReal/StrategyOne.py
@@ -104,19 +104,17 @@ def code_select(target_list, position, day_temp_data, sec_temp_data, trading_dat
     res30=pd.concat([temp.min30, day_temp_data[1].data])
     res15 = res15[~res15.index.duplicated(keep='first')]
     res30 = res30[~res30.index.duplicated(keep='first')]
-    close_15 = res15[['close']].rename(columns={'close':'close_15'})
-    close_30 = res30[['close']].rename(columns={'close':'close_30'})
     res15=get_indicator(QA_DataStruct_Stock_min(res15.sort_index()), 'min')
     res30=get_indicator(QA_DataStruct_Stock_min(res30.sort_index()), 'min')
     res15.columns = [x.upper() + '_15M' for x in res15.columns]
     res30.columns = [x.upper() + '_30M' for x in res30.columns]
-    sec_temp_data = [res15.join(res30).join(close_15).join(close_30).groupby('code').fillna(method='ffill')]
-    sec_temp_data = [sec_temp_data[0].assign(BOLL_15M_V = sec_temp_data[0].close_15 / (sec_temp_data[0].BOLL_15M + 1),
-                                            LB_15M_V = sec_temp_data[0].close_15 / (sec_temp_data[0].LB_15M + 1),
-                                            UB_15M_V = sec_temp_data[0].close_15 / (sec_temp_data[0].UB_15M + 1),
-                                            BOLL_30M_V = sec_temp_data[0].close_30 / (sec_temp_data[0].BOLL_30M + 1),
-                                            LB_30M_V = sec_temp_data[0].close_30 / (sec_temp_data[0].LB_30M + 1),
-                                            UB_30M_V = sec_temp_data[0].close_30 / (sec_temp_data[0].UB_30M + 1)
+    sec_temp_data = [res15.join(res30).groupby('code').fillna(method='ffill')]
+    sec_temp_data = [sec_temp_data[0].assign(BOLL_15M_V = sec_temp_data[0].CLOSE_15M / (sec_temp_data[0].BOLL_15M + 1),
+                                            LB_15M_V = sec_temp_data[0].CLOSE_15M / (sec_temp_data[0].LB_15M + 1),
+                                            UB_15M_V = sec_temp_data[0].CLOSE_15M / (sec_temp_data[0].UB_15M + 1),
+                                            BOLL_30M_V = sec_temp_data[0].CLOSE_30M / (sec_temp_data[0].BOLL_30M + 1),
+                                            LB_30M_V = sec_temp_data[0].CLOSE_30M / (sec_temp_data[0].LB_30M + 1),
+                                            UB_30M_V = sec_temp_data[0].CLOSE_30M / (sec_temp_data[0].UB_30M + 1)
                                             )]
     sec_temp_data[0][['UB_15M_S','UB_30M_S','BOLL_15M_S','BOLL_30M_S','LB_15M_S','LB_30M_S']] = sec_temp_data[0].groupby('code')[
         ['UB_15M','UB_30M','BOLL_15M','BOLL_30M','LB_15M','LB_30M']].shift()
