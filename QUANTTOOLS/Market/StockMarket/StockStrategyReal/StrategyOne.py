e5921917cdbe77cf6972c09b84439d62182de04a
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2023-04-24 21:14:42 +0800

    ADD

--- QUANTTOOLS/Market/StockMarket/StockStrategyReal/StrategyOne.py
+++ QUANTTOOLS/Market/StockMarket/StockStrategyReal/StrategyOne.py
@@ -37,11 +37,17 @@ def data_collect(code_list, trading_date, day_temp_data, sec_temp_data, source_d
     data.loc[(data.price > data.UB_15M_V * 1.05)&(data.price < data.UB_30M_V * 1.05)&(data.ask2 > 0),"signal"] = 0
     data.loc[(data.price > data.UB_15M_V * 1.05)&(data.price < data.UB_30M_V * 1.05)&(data.ask2 > 0),"msg"] = '超涨止盈信号'
 
+    data.loc[(data.BOLL_15M_S2 > 0)&(data.BOLL_15M_S > 0)&(data.BOLL_15M < 0)&(data.price >= data.BOLL_15M * 0.98), "signal"] = 1
+    data.loc[(data.BOLL_15M_S2 > 0)&(data.BOLL_15M_S > 0)&(data.BOLL_15M < 0)&(data.price >= data.BOLL_15M * 0.98), "msg"] = '15Min BOLL出场信号'
+
+    data.loc[(data.BOLL_30M_S2 > 0)&(data.BOLL_30M_S > 0)&(data.BOLL_30M < 0)&(data.price >= data.BOLL_30M * 0.98), "signal"] = 1
+    data.loc[(data.BOLL_30M_S2 > 0)&(data.BOLL_30M_S > 0)&(data.BOLL_30M < 0)&(data.price >= data.BOLL_30M * 0.98), "msg"] = '30Min BOLL出场信号'
+
     data.loc[(data.UB_15M_S2 > 0)&(data.UB_15M_S > 0)&(data.UB_15M < 0)&(data.ask2 > 0)&(data.price < data.UB_15M_V),"signal"] = 0
     data.loc[(data.UB_15M_S2 > 0)&(data.UB_15M_S > 0)&(data.UB_15M < 0)&(data.ask2 > 0)&(data.price < data.UB_15M_V),"msg"] = '15M见顶信号'
 
-    data.loc[(data.UB_30M_S2 > 0)&(data.UB_30M_S > 0)&(data.UB_30M < 0)&(data.ask2 > 0)&(data.price < data.UB_30M_V),"signal"] = 0
-    data.loc[(data.UB_30M_S2 > 0)&(data.UB_30M_S > 0)&(data.UB_30M < 0)&(data.ask2 > 0)&(data.price < data.UB_30M_V),"msg"] = '30M见顶信号'
+    data.loc[(data.UB_30M_S > 0)&(data.UB_30M < 0)&(data.ask2 > 0)&(data.price < data.UB_30M_V),"signal"] = 0
+    data.loc[(data.UB_30M_S > 0)&(data.UB_30M < 0)&(data.ask2 > 0)&(data.price < data.UB_30M_V),"msg"] = '30M见顶信号'
 
     data.loc[(data.LB_15M_S2 > 0)&(data.UB_15M_S < 0)&(data.LB_15M < 0)&(data.price < data.LB_15M_V),"signal"] = 0
     data.loc[(data.LB_15M_S2 > 0)&(data.UB_15M_S < 0)&(data.LB_15M < 0)&(data.price < data.LB_15M_V),"msg"] = '15M止损信号'
@@ -61,11 +67,11 @@ def data_collect(code_list, trading_date, day_temp_data, sec_temp_data, source_d
     data.loc[(data.BOLL_30M_S2 < 0)&(data.BOLL_30M_S < 0)&(data.BOLL_30M > 0)&(data.price <= data.UB_30M * 0.97), "signal"] = 1
     data.loc[(data.BOLL_30M_S2 < 0)&(data.BOLL_30M_S < 0)&(data.BOLL_30M > 0)&(data.price <= data.UB_30M * 0.97), "msg"] = '30Min BOLL进场信号'
 
-    data.loc[(data.LB_15M_S2 < 0)&(data.LB_15M_S < 0)&(data.LB_15M > 0)&(data.price <= data.BOLL_15M * 0.97), "signal"] = 1
-    data.loc[(data.LB_15M_S2 < 0)&(data.LB_15M_S < 0)&(data.LB_15M > 0)&(data.price <= data.BOLL_15M * 0.97), "msg"] = '15M LB进场信号'
+    data.loc[(data.LB_15M_S < 0)&(data.LB_15M > 0)&(data.price <= data.BOLL_15M * 0.97), "signal"] = 1
+    data.loc[(data.LB_15M_S < 0)&(data.LB_15M > 0)&(data.price <= data.BOLL_15M * 0.97), "msg"] = '15M LB进场信号'
 
-    data.loc[(data.LB_30M_S2 < 0)&(data.LB_30M_S < 0)&(data.LB_30M > 0)&(data.price <= data.BOLL_30M * 0.97), "signal"] = 1
-    data.loc[(data.LB_30M_S2 < 0)&(data.LB_30M_S < 0)&(data.LB_30M > 0)&(data.price <= data.BOLL_30M * 0.97), "msg"] = '30M LB进场信号'
+    data.loc[(data.LB_30M_S < 0)&(data.LB_30M > 0)&(data.price <= data.BOLL_30M * 0.97), "signal"] = 1
+    data.loc[(data.LB_30M_S < 0)&(data.LB_30M > 0)&(data.price <= data.BOLL_30M * 0.97), "msg"] = '30M LB进场信号'
 
 
     QA_util_log_info('##IN_SIG DataFrame ====================', ui_log=None)
