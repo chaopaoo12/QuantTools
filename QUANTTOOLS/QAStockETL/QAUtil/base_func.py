62269626519fc1c4401a2a9ff4ea53ff99a02bb1
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2023-04-24 21:06:03 +0800

    ADD

--- QUANTTOOLS/QAStockETL/QAUtil/base_func.py
+++ QUANTTOOLS/QAStockETL/QAUtil/base_func.py
@@ -2,6 +2,25 @@ import numpy as np
 import pandas as pd
 from scipy import stats
 from math import degrees,atan
+import datetime
+import traceback
+from functools import wraps
+
+
+# 异常输出
+def except_output(msg='异常'):
+    # msg用于自定义函数的提示信息
+    def except_execute(func):
+        @wraps(func)
+        def execept_print(*args, **kwargs):
+            try:
+                return func(*args, **kwargs)
+            except Exception as e:
+                sign = '=' * 60 + '\n'
+                print(f'{sign}>>>异常时间：\t{datetime.datetime.now()}\n>>>异常函数：\t{func.__name__}\n>>>{msg}：\t{e}')
+                print(f'{sign}{traceback.format_exc()}{sign}')
+        return execept_print
+    return except_execute
 
 def rolling_ols(y):
     '''
