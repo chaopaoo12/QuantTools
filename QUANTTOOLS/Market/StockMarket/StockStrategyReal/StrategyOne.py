3cf67effbb6230edd4f52b9aed1e85c482debb12
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2023-04-27 10:04:41 +0800

    ADD

--- QUANTTOOLS/Market/StockMarket/StockStrategyReal/StrategyOne.py
+++ QUANTTOOLS/Market/StockMarket/StockStrategyReal/StrategyOne.py
@@ -93,14 +93,18 @@ def data_collect(code_list, trading_date, day_temp_data, sec_temp_data, source_d
     QA_util_log_info('##JOB In Signal Decide ====================', ui_log=None)
     # 放量金叉
     data.loc[(data.price < data.LB_15M_V)&
-             ((data.BOLL_5M_S2 < 0)&(data.BOLL_5M_S < 0)&(data.BOLL_5M > 0)), "signal"] = 1
+             (((data.BOLL_5M_S2 < 0)&(data.BOLL_5M_S < 0)&(data.BOLL_5M > 0)) |
+              ((data.LB_5M_S2 < 0)&(data.LB_5M_S < 0)&(data.LB_5M > 0))), "signal"] = 1
     data.loc[(data.price < data.LB_15M_V)&
-             ((data.BOLL_5M_S2 < 0)&(data.BOLL_5M_S < 0)&(data.BOLL_5M > 0)), "msg"] = '15min LB抄底进场信号'
+             (((data.BOLL_5M_S2 < 0)&(data.BOLL_5M_S < 0)&(data.BOLL_5M > 0)) |
+              ((data.LB_5M_S2 < 0)&(data.LB_5M_S < 0)&(data.LB_5M > 0))), "msg"] = '15min LB抄底进场信号'
 
     data.loc[(data.price < data.LB_30M_V)&
-             ((data.BOLL_5M_S2 < 0)&(data.BOLL_5M_S < 0)&(data.BOLL_5M > 0)), "signal"] = 1
+             (((data.BOLL_5M_S2 < 0)&(data.BOLL_5M_S < 0)&(data.BOLL_5M > 0)) |
+              ((data.LB_5M_S2 < 0)&(data.LB_5M_S < 0)&(data.LB_5M > 0))), "signal"] = 1
     data.loc[(data.price < data.LB_30M_V)&
-             ((data.BOLL_5M_S2 < 0)&(data.BOLL_5M_S < 0)&(data.BOLL_5M > 0)), "msg"] = '30min LB抄底进场信号'
+             (((data.BOLL_5M_S2 < 0)&(data.BOLL_5M_S < 0)&(data.BOLL_5M > 0)) |
+              ((data.LB_5M_S2 < 0)&(data.LB_5M_S < 0)&(data.LB_5M > 0))), "msg"] = '30min LB抄底进场信号'
 
     data.loc[(data.price <= data.LB_15M_V * 1.01)&
              (((data.BOLL_5M_S2 < 0)&(data.BOLL_5M_S < 0)&(data.BOLL_5M > 0)) |
