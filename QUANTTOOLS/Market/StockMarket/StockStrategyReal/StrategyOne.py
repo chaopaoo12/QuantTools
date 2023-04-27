1f21c01fdf767e49e7083b225fd13dafd33f41aa
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2023-04-27 09:52:32 +0800

    ADD

--- QUANTTOOLS/Market/StockMarket/StockStrategyReal/StrategyOne.py
+++ QUANTTOOLS/Market/StockMarket/StockStrategyReal/StrategyOne.py
@@ -35,53 +35,53 @@ def data_collect(code_list, trading_date, day_temp_data, sec_temp_data, source_d
     QA_util_log_info('##JOB Out Signal Decide ====================', ui_log=None)
 
     data.loc[(data.price > data.UB_15M_V * 1.05)&
-             ((data.UB_5M_S2 > 0)&(data.UB_5M_S > 0)&(data.UB_5M < 0)), "signal"] = 1
+             ((data.UB_5M_S2 > 0)&(data.UB_5M_S > 0)&(data.UB_5M < 0)), "signal"] = 0
     data.loc[(data.price > data.UB_15M_V * 1.05)&
              ((data.UB_5M_S2 > 0)&(data.UB_5M_S > 0)&(data.UB_5M < 0)), "msg"] = '15min 超涨止盈信号'
 
     data.loc[(data.price > data.UB_30M_V * 1.05)&
-             ((data.UB_5M_S2 > 0)&(data.UB_5M_S > 0)&(data.UB_5M < 0)), "signal"] = 1
+             ((data.UB_5M_S2 > 0)&(data.UB_5M_S > 0)&(data.UB_5M < 0)), "signal"] = 0
     data.loc[(data.price > data.UB_30M_V * 1.05)&
              ((data.UB_5M_S2 > 0)&(data.UB_5M_S > 0)&(data.UB_5M < 0)), "msg"] = '30min 超涨止盈信号'
 
     data.loc[(data.price >= data.UB_15M_V * 0.99)&
              (((data.UB_5M_S2 > 0)&(data.UB_5M_S > 0)&(data.UB_5M < 0))|
-              ((data.BOLL_5M_S2 > 0)&(data.BOLL_5M_S > 0)&(data.BOLL_5M < 0))), "signal"] = 1
+              ((data.BOLL_5M_S2 > 0)&(data.BOLL_5M_S > 0)&(data.BOLL_5M < 0))), "signal"] = 0
     data.loc[(data.price >= data.UB_15M_V * 0.99)&
              (((data.UB_5M_S2 > 0)&(data.UB_5M_S > 0)&(data.UB_5M < 0))|
               ((data.BOLL_5M_S2 > 0)&(data.BOLL_5M_S > 0)&(data.BOLL_5M < 0))), "msg"] = '15Min 触顶出场信号'
 
     data.loc[(data.price >= data.UB_30M_V * 0.99)&
              (((data.UB_5M_S2 > 0)&(data.UB_5M_S > 0)&(data.UB_5M < 0))|
-              ((data.BOLL_5M_S2 > 0)&(data.BOLL_5M_S > 0)&(data.BOLL_5M < 0))), "signal"] = 1
+              ((data.BOLL_5M_S2 > 0)&(data.BOLL_5M_S > 0)&(data.BOLL_5M < 0))), "signal"] = 0
     data.loc[(data.price >= data.UB_30M_V * 0.99)&
              (((data.UB_5M_S2 > 0)&(data.UB_5M_S > 0)&(data.UB_5M < 0))|
               ((data.BOLL_5M_S2 > 0)&(data.BOLL_5M_S > 0)&(data.BOLL_5M < 0))), "msg"] = '30Min 触顶出场信号'
 
     data.loc[(data.price >= data.BOLL_15M_V * 0.99)&(data.price <= data.BOLL_15M_V * 1.01)&
              (((data.UB_5M_S2 > 0)&(data.UB_5M_S > 0)&(data.UB_5M < 0))|
-              ((data.BOLL_5M_S2 > 0)&(data.BOLL_5M_S > 0)&(data.BOLL_5M < 0))), "signal"] = 1
+              ((data.BOLL_5M_S2 > 0)&(data.BOLL_5M_S > 0)&(data.BOLL_5M < 0))), "signal"] = 0
     data.loc[(data.price >= data.BOLL_15M_V * 0.99)&(data.price <= data.BOLL_15M_V * 1.01)&
              (((data.UB_5M_S2 > 0)&(data.UB_5M_S > 0)&(data.UB_5M < 0))|
               ((data.BOLL_5M_S2 > 0)&(data.BOLL_5M_S > 0)&(data.BOLL_5M < 0))), "msg"] = '15Min BOLL触顶出场信号'
 
     data.loc[(data.price >= data.BOLL_30M_V * 0.99)&(data.price <= data.BOLL_30M_V * 1.01)&
              (((data.UB_5M_S2 > 0)&(data.UB_5M_S > 0)&(data.UB_5M < 0))|
-              ((data.BOLL_5M_S2 > 0)&(data.BOLL_5M_S > 0)&(data.BOLL_5M < 0))), "signal"] = 1
+              ((data.BOLL_5M_S2 > 0)&(data.BOLL_5M_S > 0)&(data.BOLL_5M < 0))), "signal"] = 0
     data.loc[(data.price >= data.BOLL_30M_V * 0.99)&(data.price <= data.BOLL_30M_V * 1.01)&
              (((data.UB_5M_S2 > 0)&(data.UB_5M_S > 0)&(data.UB_5M < 0))|
               ((data.BOLL_5M_S2 > 0)&(data.BOLL_5M_S > 0)&(data.BOLL_5M < 0))), "msg"] = '30Min BOLL触顶出场信号'
 
     data.loc[(data.price <= data.LB_15M_V)&
              (((data.UB_5M_S2 > 0)&(data.UB_5M_S > 0)&(data.UB_5M < 0))|
-              ((data.BOLL_5M_S2 > 0)&(data.BOLL_5M_S > 0)&(data.BOLL_5M < 0))), "signal"] = 1
+              ((data.BOLL_5M_S2 > 0)&(data.BOLL_5M_S > 0)&(data.BOLL_5M < 0))), "signal"] = 0
     data.loc[(data.price <= data.LB_15M_V)&
              (((data.UB_5M_S2 > 0)&(data.UB_5M_S > 0)&(data.UB_5M < 0))|
               ((data.BOLL_5M_S2 > 0)&(data.BOLL_5M_S > 0)&(data.BOLL_5M < 0))), "msg"] = '15Min LB止损出场信号'
 
     data.loc[(data.price <= data.LB_30M_V)&
              (((data.UB_5M_S2 > 0)&(data.UB_5M_S > 0)&(data.UB_5M < 0))|
-              ((data.BOLL_5M_S2 > 0)&(data.BOLL_5M_S > 0)&(data.BOLL_5M < 0))), "signal"] = 1
+              ((data.BOLL_5M_S2 > 0)&(data.BOLL_5M_S > 0)&(data.BOLL_5M < 0))), "signal"] = 0
     data.loc[(data.price <= data.LB_30M_V)&
              (((data.UB_5M_S2 > 0)&(data.UB_5M_S > 0)&(data.UB_5M < 0))|
               ((data.BOLL_5M_S2 > 0)&(data.BOLL_5M_S > 0)&(data.BOLL_5M < 0))), "msg"] = '30Min LB止损出场信号'
