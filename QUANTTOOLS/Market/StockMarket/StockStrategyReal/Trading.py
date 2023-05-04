78986a4c948b4e4ad69487096197f646bb6d2daf
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2023-05-04 21:42:49 +0800

    ADD

--- QUANTTOOLS/Market/StockMarket/StockStrategyReal/Trading.py
+++ QUANTTOOLS/Market/StockMarket/StockStrategyReal/Trading.py
@@ -70,8 +70,8 @@ def trading_new(trading_date, working_dir=working_dir):
     mars_day = mars_day.reset_index().set_index('code').join(mars_day.groupby('code')[['BOLL','UB']].last().rename(columns={'BOLL':'BOLL_V','UB':'UB_V'})).reset_index().set_index(['date','code']).sort_index()
     xg = xg.reset_index().set_index('code').join(xg.groupby('code')[['BOLL','UB']].last().rename(columns={'BOLL':'BOLL_V','UB':'UB_V'})).reset_index().set_index(['date','code']).sort_index()
 
-    xg_sh = xg_sh[(xg.RANK <= 20)&(xg.TARGET5.isnull())&(xg_sh.BOLL_V>0)&(xg_sh.BOLL_V.abs()<xg_sh.UB_V.abs())]
-    mars_day = mars_day[(xg.RANK <= 20)&(xg.TARGET5.isnull())&(mars_day.BOLL_V>0)&(mars_day.BOLL_V.abs()<mars_day.UB_V.abs())]
+    xg_sh = xg_sh[(xg_sh.RANK <= 20)&(xg_sh.BOLL_V>0)&(xg_sh.BOLL_V.abs()<xg_sh.UB_V.abs())&(xg_sh.TARGET5.isnull())]
+    mars_day = mars_day[(mars_day.RANK <= 20)&(mars_day.TARGET5.isnull())&(mars_day.BOLL_V>0)&(mars_day.BOLL_V.abs()<mars_day.UB_V.abs())]
     xg = xg[(xg.RANK <= 20)&(xg.TARGET5.isnull())&(xg.BOLL_V>0)&(xg.BOLL_V.abs()<xg.UB_V.abs())]
 
     base_report(trading_date, '交易股池', **{
