d6d69bb1effb8462dbb388ab00961a50a51ef3ce
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2023-05-05 21:19:05 +0800

    ADD

--- QUANTTOOLS/Market/StockMarket/StockStrategyReal/StrategyOne.py
+++ QUANTTOOLS/Market/StockMarket/StockStrategyReal/StrategyOne.py
@@ -99,38 +99,38 @@ def data_collect(code_list, trading_date, day_temp_data, sec_temp_data, source_d
              (((data.BOLL_5M_S2 < 0)&(data.BOLL_5M_S < 0)&(data.BOLL_5M > 0)) |
               ((data.LB_5M_S2 < 0)&(data.LB_5M_S < 0)&(data.LB_5M > 0))), "msg"] = '15min LB抄底进场信号'
 
-    data.loc[(data.price < data.LB_30M_V)&(data.WIDTH_30M >= 0.1)&
+    data.loc[(data.price < data.LB_30M_V)&(data.WIDTH_30M >= 0.1)&(data.WIDTH_5M <= 0.05)&
              (((data.BOLL_5M_S2 < 0)&(data.BOLL_5M_S < 0)&(data.BOLL_5M > 0)) |
               ((data.LB_5M_S2 < 0)&(data.LB_5M_S < 0)&(data.LB_5M > 0))), "signal"] = 1
-    data.loc[(data.price < data.LB_30M_V)&(data.WIDTH_30M >= 0.1)&
+    data.loc[(data.price < data.LB_30M_V)&(data.WIDTH_30M >= 0.1)&(data.WIDTH_5M <= 0.05)&
              (((data.BOLL_5M_S2 < 0)&(data.BOLL_5M_S < 0)&(data.BOLL_5M > 0)) |
               ((data.LB_5M_S2 < 0)&(data.LB_5M_S < 0)&(data.LB_5M > 0))), "msg"] = '30min LB抄底进场信号'
 
-    data.loc[(data.price <= data.LB_15M_V * 1.01)&(data.WIDTH_15M >= 0.1)&
+    data.loc[(data.price <= data.LB_15M_V * 1.01)&(data.WIDTH_15M >= 0.1)&(data.WIDTH_5M <= 0.05)&
              (((data.BOLL_5M_S2 < 0)&(data.BOLL_5M_S < 0)&(data.BOLL_5M > 0)) |
               ((data.LB_5M_S2 < 0)&(data.LB_5M_S < 0)&(data.LB_5M > 0))), "signal"] = 1
-    data.loc[(data.price <= data.LB_15M_V * 1.01)&(data.WIDTH_15M >= 0.1)&
+    data.loc[(data.price <= data.LB_15M_V * 1.01)&(data.WIDTH_15M >= 0.1)&(data.WIDTH_5M <= 0.05)&
              (((data.BOLL_5M_S2 < 0)&(data.BOLL_5M_S < 0)&(data.BOLL_5M > 0)) |
               ((data.LB_5M_S2 < 0)&(data.LB_5M_S < 0)&(data.LB_5M > 0))), "msg"] = '15Min LB进场信号'
 
-    data.loc[(data.price <= data.LB_30M_V * 1.01)&(data.WIDTH_30M >= 0.1)&
+    data.loc[(data.price <= data.LB_30M_V * 1.01)&(data.WIDTH_30M >= 0.1)&(data.WIDTH_5M <= 0.05)&
              (((data.BOLL_5M_S2 < 0)&(data.BOLL_5M_S < 0)&(data.BOLL_5M > 0)) |
               ((data.LB_5M_S2 < 0)&(data.LB_5M_S < 0)&(data.LB_5M > 0))), "signal"] = 1
-    data.loc[(data.price <= data.LB_30M_V * 1.01)&(data.WIDTH_30M >= 0.1)&
+    data.loc[(data.price <= data.LB_30M_V * 1.01)&(data.WIDTH_30M >= 0.1)&(data.WIDTH_5M <= 0.05)&
              (((data.BOLL_5M_S2 < 0)&(data.BOLL_5M_S < 0)&(data.BOLL_5M > 0)) |
               ((data.LB_5M_S2 < 0)&(data.LB_5M_S < 0)&(data.LB_5M > 0))), "msg"] = '30Min LB进场信号'
 
-    data.loc[(data.price <= data.BOLL_15M_V * 1.01)&(data.price >= data.BOLL_15M_V * 0.99)&(data.WIDTH_15M >= 0.1)&
+    data.loc[(data.price <= data.BOLL_15M_V * 1.01)&(data.price >= data.BOLL_15M_V * 0.99)&(data.WIDTH_15M >= 0.1)&(data.WIDTH_5M <= 0.05)&
              (((data.BOLL_5M_S2 < 0)&(data.BOLL_5M_S < 0)&(data.BOLL_5M > 0)) |
               ((data.LB_5M_S2 < 0)&(data.LB_5M_S < 0)&(data.LB_5M > 0))), "signal"] = 1
-    data.loc[(data.price <= data.BOLL_15M_V * 1.01)&(data.price >= data.BOLL_15M_V * 0.99)&(data.WIDTH_15M >= 0.1)&
+    data.loc[(data.price <= data.BOLL_15M_V * 1.01)&(data.price >= data.BOLL_15M_V * 0.99)&(data.WIDTH_15M >= 0.1)&(data.WIDTH_5M <= 0.05)&
              (((data.BOLL_5M_S2 < 0)&(data.BOLL_5M_S < 0)&(data.BOLL_5M > 0)) |
               ((data.LB_5M_S2 < 0)&(data.LB_5M_S < 0)&(data.LB_5M > 0))), "msg"] = '15Min BOLL进场信号'
 
-    data.loc[(data.price <= data.BOLL_30M_V * 1.01)&(data.price >= data.BOLL_30M_V * 0.99)&(data.WIDTH_30M >= 0.1)&
+    data.loc[(data.price <= data.BOLL_30M_V * 1.01)&(data.price >= data.BOLL_30M_V * 0.99)&(data.WIDTH_30M >= 0.1)&(data.WIDTH_5M <= 0.05)&
              (((data.BOLL_5M_S2 < 0)&(data.BOLL_5M_S < 0)&(data.BOLL_5M > 0)) |
               ((data.LB_5M_S2 < 0)&(data.LB_5M_S < 0)&(data.LB_5M > 0))), "signal"] = 1
-    data.loc[(data.price <= data.BOLL_30M_V * 1.01)&(data.price >= data.BOLL_30M_V * 0.99)&(data.WIDTH_30M >= 0.1)&
+    data.loc[(data.price <= data.BOLL_30M_V * 1.01)&(data.price >= data.BOLL_30M_V * 0.99)&(data.WIDTH_30M >= 0.1)&(data.WIDTH_5M <= 0.05)&
              (((data.BOLL_5M_S2 < 0)&(data.BOLL_5M_S < 0)&(data.BOLL_5M > 0)) |
               ((data.LB_5M_S2 < 0)&(data.LB_5M_S < 0)&(data.LB_5M > 0))), "msg"] = '30Min BOLL进场信号'
 
