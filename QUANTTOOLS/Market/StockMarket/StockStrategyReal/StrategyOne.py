35d42e3bf3297cb68ef410da1d88cce8efe50698
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2023-05-09 21:31:58 +0800

    ADD

--- QUANTTOOLS/Market/StockMarket/StockStrategyReal/StrategyOne.py
+++ QUANTTOOLS/Market/StockMarket/StockStrategyReal/StrategyOne.py
@@ -100,7 +100,7 @@ def data_collect(code_list, trading_date, day_temp_data, sec_temp_data, source_d
               ((data.LB_5M_S2 < 0)&(data.LB_5M_S < 0)&(data.LB_5M > 0))), "signal"] = 1
     data.loc[(data.price < data.LB_15M_V)&(data.WIDTH_30M >= 0.1)&
              (((data.BOLL_5M_S2 < 0)&(data.BOLL_5M_S < 0)&(data.BOLL_5M > 0)) |
-              ((data.LB_5M_S2 < 0)&(data.LB_5M_S < 0)&(data.LB_5M > 0))), "msg"] = '15min LB抄底进场信号'
+              ((data.LB_5M_S2 < 0)&(data.LB_5M_S < 0)&(data.LB_5M > 0))), "msg"] = '15min LB进场信号'
 
     data.loc[(data.price < data.LB_30M_V)&(data.WIDTH_30M >= 0.1)&
              (((data.BOLL_5M_S2 < 0)&(data.BOLL_5M_S < 0)&(data.BOLL_5M > 0)) |
@@ -114,7 +114,7 @@ def data_collect(code_list, trading_date, day_temp_data, sec_temp_data, source_d
               ((data.LB_5M_S2 < 0)&(data.LB_5M_S < 0)&(data.LB_5M > 0))), "signal"] = 1
     data.loc[(data.price <= data.LB_15M_V * 0.95)&(data.WIDTH_5M <= 0.05)&(data.WIDTH_30M >= 0.1)&
              (((data.BOLL_5M_S2 < 0)&(data.BOLL_5M_S < 0)&(data.BOLL_5M > 0)) |
-              ((data.LB_5M_S2 < 0)&(data.LB_5M_S < 0)&(data.LB_5M > 0))), "msg"] = '15Min LB进场信号'
+              ((data.LB_5M_S2 < 0)&(data.LB_5M_S < 0)&(data.LB_5M > 0))), "msg"] = '15Min LB抄底进场信号'
 
     data.loc[(data.price <= data.LB_30M_V * 1.01)&(data.WIDTH_5M <= 0.05)&(data.WIDTH_30M >= 0.1)&
              (((data.BOLL_5M_S2 < 0)&(data.BOLL_5M_S < 0)&(data.BOLL_5M > 0)) |
