37d87c1dddd1b927e71d1be58deaac31c45439cc
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2023-05-17 21:05:03 +0800

    ADD

--- QUANTTOOLS/QAStockETL/QAFetch/QAUsFinancial.py
+++ QUANTTOOLS/QAStockETL/QAFetch/QAUsFinancial.py
@@ -81,12 +81,14 @@ def QA_fetch_get_stock_min_sina(code, period='30', type='',proxies=[]):
             if len(proxies) > len(code):
                 proxies = proxies[0:len(code)]
             symbol_proxies = list(zip(random.choices(proxies, k=len(code)),code))
+            pool = multiprocessing.Pool(20)
+            with pool as p:
+                res = p.map(partial(proxy_stock_zh_a_hist_min_em, period=period, adjust=type), symbol_proxies)
         else:
-            symbol_proxies = list(zip(random.choices([proxies], k=len(code)),code))
+            pool = multiprocessing.Pool(20)
+            with pool as p:
+                res = p.map(partial(stock_zh_a_hist_min_em, period=period, adjust=type), code)
 
-        pool = multiprocessing.Pool(20)
-        with pool as p:
-            res = p.map(partial(proxy_stock_zh_a_hist_min_em, period=period, adjust=type), symbol_proxies)
         data = pd.concat(res,axis=0)
 
     elif isinstance(code, str):
