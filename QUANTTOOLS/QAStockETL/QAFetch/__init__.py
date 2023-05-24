da5264cf173c2b4a97fb5d69fee882beff7d6c05
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2023-05-24 21:58:47 +0800

    ADD

--- QUANTTOOLS/QAStockETL/QAFetch/__init__.py
+++ QUANTTOOLS/QAStockETL/QAFetch/__init__.py
@@ -142,6 +142,7 @@ from QUANTTOOLS.QAStockETL.QAFetch.QATdx import (QA_fetch_get_stock_close,fetch_
                                                  QA_fetch_get_stock_realtm_bid,QA_fetch_get_stock_realtm_bidvol,
                                                  QA_fetch_get_stock_realtm_askvol5,QA_fetch_get_stock_realtm_bidvol5,
                                                  QA_fetch_get_stock_min_tdx,QA_fetch_get_stock_realtime,
+                                                 QA_fetch_get_index_min_tdx,
 
                                                  QA_fetch_get_stock_industryinfo,QA_fetch_get_index_info,
                                                  QA_fetch_get_stock_industry,QA_fetch_get_index_code,
