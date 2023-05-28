792f62fcc7d06736adfd538904286f6c1487a5a5
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2023-05-28 20:38:33 +0800

    ADD

--- QUANTTOOLS/Market/StockMarket/StockStrategyReal/StrategyOne.py
+++ QUANTTOOLS/Market/StockMarket/StockStrategyReal/StrategyOne.py
@@ -95,22 +95,20 @@ def data_collect(code_list, trading_date, day_temp_data, sec_temp_data, source_d
 
     QA_util_log_info('##JOB In Signal Decide ====================', ui_log=None)
     # 放量金叉
-    data.loc[#(data.price < data.UB_5M * 1.01)&
-            (data.stock_chose == 1)&
+    data.loc[(data.price >= data.UB_15M)&(data.price >= data.UB_30M)&(data.stock_chose == 1)&
              ((data.UB_5M_S2 < 0)&(data.UB_5M_S < 0)&(data.UB_5M > 0)), "signal"] = 1
-    data.loc[#(data.price < data.UB_5M * 1.01)&
-        (data.stock_chose == 1)&
-             ((data.UB_5M_S2 < 0)&(data.UB_5M_S < 0)&(data.UB_5M > 0)), "msg"] = '5minUB突破进场信号'
+    data.loc[(data.price >= data.UB_15M)&(data.price >= data.UB_30M)&(data.stock_chose == 1)&
+             ((data.UB_5M_S2 < 0)&(data.UB_5M_S < 0)&(data.UB_5M > 0)), "msg"] = 'UB突破进场信号'
 
     #data.loc[(data.price < data.LB_15M_V * 1.01)&
     #         ((data.UB_5M_S2 < 0)&(data.UB_5M_S < 0)&(data.UB_5M > 0)), "signal"] = 1
     #data.loc[(data.price < data.LB_15M_V * 1.01)&
     #          ((data.UB_5M_S2 < 0)&(data.UB_5M_S < 0)&(data.UB_5M > 0)), "msg"] = '15min LB进场信号'
 
-    data.loc[(data.stock_chose == 1)&(data.price <= data.LB_30M_V * 0.95)&(data.price <= data.LB_15M_V * 0.95)&
+    data.loc[(data.price <= data.LB_30M_V * 0.95)&(data.price <= data.LB_15M_V * 0.95)&
              (((data.BOLL_5M_S2 < 0)&(data.BOLL_5M_S < 0)&(data.BOLL_5M > 0)) |
               ((data.LB_5M_S2 < 0)&(data.LB_5M_S < 0)&(data.LB_5M > 0))), "signal"] = 1
-    data.loc[(data.stock_chose == 1)&(data.price <= data.LB_30M_V * 0.95)&(data.price <= data.LB_15M_V * 0.95)&
+    data.loc[(data.price <= data.LB_30M_V * 0.95)&(data.price <= data.LB_15M_V * 0.95)&
              (((data.BOLL_5M_S2 < 0)&(data.BOLL_5M_S < 0)&(data.BOLL_5M > 0)) |
               ((data.LB_5M_S2 < 0)&(data.LB_5M_S < 0)&(data.LB_5M > 0))), "msg"] = 'BOLL LB抄底进场信号'
 
