bc760cca21816ce0a98271b5ca47d372c74f7da3
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2023-05-31 08:27:58 +0800

    ADD

--- QUANTTOOLS/Market/StockMarket/StockStrategyReal/Trading.py
+++ QUANTTOOLS/Market/StockMarket/StockStrategyReal/Trading.py
@@ -76,7 +76,7 @@ def trading_new(trading_date, working_dir=working_dir):
     ]
     mars_day = mars_day[(mars_day.RANK <= 20)&(mars_day.TARGET5.isnull())#&(mars_day.BOLL_V>0)
     ]
-    xg = xg[(xg.RANK <= 20)&(xg.BOLL_V>0)&(xg.TARGET5.isnull())#&(xg.BOLL_V>0)
+    xg = xg[(xg.RANK <= 20)&(xg.TARGET5.isnull())#&(xg.BOLL_V>0)
     ]
     xg_nn = xg_nn[(xg_nn.RANK <= 20)&(xg_nn.TARGET5.isnull())#&(xg_nn.BOLL_V>0)
     ]
