2a90cb732f4c0d2500724c3c2cfc19141d00670e
chaopaoo12@hotmail.com <chaopaoo12@hotmail.com>
2023-06-25 20:56:11 +0800

    ADD

--- QUANTTOOLS/QAStockETL/QAData/database_settings.py
+++ QUANTTOOLS/QAStockETL/QAData/database_settings.py
@@ -2,9 +2,9 @@
 Oracle_Database = 'quantaxis'
 Oracle_User = 'quantaxis'
 Oracle_Password = '123'
-Oralce_Server = '192.168.3.56'
+Oralce_Server = '192.168.0.102'
 
-MongoDB_Server = '192.168.3.56'
+MongoDB_Server = '192.168.0.102'
 MongoDB_Database = 'quantaxis'
 
 tdx_dir='D:\\new_tdx\\T0002\\hq_cache\\'
